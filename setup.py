a3da6841dee750fd1314def49d139ecaa677df5c
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2022-09-15 15:08:39 +0530

    update

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
-    keywords=\
-    "machine-learning, deep-learning, ml, pytorch, YOLO, object-detection, YOLOv3, YOLOv4, YOLOv5",
+    keywords="machine-learning, deep-learning, ml, pytorch, YOLO, \
+    object-detection, YOLOv3, YOLOv4, YOLOv5",
     package_dir={'': 'ultralytics'},
     packages=find_packages(where='ultralytics'),
     python_requires='>=3.7, <4',
@@ -60,7 +60,7 @@ setup(
     },
     # data_files=[('my_data', ['data/data_file'])],
 
-    # entry_points={'console_scripts': ['ultralytics=ultralytics.console:main', ],},
+    # entry_points={'console_scripts': ['...',],},
     project_urls={
         'Bug Reports': 'https://github.com/ultralytics/ultralytics/issues',
         'Funding': 'https://www.ultralytics.com',

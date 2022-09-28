7d7b0879dca1dc76160610f80b2b2123d8d8ab09
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2022-09-28 19:25:37 +0530

    update

--- ultralytics/yolo/utils/__init__.py
+++ ultralytics/yolo/utils/__init__.py
@@ -1,14 +1,17 @@
-from .general import WorkingDirectory, download, increment_path, save_yaml
-from .torch_utils import LOCAL_RANK, RANK, WORLD_SIZE, torch_distributed_zero_first
+from .general import WorkingDirectory, download, increment_path, save_yaml, check_version
+from .torch_utils import LOCAL_RANK, RANK, WORLD_SIZE, torch_distributed_zero_first, DDP_model
 
 __all__ = [
     # general
     "increment_path",
     "save_yaml",
     "WorkingDirectory",
-    "download"
+    "download",
+    "check_version"
     # torch
     "torch_distributed_zero_first",
     "LOCAL_RANK",
     "RANK",
-    "WORLD_SIZE"]
+    "WORLD_SIZE",
+    "DDP_model"
+    ]

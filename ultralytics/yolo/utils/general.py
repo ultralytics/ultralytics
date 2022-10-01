363eae04b75c54f73f82e2045c8240164fb9fef0
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-01 13:57:23 +0200

    Fix precommit check_version redefinition error

--- ultralytics/yolo/utils/general.py
+++ ultralytics/yolo/utils/general.py
@@ -263,15 +263,3 @@ class WorkingDirectory(contextlib.ContextDecorator):
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         os.chdir(self.cwd)
-
-
-def check_version(current='0.0.0', minimum='0.0.0', name='version ', pinned=False, hard=False, verbose=False):
-    # Check version vs. required version
-    current, minimum = (pkg.parse_version(x) for x in (current, minimum))
-    result = (current == minimum) if pinned else (current >= minimum)  # bool
-    s = f'WARNING ⚠️ {name}{minimum} is required by YOLOv5, but {name}{current} is currently installed'  # string
-    if hard:
-        assert result, s  # assert min requirements met
-    if verbose and not result:
-        LOGGER.warning(s)
-    return result

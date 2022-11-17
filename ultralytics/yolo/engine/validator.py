3e1f737857c3a5b7ea73a350f01becb379d17160
Laughing-q <1185102784@qq.com>
2022-11-17 09:15:34 +0000

    fix classify

--- ultralytics/yolo/engine/validator.py
+++ ultralytics/yolo/engine/validator.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 from ultralytics.yolo.engine.trainer import DEFAULT_CONFIG
 from ultralytics.yolo.utils.ops import Profile
 from ultralytics.yolo.utils.torch_utils import select_device
+from ultralytics.yolo.utils.torch_utils import de_parallel
 
 
 class BaseValidator:
@@ -48,7 +49,7 @@ class BaseValidator:
         n_batches = len(self.dataloader)
         desc = self.get_desc()
         bar = tqdm(self.dataloader, desc, n_batches, not training, bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}')
-        self.init_metrics(model)
+        self.init_metrics(de_parallel(model))
         with torch.no_grad():
             for batch_i, batch in enumerate(bar):
                 self.batch_i = batch_i

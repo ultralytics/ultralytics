1c3ef4ea9ea0cbddc63b629b44bc5891149c26b1
ayush chaurasia <ayush.chaurarsia@gmail.com>
2022-12-01 14:17:15 +0530

    update

--- ultralytics/yolo/v8/__init__.py
+++ ultralytics/yolo/v8/__init__.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-from ultralytics.yolo.v8 import classify, detect, segment
+from ultralytics.yolo.v8 import classify,segment
+from ultralytics.yolo.v8 import detect
 
 ROOT = Path(__file__).parents[0]  # yolov8 ROOT
 

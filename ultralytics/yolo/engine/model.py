8a5af2095cbe52d61756c3cdacd16da68b1a4f34
Laughing-q <1185102784@qq.com>
2022-12-08 18:33:14 +0800

    fix model

--- ultralytics/yolo/engine/model.py
+++ ultralytics/yolo/engine/model.py
@@ -4,6 +4,7 @@ Top-level YOLO model interface. First principle usage example - https://github.c
 import torch
 import yaml
 
+from ultralytics import yolo
 from ultralytics.yolo.utils import LOGGER
 from ultralytics.yolo.utils.checks import check_yaml
 from ultralytics.yolo.utils.modeling import attempt_load_weights

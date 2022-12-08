1909e6e706daeb05490c7618107ced2c9d68b15c
Laughing-q <1185102784@qq.com>
2022-12-08 18:29:26 +0800

    clean metrics

--- ultralytics/yolo/v8/segment/__init__.py
+++ ultralytics/yolo/v8/segment/__init__.py
@@ -1,3 +1,3 @@
-from ultralytics.yolo.v8.segment.predict import SegmentationPredictor, predict
-from ultralytics.yolo.v8.segment.train import SegmentationTrainer, train
-from ultralytics.yolo.v8.segment.val import SegmentationValidator, val
+from .predict import SegmentationPredictor, predict
+from .train import SegmentationTrainer, train
+from .val import SegmentationValidator, val

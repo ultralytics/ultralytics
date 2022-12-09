98885fb1239d2f6bc4f858f0736e3760c0ab618d
ayush chaurasia <ayush.chaurarsia@gmail.com>
2022-12-09 15:39:30 +0530

    push

--- docs/sdk.md
+++ docs/sdk.md
@@ -2,10 +2,58 @@
 
 We provide 2 pythonic interfaces for YOLO models:
 
-<b> Model Interface </b> - To simply build, load, train or run inference on a model in a python application
+<b> Model Interface </b> - High-level wrapper around the `Trainer` Classes
 
-<b> Trainer Interface </b> - To customize trainier elements depending on the task. Suitable for R&D ideas like architecutres.
+<b> Trainers </b> - Provides access to Trainer Classes that can be extended for pursuing R&D ideas.
 
 ______________________________________________________________________
 
 ### Model Interface
+This is the easiest, no-nonsense way of simply using yolo models in a python environment. 
+It can be imported from the `ultralytics` module.
+!!! tip "Model Usage"
+
+    | Name                       |Function                                      | Usage                             |           
+    | --------------------------------| -------------------------------------------- | --------------------------------- |       
+    |` YOLO()`                     | Initialize a model wrapper. **Optionally**, <br> accepts model `type` which defaults to `'v8'`                      |                         ```model = YOLO()```                                                     |   |
+    |`.new(cfg)`                  | Initialize a new model from a given config `cfg`                                                                | ```model.new("cfg.yaml")```                                                                      |   |
+    |`.load(model)`               | Load a model from a given model weight `model`                                                                  | ```model.load("model.pt")```                                                                     |
+    |`__call__(imgs)`             | Runs inference on given `imgs` tensor                                                                           | ```model(imgs)```                                                                                |
+    |`train(data, **kwargs) `     | Trains the model on given `data`. Accepts additional train args can be passed                              | ```model.train(data="", epochs=1, ...)```                                                        |   |
+    |
+    |`.resume(task)`         | Resumes training of the last given `task` type if **optionally** `model` is not passed, else resumes from the given model | ```model.resume(task="detect") model.resume(task="detect", model="*.pt")``` |   |
+    
+    !!! note ""
+        More functionality coming soon
+Model Class reference >
+
+!!! example "Examples"
+    === "Training"
+        ```python
+        from ultralytics import YOLO
+
+        model = YOLO()
+        model.new("n.yaml") # pass any model type
+        model.train(data="coco128.yaml", epochs=5)
+        ```
+
+    === "Training pretrained"
+        ```python
+        from ultralytics import YOLO
+
+        model = YOLO()
+        model.load("n.pt") # pass any model type
+        model(...) # inference
+        model.train(data="coco128.yaml", epochs=5)
+        ```
+
+    === "Resume Training"
+        ```python
+        from ultralytics import YOLO
+
+        model = YOLO()
+        model.resume(task="detect") # resume last detection training
+        model.resume(task="detect", model="last.pt") # resume from a given model
+        ```
+
+### Working with Trainers
--- ultralytics/yolo/engine/model.py
+++ ultralytics/yolo/engine/model.py
@@ -1,6 +1,3 @@
-"""
-Top-level YOLO model interface. First principle usage example - https://github.com/ultralytics/ultralytics/issues/13
-"""
 import torch
 import yaml
 
@@ -11,15 +8,22 @@ from ultralytics.yolo.utils.modeling.tasks import ClassificationModel, Detection
 
 # map head: [model, trainer]
 MODEL_MAP = {
-    "classify": [ClassificationModel, 'yolo.VERSION.classify.ClassificationTrainer'],
-    "detect": [DetectionModel, 'yolo.VERSION.detect.DetectionTrainer'],
-    "segment": [SegmentationModel, 'yolo.VERSION.segment.SegmentationTrainer']}
+    "classify": [ClassificationModel, 'yolo.TYPE.classify.ClassificationTrainer'],
+    "detect": [DetectionModel, 'yolo.TYPE.detect.DetectionTrainer'],
+    "segment": [SegmentationModel, 'yolo.TYPE.segment.SegmentationTrainer']}
 
 
 class YOLO:
+    """
+    Python interface which emulates a model-like behaviour by wrapping trainers.
+    """
 
-    def __init__(self, version=8) -> None:
-        self.version = version
+    def __init__(self, type="v8") -> None:
+        """
+        Args:
+            type (str): Type/version of models to use        
+        """
+        self.type = type
         self.ModelClass = None
         self.TrainerClass = None
         self.model = None
@@ -28,53 +32,78 @@ class YOLO:
         self.ckpt = None
 
     def new(self, cfg: str):
+        """
+        Initializes a new model and infers the task type from the model definitions
+
+        Args:
+            cfg (str): model configuration file
+        """
         cfg = check_yaml(cfg)  # check YAML
         with open(cfg, encoding='ascii', errors='ignore') as f:
             cfg = yaml.safe_load(f)  # model dict
         self.ModelClass, self.TrainerClass, self.task = self._guess_model_trainer_and_task(cfg["head"][-1][-2])
         self.model = self.ModelClass(cfg)  # initialize
 
-    def load(self, weights):
+    def load(self, weights: str):
+        """
+        Initializes a new model and infers the task type from the model head
+
+        Args:
+            weights (str): model checkpoint to be loaded
+
+        """
         self.ckpt = torch.load(weights, map_location="cpu")
         self.task = self.ckpt["train_args"]["task"]
         _, trainer_class_literal = MODEL_MAP[self.task]
-        self.TrainerClass = eval(trainer_class_literal.replace("VERSION", f"v{self.version}"))
+        self.TrainerClass = eval(trainer_class_literal.replace("TYPE", f"v{self.type}"))
         self.model = attempt_load_weights(weights)
 
     def reset(self):
+        """
+        Resets the model modules .
+        """
         for m in self.model.modules():
             if hasattr(m, 'reset_parameters'):
                 m.reset_parameters()
         for p in self.model.parameters():
             p.requires_grad = True
 
-    def train(self, **kwargs):
-        if 'data' not in kwargs:
-            raise Exception("data is required to train")
+    def train(self, data, **kwargs):
+        """
+        Trains the model on given dataset.
+
+        Args:
+            data (str): dataset name, directory or config file depending on the model and task-type.
+                        See the supported datasets input format on the "task" section
+            **kwargs (Any): Any number of arguments representing the training configuration. List of all args can be founf in 'config' section.
+        """
         if not self.model and not self.ckpt:
             raise Exception("model not initialized. Use .new() or .load()")
 
         kwargs["task"] = self.task
         kwargs["mode"] = "train"
+        kwargs["data"] = data
         self.trainer = self.TrainerClass(overrides=kwargs)
         # load pre-trained weights if found, else use the loaded model
         self.trainer.model = self.trainer.load_model(weights=self.ckpt) if self.ckpt else self.model
         self.trainer.train()
 
-    def resume(self, task=None, model=None):
-        if not task:
-            raise Exception(
-                "pass the task type and/or model(optional) from which you want to resume: `model.resume(task="
-                ")`")
+    def resume(self, task, model=None):
+        """
+        Resume a training task.
+
+        Args:
+            task (str): The task type you want to resume. Automatically finds the last run to resume if `model` is not specified. 
+            model (str): [Optional] The model checkpoint to resume from. If not found, the last run of the given task type is resumed.
+        """
         if task.lower() not in MODEL_MAP:
             raise Exception(f"unrecognised task - {task}. Supported tasks are {MODEL_MAP.keys()}")
         _, trainer_class_literal = MODEL_MAP[task.lower()]
-        self.TrainerClass = eval(trainer_class_literal.replace("VERSION", f"v{self.version}"))
+        self.TrainerClass = eval(trainer_class_literal.replace("TYPE", f"v{self.type}"))
         self.trainer = self.TrainerClass(overrides={"task": task.lower(), "resume": model if model else True})
         self.trainer.train()
 
     def _guess_model_trainer_and_task(self, head):
-        # TODO: warn
         task = None
         if head.lower() in ["classify", "classifier", "cls", "fc"]:
             task = "classify"
@@ -84,7 +113,7 @@ class YOLO:
             task = "segment"
         model_class, trainer_class = MODEL_MAP[task]
         # warning: eval is unsafe. Use with caution
-        trainer_class = eval(trainer_class.replace("VERSION", f"v{self.version}"))
+        trainer_class = eval(trainer_class.replace("TYPE", f"v{self.type}"))
 
         return model_class, trainer_class, task
 

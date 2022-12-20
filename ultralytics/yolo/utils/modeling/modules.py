c4d6e8652b09ac933f70df29c2205d4996577d0f
ayush chaurasia <ayush.chaurarsia@gmail.com>
2022-12-20 22:06:17 +0530

    update

--- ultralytics/yolo/utils/modeling/modules.py
+++ ultralytics/yolo/utils/modeling/modules.py
@@ -387,7 +387,7 @@ class C3x(C3):
     # C3 module with cross-convolutions
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
         super().__init__(c1, c2, n, shortcut, g, e)
-        c_ = int(c2 * e)
+        self.c_ = int(c2 * e)
         self.m = nn.Sequential(*(Bottleneck(self.c_, self.c_, shortcut, g, k=((1, 3), (3, 1)), e=1) for _ in range(n)))
 
 

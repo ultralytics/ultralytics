03418471ac528a4aa096b92c71362b32e06ed715
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-01-01 19:14:01 +0100

    Move HUB callbacks to yolo/utils/callbacks

--- ultralytics/yolo/utils/callbacks/hub.py
+++ ultralytics/yolo/utils/callbacks/hub.py
@@ -3,51 +3,70 @@ from time import time
 
 import torch
 
-from ultralytics.hub.utils import PREFIX
+from ultralytics.hub.utils import PREFIX, sync_analytics
 from ultralytics.yolo.utils import LOGGER
 
 
 def on_pretrain_routine_end(trainer):
-    # Start timer for upload rate limit
-    LOGGER.info(f"{PREFIX}View model at https://hub.ultralytics.com/models/{trainer.hub_session.model_id} 🚀")
-    trainer.hub_session.t = {'metrics': time(), 'ckpt': time()}  # start timer on self.rate_limit
+    session = getattr(trainer, 'hub_session', None)
+    if session:
+        # Start timer for upload rate limit
+        LOGGER.info(f"{PREFIX}View model at https://hub.ultralytics.com/models/{session.model_id} 🚀")
+        session.t = {'metrics': time(), 'ckpt': time()}  # start timer on self.rate_limit
 
 
 def on_fit_epoch_end(trainer):
-    # Upload metrics after val end
-    session = trainer.hub_session
+    session = getattr(trainer, 'hub_session', None)
+    if session:
+        # Upload metrics after val end
+        metrics = trainer.metrics
+        for k, v in metrics.items():
+            if isinstance(v, torch.Tensor):
+                metrics[k] = v.item()
 
-    # Temp. Figure out the source of this problem
-    metrics = trainer.metrics
-    for k, v in metrics.items():
-        if isinstance(v, torch.Tensor):
-            metrics[k] = v.item()
-
-    session.metrics_queue[trainer.epoch] = json.dumps(metrics)  # json string
-    if time() - session.t['metrics'] > session.rate_limits['metrics']:
-        session.upload_metrics()
-        session.t['metrics'] = time()  # reset timer
-        session.metrics_queue = {}  # reset queue
+        session.metrics_queue[trainer.epoch] = json.dumps(metrics)  # json string
+        if time() - session.t['metrics'] > session.rate_limits['metrics']:
+            session.upload_metrics()
+            session.t['metrics'] = time()  # reset timer
+            session.metrics_queue = {}  # reset queue
 
 
 def on_model_save(trainer):
-    # Upload checkpoints with rate limiting
-    session = trainer.hub_session
-    is_best = trainer.best_fitness == trainer.fitness
-    if time() - session.t['ckpt'] > session.rate_limits['ckpt']:
-        LOGGER.info(f"{PREFIX}Uploading checkpoint {session.model_id}")
-        session.upload_model(trainer.epoch, trainer.last, is_best)
-        session.t['ckpt'] = time()  # reset timer
+    session = getattr(trainer, 'hub_session', None)
+    if session:
+        # Upload checkpoints with rate limiting
+        is_best = trainer.best_fitness == trainer.fitness
+        if time() - session.t['ckpt'] > session.rate_limits['ckpt']:
+            LOGGER.info(f"{PREFIX}Uploading checkpoint {session.model_id}")
+            session.upload_model(trainer.epoch, trainer.last, is_best)
+            session.t['ckpt'] = time()  # reset timer
 
 
 def on_train_end(trainer):
-    # Upload final model and metrics with exponential standoff
-    session = trainer.hub_session
-    LOGGER.info(f"{PREFIX}Training completed successfully ✅")
-    LOGGER.info(f"{PREFIX}Uploading final {session.model_id}")
-    session.upload_model(trainer.epoch, trainer.best, map=trainer.metrics['metrics/mAP50(B)'], final=True)
-    session.alive = False  # stop heartbeats
-    LOGGER.info(f"{PREFIX}View model at https://hub.ultralytics.com/models/{session.model_id} 🚀")
+    session = getattr(trainer, 'hub_session', None)
+    if session:
+        # Upload final model and metrics with exponential standoff
+        LOGGER.info(f"{PREFIX}Training completed successfully ✅\n"
+                    f"{PREFIX}Uploading final {session.model_id}")
+        session.upload_model(trainer.epoch, trainer.best, map=trainer.metrics['metrics/mAP50(B)'], final=True)
+        session.alive = False  # stop heartbeats
+        LOGGER.info(f"{PREFIX}View model at https://hub.ultralytics.com/models/{session.model_id} 🚀")
+
+
+def on_train_start(trainer):
+    sync_analytics(trainer.args)
+
+
+def on_val_start(validator):
+    sync_analytics(validator.args)
+
+
+def on_predict_start(predictor):
+    sync_analytics(predictor.args)
+
+
+def on_export_start(exporter):
+    sync_analytics(exporter.args)
 
 
 callbacks = {

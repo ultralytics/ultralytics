0b64c95899671c90410dcf36fa1f87c475926cef
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-01-02 18:41:15 +0100

    add seg train

--- tests/tests_cli.py
+++ tests/tests_cli.py
@@ -3,3 +3,15 @@ import os
 
 def test_checks():
     os.system('yolo mode=checks')
+
+
+def test_train_detect():
+    os.system('yolo mode=train task=detect model=yolov8n.yaml data=coco128.yaml imgsz=32 epochs=1')
+
+
+def test_train_segment():
+    os.system('yolo mode=train task=segment model=yolov8n-seg.yaml data=coco128-seg.yaml imgsz=32 epochs=1')
+
+
+def test_train_classify():
+    pass

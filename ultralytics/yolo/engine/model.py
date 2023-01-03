8e14b2f74cd2e77e75522803b464d63c5de2c3bc
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-01-03 17:36:59 +0100

    fix inference device

--- ultralytics/yolo/engine/model.py
+++ ultralytics/yolo/engine/model.py
@@ -213,9 +213,8 @@ class YOLO:
 
     @smart_inference_mode()
     def __call__(self, imgs):
-        if not self.model:
-            LOGGER.info("model not initialized!")
-        return self.model(imgs)
+        device = next(self.model.parameters()).device  # get model device
+        return self.model(imgs.to(device))
 
     def forward(self, imgs):
         return self.__call__(imgs)

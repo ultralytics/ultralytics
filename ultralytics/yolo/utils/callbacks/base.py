21aa47a6e0e500f694b89d76e4400d381b6d4dfc
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-01-05 17:42:47 +0100

    Update base.py

--- ultralytics/yolo/utils/callbacks/base.py
+++ ultralytics/yolo/utils/callbacks/base.py
@@ -141,6 +141,7 @@ def add_integration_callbacks(instance):
     from .hub import callbacks as hub_callbacks
     from .tensorboard import callbacks as tb_callbacks
     from .wb import callbacks as wb_callbacks
-    for x in clearml_callbacks, hub_callbacks, tb_callbacks, wb_callbacks, comet_callbacks:
+    
+    for x in clearml_callbacks, comet_callbacks, hub_callbacks, tb_callbacks, wb_callbacks:
         for k, v in x.items():
             instance.callbacks[k].append(v)  # callback[name].append(func)

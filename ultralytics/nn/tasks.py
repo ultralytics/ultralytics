c34a8926ac8b2c96a71606f4cea560f97e75afef
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-01-08 14:16:04 +0100

    print model info

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -259,6 +259,7 @@ class ClassificationModel(BaseModel):
             self.yaml['nc'] = nc  # override yaml value
         self.model, self.save = parse_model(deepcopy(self.yaml), ch=[ch], verbose=verbose)  # model, savelist
         self.names = {i: f'{i}' for i in range(self.yaml['nc'])}  # default names dict
+        self.info()
 
     def load(self, weights):
         model = weights["model"] if isinstance(weights, dict) else weights  # torchvision models are not dicts

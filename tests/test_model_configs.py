369872da2d416079491d7700c9594d83190eb062
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-01-10 12:18:29 +0100

    Move models

--- tests/test_model_configs.py
+++
@@ -1,9 +0,0 @@
-import glob
-
-from ultralytics import YOLO
-from ultralytics.yolo.utils import ROOT
-
-
-def test_all_configs():
-    for cfg in glob.glob(str(ROOT / "models/hub/*.yaml")):
-        YOLO(cfg).info()

ab1dce24b26a609f65ccf6fa83186bc0163157a7
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-01-12 20:32:07 +0100

    Rename setup.py ROOT to PARENT

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ from setuptools import find_packages, setup
 
 # Settings
 FILE = Path(__file__).resolve()
-ROOT = FILE.parent  # root directory
-README = (ROOT / "README.md").read_text(encoding="utf-8")
-REQUIREMENTS = [f'{x.name}{x.specifier}' for x in pkg.parse_requirements((ROOT / 'requirements.txt').read_text())]
+PARENT = FILE.parent  # root directory
+README = (PARENT / "README.md").read_text(encoding="utf-8")
+REQUIREMENTS = [f'{x.name}{x.specifier}' for x in pkg.parse_requirements((PARENT / 'requirements.txt').read_text())]
 
 
 def get_version():
-    file = ROOT / 'ultralytics/__init__.py'
+    file = PARENT / 'ultralytics/__init__.py'
     return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', file.read_text(), re.M)[1]
 
 
@@ -38,7 +38,8 @@ setup(
     install_requires=REQUIREMENTS,
     extras_require={
         'dev':
-        ['check-manifest', 'pytest', 'pytest-cov', 'coverage', 'mkdocs', 'mkdocstrings[python]', 'mkdocs-material'],},
+            ['check-manifest', 'pytest', 'pytest-cov', 'coverage', 'mkdocs', 'mkdocstrings[python]',
+             'mkdocs-material'], },
     classifiers=[
         "Intended Audience :: Developers", "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)", "Programming Language :: Python :: 3",
@@ -50,4 +51,4 @@ setup(
         "Operating System :: MacOS", "Operating System :: Microsoft :: Windows"],
     keywords="machine-learning, deep-learning, vision, ML, DL, AI, YOLO, YOLOv3, YOLOv5, YOLOv8, HUB, Ultralytics",
     entry_points={
-        'console_scripts': ['yolo = ultralytics.yolo.cli:cli', 'ultralytics = ultralytics.yolo.cli:cli'],})
+        'console_scripts': ['yolo = ultralytics.yolo.cli:cli', 'ultralytics = ultralytics.yolo.cli:cli'], })

cdd8b35a1218d5956145fe6d48a63535d72426f1
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-01-12 20:29:03 +0100

    delete v8 ROOT definition

--- ultralytics/yolo/v8/__init__.py
+++ ultralytics/yolo/v8/__init__.py
@@ -4,8 +4,6 @@ from pathlib import Path
 
 from ultralytics.yolo.v8 import classify, detect, segment
 
-ROOT = Path(__file__).parents[0]  # yolov8 ROOT
-
 __all__ = ["classify", "segment", "detect"]
 
 from ultralytics.yolo.configs import hydra_patch  # noqa (patch hydra cli)

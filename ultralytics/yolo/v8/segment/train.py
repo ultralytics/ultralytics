43890f6ea994be7dce79a8d7e46c40cc901909c7
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-01-14 14:56:27 +0100

    Fix relative import error when running seg directly

--- ultralytics/yolo/v8/segment/train.py
+++ ultralytics/yolo/v8/segment/train.py
@@ -14,7 +14,7 @@ from ultralytics.yolo.utils.plotting import plot_images, plot_results
 from ultralytics.yolo.utils.tal import make_anchors
 from ultralytics.yolo.utils.torch_utils import de_parallel
 
-from ..detect.train import Loss
+from ultralytics.yolo.v8.detect.train import Loss
 
 
 # BaseTrainer python usage

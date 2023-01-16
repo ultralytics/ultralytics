d0fcac5d2f219702d7ce1c1915aaea6167690aea
陈贵祥 <chen.guixiang@h3c.com>
2023-01-16 17:05:57 +0800

    Add shufflev2block

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -461,6 +461,19 @@ class Classify(nn.Module):
         return self.linear(self.drop(self.pool(self.conv(x)).flatten(1)))
 
 
+def channel_shuffle(x, groups):
+    batchsize, num_channels, height, width = x.data.size()
+    channels_per_group = num_channels // groups
+
+    # reshape
+    x = x.view(batchsize, groups, channels_per_group, height, width)
+    x = torch.transpose(x, 1, 2).contiguous()
+
+    # flatten
+    x = x.view(batchsize, -1, height, width)
+    return x
+
+
 class ShuffleV2Block(nn.Module):
 
     def __init__(self, inp, oup, stride):

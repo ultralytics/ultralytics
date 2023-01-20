fbe1265321911ecf52cec47c2c1a7b5bd5010904
Kamlesh Kumar <patelkamleshpatel364@gmail.com>
2023-01-20 19:52:53 +0530

    Update predict.py

--- ultralytics/yolo/v8/detect/predict.py
+++ ultralytics/yolo/v8/detect/predict.py
@@ -20,13 +20,13 @@ class DetectionPredictor(BasePredictor):
         img /= 255  # 0 - 255 to 0.0 - 1.0
         return img
 
-    def postprocess(self, preds, img, orig_img):
+    def postprocess(self, preds, img, orig_img, classes=None):
         preds = ops.non_max_suppression(preds,
                                         self.args.conf,
                                         self.args.iou,
                                         agnostic=self.args.agnostic_nms,
                                         max_det=self.args.max_det,
-                                        classes=[0])
+                                        classes=classes)
 
         results = []
         for i, pred in enumerate(preds):
@@ -87,6 +87,7 @@ def predict(cfg):
     cfg.model = cfg.model or "yolov8n.pt"
     cfg.source = cfg.source if cfg.source is not None else ROOT / "assets" if is_git_directory() \
         else "https://ultralytics.com/images/bus.jpg"
+    cfg.classes = cfg.classes or None
     predictor = DetectionPredictor(cfg)
     predictor.predict_cli()
 

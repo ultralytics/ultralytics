287ad9be755a436852cd125d9305bd4d149598b0
ayush chaurasia <ayush.chaurarsia@gmail.com>
2023-01-21 15:01:43 +0530

    don't reverse masks

--- ultralytics/yolo/engine/results.py
+++ ultralytics/yolo/engine/results.py
@@ -265,7 +265,7 @@ class Masks:
     def segments(self):
         return [
             ops.scale_segments(self.masks.shape[1:], x, self.orig_shape, normalize=True)
-            for x in reversed(ops.masks2segments(self.masks))]
+            for x in ops.masks2segments(self.masks)]
 
     @property
     def shape(self):

5c224bb3d2c7f3a27d044147b772fdefad0313af
Kamlesh Kumar <patelkamleshpatel364@gmail.com>
2023-01-21 19:24:41 +0530

    Update predictor.py

--- ultralytics/yolo/engine/predictor.py
+++ ultralytics/yolo/engine/predictor.py
@@ -101,7 +101,7 @@ class BasePredictor:
     def write_results(self, results, batch, print_string):
         raise NotImplementedError("print_results function needs to be implemented")
 
-    def postprocess(self, preds, img, orig_img,classes=None):
+    def postprocess(self, preds, img, orig_img, classes=None):
         return preds
 
     def setup_source(self, source=None):

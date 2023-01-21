99f7149f4931164578359941c9e4585158c0ce37
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-01-21 20:04:41 +0100

    Always cache with Threadpool instead of Pool

--- ultralytics/yolo/data/dataloaders/v5loader.py
+++ ultralytics/yolo/data/dataloaders/v5loader.py
@@ -615,7 +615,7 @@ class LoadImagesAndLabels(Dataset):
         nm, nf, ne, nc, msgs = 0, 0, 0, 0, []  # number missing, found, empty, corrupt, messages
         desc = f"{prefix}Scanning {path.parent / path.stem}..."
         total = len(self.im_files)
-        with (Pool if total > 10000 else ThreadPool)(NUM_THREADS) as pool:
+        with ThreadPool(NUM_THREADS) as pool:
             results = pool.imap(verify_image_label, zip(self.im_files, self.label_files, repeat(prefix)))
             pbar = tqdm(results, desc=desc, total=total, bar_format=TQDM_BAR_FORMAT)
             for im_file, lb, shape, segments, nm_f, nf_f, ne_f, nc_f, msg in pbar:

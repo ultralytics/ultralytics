ad5e49fd97845bb2dd4f657e0c150c97eed7c601
Kamlesh Kumar <patelkamleshpatel364@gmail.com>
2023-01-21 19:26:06 +0530

    Update predict.py

--- ultralytics/yolo/v8/segment/predict.py
+++ ultralytics/yolo/v8/segment/predict.py
@@ -10,7 +10,7 @@ from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 
 class SegmentationPredictor(DetectionPredictor):
 
-    def postprocess(self, preds, img, orig_img):
+    def postprocess(self, preds, img, orig_img,classes=None):
         # TODO: filter by classes
         p = ops.non_max_suppression(preds[0],
                                     self.args.conf,

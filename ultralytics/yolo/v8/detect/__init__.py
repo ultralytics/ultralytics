781291a3ff4a6144b4267fecf616a2f451846fb2
Prince kumar <85225054+prince0310@users.noreply.github.com>
2023-01-23 03:31:33 +0530

    Update __init__.py

--- ultralytics/yolo/v8/detect/__init__.py
+++ ultralytics/yolo/v8/detect/__init__.py
@@ -1,5 +1,5 @@
 # Ultralytics YOLO 🚀, GPL-3.0 license
 
-from .predict import DetectionPredictor, predict
-from .train import DetectionTrainer, train
-from .val import DetectionValidator, val
+from  ultralytics.yolo.v8.detect.predict import DetectionPredictor, predict
+from  ultralytics.yolo.v8.detect.train import DetectionTrainer, train
+from  ultralytics.yolo.v8.detect.val import DetectionValidator, val

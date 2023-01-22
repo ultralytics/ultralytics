824075070111d32c22da486f9dd9ce289c9e3706
Prince kumar <85225054+prince0310@users.noreply.github.com>
2023-01-23 02:58:36 +0530

    Update and rename init.py to __init__.py

---
+++ ultralytics/models/__init__.py
@@ -0,0 +1,9 @@
+# Ultralytics YOLO 🚀, GPL-3.0 license
+
+__version__ = "8.0.15"
+
+from ultralytics.yolo.engine.model import YOLO
+from ultralytics.yolo.utils import ops
+from ultralytics.yolo.utils.checks import check_yolo as checks
+
+__all__ = ["__version__", "YOLO", "hub", "checks"]  # allow simpler import

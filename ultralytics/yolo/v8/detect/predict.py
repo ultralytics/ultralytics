ea4139cdce310680d7ea4140d83746251aaa95cb
Prince kumar <85225054+prince0310@users.noreply.github.com>
2023-01-23 12:30:04 +0530

    Update predict.py

--- ultralytics/yolo/v8/detect/predict.py
+++ ultralytics/yolo/v8/detect/predict.py
@@ -7,12 +7,11 @@ from ultralytics.yolo.engine.predictor import BasePredictor
 from ultralytics.yolo.engine.results import Results
 from ultralytics.yolo.utils import DEFAULT_CFG, ROOT, ops
 from ultralytics.yolo.utils.plotting import Annotator, colors, save_one_box
-
-global count
+global val
 
 
 class DetectionPredictor(BasePredictor):
-    count = 0
+    val = 0
 
     def get_annotator(self, img):
         return Annotator(img, line_width=self.args.line_thickness, example=str(self.model.names))
@@ -29,13 +28,15 @@ class DetectionPredictor(BasePredictor):
                                         self.args.iou,
                                         agnostic=self.args.agnostic_nms,
                                         max_det=self.args.max_det,
-                                        classes=self.args.classes)
+                                        classes=self.args.classes,
+                                        count = self.args.count)
 
         results = []
         for i, pred in enumerate(preds):
             shape = orig_img[i].shape if isinstance(orig_img, list) else orig_img.shape
             pred[:, :4] = ops.scale_boxes(img.shape[2:], pred[:, :4], shape).round()
             results.append(Results(boxes=pred, orig_shape=shape[:2]))
+        results.append(count)
         return results
 
     def write_results(self, idx, results, batch):
@@ -54,26 +55,27 @@ class DetectionPredictor(BasePredictor):
         self.txt_path = str(self.save_dir / 'labels' / p.stem) + ('' if self.dataset.mode == 'image' else f'_{frame}')
         log_string += '%gx%g ' % im.shape[2:]  # print string
         self.annotator = self.get_annotator(im0)
-        if len(self.annotator.ans()) > 0:
-            (cx, cy) = self.annotator.ans()[0]
-        else:
-            cy = 0
-        self.annotator = self.get_annotator(im0)
-
         det = results[idx].boxes  # TODO: make boxes inherit from tensors
         if len(det) == 0:
             return log_string
         for c in det.cls.unique():
             n = (det.cls == c).sum()  # detections per class
-            if cy < (400) and cy > 385:
-                DetectionPredictor.count += 1
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            align = im0.shape
-            align_bottom = align[0]
-            align_bottom -= 110
-            align_left = (align[1] / 8)  # Text allignment (align left,(int(align bottom))
-            cv2.putText(im0, f'{self.model.names[int(c)]} : {str(DetectionPredictor.count)}',
-                        (int(align_left), align_bottom), font, 3, (0, 255, 1), 5, cv2.LINE_AA)
+            if results[-1] == True:
+                if len(self.annotator.ans()) > 0:
+                    (cx, cy) = self.annotator.ans()[0]
+                else:
+                    cy = 0
+                if cy < (400) and cy > 385 :
+                    DetectionPredictor.val  +=1
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                align = im0.shape 
+                align_bottom = align[0]
+                align_bottom -= 110
+                align_left = (align[1]/8) # Text allignment (align left,(int(align bottom))
+                cv2.putText(im0, f'{self.model.names[int(c)]} : {str(DetectionPredictor.val)}', (int(align_left),align_bottom), font, 
+                    3, (0, 255, 1), 5, cv2.LINE_AA)
+            else:
+                pass
             log_string += f"{n} {self.model.names[int(c)]}{'s' * (n > 1)}, "
 
         # write

cb34710717cddf0e48444f8c8efccba22effb8ea
ayush chaurasia <ayush.chaurarsia@gmail.com>
2023-01-28 11:09:56 +0530

    improve preds

--- ultralytics/yolo/data/__init__.py
+++ ultralytics/yolo/data/__init__.py
@@ -1,6 +1,6 @@
 # Ultralytics YOLO 🚀, GPL-3.0 license
 
 from .base import BaseDataset
-from .build import build_classification_dataloader, build_dataloader
+from .build import build_classification_dataloader, build_dataloader, load_inference_source
 from .dataset import ClassificationDataset, SemanticDataset, YOLODataset
 from .dataset_wrappers import MixAndRectDataset

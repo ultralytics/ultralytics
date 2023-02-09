4d424a3241f0585cbc30577be449bb0d0846d7c1
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-02-10 01:37:57 +0400

    `ultralytics 8.0.33` security updates and fixes (#896)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Mert Can Demir <validatedev@gmail.com>

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,9 +1,8 @@
 # Ultralytics YOLO 🚀, GPL-3.0 license
 
-__version__ = "8.0.32"
+__version__ = "8.0.33"
 
 from ultralytics.yolo.engine.model import YOLO
-from ultralytics.yolo.utils import ops
 from ultralytics.yolo.utils.checks import check_yolo as checks
 
 __all__ = ["__version__", "YOLO", "hub", "checks"]  # allow simpler import

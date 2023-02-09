f428d5e6286ab741ff979d75c13d51b60bae9e5b
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-02-10 01:00:51 +0400

    add remove_ansi_codes

--- ultralytics/yolo/utils/__init__.py
+++ ultralytics/yolo/utils/__init__.py
@@ -451,6 +451,19 @@ def colorstr(*input):
     return "".join(colors[x] for x in args) + f"{string}" + colors["end"]
 
 
+def remove_ansi_codes(string):
+    """
+    Remove ANSI escape sequences from a string.
+
+    Args:
+        string (str): The input string that may contain ANSI escape sequences.
+
+    Returns:
+        str: The input string with ANSI escape sequences removed.
+    """
+    return re.sub(r'\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]', '', string)
+
+
 def set_logging(name=LOGGING_NAME, verbose=True):
     # sets up logging for the given name
     rank = int(os.getenv('RANK', -1))  # rank in world for Multi-GPU trainings

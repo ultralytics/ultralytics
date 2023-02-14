3d090c9bb47a78bd7925e0624e092bc791f110c1
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-02-14 13:57:42 +0400

    Update ultralytics/nn/tasks.py
    Test updated version without eval()
    
    Co-authored-by: Aarni Koskela <akx@iki.fi>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -428,7 +428,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
     for i, (f, n, m, args) in enumerate(d['backbone'] + d['head']):  # from, number, module, args
         m = eval(m) if isinstance(m, str) else m  # eval strings
         for j, a in enumerate(args):
-            args[j] = eval(a) if isinstance(a, str) and (keyword.iskeyword(a) or a in locals()) else a  # eval strings
+            args[j] = (locals()[a] if a in locals() else ast.literal_eval(a)) if isinstance(a, str) else a  # eval strings
 
         n = n_ = max(round(n * gd), 1) if n > 1 else n  # depth gain
         if m in {

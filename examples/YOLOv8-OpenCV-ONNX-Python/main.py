a0e41eb84ed75d534f841fa97cc7bb78ea3f06e4
Farid Inawan <frdteknikelektro@gmail.com>
2023-02-16 19:27:53 +0700

    [Example] YOLOv8-OpenCV-ONNX-Python

---
+++ examples/YOLOv8-OpenCV-ONNX-Python/main.py
@@ -0,0 +1,79 @@
+import cv2.dnn
+import numpy as np
+
+with open("classes.txt", "r") as f:
+    classes = f.read().split("\n")
+
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+
+def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+    label = "{0} ({1:.2f})".format(classes[class_id], confidence)
+    color = colors[class_id]
+    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+    cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+def main():
+    model: cv2.dnn.Net = cv2.dnn.readNetFromONNX('yolov8n.onnx')
+    original_image: np.ndarray = cv2.imread('bus.jpg')
+    [height, width, _] = original_image.shape
+    length = max((height, width))
+    image = np.zeros((length, length, 3), np.uint8)
+    image[0:height, 0:width] = original_image
+    scale = length / 640
+
+    blob = cv2.dnn.blobFromImage(image, scalefactor=1/255, size=(640, 640))
+    model.setInput(blob)
+    outputs = model.forward()
+
+    outputs = np.array([cv2.transpose(outputs[0])])
+    rows = outputs.shape[1]
+
+    boxes = []
+    scores = []
+    class_ids = []
+
+    for i in range(rows):
+        classes_scores = outputs[0][i][4:]
+        (minScore, maxScore, minClassLoc, (x, maxClassIndex)) = cv2.minMaxLoc(classes_scores)
+        if maxScore >= 0.25:
+            box = [
+                outputs[0][i][0] - (0.5 * outputs[0][i][2]),
+                outputs[0][i][1] - (0.5 * outputs[0][i][3]),
+                outputs[0][i][2],
+                outputs[0][i][3]
+            ]
+            boxes.append(box)
+            scores.append(maxScore)
+            class_ids.append(maxClassIndex)
+
+    result_boxes = cv2.dnn.NMSBoxes(boxes, scores, 0.25, 0.45, 0.5)
+
+    detections = []
+    for i in range(len(result_boxes)):
+        index = result_boxes[i]
+        box = boxes[index]
+        detection = {
+            'class_id': class_ids[index],
+            'class_name': classes[class_ids[index]],
+            'confidence': scores[index],
+            'box': box,
+            'scale': scale
+        }
+        detections.append(detection)
+        draw_bounding_box(
+            original_image, class_ids[index], scores[index],
+            round(box[0] * scale), round(box[1] * scale),
+            round((box[0] + box[2]) * scale), round((box[1] + box[3]) * scale)
+        )
+
+    cv2.imshow('image', original_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return detections
+
+
+if __name__ == '__main__':
+    main()

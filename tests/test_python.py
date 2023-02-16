7c0e29a1664ce0b194401eae35eccebb88e2ff39
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-02-16 20:37:32 +0100

    do not test tflite by default

--- tests/test_python.py
+++ tests/test_python.py
@@ -129,10 +129,12 @@ def test_export_coreml():  # sourcery skip: move-assign
         YOLO(f)(SOURCE)  # model prediction only supported on macOS
 
 
-def test_export_tflite():
-    model = YOLO(MODEL)
-    f = model.export(format='tflite')
-    YOLO(f)(SOURCE)
+def test_export_tflite(enabled=False):
+    # TF suffers from install conflicts on Windows and macOS
+    if enabled:
+        model = YOLO(MODEL)
+        f = model.export(format='tflite')
+        YOLO(f)(SOURCE)
 
 
 def test_export_paddle(enabled=False):

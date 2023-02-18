09b29dd7f2b120d609bb86707f7e9d3ae4bc9c48
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-02-18 14:38:05 +0100

    update check_requirements()

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -174,7 +174,7 @@ class C2(nn.Module):
         self.m = nn.Sequential(*(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n)))
 
     def forward(self, x):
-        a, b = self.cv1(x).split((self.c, self.c), 1)
+        a, b = self.cv1(x).chunk(2, 1)
         return self.cv2(torch.cat((self.m(a), b), 1))
 
 
@@ -188,7 +188,7 @@ class C2f(nn.Module):
         self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
 
     def forward(self, x):
-        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y = list(self.cv1(x).chunk(2, 1))
         y.extend(m(y[-1]) for m in self.m)
         return self.cv2(torch.cat(y, 1))
 

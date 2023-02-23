8282ddeb1cb4a5cada4678790742c6e2201db705
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-02-23 14:06:07 -0800

    DDP callback fix

--- ultralytics/yolo/utils/dist.py
+++ ultralytics/yolo/utils/dist.py
@@ -5,6 +5,7 @@ import shutil
 import socket
 import sys
 import tempfile
+from pathlib import Path
 
 from . import USER_CONFIG_DIR
 from .torch_utils import TORCH_1_9
@@ -46,15 +47,15 @@ def generate_ddp_command(world_size, trainer):
     import __main__  # noqa local import to avoid https://github.com/Lightning-AI/lightning/issues/15218
 
     # Get file and args (do not use sys.argv due to security vulnerability)
-    exclude_args = ['save_dir']
+    exclude_args = {'save_dir'}
     args = [f'{k}={v}' for k, v in vars(trainer.args).items() if k not in exclude_args]
-    file = generate_ddp_file(trainer)  # if argv[0].endswith('yolo') else os.path.abspath(argv[0])
+    script_name = Path(sys.argv[0])
+    file = generate_ddp_file(trainer) if script_name.stem == 'yolo' else script_name
 
     # Build command
-    torch_distributed_cmd = 'torch.distributed.run' if TORCH_1_9 else 'torch.distributed.launch'
-    cmd = [
-        sys.executable, '-m', torch_distributed_cmd, '--nproc_per_node', f'{world_size}', '--master_port',
-        f'{find_free_network_port()}', file] + args
+    port = find_free_network_port()
+    dist_cmd = 'torch.distributed.run' if TORCH_1_9 else 'torch.distributed.launch'
+    cmd = [sys.executable, '-m', dist_cmd, '--nproc_per_node', f'{world_size}', '--master_port', f'{port}', file] + args
     return cmd, file
 
 

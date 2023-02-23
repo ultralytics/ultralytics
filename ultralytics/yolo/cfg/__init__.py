13bb92b02db7d1145c62c34a1cca72797b6b3c90
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-02-23 07:33:00 -0800

    Update __init__.py

--- ultralytics/yolo/cfg/__init__.py
+++ ultralytics/yolo/cfg/__init__.py
@@ -47,11 +47,12 @@ CLI_HELP_MSG = \
     GitHub: https://github.com/ultralytics/ultralytics
     """
 
+# Define keys for arg type checks
 CFG_FLOAT_KEYS = {'warmup_epochs', 'box', 'cls', 'dfl', 'degrees', 'shear', 'fl_gamma'}
 CFG_FRACTION_KEYS = {
     'dropout', 'iou', 'lr0', 'lrf', 'momentum', 'weight_decay', 'warmup_momentum', 'warmup_bias_lr', 'label_smoothing',
     'hsv_h', 'hsv_s', 'hsv_v', 'translate', 'scale', 'perspective', 'flipud', 'fliplr', 'mosaic', 'mixup', 'copy_paste',
-    'conf', 'iou'}
+    'conf', 'iou'}  # floats limited to 0.0 - 1.0
 CFG_INT_KEYS = {
     'epochs', 'patience', 'batch', 'workers', 'seed', 'close_mosaic', 'mask_ratio', 'max_det', 'vid_stride',
     'line_thickness', 'workspace', 'nbs', 'save_period'}

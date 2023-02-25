ff25eaf8e2e94f55bbccda3f32b36807aec9ab22
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-02-25 12:10:02 -0800

    add save_crop and save_txt to tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -48,7 +48,7 @@ def test_val_classify():
 
 # Predict checks -------------------------------------------------------------------------------------------------------
 def test_predict_detect():
-    run(f"yolo predict model={MODEL}.pt source={ROOT / 'assets'} imgsz=32 save")
+    run(f"yolo predict model={MODEL}.pt source={ROOT / 'assets'} imgsz=32 save save_crop save_txt")
     if checks.check_online():
         run(f'yolo predict model={MODEL}.pt source=https://ultralytics.com/images/bus.jpg imgsz=32')
         run(f'yolo predict model={MODEL}.pt source=https://ultralytics.com/assets/decelera_landscape_min.mov imgsz=32')

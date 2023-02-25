fd2fcb9df846cb27266fafd652d1f370f9f11845
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-02-24 22:09:57 -0800

    attempt ddp improvement

--- ultralytics/yolo/utils/dist.py
+++ ultralytics/yolo/utils/dist.py
@@ -6,6 +6,7 @@ import socket
 import sys
 import tempfile
 from pathlib import Path
+import re
 
 from . import USER_CONFIG_DIR
 from .torch_utils import TORCH_1_9
@@ -45,8 +46,9 @@ def generate_ddp_command(world_size, trainer):
     import __main__  # noqa local import to avoid https://github.com/Lightning-AI/lightning/issues/15218
     if not trainer.resume:
         shutil.rmtree(trainer.save_dir)  # remove the save_dir
-    file = Path(sys.argv[0]).resolve()
-    if not (file.exists() and file.suffix == '.py'):  # using CLI
+    file = str(Path(sys.argv[0]).resolve())
+    safe_pattern = re.compile(r'^[a-zA-Z0-9_. /\\-]{1,100}$')  # allowed characters and maximum of 100 characters
+    if not (safe_pattern.match(file) and Path(file).exists() and file.endswith('.py')):  # using CLI
         file = generate_ddp_file(trainer)
     dist_cmd = 'torch.distributed.run' if TORCH_1_9 else 'torch.distributed.launch'
     port = find_free_network_port()

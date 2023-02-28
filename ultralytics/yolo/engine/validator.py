144b6a10a83afa45aacc67bd239f39383b42281a
Mehran Ghandehari <mehran.maps@gmail.com>
2023-02-28 14:49:43 -0700

    Update training validator for plotting
    when an early stopping occurs, the training validator does not plot the graphs because the `plots` arg only was changing to `True` when reaching the final epoch. The revised code was tested on a real data and it works as expected.

--- ultralytics/yolo/engine/validator.py
+++ ultralytics/yolo/engine/validator.py
@@ -102,7 +102,10 @@ class BaseValidator:
             model = model.half() if self.args.half else model.float()
             self.model = model
             self.loss = torch.zeros_like(trainer.loss_items, device=trainer.device)
-            self.args.plots = trainer.epoch == trainer.epochs - 1  # always plot final epoch
+            if trainer.stopper.possible_stop or (trainer.epoch == trainer.epochs - 1): # always plot final epoch or for early stopping
+                self.args.plots = True
+            else:
+                self.args.plots = False
             model.eval()
         else:
             callbacks.add_integration_callbacks(self)

c37423b2c53bf401d7a4dc80ec53bcf53d7e788e
Mehran Ghandehari <mehran.maps@gmail.com>
2023-03-01 09:24:40 -0700

    Update training validator for plotting when early stopping occurs (#1204)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/yolo/engine/validator.py
+++ ultralytics/yolo/engine/validator.py
@@ -102,7 +102,7 @@ class BaseValidator:
             model = model.half() if self.args.half else model.float()
             self.model = model
             self.loss = torch.zeros_like(trainer.loss_items, device=trainer.device)
-            self.args.plots = trainer.epoch == trainer.epochs - 1  # always plot final epoch
+            self.args.plots = trainer.stopper.possible_stop or (trainer.epoch == trainer.epochs - 1)
             model.eval()
         else:
             callbacks.add_integration_callbacks(self)

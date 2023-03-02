8a325297706a5880a124da05580641b6b64a710f
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-03-02 09:12:30 -0800

    Add YOLOv5 P6u model YAMLs

--- ultralytics/yolo/engine/exporter.py
+++ ultralytics/yolo/engine/exporter.py
@@ -693,12 +693,15 @@ class Exporter:
         model_meta.license = self.metadata['license']
 
         # Label file
-        tmp_file = Path(file).parent / 'temp_meta.txt'
+        names = self.metadata['names']
+        tmp_file = Path(file).parent / 'labels.txt'
         with open(tmp_file, 'w') as f:
-            f.write(str(self.metadata))
+            for value in names.values():
+                f.write(value + '\n')
 
         label_file = _metadata_fb.AssociatedFileT()
         label_file.name = tmp_file.name
+        label_file.description = "Labels for objects that the model can detect."
         label_file.type = _metadata_fb.AssociatedFileType.TENSOR_AXIS_LABELS
 
         # Create input info

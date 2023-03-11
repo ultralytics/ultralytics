2c5b3bb8eb46a3b1a313741692b4b745ee579c76
LinirZamir <alzcorp@gmail.com>
2023-03-10 23:00:12 -0500

    Fix path capability

--- testing.py
+++
@@ -1,6 +0,0 @@
-from ultralytics.yolo.engine.model import YOLO
-
-
-
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-results = model("https://ultralytics.com/images/bus.jpg", stream=False, save=True)  

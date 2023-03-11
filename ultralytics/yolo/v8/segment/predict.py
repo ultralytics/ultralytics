8987a1398cd9aef6b68b467b31a517b3e96299ab
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-03-11 17:18:10 +0100

    Update predict.py
    CI fix

--- ultralytics/yolo/v8/segment/predict.py
+++ ultralytics/yolo/v8/segment/predict.py
@@ -31,7 +31,7 @@ class SegmentationPredictor(DetectionPredictor):
             if self.args.retina_masks:
                 if not isinstance(orig_imgs, torch.Tensor):
                     pred[:, :4] = ops.scale_boxes(img.shape[2:], pred[:, :4], orig_img.shape)
-                masks = ops.process_mask_native(proto[i], pred[:, 6:], pred[:, :4], shape[:2])  # HWC
+                masks = ops.process_mask_native(proto[i], pred[:, 6:], pred[:, :4], orig_img.shape[:2])  # HWC
             else:
                 masks = ops.process_mask(proto[i], pred[:, 6:], pred[:, :4], img.shape[2:], upsample=True)  # HWC
                 if not isinstance(orig_imgs, torch.Tensor):

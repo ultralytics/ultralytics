d64aeef2451ab91e71865e6b0c2cb7f290df5f0d
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-03-12 01:43:04 +0100

    attempt broadcast fix

--- ultralytics/yolo/engine/trainer.py
+++ ultralytics/yolo/engine/trainer.py
@@ -208,7 +208,8 @@ class BaseTrainer:
             callbacks_backup = callbacks.default_callbacks.copy()  # backup callbacks as check_amp() resets them
             self.amp = torch.tensor(check_amp(self.model), device=self.device)
             callbacks.default_callbacks = callbacks_backup  # restore callbacks
-        dist.broadcast(self.amp, src=0)  # broadcast the tensor from rank 0 to all other ranks
+        dist_output = dist.broadcast(self.amp, src=0)  # broadcast the tensor from rank 0 to all other ranks
+        print(RANK, dist_output)
         self.amp = bool(self.amp)  # as boolean
         self.scaler = amp.GradScaler(enabled=self.amp)
         if world_size > 1:

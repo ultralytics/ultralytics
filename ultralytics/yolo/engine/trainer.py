attempt broadcast fix

ultralytics/yolo/engine/trainer.py
@@ -208,8 +208,7 @@ class BaseTrainer:
             callbacks_backup = callbacks.default_callbacks.copy()  # backup callbacks as check_amp() resets them
             self.amp = torch.tensor(check_amp(self.model), device=self.device)
             callbacks.default_callbacks = callbacks_backup  # restore callbacks
-        dist_output = dist.broadcast(self.amp, src=0)  # broadcast the tensor from rank 0 to all other ranks
-        print(RANK, dist_output)
+        dist.broadcast(self.amp, src=0)  # broadcast the tensor from rank 0 to all other ranks (returns None)
         self.amp = bool(self.amp)  # as boolean
         self.scaler = amp.GradScaler(enabled=self.amp)
         if world_size > 1:
@@ -623,7 +622,6 @@ def check_amp(model):
         AssertionError: If the AMP checks fail, indicating anomalies with the AMP functionality on the system.
     """
     device = next(model.parameters()).device  # get model device
-    print('RANK, DEVICE', RANK, device)
     if device.type in ('cpu', 'mps'):
         return False  # AMP only used on CUDA devices
 
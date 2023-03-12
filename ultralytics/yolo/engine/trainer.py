attempt broadcast fix

ultralytics/yolo/engine/trainer.py
@@ -203,10 +203,13 @@ class BaseTrainer:
         self.model = self.model.to(self.device)
         self.set_model_attributes()
         # Check AMP
+        self.amp = torch.tensor([True]).to(self.device)
         with torch_distributed_zero_first(RANK):
-            callbacks_backup = callbacks.default_callbacks.copy()  # backup callbacks as they are reset by check_amp()
-            self.amp = check_amp(self.model)
-            callbacks.default_callbacks = callbacks_backup  # restore callbacks
+            if RANK in (-1, 0):
+                callbacks_backup = callbacks.default_callbacks.copy()  # backup callbacks as they are reset by check_amp()
+                self.amp = check_amp(self.model)
+                callbacks.default_callbacks = callbacks_backup  # restore callbacks
+        dist.broadcast(self.amp, src=0)  # broadcast the tensor from rank 0 to all other ranks
         self.scaler = amp.GradScaler(enabled=self.amp)
         if world_size > 1:
             self.model = DDP(self.model, device_ids=[rank])
8e6e48d6ef1b30fea5f5527c3d22df01aa291416
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-03-15 21:34:58 +0100

    ci fix

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -416,7 +416,7 @@ class Detect(nn.Module):
             box = x_cat[:, :self.reg_max * 4]
             cls = x_cat[:, self.reg_max * 4:]
         else:
-            box, cls = x_cat
+            box, cls = x_cat.split((self.reg_max * 4, self.nc), 1)
         dbox = dist2bbox(self.dfl(box), self.anchors.unsqueeze(0), xywh=True, dim=1) * self.strides
         y = torch.cat((dbox, cls.sigmoid()), 1)
         return y if self.export else (y, x)

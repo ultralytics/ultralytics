4fc7e39817307463420748d6c25e6f05f4ed3c51
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-03-17 15:02:07 +0100

    fix zip function

--- ultralytics/yolo/data/utils.py
+++ ultralytics/yolo/data/utils.py
@@ -479,10 +479,9 @@ def zip_directory(dir):
         from ultralytics.yolo.data.utils import zip_directory
         zip_directory('/Users/glennjocher/Downloads/playground/coco8')
     """
+    dir = Path(dir)
     delete_dsstore(dir)
-    output_path = Path(dir).with_suffix('.zip')
-    with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zip_file:
-        for root, dirs, files in os.walk(dir):
-            for file in files:
-                file_path = os.path.join(root, file)
-                zip_file.write(file_path, os.path.relpath(file_path, dir))
+    with zipfile.ZipFile(dir.with_suffix('.zip'), 'w', zipfile.ZIP_DEFLATED) as zip_file:
+        for file_path in dir.glob('**/*'):
+            if file_path.is_file():
+                zip_file.write(file_path, file_path.relative_to(dir))

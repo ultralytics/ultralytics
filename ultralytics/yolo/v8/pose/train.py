fb2125a71d8fa2aacf7a9cf47bebfa6c9853b8d6
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-03-19 01:38:54 +0100

    cleanup

--- ultralytics/yolo/v8/pose/train.py
+++ ultralytics/yolo/v8/pose/train.py
@@ -146,8 +146,7 @@ class PoseLoss(Loss):
 
     def kpts_decode(self, anchor_points, pred_kpts):
         y = pred_kpts.clone()
-        y[..., 0] *= 2.0
-        y[..., 1] *= 2.0
+        y[..., :2] *= 2.0
         y[..., 0] += anchor_points[:, [0]] - 0.5
         y[..., 1] += anchor_points[:, [1]] - 0.5
         return y

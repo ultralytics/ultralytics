47405bde631b8fb105e7839e6f5ea1a8ec015ab4
Laughing-q <1185102784@qq.com>
2023-03-25 18:18:07 +0800

    fix nan val/seg_loss

--- ultralytics/yolo/v8/segment/train.py
+++ ultralytics/yolo/v8/segment/train.py
@@ -126,11 +126,11 @@ class SegLoss(Loss):
 
                 # WARNING: lines below prevents Multi-GPU DDP 'unused gradient' PyTorch errors, do not remove
                 else:
-                    loss[1] += proto.sum() * 0 + pred_masks.sum() * 0
+                    loss[1] += (proto * 0).sum() + (pred_masks * 0).sum()
 
         # WARNING: lines below prevent Multi-GPU DDP 'unused gradient' PyTorch errors, do not remove
         else:
-            loss[1] += proto.sum() * 0 + pred_masks.sum() * 0
+            loss[1] += (proto * 0).sum() + (pred_masks * 0).sum()
 
         loss[0] *= self.hyp.box  # box gain
         loss[1] *= self.hyp.box / batch_size  # seg gain

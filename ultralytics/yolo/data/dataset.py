ec3e3b02b0054722aa831c25b37c1a6d72a98ba8
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-03-26 16:19:08 +0200

    Fix CI

--- ultralytics/yolo/data/dataset.py
+++ ultralytics/yolo/data/dataset.py
@@ -10,10 +10,10 @@ import torch
 import torchvision
 from tqdm import tqdm
 
-from ..utils import NUM_THREADS, TQDM_BAR_FORMAT, is_dir_writeable
+from ..utils import LOCAL_RANK, NUM_THREADS, TQDM_BAR_FORMAT, is_dir_writeable
 from .augment import Compose, Format, Instances, LetterBox, classify_albumentations, classify_transforms, v8_transforms
 from .base import BaseDataset
-from .utils import HELP_URL, LOCAL_RANK, LOGGER, get_hash, img2label_paths, verify_image_label
+from .utils import HELP_URL, LOGGER, get_hash, img2label_paths, verify_image_label
 
 
 class YOLODataset(BaseDataset):

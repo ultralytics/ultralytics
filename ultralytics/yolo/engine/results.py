2074feeea8be803e67689b02b7f976bc136f81df
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-03-30 13:34:45 +0200

    Update results.py
    Use new TORCHVISION_0_10 variable

--- ultralytics/yolo/engine/results.py
+++ ultralytics/yolo/engine/results.py
@@ -14,6 +14,7 @@ import torchvision.transforms.functional as F
 from torchvision import __version__ as torchvision_version
 
 from ultralytics.yolo.utils import LOGGER, SimpleClass, ops
+from ultralytics.yolo.utils.torch_utils import TORCHVISION_0_10
 from ultralytics.yolo.utils.plotting import Annotator, colors
 
 
@@ -130,12 +131,10 @@ class Results(SimpleClass):
 
         if masks is not None:
             im = torch.as_tensor(annotator.im, dtype=torch.float16, device=masks.data.device).permute(2, 0, 1).flip(0)
-
-            if int(torchvision_version.split('.')[1]) > 9:
+            if TORCHVISION_0_10:
                 im = F.resize(im.contiguous(), masks.data.shape[1:], antialias=True) / 255
             else:
                 im = F.resize(im.contiguous(), masks.data.shape[1:]) / 255
-
             annotator.masks(masks.data, colors=[colors(x, True) for x in boxes.cls], im_gpu=im)
 
         if probs is not None:

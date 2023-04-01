a1e83e831b5c0c4b4e0e9329dca774b07899905b
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-04-02 00:06:36 +0200

    Add docstrings to Auth

--- ultralytics/hub/auth.py
+++ ultralytics/hub/auth.py
@@ -12,17 +12,36 @@ class Auth:
     id_token = api_key = model_key = False
 
     def __init__(self, api_key=None):
+        """
+        Initialize the Auth class with an optional API key.
+
+        Args:
+            api_key (str, optional): The API key to authenticate with.
+        """
         self.api_key = self._clean_api_key(api_key)
         self.authenticate() if self.api_key else self.auth_with_cookies()
 
     @staticmethod
     def _clean_api_key(key: str) -> str:
-        """Strip model from key if present"""
+        """
+        Strip the model from the key if present.
+
+        Args:
+            key (str): The API key string.
+
+        Returns:
+            str: The cleaned API key string.
+        """
         separator = '_'
         return key.split(separator)[0] if separator in key else key
 
     def authenticate(self) -> bool:
-        """Attempt to authenticate with server"""
+        """
+        Attempt to authenticate with the server using either id_token or API key.
+
+        Returns:
+            bool: True if authentication is successful, False otherwise.
+        """
         try:
             header = self.get_auth_header()
             if header:
@@ -39,6 +58,9 @@ class Auth:
         """
         Attempt to fetch authentication via cookies and set id_token.
         User must be logged in to HUB and running in a supported browser.
+
+        Returns:
+            bool: True if authentication is successful, False otherwise.
         """
         if not is_colab():
             return False  # Currently only works with Colab
@@ -54,6 +76,12 @@ class Auth:
             return False
 
     def get_auth_header(self):
+        """
+        Get the authentication header for making API requests.
+
+        Returns:
+            dict: The authentication header if id_token or API key is set, None otherwise.
+        """
         if self.id_token:
             return {'authorization': f'Bearer {self.id_token}'}
         elif self.api_key:
@@ -62,9 +90,20 @@ class Auth:
             return None
 
     def get_state(self) -> bool:
-        """Get the authentication state"""
+        """
+        Get the authentication state.
+
+        Returns:
+            bool: True if either id_token or API key is set, False otherwise.
+        """
         return self.id_token or self.api_key
 
     def set_api_key(self, key: str):
-        """Get the authentication state"""
+        """
+        Set the API key for authentication.
+
+        Args:
+            key (str): The API key string.
+        """
         self.api_key = key
+

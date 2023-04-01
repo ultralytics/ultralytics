85ac5410fd6dda9697e67ad5c6b8a41587cb2dfb
Rafael Pierre <rafael.pierre@databricks.com>
2023-04-01 15:12:56 +0200

    run

--- ultralytics/yolo/utils/callbacks/mlflow.py
+++ ultralytics/yolo/utils/callbacks/mlflow.py
@@ -33,12 +33,12 @@ def on_pretrain_routine_end(trainer):
 
         prefix = colorstr('MLFlow: ')
         try:
-            run = mlflow.active_run()
-            if not run:
-                run = mlflow.start_run(experiment_id=experiment.experiment_id)
-            run_id = run.info.run_id
+            run, active_run = mlflow, mlflow.active_run()
+            if not active_run:
+                active_run = mlflow.start_run(experiment_id = experiment.experiment_id)
+            run_id = active_run.info.run_id
             LOGGER.info(f'{prefix}Using run_id({run_id}) at {mlflow_location}')
-            mlflow.log_params(vars(trainer.model.args))
+            run.log_params(vars(trainer.model.args))
         except Exception as err:
             LOGGER.error(f'{prefix}Failing init - {repr(err)}')
             LOGGER.warning(f'{prefix}Continuing without Mlflow')

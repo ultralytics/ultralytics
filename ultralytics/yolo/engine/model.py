ed82a65a349e829d2464b39cad699032191a1a24
ayush chaurasia <ayush.chaurarsia@gmail.com>
2023-04-06 17:55:16 +0530

    update

--- ultralytics/yolo/engine/model.py
+++ ultralytics/yolo/engine/model.py
@@ -405,8 +405,6 @@ class YOLO:
             LOGGER.warning('WARNING: search space not provided. Using default search space')
             space = default_space
         space['data'] = data
-        if not space.get('epochs'):
-            space['epochs'] = max_epochs
 
         trainable_with_resources = tune.with_resources(_tune, {'cpu': 8, 'gpu': gpu_per_trial if gpu_per_trial else 0})
         pbt_interval = pbt_interval

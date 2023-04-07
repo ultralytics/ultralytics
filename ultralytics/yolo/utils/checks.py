ba5a8b7af38e74f65000e045133e362b9140c066
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-04-07 14:48:06 +0200

    fix print_args auth stripping

--- ultralytics/yolo/utils/checks.py
+++ ultralytics/yolo/utils/checks.py
@@ -237,7 +237,7 @@ def check_suffix(file='yolov8n.pt', suffix='.pt', msg=''):
     # Check file(s) for acceptable suffix
     if file and suffix:
         if isinstance(suffix, str):
-            suffix = (suffix, )
+            suffix = (suffix,)
         for f in file if isinstance(file, (list, tuple)) else [file]:
             s = Path(f).suffix.lower().strip()  # file suffix
             if len(s):
@@ -345,10 +345,10 @@ def print_args(args: Optional[dict] = None, show_file=True, show_func=False):
     file, _, func, _, _ = inspect.getframeinfo(x)
     if args is None:  # get args automatically
         args, _, _, frm = inspect.getargvalues(x)
-        args = {k: strip_auth(v) for k, v in frm.items() if k in args}
+        args = {k: v for k, v in frm.items() if k in args}
     try:
         file = Path(file).resolve().relative_to(ROOT).with_suffix('')
     except ValueError:
         file = Path(file).stem
     s = (f'{file}: ' if show_file else '') + (f'{func}: ' if show_func else '')
-    LOGGER.info(colorstr(s) + ', '.join(f'{k}={v}' for k, v in args.items()))
+    LOGGER.info(colorstr(s) + ', '.join(f'{k}={strip_auth(v)}' for k, v in args.items()))

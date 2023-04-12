d6c099577d6fb6115f96b96e185864fc5ddd7c3d
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-04-12 16:24:20 +0200

    Update metrics.py

--- ultralytics/yolo/utils/metrics.py
+++ ultralytics/yolo/utils/metrics.py
@@ -11,8 +11,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-from ultralytics.yolo.utils import LOGGER, SimpleClass, TryExcept
-from ultralytics.yolo.utils.plotting import plt_settings
+from ultralytics.yolo.utils import LOGGER, SimpleClass, TryExcept, plt_settings
 
 OKS_SIGMA = np.array([.26, .25, .25, .35, .35, .79, .79, .72, .72, .62, .62, 1.07, 1.07, .87, .87, .89, .89]) / 10.0
 

ac9a1a21573d146169320990332228114ab91d3a
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-04-13 15:11:55 +0200

    Improved HUB model errors

---
+++ ultralytics/yolo/utils/errors.py
@@ -0,0 +1,8 @@
+# Ultralytics YOLO 🚀, GPL-3.0 license
+
+from ultralytics.yolo.utils import emojis
+
+
+class HUBModelError(Exception):
+    def __init__(self, message="Model not found. Please check model URL and try again."):
+        super().__init__(emojis(message))

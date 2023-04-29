8405aab668042c97e50ce27d196bc7c4931b66b7
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-04-29 16:47:58 +0200

    Update plotting.py

--- ultralytics/yolo/utils/plotting.py
+++ ultralytics/yolo/utils/plotting.py
@@ -473,11 +473,17 @@ def output_to_target(output, max_det=300):
 
 def feature_visualization(x, module_type, stage, n=32, save_dir=Path('runs/detect/exp')):
     """
-    x:              Features to be visualized
-    module_type:    Module type
-    stage:          Module stage within model
-    n:              Maximum number of feature maps to plot
-    save_dir:       Directory to save results
+    Visualize feature maps of a given model module during inference.
+
+    Args:
+        x (torch.Tensor): Features to be visualized.
+        module_type (str): Module type.
+        stage (int): Module stage within the model.
+        n (int, optional): Maximum number of feature maps to plot. Defaults to 32.
+        save_dir (Path, optional): Directory to save results. Defaults to Path('runs/detect/exp').
+
+    Returns:
+        None: This function does not return any value; it saves the visualization to the specified directory.
     """
     for m in ['Detect', 'Pose', 'Segment']:
         if m in module_type:

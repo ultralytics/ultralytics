229261bae55ce4f8a620704a5efd03c0a4c25525
Laughing <61612323+Laughing-q@users.noreply.github.com>
2023-05-01 20:36:48 +0800

    Fix tracker & add test_track (#2336)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- tests/test_python.py
+++ tests/test_python.py
@@ -236,3 +236,13 @@ def test_result():
     res[0].plot()
     res[0] = res[0].cpu().numpy()
     print(res[0].path)
+
+
+def test_track():
+    im = cv2.imread(str(SOURCE))
+    model = YOLO(MODEL)
+    seg_model = YOLO('yolov8n-seg.pt')
+    pose_model = YOLO('yolov8n-pose.pt')
+    model.track(source=im)
+    seg_model.track(source=im)
+    pose_model.track(source=im)
--- ultralytics/tracker/track.py
+++ ultralytics/tracker/track.py
@@ -39,8 +39,7 @@ def on_predict_start(predictor, persist=False):
 def on_predict_postprocess_end(predictor):
     """Postprocess detected boxes and update with object tracking."""
     bs = predictor.dataset.bs
-    im0s = predictor.batch[2]
-    im0s = im0s if isinstance(im0s, list) else [im0s]
+    im0s = predictor.batch[1]
     for i in range(bs):
         det = predictor.results[i].boxes.cpu().numpy()
         if len(det) == 0:

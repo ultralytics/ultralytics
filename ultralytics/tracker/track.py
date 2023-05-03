0ebd3f2959992be55f65cfc908bdf2fd3b618cef
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-05-04 01:27:00 +0200

    `ultralytics 8.0.92` updates and fixes (#2361)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Yonghye Kwon <developer.0hye@gmail.com>
    Co-authored-by: introvin <vinod.4166@gmail.com>
    Co-authored-by: marinmarcillat <58145636+marinmarcillat@users.noreply.github.com>
    Co-authored-by: BIGBOSS-FOX <47949596+BIGBOSS-FOX@users.noreply.github.com>

--- ultralytics/tracker/track.py
+++ ultralytics/tracker/track.py
@@ -47,7 +47,7 @@ def on_predict_postprocess_end(predictor):
         tracks = predictor.trackers[i].update(det, im0s[i])
         if len(tracks) == 0:
             continue
-        idx = tracks[:, -1].tolist()
+        idx = tracks[:, -1].astype(int)
         predictor.results[i] = predictor.results[i][idx]
         predictor.results[i].update(boxes=torch.as_tensor(tracks[:, :-1]))
 

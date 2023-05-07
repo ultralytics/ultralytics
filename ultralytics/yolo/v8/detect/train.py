d01e03e230971791d9f72240c2109a59045da4f5
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-05-07 12:46:52 +0200

    fix warnings

--- ultralytics/yolo/v8/detect/train.py
+++ ultralytics/yolo/v8/detect/train.py
@@ -32,7 +32,7 @@ class DetectionTrainer(BaseTrainer):
         gs = max(int(de_parallel(self.model).stride.max() if self.model else 0), 32)
         return build_yolo_dataset(self.args, img_path, batch, self.data, mode=mode, rect=mode == 'val', stride=gs)
 
-    def get_dataloader(self, dataset_path, batch_size, rank=0, mode='train'):
+    def get_dataloader(self, dataset_path, batch_size=16, rank=0, mode='train'):
         """TODO: manage splits differently."""
         # Calculate stride - check if model is initialized
         if self.args.v5loader:

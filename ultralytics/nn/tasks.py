570406a2c4312de53699aaca2fea9710256f6b02
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-05-13 13:49:16 +0200

    Return model info

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -489,7 +489,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
 
         n = n_ = max(round(n * depth), 1) if n > 1 else n  # depth gain
         if m in (Classify, Conv, ConvTranspose, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, Focus,
-                 BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x, RepC3, , Cxa, Cxb,
+                 BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x, RepC3, Cxa, Cxb,
                  Cxc, Cxd, Cxe, Cxf, Cxc_act):
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)

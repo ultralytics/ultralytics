a83ecdb386d4063e4126144c749afc00d926476b
Yonghye Kwon <developer.0hye@gmail.com>
2023-05-14 22:39:37 +0900

    zero-filled incremental

--- ultralytics/yolo/utils/files.py
+++ ultralytics/yolo/utils/files.py
@@ -49,7 +49,7 @@ def increment_path(path, exist_ok=False, sep='', mkdir=False):
 
         # Method 1
         for n in range(2, 9999):
-            p = f'{path}{sep}{n}{suffix}'  # increment path
+            p = f'{path}{sep}{n:04d}{suffix}'  # increment path
             if not os.path.exists(p):  #
                 break
         path = Path(p)

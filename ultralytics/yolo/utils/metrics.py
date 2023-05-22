0185dcf728d592565903973508e5d221407834a7
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-05-22 20:27:28 +0200

    fix prod_

--- ultralytics/yolo/utils/metrics.py
+++ ultralytics/yolo/utils/metrics.py
@@ -67,10 +67,10 @@ def box_iou(box1, box2, eps=1e-7):
 
     # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
     (a1, a2), (b1, b2) = box1.unsqueeze(1).chunk(2, 2), box2.unsqueeze(0).chunk(2, 2)
-    inter = (torch.min(a2, b2) - torch.max(a1, b1)).clamp_(0).prod_(2)
+    inter = (torch.min(a2, b2) - torch.max(a1, b1)).clamp_(0).prod(2)
 
     # IoU = inter / (area1 + area2 - inter)
-    return inter / ((a2 - a1).prod_(2) + (b2 - b1).prod_(2) - inter + eps)
+    return inter / ((a2 - a1).prod(2) + (b2 - b1).prod(2) - inter + eps)
 
 
 def bbox_iou(box1, box2, xywh=True, GIoU=False, DIoU=False, CIoU=False, eps=1e-7):

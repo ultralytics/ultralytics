a3a028b88df2720026bd225833fb372d9a1e8792
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-05-24 16:51:47 +0200

    fix precommit

--- ultralytics/yolo/utils/torch_utils.py
+++ ultralytics/yolo/utils/torch_utils.py
@@ -176,7 +176,7 @@ def model_info(model, detailed=False, verbose=True, imgsz=640):
     flops = get_flops(model, imgsz)
     fused = ' (fused)' if getattr(model, 'is_fused', lambda: False)() else ''
     fs = f', {flops:.1f} GFLOPs' if flops else ''
-    name = Path(getattr(model, 'yaml_file', '') or \
+    name = Path(getattr(model, 'yaml_file', '') or
                 getattr(model, 'yaml', {}).get('yaml_file', '')).stem.replace('yolo', 'YOLO') or 'Model'
     LOGGER.info(f'{name} summary{fused}: {n_l} layers, {n_p} parameters, {n_g} gradients{fs}')
     return n_l, n_p, n_g, flops

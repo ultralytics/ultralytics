840f145267cafe7980e9dae39b15bb10f66108ba
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-05-25 14:56:57 +0200

    Add visualization code

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -43,6 +43,34 @@ class Conv(nn.Module):
         return self.act(self.conv(x))
 
 
+class Conv2(nn.Module):
+    """Simplified RepConv module with Conv fusing."""
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=3, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, 3, s, autopad(3, p, d), groups=g, dilation=d, bias=False)
+        self.cv2 = nn.Conv2d(c1, c2, 1, s, autopad(1, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x) + self.cv2(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+
+    def fuse_convs(self):
+        """Fuse parallel convolutions."""
+        w = torch.zeros_like(self.conv.weight.data)
+        w[:, :, 1, 1] = self.cv2.weight.data.clone()
+        self.conv.weight.data += w
+        self.__delattr__('cv2')
+
+
 class LightConv(nn.Module):
     """Light convolution with args(ch_in, ch_out, kernel).
     https://github.com/PaddlePaddle/PaddleDetection/blob/develop/ppdet/modeling/backbones/hgnet_v2.py

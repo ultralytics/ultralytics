a42f90ca1424a87ee6c9f906ee62c0ce3e788ddc
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-05-28 03:49:28 +0200

    limit ONNX threads

--- ultralytics/yolo/utils/benchmarks.py
+++ ultralytics/yolo/utils/benchmarks.py
@@ -258,6 +258,7 @@ class ProfileModels:
         # Session with either 'TensorrtExecutionProvider', 'CUDAExecutionProvider', 'CPUExecutionProvider'
         sess_options = ort.SessionOptions()
         sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
+        sess_options.intra_op_num_threads = 8  # Limit the number of threads
         sess = ort.InferenceSession(onnx_file, sess_options, providers=['CPUExecutionProvider'])
 
         input_tensor = sess.get_inputs()[0]

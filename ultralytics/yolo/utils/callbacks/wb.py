4122fe34d6e933e2f065e61a4a309f946701f247
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-05-28 03:19:16 +0200

    fix wb

--- ultralytics/yolo/utils/callbacks/wb.py
+++ ultralytics/yolo/utils/callbacks/wb.py
@@ -14,7 +14,8 @@ except (ImportError, AssertionError):
 
 def on_pretrain_routine_start(trainer):
     """Initiate and start project if module is present."""
-    wb.run or wb.init(project=trainer.args.project or 'YOLOv8', name=trainer.args.name, config=vars(trainer.args))
+    wb.init(project=trainer.args.project or 'YOLOv8', name=trainer.args.name,
+            config=vars(trainer.args)) if not wb.run else wb.run
 
 
 def on_fit_epoch_end(trainer):

a65dd76a65bb3d5cc29c8d6c8b2f54442bdf49d8
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2023-05-29 15:17:50 +0530

    improve import structure

---
+++ ultralytics/nn/__init__.py
@@ -0,0 +1,3 @@
+from .tasks import attempt_load_one_weight, attempt_load_weights, parse_model, yaml_model_load, guess_model_task, guess_model_scale, torch_safe_load, DetectionModel, SegmentationModel, ClassificationModel, BaseModel
+
+__all__ = ['attempt_load_one_weight', 'attempt_load_weights', 'parse_model', 'yaml_model_load', 'guess_model_task', 'guess_model_scale', 'torch_safe_load', 'DetectionModel', 'SegmentationModel', 'ClassificationModel', 'BaseModel']

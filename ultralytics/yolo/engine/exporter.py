f3b6624c003c01bfeb92aa119b2bb22fb9ebe8ea
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-05-30 19:58:05 +0200

    Cleanup and omit classify models from letterbox resize_type

--- ultralytics/yolo/engine/exporter.py
+++ ultralytics/yolo/engine/exporter.py
@@ -362,16 +362,17 @@ class Exporter:
                                     model_name=self.pretty_name,
                                     framework='onnx',
                                     compress_to_fp16=self.args.half)  # export
+
+        # Set RT info
         ov_model.set_rt_info('YOLOv8', ['model_info', 'model_type'])
-        ov_model.set_rt_info('fit_to_window_letterbox', ['model_info', 'resize_type'])
         ov_model.set_rt_info(True, ['model_info', 'reverse_input_channels'])
         ov_model.set_rt_info(114, ['model_info', 'pad_value'])
         ov_model.set_rt_info([255.0], ['model_info', 'scale_values'])
         ov_model.set_rt_info(self.args.iou, ['model_info', 'iou_threshold'])
-        labels = []
-        for i in range(len(self.model.names)):
-            labels.append(self.model.names[i].replace(' ', '_'))
-        ov_model.set_rt_info(labels, ['model_info', 'labels'])
+        ov_model.set_rt_info([x.replace(' ', '_') for x in self.model.names.values()], ['model_info', 'labels'])
+        if self.model.task != 'classify':
+            ov_model.set_rt_info('fit_to_window_letterbox', ['model_info', 'resize_type'])
+
         ov.serialize(ov_model, f_ov)  # save
         yaml_save(Path(f) / 'metadata.yaml', self.metadata)  # add metadata.yaml
         return f, None

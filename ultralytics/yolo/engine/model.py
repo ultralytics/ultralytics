41c48bcf2acc0130d146ec3490191daed720d70d
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-06-06 16:26:27 +0200

    Fix code scanning alert - Incomplete URL substring sanitization
    Resolves https://github.com/ultralytics/ultralytics/issues/3053

--- ultralytics/yolo/engine/model.py
+++ ultralytics/yolo/engine/model.py
@@ -119,7 +119,7 @@ class YOLO:
     def is_hub_model(model):
         """Check if the provided model is a HUB model."""
         return any((
-            model.startswith('https://hub.ultra'),  # i.e. https://hub.ultralytics.com/models/MODEL_ID
+            model.startswith('https://hub.ultralytics.com/models/'),  # i.e. https://hub.ultralytics.com/models/MODEL_ID
             [len(x) for x in model.split('_')] == [42, 20],  # APIKEY_MODELID
             len(model) == 20 and not Path(model).exists() and all(x not in model for x in './\\')))  # MODELID
 

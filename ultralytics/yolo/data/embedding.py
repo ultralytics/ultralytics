a55b698732586173c192d79a28671088ab8261a1
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2023-06-08 18:25:00 +0530

    add dim reduction

--- ultralytics/yolo/data/embedding.py
+++ ultralytics/yolo/data/embedding.py
@@ -1,14 +1,25 @@
 import lancedb
-import torch
+import pyarrow as pa
+import numpy as np
+import torch.nn.functional as F
+from sklearn.decomposition import PCA
 
 from ultralytics import YOLO
 from ultralytics.yolo.data.utils import check_cls_dataset, check_det_dataset
-from ultralytics.yolo.utils import LOGGER, ops
+from ultralytics.yolo.utils.checks import check_requirements
+from ultralytics.yolo.utils import LOGGER
+from ultralytics.yolo.utils import ops
+
 from ultralytics.yolo.utils.torch_utils import smart_inference_mode
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 
 
 class EmbeddingsPredictor(DetectionPredictor):
+    def postprocess(self, preds, img, orig_imgs):
+        embeddings = preds[1]
+        embeddings = F.adaptive_avg_pool2d(embeddings, 2).flatten(1)
+
+        return embeddings
 
     @smart_inference_mode()
     def stream_inference(self, source=None, model=None):
@@ -39,9 +50,10 @@ class EmbeddingsPredictor(DetectionPredictor):
             with profilers[1]:
                 preds = self.model(im, augment=self.args.augment, embed_from=-1)
 
-            import pdb
-            pdb.set_trace()
-            yield from preds
+            with profilers[2]:
+                embeddings = self.postprocess(preds, im, im0s)
+
+            yield embeddings, path
 
 
 def get_train_split(data='coco128.yaml', task='detect', batch=16):
@@ -57,9 +69,32 @@ def get_train_split(data='coco128.yaml', task='detect', batch=16):
 def build_table(data='coco128.yaml', model='yolov8n.pt'):
     model = YOLO(model)
     predictor = EmbeddingsPredictor()
-    preds = predictor(get_train_split(data, task=model.task), model=model.model, stream=True)
-    for pred in preds:
-        print(pred)
+    predictor.imgsz = [640, 640] # hardcode imgsz
+    cols = [[], []] # path, embeddings
+
+    embeddings = predictor(get_train_split(data, task=model.task), model=model.model, stream=True)
+    for embedding in embeddings:
+        print(embedding[0])
+
+        cols[0].append(embedding[0].squeeze().cpu())
+        cols[1].append(embedding[1])
+    
+    db = lancedb.connect(f"db/")
+    table = pa.table(cols, names=["vector", "path"])
+    table = db.create_table(name=data.split('.')[0], data=table, mode="overwrite")
+
+    return table
+
+def project_embeddings(table, n_components=2):
+    pca = PCA(n_components=n_components)
+    embeddings = np.array(table.to_arrow()["vector"].to_pylist())
+    embeddings_reduced = pca.fit_transform(embeddings)
+    import pdb; pdb.set_trace()
+    # TODO: plot embeddings_reduced
 
 
-build_table()
+#build_table()
+db = lancedb.connect("db/")
+table = db.open_table("coco128")
+project_embeddings(table)
+    

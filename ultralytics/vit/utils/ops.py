213cae7a6f46309f3908048de301c9370446de42
Laughing-q <1185102784@qq.com>
2023-06-08 16:39:17 +0800

    update get_cdn_group

--- ultralytics/vit/utils/ops.py
+++ ultralytics/vit/utils/ops.py
@@ -218,6 +218,99 @@ def get_cdn_group(targets,
         class_embed.device), dn_meta
 
 
+def get_cdn_group_(targets,
+                  num_classes,
+                  num_queries,
+                  class_embed,
+                  num_dn=100,
+                  cls_noise_ratio=0.5,
+                  box_noise_scale=1.0,
+                  training=False):
+    """get contrastive denoising training group"""
+    if (not training) or num_dn <= 0:
+        return None, None, None, None
+    num_gts = [len(t) for t in targets['cls']]
+    total_num = sum(num_gts)
+    max_nums = max(num_gts)
+    if max_nums == 0:
+        return None, None, None, None
+
+    num_group = num_dn // max_nums
+    num_group = 1 if num_group == 0 else num_group
+    # pad gt to max_num of a batch
+    bs = len(targets['cls'])
+    gt_cls = torch.cat(targets['cls']).squeeze(-1)  # (bs*num, )
+    gt_bbox = torch.cat(targets['bboxes']).squeeze(-1)  # bs*num, 4
+    b_idx = torch.cat([torch.full_like(t.long(), i) for i, t in enumerate(targets['cls'])])
+
+    # each group has positive and negative queries.
+    dn_cls = gt_cls.repeat(2 * num_group)  # (2*num_group*bs*num, )
+    dn_bbox = gt_bbox.repeat(2 * num_group, 1)  # 2*num_group*bs*num, 4
+    dn_b_idx = b_idx.repeat(2 * num_group, 1).view(-1) # (2*num_group*bs*num, )
+
+    num_dn = int(max_nums * 2 * num_group)
+    # positive and negative mask
+    # (bs*num*num_group, ), the first part as positive sample
+    pos_idx = torch.tensor(range(total_num), dtype=torch.long, device=gt_bbox.device).repeat(num_group)
+    
+    # the second part as positive sample
+    neg_idx = pos_idx + num_group * total_num
+    # total denoising queries
+
+    if cls_noise_ratio > 0:
+        # half of bbox prob
+        mask = torch.rand(dn_cls.shape) < (cls_noise_ratio * 0.5)
+        idx = torch.nonzero(mask).squeeze(-1)
+        # randomly put a new one here
+        new_label = torch.randint_like(idx, 0, num_classes, dtype=dn_cls.dtype, device=dn_cls.device)
+        dn_cls[idx] = new_label
+
+    if box_noise_scale > 0:
+        known_bbox = xywh2xyxy(dn_bbox)
+
+        diff = (dn_bbox[..., 2:] * 0.5).repeat(1, 2) * box_noise_scale  # 2*num_group*bs*num, 4
+
+        rand_sign = torch.randint_like(dn_bbox, 0, 2) * 2.0 - 1.0
+        rand_part = torch.rand_like(dn_bbox)
+        rand_part[neg_idx] += 1.0
+        rand_part *= rand_sign
+        known_bbox += rand_part * diff
+        known_bbox.clip_(min=0.0, max=1.0)
+        dn_bbox = xyxy2xywh(known_bbox)
+        dn_bbox = inverse_sigmoid(dn_bbox)
+
+    # class_embed = torch.cat([class_embed, torch.zeros([1, class_embed.shape[-1]], device=class_embed.device)])
+    dn_cls_embed = class_embed[dn_cls]  # bs*num * 2 * num_group, 256
+    padding_cls = torch.zeros(bs, num_dn, dn_cls_embed.shape[-1], device=gt_cls.device)
+    padding_bbox = torch.zeros(bs, num_dn, 4, device=gt_bbox.device)
+    map_indices = torch.tensor([], device=gt_bbox.device)
+    if total_num:
+        map_indices = torch.cat([torch.tensor(range(num)) for num in num_gts])
+        map_indices = torch.cat([map_indices + max_nums * i for i in range(2 * num_group)]).long()
+    if len(dn_b_idx):
+        padding_cls[(dn_b_idx, map_indices)] = dn_cls_embed
+        padding_bbox[(dn_b_idx, map_indices)] = dn_bbox
+
+    tgt_size = num_dn + num_queries
+    attn_mask = torch.zeros([tgt_size, tgt_size], dtype=torch.bool)
+    # match query cannot see the reconstruct
+    attn_mask[num_dn:, :num_dn] = True
+    # reconstruct cannot see each other
+    for i in range(num_group):
+        if i == 0:
+            attn_mask[max_nums * 2 * i:max_nums * 2 * (i + 1), max_nums * 2 * (i + 1):num_dn] = True
+        if i == num_group - 1:
+            attn_mask[max_nums * 2 * i:max_nums * 2 * (i + 1), :max_nums * i * 2] = True
+        else:
+            attn_mask[max_nums * 2 * i:max_nums * 2 * (i + 1), max_nums * 2 * (i + 1):num_dn] = True
+            attn_mask[max_nums * 2 * i:max_nums * 2 * (i + 1), :max_nums * 2 * i] = True
+    dn_meta = {'dn_pos_idx': pos_idx.cpu().split([n * num_group for n in num_gts]),
+               'dn_num_group': num_group, 'dn_num_split': [num_dn, num_queries]}
+
+    return padding_cls.to(class_embed.device), padding_bbox.to(class_embed.device), attn_mask.to(
+        class_embed.device), dn_meta
+
+
 def inverse_sigmoid(x, eps=1e-6):
     x = x.clip(min=0., max=1.)
     return torch.log(x / (1 - x + eps) + eps)

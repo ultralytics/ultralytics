d3677f1f5bb32930c1703668dc3289dff06292a0
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-06-11 17:48:58 +0200

    Add `device=cuda` support
    Also supports `device=CUDA`.

--- ultralytics/yolo/utils/torch_utils.py
+++ ultralytics/yolo/utils/torch_utils.py
@@ -64,6 +64,8 @@ def select_device(device='', batch=0, newline=False, verbose=True):
     if cpu or mps:
         os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # force torch.cuda.is_available() = False
     elif device:  # non-cpu device requested
+        if device == 'cuda':
+            device = '0'
         visible = os.environ.get('CUDA_VISIBLE_DEVICES', None)
         os.environ['CUDA_VISIBLE_DEVICES'] = device  # set environment variable - must be before assert is_available()
         if not (torch.cuda.is_available() and torch.cuda.device_count() >= len(device.replace(',', ''))):

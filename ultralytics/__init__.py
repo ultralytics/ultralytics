949cdfa59be2afb4f623a6ec5e90fc3e19da65f7
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2023-06-18 02:05:19 +0530

    update

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -5,9 +5,9 @@ __version__ = '8.0.117'
 from ultralytics.hub import start
 from ultralytics.vit.rtdetr import RTDETR
 from ultralytics.vit.sam import SAM
-from ultralytics.yolo.data.embedding import Explorer
 from ultralytics.yolo.engine.model import YOLO
 from ultralytics.yolo.nas import NAS
+from ultralytics.yolo.data.embedding import Explorer # noqa
 from ultralytics.yolo.utils.checks import check_yolo as checks
 
 __all__ = '__version__', 'YOLO', 'NAS', 'SAM', 'RTDETR', 'checks', 'start', 'Explorer'  # allow simpler import

d3ca3e4b9bd692a880f8ec6510d280d602fa10c6
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-06-17 19:35:19 +0200

    Fix Pose val plotting bug (#3232)

--- ultralytics/yolo/v8/pose/val.py
+++ ultralytics/yolo/v8/pose/val.py
@@ -154,7 +154,7 @@ class PoseValidator(DetectionValidator):
 
     def plot_predictions(self, batch, preds, ni):
         """Plots predictions for YOLO model."""
-        pred_kpts = torch.cat([p[:, 6:].view(-1, *self.kpt_shape)[:15] for p in preds], 0)
+        pred_kpts = torch.cat([p[:, 6:].view(-1, *self.kpt_shape) for p in preds], 0)
         plot_images(batch['img'],
                     *output_to_target(preds, max_det=self.args.max_det),
                     kpts=pred_kpts,

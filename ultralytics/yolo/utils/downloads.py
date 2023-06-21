9d495cf3b34d5a6c665baeb243ca902cb808b469
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-06-22 00:58:37 +0200

    `ultralytics 8.0.121` RTDETR fix and Docs updates (#3314)

--- ultralytics/yolo/utils/downloads.py
+++ ultralytics/yolo/utils/downloads.py
@@ -18,6 +18,7 @@ from ultralytics.yolo.utils import LOGGER, checks, clean_url, emojis, is_online,
 GITHUB_ASSET_NAMES = [f'yolov8{k}{suffix}.pt' for k in 'nsmlx' for suffix in ('', '6', '-cls', '-seg', '-pose')] + \
                      [f'yolov5{k}u.pt' for k in 'nsmlx'] + \
                      [f'yolov3{k}u.pt' for k in ('', '-spp', '-tiny')] + \
+                     [f'yolo_nas_{k}.pt' for k in 'sml'] + \
                      [f'sam_{k}.pt' for k in 'bl'] + \
                      [f'rtdetr-{k}.pt' for k in 'lx']
 GITHUB_ASSET_STEMS = [Path(k).stem for k in GITHUB_ASSET_NAMES]

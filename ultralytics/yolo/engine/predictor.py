70d979cde502bff8a8834c11bba81823acfa3cca
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-06-25 22:02:13 +0200

    OS-specific cv2.VideoWriter codec definition

--- ultralytics/yolo/engine/predictor.py
+++ ultralytics/yolo/engine/predictor.py
@@ -105,7 +105,7 @@ class BasePredictor:
         self.results = None
         self.transforms = None
         self.callbacks = _callbacks or callbacks.get_default_callbacks()
-        self.fourcc = list('avc1' if MACOS else 'wmv2' if WINDOWS else 'mjpg')
+        self.fourcc = 'avc1' if MACOS else 'wmv2' if WINDOWS else 'mjpg'
         callbacks.add_integration_callbacks(self)
 
     def get_save_dir(self):

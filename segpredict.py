c722817afa64d0c4f9cc47aa866a2aedfff1a6c1
Wenchao Ding <40165666+berry-ding@users.noreply.github.com>
2023-06-26 17:24:29 +0800

    Update segpredict.py

--- segpredict.py
+++ segpredict.py
@@ -13,17 +13,19 @@ everything_results = model(
     iou=0.9,
 )
 prompt_process = FastSAMPrompt(IMAGE_PATH, everything_results, device=DEVICE)
-#everything prompt
+
+# everything prompt
 ann = prompt_process.everything_prompt()
-#bbox default shape [0,0,0,0] -> [x1,y1,x2,y2]
+
+# bbox default shape [0,0,0,0] -> [x1,y1,x2,y2]
 ann = prompt_process.box_prompt(bbox=[200, 200, 300, 300])
-#text prompt
+
+# text prompt
 ann = prompt_process.text_prompt(text='a photo of a dog')
-#
-#point prompt
-#points default [[0,0]] [[x1,y1],[x2,y2]]
-#point_label default [0] [1,0] 0:background, 1:foreground
-#
+
+# point prompt
+# points default [[0,0]] [[x1,y1],[x2,y2]]
+# point_label default [0] [1,0] 0:background, 1:foreground
 ann = prompt_process.point_prompt(points=[[200, 200]], pointlabel=[1])
 prompt_process.plot(
     annotations=ann,

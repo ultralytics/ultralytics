550494c9ea1e74868c28ebd5a1ae9bf9be5b5c8a
mario-dg <M.daGraca@synentec.com>
2023-06-27 14:04:03 +0200

    Updated to version 8.0.123

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
                  'pose estimation and image classification.'),
     long_description=README,
     long_description_content_type='text/markdown',
-    download_url='https://github.com/synentec/syml_ultralytics/archive/refs/tags/v_8.0.120.tar.gz',
+    download_url='https://github.com/synentec/syml_ultralytics/archive/refs/tags/v_8.0.123.tar.gz',
     url='https://github.com/synentec/syml_ultralytics',
     author='syml_ultralytics',
     packages=find_packages(),  # required

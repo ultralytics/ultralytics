0a5c236c82abf6218cc11a8bcbe9bc3f85581431
Wenchao Ding <40165666+berry-ding@users.noreply.github.com>
2023-06-30 10:13:20 +0800

    Update predict.py

--- ultralytics/yolo/fastsam/predict.py
+++ ultralytics/yolo/fastsam/predict.py
@@ -1,7 +1,7 @@
 import torch
 
 from ultralytics.yolo.engine.results import Results
-from ultralytics.yolo.fastsam.utils import *
+from ultralytics.yolo.fastsam.utils import adjust_bboxes_to_image_border, bbox_iou
 from ultralytics.yolo.utils import DEFAULT_CFG, ops
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 

400f3f72a1437b6c64e644337a33d5bf7076dfd5
Wenchao Ding <40165666+berry-ding@users.noreply.github.com>
2023-07-06 06:16:22 +0800

    `ultralytics 8.0.127` add FastSAM model (#3390)
    Co-authored-by: dingwenchao <12962189468@163.com>
    Co-authored-by: 丁文超 <dingwenchao@dingwenchaodeMacBook-Pro.local>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: Ayush Chaurasia <ayush.chaurarsia@gmail.com>

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,13 +1,14 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = '8.0.126'
+__version__ = '8.0.127'
 
 from ultralytics.hub import start
 from ultralytics.vit.rtdetr import RTDETR
 from ultralytics.vit.sam import SAM
 from ultralytics.yolo.engine.model import YOLO
+from ultralytics.yolo.fastsam import FastSAM
 from ultralytics.yolo.nas import NAS
 from ultralytics.yolo.utils.checks import check_yolo as checks
 from ultralytics.yolo.utils.downloads import download
 
-__all__ = '__version__', 'YOLO', 'NAS', 'SAM', 'RTDETR', 'checks', 'start', 'download'  # allow simpler import
+__all__ = '__version__', 'YOLO', 'NAS', 'SAM', 'RTDETR', 'checks', 'start', 'download', 'FastSAM'  # allow simpler import

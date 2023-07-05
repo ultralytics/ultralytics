34888f74d1a9dcf427127828497fa334b7cdc2d0
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-07-06 00:33:27 +0200

    FastSAM cleanup

--- ultralytics/yolo/fastsam/model.py
+++ ultralytics/yolo/fastsam/model.py
@@ -14,9 +14,8 @@ from ultralytics.yolo.engine.exporter import Exporter
 from ultralytics.yolo.engine.model import YOLO
 from ultralytics.yolo.utils import DEFAULT_CFG, LOGGER, ROOT, is_git_dir
 from ultralytics.yolo.utils.checks import check_imgsz
-
-from ...yolo.utils.torch_utils import model_info, smart_inference_mode
 from .predict import FastSAMPredictor
+from ...yolo.utils.torch_utils import model_info, smart_inference_mode
 
 
 class FastSAM(YOLO):

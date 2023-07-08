72160b10b901709431dd6d9e203008135e8c749a
Laughing-q <1185102784@qq.com>
2023-07-08 22:58:11 +0800

    update

--- ultralytics/vit/sam/model.py
+++ ultralytics/vit/sam/model.py
@@ -4,6 +4,7 @@ SAM model interface
 """
 
 from ultralytics.yolo.cfg import get_cfg
+from .modules.prompt_predictor import PromptPredictor
 
 from ...yolo.utils.torch_utils import model_info
 from .build import build_sam
@@ -19,6 +20,7 @@ class SAM:
         self.model = build_sam(model)
         self.task = 'segment'  # required
         self.predictor = None  # reuse predictor
+        self.prompt_predictor = None  # predictor for prompts
 
     def predict(self, source, stream=False, **kwargs):
         """Predicts and returns segmentation masks for given image or video source."""
@@ -30,6 +32,30 @@ class SAM:
         else:  # only update args if predictor is already setup
             self.predictor.args = get_cfg(self.predictor.args, overrides)
         return self.predictor(source, stream=stream)
+
+    def predict_box(self, source, input_bbox):
+        """Predicts and returns segmentation masks for given image and box."""
+        # NOTE: currently only supports np.ndarray for `source` and list for `input_bbox`.
+        import cv2
+        import numpy as np
+        image = cv2.imread(source)
+        source = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        input_bbox = np.array(input_bbox)
+        if not self.prompt_predictor:
+            self.prompt_predictor = PromptPredictor(self.model)
+        self.prompt_predictor.set_image(source)
+        masks, _, _ = self.prompt_predictor.predict(
+            point_coords=None,
+            point_labels=None,
+            box=input_bbox[None, :],
+            multimask_output=False,
+        )
+        # TODO: use Results class to handle masks
+        return masks
+
+    # TODO: predict_point
+    def predict_point(self, source, point):
+        pass
 
     def train(self, **kwargs):
         """Function trains models but raises an error as SAM models do not support training."""

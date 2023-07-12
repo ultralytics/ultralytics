7b85b17720ff40062aa7dbe02a28ee63923dcab9
Laughing-q <1185102784@qq.com>
2023-07-12 16:16:15 +0800

    fix import

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -3,7 +3,6 @@
 __version__ = '8.0.132'
 
 from ultralytics.hub import start
-from ultralytics.vit.mobilesam import MobileSAM
 from ultralytics.vit.rtdetr import RTDETR
 from ultralytics.vit.sam import SAM
 from ultralytics.yolo.engine.model import YOLO
@@ -12,4 +11,4 @@ from ultralytics.yolo.nas import NAS
 from ultralytics.yolo.utils.checks import check_yolo as checks
 from ultralytics.yolo.utils.downloads import download
 
-__all__ = '__version__', 'YOLO', 'NAS', 'SAM', 'RTDETR', 'checks', 'start', 'download', 'FastSAM', 'start', 'MobileSAM'  # allow simpler import
+__all__ = '__version__', 'YOLO', 'NAS', 'SAM', 'RTDETR', 'checks', 'start', 'download', 'FastSAM', 'start'  # allow simpler import
--- ultralytics/vit/__init__.py
+++ ultralytics/vit/__init__.py
@@ -1,7 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-from .mobilesam import MobileSAM
 from .rtdetr import RTDETR
 from .sam import SAM
 
-__all__ = 'RTDETR', 'SAM', 'MobileSAM'  # allow simpler import
+__all__ = 'RTDETR', 'SAM'  # allow simpler import

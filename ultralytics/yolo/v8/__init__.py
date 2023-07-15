1c99cfd7df49de24c1a1f03575ebc5a6f962acf1
Laughing-q <1185102784@qq.com>
2023-07-16 00:20:57 +0800

    add v8 backward compatibility

---
+++ ultralytics/yolo/v8/__init__.py
@@ -0,0 +1,11 @@
+import importlib
+import sys
+
+from ultralytics.utils import LOGGER
+
+# Set modules in sys.modules under their old name
+sys.modules['ultralytics.yolo.v8'] = importlib.import_module('ultralytics.models.yolo')
+
+LOGGER.warning(
+    "WARNING ⚠️ 'ultralytics.yolo.v8' is deprecated since '8.0.136' and will be removed. Please use 'ultralytics.models.yolo' instead."
+)

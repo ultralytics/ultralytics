a9be8edb9ea5071a268541d627817fc530ea91d1
Laughing-q <1185102784@qq.com>
2023-07-15 14:12:02 +0800

    yolo.cfg --> cfg

--- setup.py
+++ setup.py
@@ -71,4 +71,4 @@ setup(
         'Operating System :: Microsoft :: Windows', ],
     keywords='machine-learning, deep-learning, vision, ML, DL, AI, YOLO, YOLOv3, YOLOv5, YOLOv8, HUB, Ultralytics',
     entry_points={
-        'console_scripts': ['yolo = ultralytics.yolo.cfg:entrypoint', 'ultralytics = ultralytics.yolo.cfg:entrypoint']})
+        'console_scripts': ['yolo = ultralytics.cfg:entrypoint', 'ultralytics = ultralytics.cfg:entrypoint']})

b3bdb998750a9821971137a4dc83aa33136d7d47
Laughing-q <1185102784@qq.com>
2023-07-15 14:15:28 +0800

    yolo/cfg --> cfg

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -157,7 +157,7 @@ class IterableSimpleNamespace(SimpleNamespace):
             '{name}' object has no attribute '{attr}'. This may be caused by a modified or out of date ultralytics
             'default.yaml' file.\nPlease update your code with 'pip install -U ultralytics' and if necessary replace
             {DEFAULT_CFG_PATH} with the latest version from
-            https://github.com/ultralytics/ultralytics/blob/main/ultralytics/yolo/cfg/default.yaml
+            https://github.com/ultralytics/ultralytics/blob/main/ultralytics/cfg/default.yaml
             """)
 
     def get(self, key, default=None):

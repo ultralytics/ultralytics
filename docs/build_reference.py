3756894f3572e2d731845d6b2f11abebb154660d
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-07-16 16:56:50 +0200

    Update docs

--- docs/build_reference.py
+++ docs/build_reference.py
@@ -39,7 +39,7 @@ def create_markdown(py_filepath, module_path, classes, functions):
         with open(md_filepath, 'r') as file:
             existing_content = file.read()
             header_parts = existing_content.split('---', 2)
-            if 'description:' in header_parts and len(header_parts) >= 3:
+            if 'description:' in header_parts or 'comments:' in header_parts and len(header_parts) >= 3:
                 header_content = f"{header_parts[0]}---{header_parts[1]}---\n\n"
 
     module_path = module_path.replace('.__init__', '')

a11a27f0f8dc7df450465a7ce04e104c0f95b070
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-07-16 16:07:46 +0200

    Update download_weights.sh add -pose

--- ultralytics/data/scripts/download_weights.sh
+++ ultralytics/data/scripts/download_weights.sh
@@ -11,7 +11,7 @@
 python - <<EOF
 from ultralytics.utils.downloads import attempt_download_asset
 
-assets = [f'yolov8{size}{suffix}.pt' for size in 'nsmlx' for suffix in ('', '-cls', '-seg')]
+assets = [f'yolov8{size}{suffix}.pt' for size in 'nsmlx' for suffix in ('', '-cls', '-seg', '-pose')]
 for x in assets:
     attempt_download_asset(f'weights/{x}')
 

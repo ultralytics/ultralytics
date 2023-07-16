ad6d2006ff6fb09f3a20e906602849227c5eb7f7
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-07-16 17:36:12 +0200

    Revert temporary_modules back to 3

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -554,7 +554,10 @@ def torch_safe_load(weight):
     check_suffix(file=weight, suffix='.pt')
     file = attempt_download_asset(weight)  # search online if missing locally
     try:
-        with temporary_modules({'ultralytics.yolo.data': 'ultralytics.data'}):  # for legacy 8.0 Classify models
+        with temporary_modules({
+                'ultralytics.yolo.utils': 'ultralytics.utils',
+                'ultralytics.yolo.v8': 'ultralytics.models.yolo',
+                'ultralytics.yolo.data': 'ultralytics.data'}):  # for legacy 8.0 Classify and Pose models
             return torch.load(file, map_location='cpu'), file  # load
 
     except ModuleNotFoundError as e:  # e.name is missing module name

547c6390c58e51b9c4e8c7cb318ac79074e02fb3
Birk Stoveland <spillesen@gmail.com>
2023-07-21 13:31:05 +0200

    Update tasks.py to allow custom RTEDTRDecoder arguments
    I attempted to add custom arguments to create a smaller version of RTDETRDecoder in a yaml file, and got thwarted by the parser appending the channels to the end of the arguments list while the RTDETRDecoder has the channels as the second argument as seen in the definition 
    ```
    class RTDETRDecoder(nn.Module):
        export = False  # export mode
    
        def __init__(
                self,
                nc=80,
                ch=(512, 1024, 2048),
                hd=256,  # hidden dim
                nq=300,  # num queries
                ndp=4,  # num decoder points
                nh=8,  # num head
                ndl=6,  # num decoder layers
                d_ffn=1024,  # dim of feedforward
                dropout=0.,
                act=nn.ReLU(),
                eval_idx=-1,
                # training args
                nd=100,  # num denoising
                label_noise_ratio=0.5,
                box_noise_scale=1.0,
                learnt_init_query=False):
    ```
    
    This is a quick, and hopefully quite harmless fix, at least it worked for me locally.

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -700,7 +700,10 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
         elif m is Concat:
             c2 = sum(ch[x] for x in f)
         elif m in (Detect, Segment, Pose, RTDETRDecoder):
-            args.append([ch[x] for x in f])
+            if m is RTDETRDecoder and len(args) > 1:
+              args = [args[0], [ch[x] for x in f], *args[1:]]
+            else:
+              args.append([ch[x] for x in f])
             if m is Segment:
                 args[2] = make_divisible(min(args[2], max_channels) * width, 8)
         else:

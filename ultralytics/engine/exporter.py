c2eba9eb6298e8703df1e630f880ee349b226f96
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-07-22 18:57:41 +0200

    Update exporter.py

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -50,7 +50,6 @@ TensorFlow.js:
 """
 import json
 import os
-import pathlib
 import shutil
 import subprocess
 import time
@@ -69,7 +68,7 @@ from ultralytics.utils import (ARM64, DEFAULT_CFG, LINUX, LOGGER, MACOS, ROOT, W
                                colorstr, get_default_args, yaml_save)
 from ultralytics.utils.checks import check_imgsz, check_requirements, check_version
 from ultralytics.utils.downloads import attempt_download_asset, get_github_assets
-from ultralytics.utils.files import file_size
+from ultralytics.utils.files import file_size, spaces_in_path
 from ultralytics.utils.ops import Profile
 from ultralytics.utils.torch_utils import get_latest_opset, select_device, smart_inference_mode
 
@@ -113,7 +112,7 @@ def try_export(inner_func):
         try:
             with Profile() as dt:
                 f, model = inner_func(*args, **kwargs)
-            LOGGER.info(f'{prefix} export success ✅ {dt.t:.1f}s, saved as {f} ({file_size(f):.1f} MB)')
+            LOGGER.info(f"{prefix} export success ✅ {dt.t:.1f}s, saved as '{f}' ({file_size(f):.1f} MB)")
             return f, model
         except Exception as e:
             LOGGER.info(f'{prefix} export failure ❌ {dt.t:.1f}s: {e}')
@@ -231,7 +230,7 @@ class Exporter:
         if model.task == 'pose':
             self.metadata['kpt_shape'] = model.model[-1].kpt_shape
 
-        LOGGER.info(f"\n{colorstr('PyTorch:')} starting from {file} with input shape {tuple(im.shape)} BCHW and "
+        LOGGER.info(f"\n{colorstr('PyTorch:')} starting from '{file}' with input shape {tuple(im.shape)} BCHW and "
                     f'output shape(s) {self.output_shape} ({file_size(file):.1f} MB)')
 
         # Exports
@@ -341,7 +340,7 @@ class Exporter:
                 import onnxsim
 
                 LOGGER.info(f'{prefix} simplifying with onnxsim {onnxsim.__version__}...')
-                # subprocess.run(f'onnxsim {f} {f}', shell=True)
+                # subprocess.run(f'onnxsim "{f}" "{f}"', shell=True)
                 model_onnx, check = onnxsim.simplify(model_onnx)
                 assert check, 'Simplified ONNX model could not be validated'
             except Exception as e:
@@ -411,7 +410,7 @@ class Exporter:
 
         LOGGER.info(f'\n{prefix} starting export with ncnn {ncnn.__version__}...')
         f = Path(str(self.file).replace(self.file.suffix, f'_ncnn_model{os.sep}'))
-        f_ts = str(self.file.with_suffix('.torchscript'))
+        f_ts = self.file.with_suffix('.torchscript')
 
         pnnx_filename = 'pnnx.exe' if WINDOWS else 'pnnx'
         if Path(pnnx_filename).is_file():
@@ -435,7 +434,7 @@ class Exporter:
 
         cmd = [
             str(pnnx),
-            f_ts,
+            str(f_ts),
             f'pnnxparam={f / "model.pnnx.param"}',
             f'pnnxbin={f / "model.pnnx.bin"}',
             f'pnnxpy={f / "model_pnnx.py"}',
@@ -587,8 +586,8 @@ class Exporter:
 
         # Export to TF
         int8 = '-oiqt -qt per-tensor' if self.args.int8 else ''
-        # onnx2tf can handle spaces in filenames, if they are properly escaped
         cmd = f'onnx2tf -i "{f_onnx}" -o "{f}" -nuo --non_verbose {int8}'
+        LOGGER.info(f"\n{prefix} running '{cmd}'")
         subprocess.run(cmd, shell=True)
         yaml_save(f / 'metadata.yaml', self.metadata)  # add metadata.yaml
 
@@ -662,7 +661,7 @@ class Exporter:
 
         cmd = f'edgetpu_compiler -s -d -k 10 --out_dir "{Path(f).parent}" "{tflite_model}"'
         LOGGER.info(f"{prefix} running '{cmd}'")
-        subprocess.run(cmd, check=True, shell=True)
+        subprocess.run(cmd, shell=True)
         self._add_tflite_metadata(f)
         return f, None
 
@@ -675,34 +674,21 @@ class Exporter:
 
         LOGGER.info(f'\n{prefix} starting export with tensorflowjs {tfjs.__version__}...')
         f = str(self.file).replace(self.file.suffix, '_web_model')  # js dir
-        f_pb = self.file.with_suffix('.pb')  # *.pb path
+        f_pb = str(self.file.with_suffix('.pb'))  # *.pb path
 
         gd = tf.Graph().as_graph_def()  # TF GraphDef
         with open(f_pb, 'rb') as file:
             gd.ParseFromString(file.read())
         outputs = ','.join(gd_outputs(gd))
         LOGGER.info(f'\n{prefix} output node names: {outputs}')
-        # the tensorflow js converter CAN NOT handle spaces in arguments as of 21.07.23,
-        # so we need to switch to the model directory, if the space is within the complete file path,
-        # if the space is within the actual model filename, we can not do anything about it
-        if ' ' in str(f_pb):
-            previous_working_directory = Path.cwd()
-            f_name = pathlib.Path(f).name
-            if ' ' in f_name:
-                LOGGER.warning(f'{prefix} WARNING ⚠️ TensorFlow.js export does not support spaces in filenames. '
-                               f'Please rename {f_name} to {str(f_name).replace(" ", "_")} and try again.')
 
-            cmd = f'tensorflowjs_converter --input_format=tf_frozen_model --output_node_names={outputs} {f_pb.name} {f_name}'
-            try:
-                LOGGER.info(f"{prefix} switching working dir to {f_pb.parent} running '{cmd}'")
-                os.chdir(f_pb.parent)
-                subprocess.run(cmd, shell=True, check=True)
-            finally:
-                os.chdir(previous_working_directory)
-        else:
-            cmd = f'tensorflowjs_converter --input_format=tf_frozen_model --output_node_names={outputs} "{f_pb}" "{f}"'
+        with spaces_in_path(f_pb) as fpb_, spaces_in_path(f) as f_:  # exporter can not handle spaces in path
+            cmd = f'tensorflowjs_converter --input_format=tf_frozen_model --output_node_names={outputs} "{fpb_}" "{f_}"'
             LOGGER.info(f"{prefix} running '{cmd}'")
-            subprocess.run(cmd, shell=True, check=True)
+            subprocess.run(cmd, shell=True)
+
+        if ' ' in str(f):
+            LOGGER.warning(f"WARNING ⚠️ your TF.js model may not work correctly with spaces in path '{f}'.")
 
         # f_json = Path(f) / 'model.json'  # *.json path
         # with open(f_json, 'w') as j:  # sort JSON Identity_* in ascending order

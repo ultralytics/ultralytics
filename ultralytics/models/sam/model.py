df84df2c307046f89b6df201dca5717d9a7768ac
Laughing-q <1185102784@qq.com>
2023-07-24 19:20:25 +0800

    update sam

--- ultralytics/models/sam/model.py
+++ ultralytics/models/sam/model.py
@@ -4,21 +4,24 @@ SAM model interface
 """
 
 from ultralytics.cfg import get_cfg
+from ultralytics.engine.model import Model
 from ultralytics.utils.torch_utils import model_info
 
 from .build import build_sam
 from .predict import Predictor
 
 
-class SAM:
+class SAM(Model):
 
     def __init__(self, model='sam_b.pt') -> None:
         if model and not model.endswith('.pt') and not model.endswith('.pth'):
             # Should raise AssertionError instead?
             raise NotImplementedError('Segment anything prediction requires pre-trained checkpoint')
+        super().__init__(model=model, task="segment")
         self.model = build_sam(model)
-        self.task = 'segment'  # required
-        self.predictor = None  # reuse predictor
+
+    def _load(self, weights: str, task=None):
+        self.model = build_sam(weights)
 
     def predict(self, source, stream=False, bboxes=None, points=None, labels=None, **kwargs):
         """Predicts and returns segmentation masks for given image or video source."""
@@ -42,11 +45,6 @@ class SAM:
     def __call__(self, source=None, stream=False, bboxes=None, points=None, labels=None, **kwargs):
         """Calls the 'predict' function with given arguments to perform object detection."""
         return self.predict(source, stream, bboxes, points, labels, **kwargs)
-
-    def __getattr__(self, attr):
-        """Raises error if object has no requested attribute."""
-        name = self.__class__.__name__
-        raise AttributeError(f"'{name}' object has no attribute '{attr}'. See valid attributes below.\n{self.__doc__}")
 
     def info(self, detailed=False, verbose=True):
         """

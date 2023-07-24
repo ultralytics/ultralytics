2ee147838a28d3743a7f0d7a3480ba4788589f0c
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-07-24 23:56:16 +0200

    Add tests before pushing to Docker Hub (#3924)

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -465,7 +465,7 @@ class Exporter:
     @try_export
     def export_coreml(self, prefix=colorstr('CoreML:')):
         """YOLOv8 CoreML export."""
-        check_requirements('coremltools>=6.0')
+        check_requirements('coremltools>=6.0,<=6.2')
         import coremltools as ct  # noqa
 
         LOGGER.info(f'\n{prefix} starting export with coremltools {ct.__version__}...')

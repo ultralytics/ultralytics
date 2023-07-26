00f4432d69b22cfb3c8f7c063235ca10ade5511c
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-07-26 16:54:27 +0200

    capitalize warning

--- ultralytics/engine/model.py
+++ ultralytics/engine/model.py
@@ -449,7 +449,7 @@ class Model:
             name = self.__class__.__name__
             mode = inspect.stack()[1][3]  # get the function name.
             raise NotImplementedError(
-                f'Warning ⚠️ `{name}` model does not support `{mode}` mode for `{self.task}` task yet.')
+                f'WARNING ⚠️ `{name}` model does not support `{mode}` mode for `{self.task}` task yet.')
 
     @property
     def task_map(self):

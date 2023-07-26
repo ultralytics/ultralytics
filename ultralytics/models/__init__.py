1a0eb3f0999f4bfb8aad986c635f60eda3fe545f
Laughing <61612323+Laughing-q@users.noreply.github.com>
2023-07-27 06:54:02 +0800

    `ultralytics 8.0.143` add `Model` base class (#3934)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/models/__init__.py
+++ ultralytics/models/__init__.py
@@ -1,4 +1,7 @@
+# Ultralytics YOLO 🚀, AGPL-3.0 license
+
 from .rtdetr import RTDETR
 from .sam import SAM
+from .yolo import YOLO
 
-__all__ = 'RTDETR', 'SAM'  # allow simpler import
+__all__ = 'YOLO', 'RTDETR', 'SAM'  # allow simpler import
---
+++ ultralytics/models/yolo/model.py
@@ -0,0 +1,36 @@
+# Ultralytics YOLO 🚀, AGPL-3.0 license
+
+from ultralytics.engine.model import Model
+from ultralytics.models import yolo  # noqa
+from ultralytics.nn.tasks import ClassificationModel, DetectionModel, PoseModel, SegmentationModel
+
+
+class YOLO(Model):
+    """
+    YOLO (You Only Look Once) object detection model.
+    """
+
+    @property
+    def task_map(self):
+        """Map head to model, trainer, validator, and predictor classes"""
+        return {
+            'classify': {
+                'model': ClassificationModel,
+                'trainer': yolo.classify.ClassificationTrainer,
+                'validator': yolo.classify.ClassificationValidator,
+                'predictor': yolo.classify.ClassificationPredictor, },
+            'detect': {
+                'model': DetectionModel,
+                'trainer': yolo.detect.DetectionTrainer,
+                'validator': yolo.detect.DetectionValidator,
+                'predictor': yolo.detect.DetectionPredictor, },
+            'segment': {
+                'model': SegmentationModel,
+                'trainer': yolo.segment.SegmentationTrainer,
+                'validator': yolo.segment.SegmentationValidator,
+                'predictor': yolo.segment.SegmentationPredictor, },
+            'pose': {
+                'model': PoseModel,
+                'trainer': yolo.pose.PoseTrainer,
+                'validator': yolo.pose.PoseValidator,
+                'predictor': yolo.pose.PosePredictor, }, }

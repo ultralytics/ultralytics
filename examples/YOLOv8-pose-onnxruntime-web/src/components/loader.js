a9c73b2a0cc2322a589efe00407bc0a4ca8cfb48
Fatemezamanian <Fatemehzamanian.1999@gmail.com>
2023-08-04 01:49:31 +0330

    add pose onnx web

---
+++ examples/YOLOv8-pose-onnxruntime-web/src/components/loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style/loader.css";
+
+const Loader = (props) => {
+  return (
+    <div className="wrapper" {...props}>
+      <div className="spinner"></div>
+      <p>{props.children}</p>
+    </div>
+  );
+};
+
+export default Loader;

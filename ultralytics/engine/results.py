49398a16acada8a842036f7f62b7be89ff42d6aa
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-08-07 11:47:35 +0200

    docs fixes

--- ultralytics/engine/results.py
+++ ultralytics/engine/results.py
@@ -209,7 +209,7 @@ class Results(SimpleClass):
             results = model('bus.jpg')  # results list
             for r in results:
                 im_array = r.plot()  # plot a BGR numpy array of predictions
-                im = Image.fromarray(im[..., ::-1])  # RGB PIL image
+                im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
                 im.show()  # show image
                 im.save('results.jpg')  # save image
             ```

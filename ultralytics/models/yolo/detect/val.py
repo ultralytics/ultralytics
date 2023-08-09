391c1020fa9dade952f52ddaea51e761c8bc98dd
Laughing-q <1185102784@qq.com>
2023-08-09 10:54:49 +0800

    update

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -26,7 +26,7 @@ class DetectionValidator(BaseValidator):
         self.metrics = DetMetrics(save_dir=self.save_dir, on_plot=self.on_plot)
         self.iouv = torch.linspace(0.5, 0.95, 10)  # iou vector for mAP@0.5:0.95
         self.niou = self.iouv.numel()
-        self.lb = []
+        self.lb = []   # for autolabelling
 
     def preprocess(self, batch):
         """Preprocesses batch of images for YOLO training."""

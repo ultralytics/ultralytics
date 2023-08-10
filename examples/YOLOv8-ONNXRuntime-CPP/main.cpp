43c775f75c5e63c06c3a4817d72c8888f716bb3e
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-08-10 18:51:19 +0300

    Improve YOLOv8 ONNX Runtime c++ example for all OS with `CmakeList.txt` support (#4274)
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- examples/YOLOv8-ONNXRuntime-CPP/main.cpp
+++ examples/YOLOv8-ONNXRuntime-CPP/main.cpp
@@ -1,44 +1,94 @@
 #include <iostream>
-#include <stdio.h>
 #include "inference.h"
 #include <filesystem>
-
-
+#include <fstream>
 
 void file_iterator(DCSP_CORE*& p)
 {
-	std::filesystem::path img_path = R"(E:\project\Project_C++\DCPS_ONNX\TEST_ORIGIN)";
-	int k = 0;
-	for (auto& i : std::filesystem::directory_iterator(img_path))
+	std::filesystem::path current_path = std::filesystem::current_path();
+	std::filesystem::path imgs_path = current_path/"images";
+	for (auto& i : std::filesystem::directory_iterator(imgs_path))
 	{
-		if (i.path().extension() == ".jpg")
+		if (i.path().extension() == ".jpg" || i.path().extension() == ".png")
 		{
 			std::string img_path = i.path().string();
-			//std::cout << img_path << std::endl;
 			cv::Mat img = cv::imread(img_path);
 			std::vector<DCSP_RESULT> res;
-			char* ret = p->RunSession(img, res);
-			for (int i = 0; i < res.size(); i++)
-			{
-				cv::rectangle(img, res.at(i).box, cv::Scalar(125, 123, 0), 3);
-			}
+			p->RunSession(img, res);
 
-			k++;
-			cv::imshow("TEST_ORIGIN", img);
+			for (auto & re : res)
+			{
+				cv::rectangle(img, re.box, cv::Scalar(0, 0 , 255), 3);
+                std::string label = p->classes[re.classId];
+                cv::putText(
+                        img,
+                        label,
+                        cv::Point(re.box.x, re.box.y - 5),
+                        cv::FONT_HERSHEY_SIMPLEX,
+                        0.75,
+                        cv::Scalar(255, 255, 0),
+                        2
+                );
+			}
+            cv::imshow("Result", img);
 			cv::waitKey(0);
 			cv::destroyAllWindows();
-			//cv::imwrite("E:\\output\\" + std::to_string(k) + ".png", img);
 		}
 	}
 }
 
+int read_coco_yaml(DCSP_CORE*& p)
+{
+    // Open the YAML file
+    std::ifstream file("coco.yaml");
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file" << std::endl;
+        return 1;
+    }
+
+    // Read the file line by line
+    std::string line;
+    std::vector<std::string> lines;
+    while (std::getline(file, line)) {
+        lines.push_back(line);
+    }
+
+    // Find the start and end of the names section
+    std::size_t start = 0;
+    std::size_t end = 0;
+    for (std::size_t i = 0; i < lines.size(); i++) {
+        if (lines[i].find("names:") != std::string::npos) {
+            start = i + 1;
+        } else if (start > 0 && lines[i].find(':') == std::string::npos) {
+            end = i;
+            break;
+        }
+    }
+
+    // Extract the names
+    std::vector<std::string> names;
+    for (std::size_t i = start; i < end; i++) {
+        std::stringstream ss(lines[i]);
+        std::string name;
+        std::getline(ss, name, ':'); // Extract the number before the delimiter
+        std::getline(ss, name); // Extract the string after the delimiter
+        names.push_back(name);
+    }
+
+    p->classes = names;
+    return 0;
+}
 
 
 int main()
 {
 	DCSP_CORE* p1 = new DCSP_CORE;
 	std::string model_path = "yolov8n.onnx";
-	DCSP_INIT_PARAM params{ model_path, YOLO_ORIGIN_V8, {640, 640}, 80, 0.1, 0.5, false };
-	char* ret = p1->CreateSession(params);
+    read_coco_yaml(p1);
+	// GPU inference
+	DCSP_INIT_PARAM params{ model_path, YOLO_ORIGIN_V8, {640, 640},  0.1, 0.5, true };
+	// CPU inference
+    // DCSP_INIT_PARAM params{ model_path, YOLO_ORIGIN_V8, {640, 640},  0.1, 0.5, false };
+    p1->CreateSession(params);
 	file_iterator(p1);
 }

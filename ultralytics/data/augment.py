c2629d85201f0c206aeea09bf8c0d9662ee568b3
Maia Numerosky <mnumerosky@eryx.co>
2023-08-17 18:10:45 -0300

    Corrects augmentation description

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -483,7 +483,7 @@ class RandomHSV:
         self.vgain = vgain
 
     def __call__(self, labels):
-        """Applies random horizontal or vertical flip to an image with a given probability."""
+        """Applies image HSV augmentation"""
         img = labels['img']
         if self.hgain or self.sgain or self.vgain:
             r = np.random.uniform(-1, 1, 3) * [self.hgain, self.sgain, self.vgain] + 1  # random gains
@@ -501,7 +501,7 @@ class RandomHSV:
 
 
 class RandomFlip:
-
+    """Applies random horizontal or vertical flip to an image with a given probability."""
     def __init__(self, p=0.5, direction='horizontal', flip_idx=None) -> None:
         assert direction in ['horizontal', 'vertical'], f'Support direction `horizontal` or `vertical`, got {direction}'
         assert 0 <= p <= 1.0

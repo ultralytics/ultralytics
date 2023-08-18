7777ece1e3f4cd1526308a174f3ed31767cacfa8
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-08-18 13:28:48 +0200

    Add tests

--- tests/test_python.py
+++ tests/test_python.py
@@ -304,12 +304,15 @@ def test_utils_init():
 
 
 def test_utils_checks():
-    from ultralytics.utils.checks import check_requirements, check_yolov5u_filename, git_describe
+    from ultralytics.utils.checks import (check_imgsz, check_requirements, check_yolov5u_filename, git_describe,
+                                          print_args)
 
     check_yolov5u_filename('yolov5n.pt')
     # check_imshow(warn=True)
     git_describe(ROOT)
     check_requirements()  # check requirements.txt
+    check_imgsz([600, 600], max_dim=1)
+    print_args()
 
 
 def test_utils_benchmarks():

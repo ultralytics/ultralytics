30f1fafda752f6360e5cccf7e1682ef0ff021b0a
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-08-18 14:57:21 +0200

    unused import fix

--- tests/test_python.py
+++ tests/test_python.py
@@ -304,8 +304,8 @@ def test_utils_init():
 
 
 def test_utils_checks():
-    from ultralytics.utils.checks import (check_imgsz, check_imshow, check_requirements, check_yolov5u_filename,
-                                          git_describe, print_args)
+    from ultralytics.utils.checks import (check_imgsz, check_requirements, check_yolov5u_filename, git_describe,
+                                          print_args)
 
     check_yolov5u_filename('yolov5n.pt')
     # check_imshow(warn=True)

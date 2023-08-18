73a307d3d39a952568817f9f35bc226bd4b0fbc9
captainfffsama <tuanzhangsama@outlook.com>
2023-08-18 19:41:13 +0800

    like mmdet_grpc,yolo support grpc now proto is same

---
+++ ultralytics/grpc_server/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+@Author: captainfffsama
+@Date: 2023-08-18 16:22:25
+@LastEditors: captainfffsama tuanzhangsama@outlook.com
+@LastEditTime: 2023-08-18 18:35:02
+@FilePath: /ultralytics/ultralytics/grpc/__init__.py
+@Description:
+'''
+from .grpc_main import run_grpc
+from .base_config import grpc_param
---
+++ ultralytics/grpc_server/proto/v1370/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+'''
+@Author: captainfffsama
+@Date: 2023-04-13 10:33:08
+@LastEditors: captainfffsama tuanzhangsama@outlook.com
+@LastEditTime: 2023-04-13 10:33:09
+@FilePath: /mmdet_grpc/mmdet_grpc/proto/v1370/__init__.py
+@Description:
+'''

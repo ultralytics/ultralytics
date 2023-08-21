2cd5540457845dc8f1ff7871385882d01401e354
franklin-degirum <109635789+franklin-degirum@users.noreply.github.com>
2023-08-21 15:26:56 -0700

    Update conv.py

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -32,7 +32,6 @@ class Conv(nn.Module):
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
-        
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""

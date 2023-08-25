d0a9233b88949469aee72ceedc4600d347298d8d
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-08-25 17:54:51 +0200

    Update training to just 1 epoch

--- tests/test_cuda.py
+++ tests/test_cuda.py
@@ -19,13 +19,13 @@ DATA = 'coco8.yaml'
 @pytest.mark.skipif(not CUDA_IS_AVAILABLE, reason='CUDA is not available')
 def test_train():
     model = YOLO(MODEL)
-    model(data=DATA, imgsz=32, batch=-1, device=0)  # also test AutoBatch
+    model(data=DATA, imgsz=32, epochs=1, batch=-1, device=0)  # also test AutoBatch
 
 
 @pytest.mark.skipif(CUDA_DEVICE_COUNT < 2, reason=f'DDP is not available, {CUDA_DEVICE_COUNT} device(s) found')
 def test_train_ddp():
     model = YOLO(MODEL)
-    model(data=DATA, imgsz=32, device=[0, 1])
+    model(data=DATA, imgsz=32, epochs=1, device=[0, 1])
 
 
 @pytest.mark.skipif(not CUDA_IS_AVAILABLE, reason='CUDA is not available')

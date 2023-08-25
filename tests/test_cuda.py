4707a7313ec7d59444ea1177777cb66940bc63a7
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-08-25 18:31:46 +0200

    Remove matrix torch from gpu-latest

--- tests/test_cuda.py
+++ tests/test_cuda.py
@@ -3,13 +3,15 @@
 from pathlib import Path
 
 import pytest
+import torch
 
 from ultralytics import YOLO
 from ultralytics.utils import SETTINGS
-from ultralytics.utils.checks import cuda_device_count, cuda_is_available
 
-CUDA_IS_AVAILABLE = cuda_is_available()
-CUDA_DEVICE_COUNT = cuda_device_count()
+# from ultralytics.utils.checks import cuda_device_count, cuda_is_available
+
+CUDA_IS_AVAILABLE = torch.cuda.is_available()
+CUDA_DEVICE_COUNT = torch.cuda.device_count()
 
 WEIGHTS_DIR = Path(SETTINGS['weights_dir'])
 MODEL = WEIGHTS_DIR / 'path with spaces' / 'yolov8n.pt'  # test spaces in path

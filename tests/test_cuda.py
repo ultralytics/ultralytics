94bc79016da96a086cb08be02cb259f52fcb062a
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-08-25 20:48:47 +0200

    Test predict sam

--- tests/test_cuda.py
+++ tests/test_cuda.py
@@ -6,7 +6,7 @@ import pytest
 import torch
 
 from ultralytics import YOLO
-from ultralytics.utils import SETTINGS
+from ultralytics.utils import ASSETS, SETTINGS
 
 CUDA_IS_AVAILABLE = torch.cuda.is_available()
 CUDA_DEVICE_COUNT = torch.cuda.device_count()
@@ -39,3 +39,23 @@ def test_utils_benchmarks():
 
     YOLO(MODEL).export(format='engine', imgsz=32, dynamic=True, batch=1)  # pre-export engine model, auto-device
     ProfileModels([MODEL], imgsz=32, half=False, min_time=1, num_timed_runs=3, num_warmup_runs=1).profile()
+
+
+@pytest.mark.skipif(not CUDA_IS_AVAILABLE, reason='CUDA is not available')
+def test_predict_sam():
+    from ultralytics import SAM
+
+    # Load a model
+    model = SAM(WEIGHTS_DIR / 'sam_b.pt')
+
+    # Display model information (optional)
+    model.info()
+
+    # Run inference
+    model(ASSETS / 'bus.jpg')
+
+    # Run inference with bboxes prompt
+    model(ASSETS / 'zidane.jpg', bboxes=[439, 437, 524, 709])
+
+    # Run inference with points prompt
+    model(ASSETS / 'zidane.jpg', points=[900, 370], labels=[1])

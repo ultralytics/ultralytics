e355294f452cb4ca1b2ae81ed2a6d633b26bc840
shashichilappagari <36974899+shashichilappagari@users.noreply.github.com>
2023-08-27 09:20:52 -0700

    testing if predict is the issue or validate also has the issue

--- ultralytics/utils/benchmarks.py
+++ ultralytics/utils/benchmarks.py
@@ -117,7 +117,7 @@ def benchmark(model=Path(SETTINGS['weights_dir']) / 'yolov8n.pt',
             assert model.task != 'pose' or i != 7, 'GraphDef Pose inference is not supported'
             assert i not in (9, 10), 'inference not supported'  # Edge TPU and TF.js are unsupported
             assert i != 5 or platform.system() == 'Darwin', 'inference only supported on macOS>=10.13'  # CoreML
-            export.predict(ASSETS / 'bus.jpg', imgsz=imgsz, device=device, half=half,separate_outputs=separate_outputs)
+            # export.predict(ASSETS / 'bus.jpg', imgsz=imgsz, device=device, half=half,separate_outputs=separate_outputs)
 
             # Validate
             data = data or TASK2DATA[model.task]  # task to dataset, i.e. coco8.yaml for task=detect

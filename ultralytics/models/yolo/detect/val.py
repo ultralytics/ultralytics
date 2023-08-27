0853455c8188cee69ac59f3329ae9d75a42c00fd
shashichilappagari <36974899+shashichilappagari@users.noreply.github.com>
2023-08-27 00:19:59 -0700

    commenting imgsz in detection val.py

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -43,7 +43,7 @@ class DetectionValidator(BaseValidator):
         self.iouv = torch.linspace(0.5, 0.95, 10)  # iou vector for mAP@0.5:0.95
         self.niou = self.iouv.numel()
         self.lb = []  # for autolabelling
-        self.imgsz = args.imgsz
+        # self.imgsz = args.imgsz
 
     def preprocess(self, batch):
         """Preprocesses batch of images for YOLO training."""

22a7791400dbed849f84a6758dec87909c182d0a
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-08-27 19:03:41 +0200

    Update GPU tests

--- tests/test_cuda.py
+++ tests/test_cuda.py
@@ -25,14 +25,10 @@ def test_checks():
     assert cuda_is_available() == CUDA_IS_AVAILABLE
 
 
-@pytest.mark.skipif(not CUDA_IS_AVAILABLE, reason='CUDA is not available')
-def test_train():
-    YOLO(MODEL).train(data=DATA, imgsz=64, epochs=1, batch=-1, device=0)  # also test AutoBatch, requires imgsz>=64
-
-
 @pytest.mark.skipif(CUDA_DEVICE_COUNT < 2, reason=f'DDP is not available, {CUDA_DEVICE_COUNT} device(s) found')
-def test_train_ddp():
-    YOLO(MODEL).train(data=DATA, imgsz=64, epochs=1, device=[0, 1])  # requires imgsz>=64
+def test_train():
+    device = 0 if CUDA_DEVICE_COUNT < 2 else [0, 1]
+    YOLO(MODEL).train(data=DATA, imgsz=64, epochs=1, batch=-1, device=device)  # also test AutoBatch, requires imgsz>=64
 
 
 @pytest.mark.skipif(not CUDA_IS_AVAILABLE, reason='CUDA is not available')

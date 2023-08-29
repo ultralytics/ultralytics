907930f74aa786929272df9a24fb81c4a1f6660f
ConanQZ <863101876@qq.com>
2023-08-29 20:39:21 +0800

    添加数据的yaml文件和训练程序

---
+++ train.py
@@ -0,0 +1,28 @@
+from ultralytics import YOLO
+import datetime
+
+time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+model_name = 'yolov8n-baseline.yaml'
+epochs = 200
+batch_size = 32
+patience = 50
+img_size = 608
+optimizer = 'SGD'
+
+model = YOLO(f'ultralytics/cfg/models/military/{model_name}.yaml')
+
+model.train(
+    data = 'military.yaml',
+    epochs = epochs,
+    patience = patience,
+    batch = batch_size,
+    imgsz = img_size,
+    project = 'military',
+    name = f'{model_name}-{img_size}-{optimizer}-{epochs}-{time}',
+    exit_ok = True,
+    optimizer = optimizer,
+    verbose = True,
+    cos_lr = True,
+    close_mosaic = True,
+)

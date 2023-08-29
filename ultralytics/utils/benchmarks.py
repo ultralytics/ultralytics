84caefe651cb3e73f75985e56c4364b2b57cae04
franklin-degirum <c_franklin@degirum.com>
2023-08-29 15:52:50 -0700

    yaml fetch bug testing

--- ultralytics/utils/benchmarks.py
+++ ultralytics/utils/benchmarks.py
@@ -83,8 +83,7 @@ def benchmark(model=Path(SETTINGS['weights_dir']) / 'yolov8n.pt',
     pd.options.display.width = 120
     device = select_device(device, verbose=False)
     if export_hw_optimized == True:
-        print("name->",model)
-        model_yaml = model.split(".")[0]+".yaml"
+        model_yaml = model.name.split("n.")[0]+".yaml"
         model = YOLO(model_yaml).load(model)
     else:   
         if isinstance(model, (str, Path)):

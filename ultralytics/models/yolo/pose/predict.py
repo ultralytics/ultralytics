6edbc6fc9b9ac08d3b1ba1fd8dd195963a849f6a
franklin-degirum <c_franklin@degirum.com>
2023-08-29 15:15:49 -0700

    torch error pose fix

--- ultralytics/models/yolo/pose/predict.py
+++ ultralytics/models/yolo/pose/predict.py
@@ -1,4 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
+
+import torch
 from ultralytics.engine.results import Results
 from ultralytics.models.yolo.detect.predict import DetectionPredictor
 from ultralytics.utils import DEFAULT_CFG, LOGGER, ops

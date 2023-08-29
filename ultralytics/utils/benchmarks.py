6396818cc61931a95877308fc284a097d7a2cdfe
franklin-degirum <c_franklin@degirum.com>
2023-08-29 16:43:19 -0700

    yaml path hack test

--- ultralytics/utils/benchmarks.py
+++ ultralytics/utils/benchmarks.py
@@ -85,7 +85,7 @@ def benchmark(model=Path(SETTINGS['weights_dir']) / 'yolov8n.pt',
     device = select_device(device, verbose=False)
     if export_hw_optimized == True:
         model_yaml = os.path.abspath(__file__)+model.ckpt_path.split("/")[-1].replace("n.pt", ".yaml")
-        model_yaml = "../ultralytics/cfg/models/v8/" +model.ckpt_path.split("/")[-1].replace("n.pt", ".yaml")
+        # model_yaml = "../ultralytics/cfg/models/v8/" +model.ckpt_path.split("/")[-1].replace("n.pt", ".yaml")
         print(model_yaml)
         model = YOLO(model_yaml).load(model)
     else:   

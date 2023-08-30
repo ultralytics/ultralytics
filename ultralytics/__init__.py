d987450f68b2f0b02dec06214a72e8a524bae3ff
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-08-31 01:21:20 +0200

    ultralytics 8.0.167

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -9,4 +9,4 @@ from ultralytics.utils import SETTINGS as settings
 from ultralytics.utils.checks import check_yolo as checks
 from ultralytics.utils.downloads import download
 
-__all__ = '__version__', 'YOLO', 'NAS', 'SAM', 'FastSAM', 'RTDETR', 'checks', 'download', 'settings'  # allow simpler import
+__all__ = '__version__', 'YOLO', 'NAS', 'SAM', 'FastSAM', 'RTDETR', 'checks', 'download', 'settings'

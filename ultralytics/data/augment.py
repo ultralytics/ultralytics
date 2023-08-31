ca2cba0167ec30a92e37e4fb377cb062c2e4636c
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-08-31 02:35:30 +0200

    Attempt Dockerfile fix

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -65,12 +65,7 @@ class Compose:
 
     def __repr__(self):
         """Return string representation of object."""
-        format_string = f'{self.__class__.__name__}('
-        for t in self.transforms:
-            format_string += '\n'
-            format_string += f'    {t}'
-        format_string += '\n)'
-        return format_string
+        return f"{self.__class__.__name__}({', '.join([f'{t}' for t in self.transforms])})"
 
 
 class BaseMixTransform:

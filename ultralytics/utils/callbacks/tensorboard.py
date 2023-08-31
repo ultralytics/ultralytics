9949d4c6bb8cbdc2d02f8ff108de480d5af2eda1
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-08-31 20:17:03 +0200

    Reset tensorboard import order

--- ultralytics/utils/callbacks/tensorboard.py
+++ ultralytics/utils/callbacks/tensorboard.py
@@ -3,13 +3,11 @@
 from ultralytics.utils import LOGGER, SETTINGS, TESTS_RUNNING, colorstr
 
 try:
-    from torch.utils.tensorboard import SummaryWriter
-
     assert not TESTS_RUNNING  # do not log pytest
     assert SETTINGS['tensorboard'] is True  # verify integration is enabled
-
-# TypeError for handling 'Descriptors cannot not be created directly.' protobuf errors in Windows
+    from torch.utils.tensorboard import SummaryWriter
 except (ImportError, AssertionError, TypeError):
+    # TypeError for handling 'Descriptors cannot not be created directly.' protobuf errors in Windows
     SummaryWriter = None
 
 WRITER = None  # TensorBoard SummaryWriter instance

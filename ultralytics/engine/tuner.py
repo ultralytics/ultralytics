fb9679d4ee921e6c6a5391d4b4b1459445263e7f
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-09-08 21:52:20 +0200

    Update for hangs

--- ultralytics/engine/tuner.py
+++ ultralytics/engine/tuner.py
@@ -170,8 +170,10 @@ class Tuner:
             try:
                 # Train YOLO model with mutated hyperparameters
                 train_args = {**vars(self.args), **mutated_hyp}
-                results = (deepcopy(model) or YOLO(self.args.model)).train(**train_args)
+                model_i = deepcopy(model) or YOLO(self.args.model)
+                results = model_i.train(**train_args)
                 fitness = results.fitness
+                del model_i
             except Exception as e:
                 LOGGER.warning(f'WARNING ❌️ training failure for hyperparameter tuning iteration {i + 1}\n{e}')
                 fitness = 0.0

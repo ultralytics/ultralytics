02f05f7446220edc743396fe9bf81c5d494f5335
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-09-13 12:23:32 +0200

    Update test_cuda.py

--- tests/test_cuda.py
+++ tests/test_cuda.py
@@ -94,10 +94,8 @@ def test_model_ray_tune():
 
 @pytest.mark.skipif(not CUDA_IS_AVAILABLE, reason='CUDA is not available')
 def test_model_tune():
-    YOLO('yolov8n.pt').tune(data='coco8.yaml', imgsz=320, epochs=2, iterations=2, device='0')
-    YOLO('yolov8n-seg.pt').tune(data='coco8-seg.yaml', imgsz=320, epochs=2, iterations=2, device='0')
-    YOLO('yolov8n-pose.pt').tune(data='coco8-pose.yaml', imgsz=320, epochs=2, iterations=2, device='0')
-    YOLO('yolov8n-cls.pt').tune(data='imagenet10', imgsz=320, epochs=2, iterations=2, device='0')
+    YOLO('yolov8n-pose.pt').tune(data='coco8-pose.yaml', plots=False, imgsz=32, epochs=1, iterations=2, device='cpu')
+    YOLO('yolov8n-cls.pt').tune(data='imagenet10', plots=False, imgsz=32, epochs=1, iterations=2, device='cpu')
 
 
 @pytest.mark.skipif(not CUDA_IS_AVAILABLE, reason='CUDA is not available')

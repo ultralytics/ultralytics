1ad8d3f2511ecbe37669a6d31d9f4f787d79e341
Lukas Hennies <lukas.hennies@anticipate.ml>
2023-09-18 10:12:44 +0000

    chore: re add passes

--- ultralytics/utils/callbacks/base.py
+++ ultralytics/utils/callbacks/base.py
@@ -12,58 +12,72 @@ from copy import deepcopy
 
 def on_pretrain_routine_start(trainer):
     """Called before the pretraining routine starts."""
+    pass
 
 
 def on_pretrain_routine_end(trainer):
     """Called after the pretraining routine ends."""
+    pass
 
 
 def on_train_start(trainer):
     """Called when the training starts."""
+    pass
 
 
 def on_train_epoch_start(trainer):
     """Called at the start of each training epoch."""
+    pass
 
 
 def on_train_batch_start(trainer):
     """Called at the start of each training batch."""
+    pass
 
 
 def optimizer_step(trainer):
     """Called when the optimizer takes a step."""
+    pass
 
 
 def on_before_zero_grad(trainer):
     """Called before the gradients are set to zero."""
+    pass
 
 
 def on_train_batch_end(trainer):
     """Called at the end of each training batch."""
+    pass
 
 
 def on_train_epoch_end(trainer):
     """Called at the end of each training epoch."""
+    pass
 
 
 def on_fit_epoch_end(trainer):
     """Called at the end of each fit epoch (train + val)."""
+    pass
 
 
 def on_model_save(trainer):
     """Called when the model is saved."""
+    pass
 
 
 def on_train_end(trainer):
     """Called when the training ends."""
+    pass
 
 
 def on_params_update(trainer):
     """Called when the model parameters are updated."""
+    pass
 
 
 def teardown(trainer):
     """Called during the teardown of the training process."""
+    pass
 
 
 # Validator callbacks --------------------------------------------------------------------------------------------------
@@ -71,18 +85,22 @@ def teardown(trainer):
 
 def on_val_start(validator):
     """Called when the validation starts."""
+    pass
 
 
 def on_val_batch_start(validator):
     """Called at the start of each validation batch."""
+    pass
 
 
 def on_val_batch_end(validator):
     """Called at the end of each validation batch."""
+    pass
 
 
 def on_val_end(validator):
     """Called when the validation ends."""
+    pass
 
 
 # Predictor callbacks --------------------------------------------------------------------------------------------------
@@ -90,22 +108,27 @@ def on_val_end(validator):
 
 def on_predict_start(predictor):
     """Called when the prediction starts."""
+    pass
 
 
 def on_predict_batch_start(predictor):
     """Called at the start of each prediction batch."""
+    pass
 
 
 def on_predict_batch_end(predictor):
     """Called at the end of each prediction batch."""
+    pass
 
 
 def on_predict_postprocess_end(predictor):
     """Called after the post-processing of the prediction ends."""
+    pass
 
 
 def on_predict_end(predictor):
     """Called when the prediction ends."""
+    pass
 
 
 # Exporter callbacks ---------------------------------------------------------------------------------------------------
@@ -113,10 +136,12 @@ def on_predict_end(predictor):
 
 def on_export_start(exporter):
     """Called when the model export starts."""
+    pass
 
 
 def on_export_end(exporter):
     """Called when the model export ends."""
+    pass
 
 
 default_callbacks = {

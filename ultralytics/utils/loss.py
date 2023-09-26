b48c31d936d52f0958e69041b78bc21cd10ee857
yermandy <yermaand@gmail.com>
2023-09-26 22:56:38 +0200

    refactor keypoint loss

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -99,8 +99,7 @@ class KeypointLoss(nn.Module):
     def forward(self, pred_kpts, gt_kpts, kpt_mask, area):
         """Calculates keypoint loss factor and Euclidean distance loss for predicted and actual keypoints."""
         d = (pred_kpts[..., 0] - gt_kpts[..., 0]) ** 2 + (pred_kpts[..., 1] - gt_kpts[..., 1]) ** 2
-        kpt_mask_positive = torch.sum(kpt_mask != 0, dim=1)
-        kpt_loss_factor = (kpt_mask_positive + torch.sum(kpt_mask == 0, dim=1)) / (kpt_mask_positive + 1e-9)
+        kpt_loss_factor = kpt_mask.shape[1] / (torch.sum(kpt_mask != 0, dim=1) + 1e-9)
         # e = d / (2 * (area * self.sigmas) ** 2 + 1e-9)  # from formula
         e = d / (2 * self.sigmas) ** 2 / (area + 1e-9) / 2  # from cocoeval
         return (kpt_loss_factor.view(-1, 1) * ((1 - torch.exp(-e)) * kpt_mask)).mean()

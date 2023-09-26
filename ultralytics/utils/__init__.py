7732779b36d3f9b535b67c9663405c32429ab5e0
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-09-26 18:40:59 +0200

    update save_yaml for torch.device

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -327,7 +327,7 @@ def yaml_save(file='data.yaml', data=None, header=''):
         file.parent.mkdir(parents=True, exist_ok=True)
 
     # Convert Path objects to strings
-    valid_types = int, float, str, bool, list, tuple, dict
+    valid_types = int, float, str, bool, list, tuple, dict, type(None)
     for k, v in data.items():
         if not isinstance(v, valid_types):
             data[k] = str(v)

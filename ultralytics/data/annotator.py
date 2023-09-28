fdfdc9f7b48c0b2b8bfb4aeaa09998932ceabfd6
Kayzwer <68285002+Kayzwer@users.noreply.github.com>
2023-09-28 10:56:27 +0800

    Add function to auto annotate image and generate object detection labels

--- ultralytics/data/annotator.py
+++ ultralytics/data/annotator.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from ultralytics import SAM, YOLO
 
 
-def auto_annotate(data, det_model='yolov8x.pt', sam_model='sam_b.pt', device='', output_dir=None):
+def auto_annotate_segment(data, det_model='yolov8x.pt', sam_model='sam_b.pt', device='', output_dir=None):
     """
     Automatically annotates images using a YOLO object detection model and a SAM segmentation model.
 
@@ -21,7 +21,7 @@ def auto_annotate(data, det_model='yolov8x.pt', sam_model='sam_b.pt', device='',
         ```python
         from ultralytics.data.annotator import auto_annotate
 
-        auto_annotate(data='ultralytics/assets', det_model='yolov8n.pt', sam_model='mobile_sam.pt')
+        auto_annotate_segment(data='ultralytics/assets', det_model='yolov8n.pt', sam_model='mobile_sam.pt')
         ```
     """
     det_model = YOLO(det_model)
@@ -48,3 +48,39 @@ def auto_annotate(data, det_model='yolov8x.pt', sam_model='sam_b.pt', device='',
                         continue
                     segment = map(str, segments[i].reshape(-1).tolist())
                     f.write(f'{class_ids[i]} ' + ' '.join(segment) + '\n')
+
+
+def auto_annotate_detect(data, det_model='yolov8x.pt', device='', output_dir=None):
+    """
+    Automatically annotates images using a YOLO object detection model.
+
+    Args:
+        data (str): Path to a folder containing images to be annotated.
+        det_model (str, optional): Pre-trained YOLO detection model. Defaults to 'yolov8x.pt'.
+        device (str, optional): Device to run the models on. Defaults to an empty string (CPU or GPU, if available).
+        output_dir (str | None | optional): Directory to save the annotated results.
+            Defaults to a 'labels' folder in the same directory as 'data'.
+
+    Example:
+        ```python
+        from ultralytics.data.annotator import auto_annotate
+
+        auto_annotate_detect(data='ultralytics/assets', det_model='yolov8n.pt')
+        ```
+    """
+    det_model = YOLO(det_model)
+
+    data = Path(data)
+    if not output_dir:
+        output_dir = data.parent / f'{data.stem}_auto_annotate_labels'
+    Path(output_dir).mkdir(exist_ok=True, parents=True)
+
+    det_results = det_model(data, stream=True, device=device)
+    for result in det_results:
+        class_ids = result.boxes.cls.int().tolist()  # noqa
+        if len(class_ids):
+            boxes = result.boxes.xywhn
+            with open(f'{str(Path(output_dir) / Path(result.path).stem)}.txt', 'w') as f:
+                for i in range(len(boxes)):
+                    box = map(str, boxes[i].cpu().numpy())
+                    f.write(f'{class_ids[i]} ' + ' '.join(box) + '\n')

0a18b9dd6a97def4c2a90aec0df6f45f14879ad4
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-09-28 22:34:20 +0200

    'onnx2tf>=1.15.4'

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -648,7 +648,7 @@ class Exporter:
             check_requirements(f"tensorflow{'-macos' if MACOS else '-aarch64' if ARM64 else '' if cuda else '-cpu'}")
             import tensorflow as tf  # noqa
         check_requirements(
-            ('onnx', 'onnx2tf>=1.15.4,<=1.17.5', 'sng4onnx>=1.0.1', 'onnxsim>=0.4.33', 'onnx_graphsurgeon>=0.3.26',
+            ('onnx', 'onnx2tf>=1.15.4', 'sng4onnx>=1.0.1', 'onnxsim>=0.4.33', 'onnx_graphsurgeon>=0.3.26',
              'tflite_support', 'onnxruntime-gpu' if cuda else 'onnxruntime'),
             cmds='--extra-index-url https://pypi.ngc.nvidia.com')  # onnx_graphsurgeon only on NVIDIA
 

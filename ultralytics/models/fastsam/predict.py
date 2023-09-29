82b13dc19906d06f549d3fe02b7f1214bdfde3db
TheConstant3 <nikshorop@gmail.com>
2023-09-29 10:30:58 +0300

    fix fastsam number of classes

--- ultralytics/models/fastsam/predict.py
+++ ultralytics/models/fastsam/predict.py
@@ -22,7 +22,7 @@ class FastSAMPredictor(DetectionPredictor):
                                     self.args.iou,
                                     agnostic=self.args.agnostic_nms,
                                     max_det=self.args.max_det,
-                                    nc=len(self.model.names),
+                                    nc=1,
                                     classes=self.args.classes)
         full_box = torch.zeros(p[0].shape[1], device=p[0].device)
         full_box[2], full_box[3], full_box[4], full_box[6:] = img.shape[3], img.shape[2], 1.0, 1.0

2cc78c2757e4006468ecb2a56fc16e5427727e20
Kayzwer <68285002+Kayzwer@users.noreply.github.com>
2023-09-30 05:42:51 +0000

    improve auto_annotate to work for all task

--- tests/test_python.py
+++ tests/test_python.py
@@ -306,7 +306,10 @@ def test_data_converter():
 def test_data_annotator():
     from ultralytics.data.annotator import auto_annotate
 
-    auto_annotate(ASSETS, det_model='yolov8n.pt', sam_model='mobile_sam.pt', output_dir=TMP / 'auto_annotate_labels')
+    auto_annotate(ASSETS, det_model='yolov8n.pt', output_dir=TMP / 'auto_annotate_labels')
+    auto_annotate(ASSETS, det_model='yolov8n-cls.pt', output_dir=TMP / 'auto_annotate_labels')
+    auto_annotate(ASSETS, det_model='yolov8n-seg.pt', output_dir=TMP / 'auto_annotate_labels')
+    auto_annotate(ASSETS, det_model='yolov8n-pose.pt', output_dir=TMP / 'auto_annotate_labels')
 
 
 def test_events():
--- ultralytics/data/annotator.py
+++ ultralytics/data/annotator.py
@@ -1,18 +1,18 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
+import shutil
 from pathlib import Path
 
-from ultralytics import SAM, YOLO
+from ultralytics import YOLO
 
 
-def auto_annotate(data, det_model='yolov8x.pt', sam_model='sam_b.pt', device='', output_dir=None):
+def auto_annotate(data, det_model='yolov8x.pt', device='', output_dir=None):
     """
     Automatically annotates images using a YOLO object detection model and a SAM segmentation model.
 
     Args:
         data (str): Path to a folder containing images to be annotated.
-        det_model (str, optional): Pre-trained YOLO detection model. Defaults to 'yolov8x.pt'.
-        sam_model (str, optional): Pre-trained SAM segmentation model. Defaults to 'sam_b.pt'.
+        det_model (str, optional): Pre-trained YOLO detection / segmentation / pose estimation model. Defaults to 'yolov8x.pt'.
         device (str, optional): Device to run the models on. Defaults to an empty string (CPU or GPU, if available).
         output_dir (str | None | optional): Directory to save the annotated results.
             Defaults to a 'labels' folder in the same directory as 'data'.
@@ -21,47 +21,45 @@ def auto_annotate(data, det_model='yolov8x.pt', sam_model='sam_b.pt', device='',
         ```python
         from ultralytics.data.annotator import auto_annotate
 
-        auto_annotate(data='ultralytics/assets', det_model='yolov8n.pt', sam_model='mobile_sam.pt')
+        auto_annotate(data='ultralytics/assets', det_model='yolov8n.pt')
         ```
     """
-    det_model = YOLO(det_model)
-    sam_model = SAM(sam_model)
+    yolo_model = YOLO(det_model)
 
     data = Path(data)
     if not output_dir:
         output_dir = data.parent / f'{data.stem}_auto_annotate_labels'
     Path(output_dir).mkdir(exist_ok=True, parents=True)
 
-    det_results = det_model(data, stream=True, device=device)
+    results = yolo_model(data, stream=True, device=device)
 
-    for result in det_results:
-        class_ids = result.boxes.cls.int().tolist()  # noqa
-        if len(class_ids):
-            boxes = result.boxes.xyxy  # Boxes object for bbox outputs
-            sam_results = sam_model(result.orig_img, bboxes=boxes, verbose=False, save=False, device=device)
-            segments = sam_results[0].masks.xyn  # noqa
+    for result in results:
+        if result.probs:
+            classify_dir = output_dir / 'classify'
+            Path(classify_dir).mkdir(exist_ok=True, parents=True)
+            Path(classify_dir / result.names.get(result.probs.top1)).mkdir(exist_ok=True, parents=True)
+            shutil.copy(result.path, classify_dir / result.names.get(result.probs.top1))
+        else:
+            if len(classes := result.boxes.cls.int().tolist()) == 0:
+                continue
 
-            segment_dir = output_dir / 'segment'
-            Path(segment_dir).mkdir(exist_ok=True, parents=True)
-            with open(f'{str(Path(segment_dir) / Path(result.path).stem)}.txt', 'w') as f:
-                for i in range(len(segments)):
-                    s = segments[i]
-                    if len(s) == 0:
-                        continue
-                    segment = map(str, segments[i].reshape(-1).tolist())
-                    f.write(f'{class_ids[i]} ' + ' '.join(segment) + '\n')
-
-            det_bboxes = result.boxes.xywhn.cpu().numpy()
-            detection_dir = output_dir / 'detect'
-            Path(detection_dir).mkdir(exist_ok=True, parents=True)
-            result.save_txt(f'{str(Path(detection_dir) / Path(result.path).stem)}.txt')
+            if result.boxes:
+                detect_dir = output_dir / 'detect'
+                Path(detect_dir).mkdir(exist_ok=True, parents=True)
+                with open(f'{str(Path(detect_dir) / Path(result.path).stem)}.txt', 'w') as f:
+                    for cls_, box in zip(classes, result.boxes.xywhn.cpu().numpy()):
+                        f.write(f'{cls_} ' + ' '.join(list(map(str, box))) + '\n')
+            
+            if result.masks:
+                segment_dir = output_dir / 'segment'
+                Path(segment_dir).mkdir(exist_ok=True, parents=True)
+                with open(f'{str(Path(segment_dir) / Path(result.path).stem)}.txt', 'w') as f:
+                    for cls_, mask in zip(classes, result.masks.xyn):
+                        f.write(f'{cls_} ' + ' '.join(list(map(str, mask.flatten()))) + '\n')
 
             if result.keypoints:
-                keypoints = result.keypoints.xyn.cpu().numpy()
                 pose_dir = output_dir / 'pose'
                 Path(pose_dir).mkdir(exist_ok=True, parents=True)
                 with open(f'{str(Path(pose_dir) / Path(result.path).stem)}.txt', 'w') as f:
-                    for i in range(len(det_bboxes)):
-                        box = map(str, det_bboxes[i])
-                        keypoint = map(str, keypoints[i].flatten())
-                        f.write(f'{class_ids[i]} ' + ' '.join(box) + ' ' + ' '.join(keypoint) + '\n')
+                    for cls_, box, keypoint in zip(classes, result.boxes.xywhn.cpu().numpy(), result.keypoints.xyn.cpu().numpy()):
+                        f.write(f'{cls_} ' + ' '.join(list(map(str, box))) + ' ' + ' '.join(list(map(str, keypoint.flatten()))) + '\n')

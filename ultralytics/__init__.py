311c454504fb10fa423c2e69fc10e032c74a198f
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-10-02 22:43:48 +0200

    8.0.191

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,6 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = '8.0.190'
+__version__ = '8.0.191'
 
 from ultralytics.models import RTDETR, SAM, YOLO
 from ultralytics.models.fastsam import FastSAM

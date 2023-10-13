7d8079e6ac5ce0c39c6fa6e562091e8ff7652a63
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-10-13 02:35:31 +0200

    rename YOLOv5 to YOLO
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -894,7 +894,7 @@ class Format:
         return labels
 
     def _format_img(self, img):
-        """Format the image for YOLOv5 from Numpy array to PyTorch tensor."""
+        """Format the image for YOLO from Numpy array to PyTorch tensor."""
         if len(img.shape) < 3:
             img = np.expand_dims(img, -1)
         img = np.ascontiguousarray(img.transpose(2, 0, 1)[::-1])

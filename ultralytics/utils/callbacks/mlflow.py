4203e0ac62cf356eeec4b0e7c5e5a77065ff89f1
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-10-13 16:10:23 +0200

    Improve MLflow
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/utils/callbacks/mlflow.py
+++ ultralytics/utils/callbacks/mlflow.py
@@ -1,6 +1,31 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
+"""
+MLflow Logging for Ultralytics YOLO
+===================================
 
-from ultralytics.utils import LOGGER, SETTINGS, TESTS_RUNNING, colorstr
+This module enables MLflow logging for Ultralytics YOLO. It logs metrics, parameters, and model artifacts.
+For setting up, a tracking URI should be specified. The logging can be customized using environment variables.
+
+Commands:
+---------
+1. To set a project name:
+    `export MLFLOW_EXPERIMENT_NAME=<your_experiment_name>` or use the project=<project> argument
+
+2. To set a run name:
+    `export MLFLOW_RUN=<your_run_name>` or use the name=<name> argument
+
+3. To start a local MLflow server:
+    mlflow ui
+   It will by default start a local server at http://127.0.0.1:5000.
+   To specify a different URI, set the MLFLOW_TRACKING_URI environment variable.
+
+4. To kill all running MLflow server instances:
+    ps aux | grep 'mlflow' | grep -v 'grep' | awk '{print $2}' | xargs kill -9
+"""
+
+from pathlib import Path
+
+from ultralytics.utils import LOGGER, RUNS_DIR, SETTINGS, TESTS_RUNNING, colorstr
 
 try:
     assert not TESTS_RUNNING  # do not log pytest
@@ -17,50 +42,48 @@ except (ImportError, AssertionError):
 
 def on_pretrain_routine_end(trainer):
     """Log training parameters to MLflow."""
-    global mlflow, run, uri
+    global mlflow
 
-    uri = os.environ.get('MLFLOW_TRACKING_URI')
-    if uri is None:
-        mlflow = None
-        LOGGER.info(f'{PREFIX}installed but no tracking URI set, '
-                    "i.e. run 'mlflow ui && export MLFLOW_TRACKING_URI=http://127.0.0.1:5000'")
+    uri = os.environ.get('MLFLOW_TRACKING_URI') or str(RUNS_DIR / 'mlflow')
+    LOGGER.debug(f'{PREFIX} tracking uri: {uri}')
+    mlflow.set_tracking_uri(uri)
 
-    else:
-        LOGGER.debug(f'{PREFIX} tracking uri: {uri}')
-        mlflow.set_tracking_uri(uri)
+    # Set experiment and run names
+    experiment_name = os.environ.get('MLFLOW_EXPERIMENT_NAME') or trainer.args.project or '/Shared/YOLOv8'
+    run_name = os.environ.get('MLFLOW_RUN') or trainer.args.name
+    experiment = mlflow.set_experiment(experiment_name)  # change since mlflow does this now by default
 
-        # Set experiment and run names
-        experiment_name = os.environ.get('MLFLOW_EXPERIMENT_NAME') or trainer.args.project or '/Shared/YOLOv8'
-        run_name = os.environ.get('MLFLOW_RUN') or trainer.args.name
-        experiment = mlflow.set_experiment(experiment_name)  # change since mlflow does this now by default
-
-        mlflow.autolog()
-        try:
-            run, active_run = mlflow, mlflow.active_run()
-            if not active_run:
-                active_run = mlflow.start_run(experiment_id=experiment.experiment_id, run_name=run_name)
-            LOGGER.info(f'{PREFIX}logging run_id({active_run.info.run_id}) to {uri}')
-            run.log_params(dict(trainer.args))
-        except Exception as e:
-            LOGGER.warning(f'{PREFIX}WARNING ⚠️ Failed to initialize: {e}\n'
-                           f'{PREFIX}Continuing without MLflow')
+    mlflow.autolog()
+    try:
+        active_run = mlflow.active_run() or mlflow.start_run(experiment_id=experiment.experiment_id, run_name=run_name)
+        LOGGER.info(f'{PREFIX}logging run_id({active_run.info.run_id}) to {uri}')
+        if Path(uri).is_dir():
+            LOGGER.info(f"{PREFIX}view at http://127.0.0.1:5000 with 'mlflow server --backend-store-uri {uri}'")
+        LOGGER.info(f"{PREFIX}disable with 'yolo settings mlflow=False'")
+        mlflow.log_params(dict(trainer.args))
+    except Exception as e:
+        LOGGER.warning(f'{PREFIX}WARNING ⚠️ Failed to initialize: {e}\n'
+                       f'{PREFIX}WARNING ⚠️ Not tracking this run')
 
 
 def on_fit_epoch_end(trainer):
     """Log training metrics to MLflow."""
     if mlflow:
         sanitized_metrics = {k.replace('(', '').replace(')', ''): float(v) for k, v in trainer.metrics.items()}
-        run.log_metrics(metrics=sanitized_metrics, step=trainer.epoch)
+        mlflow.log_metrics(metrics=sanitized_metrics, step=trainer.epoch)
 
 
 def on_train_end(trainer):
     """Log model artifacts at the end of the training."""
     if mlflow:
-        run.log_artifact(trainer.last)
-        run.log_artifact(trainer.best)
-        run.log_artifact(trainer.save_dir)
+        mlflow.log_artifact(str(trainer.best.parent))  # log save_dir/weights directory with best.pt and last.pt
+        for f in trainer.save_dir.glob('*'):  # log all other files in save_dir
+            if f.suffix in {'.png', '.jpg', '.csv', '.pt', '.yaml'}:
+                mlflow.log_artifact(str(f))
+
         mlflow.end_run()
-        LOGGER.info(f'{PREFIX}results logged to {uri}')
+        LOGGER.info(f'{PREFIX}results logged to {mlflow.get_tracking_uri()}\n'
+                    f"{PREFIX}disable with 'yolo settings mlflow=False'")
 
 
 callbacks = {

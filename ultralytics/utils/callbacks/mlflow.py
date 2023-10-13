c7f827061681207e768a83bb26729f6159a94cc3
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-10-13 16:16:54 +0200

    Improve MLflow
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/utils/callbacks/mlflow.py
+++ ultralytics/utils/callbacks/mlflow.py
@@ -1,26 +1,24 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 """
 MLflow Logging for Ultralytics YOLO
-===================================
 
 This module enables MLflow logging for Ultralytics YOLO. It logs metrics, parameters, and model artifacts.
 For setting up, a tracking URI should be specified. The logging can be customized using environment variables.
 
 Commands:
----------
-1. To set a project name:
-    `export MLFLOW_EXPERIMENT_NAME=<your_experiment_name>` or use the project=<project> argument
+    1. To set a project name:
+        `export MLFLOW_EXPERIMENT_NAME=<your_experiment_name>` or use the project=<project> argument
 
-2. To set a run name:
-    `export MLFLOW_RUN=<your_run_name>` or use the name=<name> argument
+    2. To set a run name:
+        `export MLFLOW_RUN=<your_run_name>` or use the name=<name> argument
 
-3. To start a local MLflow server:
-    mlflow ui
-   It will by default start a local server at http://127.0.0.1:5000.
-   To specify a different URI, set the MLFLOW_TRACKING_URI environment variable.
+    3. To start a local MLflow server:
+        mlflow ui
+       It will by default start a local server at http://127.0.0.1:5000.
+       To specify a different URI, set the MLFLOW_TRACKING_URI environment variable.
 
-4. To kill all running MLflow server instances:
-    ps aux | grep 'mlflow' | grep -v 'grep' | awk '{print $2}' | xargs kill -9
+    4. To kill all running MLflow server instances:
+        ps aux | grep 'mlflow' | grep -v 'grep' | awk '{print $2}' | xargs kill -9
 """
 
 from pathlib import Path
@@ -41,7 +39,24 @@ except (ImportError, AssertionError):
 
 
 def on_pretrain_routine_end(trainer):
-    """Log training parameters to MLflow."""
+    """
+    Log training parameters to MLflow at the end of the pretraining routine.
+
+    This function sets up MLflow logging based on environment variables and trainer arguments. It sets the tracking URI,
+    experiment name, and run name, then starts the MLflow run if not already active. It finally logs the parameters
+    from the trainer.
+
+    Args:
+        trainer: The training object containing arguments and parameters to log.
+
+    Global:
+        mlflow: The imported mlflow module to use for logging.
+
+    Environment Variables:
+        MLFLOW_TRACKING_URI: The URI for MLflow tracking. If not set, defaults to 'runs/mlflow'.
+        MLFLOW_EXPERIMENT_NAME: The name of the MLflow experiment. If not set, defaults to trainer.args.project.
+        MLFLOW_RUN: The name of the MLflow run. If not set, defaults to trainer.args.name.
+    """
     global mlflow
 
     uri = os.environ.get('MLFLOW_TRACKING_URI') or str(RUNS_DIR / 'mlflow')
@@ -51,11 +66,11 @@ def on_pretrain_routine_end(trainer):
     # Set experiment and run names
     experiment_name = os.environ.get('MLFLOW_EXPERIMENT_NAME') or trainer.args.project or '/Shared/YOLOv8'
     run_name = os.environ.get('MLFLOW_RUN') or trainer.args.name
-    experiment = mlflow.set_experiment(experiment_name)  # change since mlflow does this now by default
+    mlflow.set_experiment(experiment_name)
 
     mlflow.autolog()
     try:
-        active_run = mlflow.active_run() or mlflow.start_run(experiment_id=experiment.experiment_id, run_name=run_name)
+        active_run = mlflow.active_run() or mlflow.start_run(run_name=run_name)
         LOGGER.info(f'{PREFIX}logging run_id({active_run.info.run_id}) to {uri}')
         if Path(uri).is_dir():
             LOGGER.info(f"{PREFIX}view at http://127.0.0.1:5000 with 'mlflow server --backend-store-uri {uri}'")
@@ -67,7 +82,7 @@ def on_pretrain_routine_end(trainer):
 
 
 def on_fit_epoch_end(trainer):
-    """Log training metrics to MLflow."""
+    """Log training metrics at the end of each fit epoch to MLflow."""
     if mlflow:
         sanitized_metrics = {k.replace('(', '').replace(')', ''): float(v) for k, v in trainer.metrics.items()}
         mlflow.log_metrics(metrics=sanitized_metrics, step=trainer.epoch)

485f73f739356dbccd75ec90fb668cadecf827e1
Yonghye Kwon <developer.0hye@gmail.com>
2023-10-15 14:55:10 +0900

    modify index to propely plot val metrics on results.png when we record all metrics

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -531,7 +531,7 @@ def plot_results(file='path/to/results.csv', dir='', segment=False, pose=False, 
         index = [1, 2, 3, 4, 5, 6, 7, 10, 11, 14, 15, 16, 17, 18, 8, 9, 12, 13]
     else:
         fig, ax = plt.subplots(2, 5, figsize=(12, 6), tight_layout=True)
-        index = [1, 2, 3, 4, 5, 8, 9, 10, 6, 7]
+        index = [1, 2, 3, 4, 5, -6, -5, -4, 6, 7]
     ax = ax.ravel()
     files = list(save_dir.glob('results*.csv'))
     assert len(files), f'No results.csv files found in {save_dir.resolve()}, nothing to plot.'

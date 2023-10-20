ef6f0fcfe1a5991c3a4494f1a1f981466c0a48d2
Mehrdad <khatami-mehrdad@github.com>
2023-10-20 13:44:42 -0700

    changing default values

--- dg_val.py
+++ dg_val.py
@@ -6,9 +6,9 @@ import argparse
 def parser_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, help='initial weights path')
-    parser.add_argument('--data', type=str, default='coco.yaml', help='dataset.yaml path')
+    parser.add_argument('--data', type=str, default='coco128.yaml', help='dataset.yaml path')
     parser.add_argument('--anno', type=str, help='dataset.yaml path')
-    parser.add_argument('--device', type=str, default='cuda:7', help='device to use')
+    parser.add_argument('--device', type=str, default='cpu', help='device to use')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
 
     return parser.parse_args()
@@ -24,6 +24,6 @@ if __name__ == '__main__':
     model = YOLO(args.weights)
 
     separate_outputs = False if args.weights.endswith('.pt') else True
-    success = model.val(data=args.data, imgsz=args.imgsz, rect=False, device=args.device, separate_outputs=separate_outputs, save_json=True)
+    success = model.val(data=args.data, imgsz=args.imgsz, rect=False, device=args.device, separate_outputs=separate_outputs, save_json=False)
 
     

e58db228c2fd9856e7bff54a708bf5acde26fb29
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-10-26 20:32:51 +0200

    `ultralytics 8.0.202` sort Triton model outputs (#5945)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Mike Tune <mtuneoff@gmail.com>

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,6 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = '8.0.201'
+__version__ = '8.0.202'
 
 from ultralytics.models import RTDETR, SAM, YOLO
 from ultralytics.models.fastsam import FastSAM

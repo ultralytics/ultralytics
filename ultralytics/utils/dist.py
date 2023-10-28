af5792af567a741f086443c398ae265a8a20df75
Finlay Morrison <57669260+finlaymorrison@users.noreply.github.com>
2023-10-29 00:41:18 +0100

    Split long line into two more cleanly.

--- ultralytics/utils/dist.py
+++ ultralytics/utils/dist.py
@@ -58,8 +58,8 @@ def generate_ddp_command(world_size, distributed_args, trainer):
         file = generate_ddp_file(trainer)
     dist_cmd = 'torch.distributed.run' if TORCH_1_9 else 'torch.distributed.launch'
     port = find_free_network_port()
-    cmd = [sys.executable, '-m', dist_cmd, '--nproc_per_node', f'{world_size}', '--master_port', f'{port}', file
-           ] + distributed_args
+    cmd = [sys.executable, '-m', dist_cmd, '--nproc_per_node', f'{world_size}', '--master_port', f'{port}', file]
+    cmd.extend(distributed_args)
     return cmd, file
 
 

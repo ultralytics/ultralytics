ab7c02f84434ff98c71702565ff50eb3fcd9fc9c
Mehrdad <khatami-mehrdad@github.com>
2023-11-06 18:01:58 -0800

    --quantize

--- dg_export.py
+++ dg_export.py
@@ -7,6 +7,7 @@ def parser_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='', help='initial weights path')
     parser.add_argument('--format', type=str, default='tflite', help='export format')
+    parser.add_argument('--quantize', action='store_true', help='int8 export')
     parser.add_argument('--data', type=str, default='coco128.yaml', help='dataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
 
@@ -21,6 +22,6 @@ if __name__ == '__main__':
 
     model = YOLO(args.weights)
 
-    success = model.export(format=args.format, imgsz=args.imgsz, data=args.data, int8=True, separate_outputs=True, export_hw_optimized=True, uint8_io_dtype=True, max_ncalib_imgs=100)
+    success = model.export(format=args.format, imgsz=args.imgsz, data=args.data, int8=args.quantize, separate_outputs=True, export_hw_optimized=True, uint8_io_dtype=True, max_ncalib_imgs=100)
 
 

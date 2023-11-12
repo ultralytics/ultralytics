dd6dcac0b484e8a4d6a0f7bfed41500a7306aaf8
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-11-13 00:24:06 +0100

    Add fix broken links script
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

---
+++ docs/fix_relative_links.py
@@ -0,0 +1,68 @@
+"""
+Script to fix broken Markdown links in language-specific directories.
+
+This script processes markdown files in language-specific directories (like /zh/).
+It finds Markdown links and checks their existence. If a link is broken and does not exist
+in the language-specific directory but exists in the /en/ directory, the script updates
+the link to point to the corresponding file in the /en/ directory.
+"""
+
+import re
+from pathlib import Path
+
+
+class MarkdownLinkFixer:
+    """Class to fix Markdown links in language-specific directories."""
+
+    def __init__(self, base_dir):
+        """Initialize the MarkdownLinkFixer with the base directory."""
+        self.base_dir = Path(base_dir)
+        self.md_link_regex = re.compile(r'\[([^\]]+)\]\(([^:\)]+)\.md\)')
+
+    def link_replacer(self, match, parent_dir, lang_dir):
+        """Replace broken links with corresponding links in the /en/ directory."""
+        text, path = match.groups()
+        linked_path = (parent_dir / path).with_suffix('.md')
+
+        if not linked_path.exists():
+            en_linked_path = Path(str(linked_path).replace(str(lang_dir), str(lang_dir.parent / 'en')))
+            if en_linked_path.exists():
+                steps_up = len(parent_dir.relative_to(self.base_dir).parts)
+                relative_path = Path('../' * steps_up) / en_linked_path.relative_to(self.base_dir)
+                relative_path = str(relative_path).replace('/en/', '/')
+                print(f"Redirecting link '[{text}]({path})' from {parent_dir} to {relative_path}")
+                return f'[{text}]({relative_path})'
+            else:
+                print(f"Warning: Broken link '[{text}]({path})' found in {parent_dir} does not exist in /docs/en/.")
+        return match.group(0)
+
+    def process_markdown_file(self, md_file_path, lang_dir):
+        """Process each markdown file in the language directory."""
+        print(f"Processing file: {md_file_path}")
+        with open(md_file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+
+        parent_dir = md_file_path.parent
+        updated_content = self.md_link_regex.sub(lambda m: self.link_replacer(m, parent_dir, lang_dir), content)
+
+        with open(md_file_path, 'w', encoding='utf-8') as file:
+            file.write(updated_content)
+
+    def process_language_directory(self, lang_dir):
+        """Process each language-specific directory."""
+        print(f"Processing language directory: {lang_dir}")
+        for md_file in lang_dir.rglob('*.md'):
+            self.process_markdown_file(md_file, lang_dir)
+
+    def run(self):
+        """Run the link fixing process for each language-specific directory."""
+        for subdir in self.base_dir.iterdir():
+            if subdir.is_dir() and re.match(r'^\w\w$', subdir.name) and subdir.name != 'en':
+                self.process_language_directory(subdir)
+
+
+if __name__ == '__main__':
+    # Set the path to your MkDocs 'docs' directory here
+    docs_dir = str(Path(__file__).parent.resolve())
+    fixer = MarkdownLinkFixer(docs_dir)
+    fixer.run()

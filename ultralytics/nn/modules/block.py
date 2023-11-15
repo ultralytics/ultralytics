05cc2da540e77cffe821544872a03a406872287d
NourAmr21 <68479961+NourAmr21@users.noreply.github.com>
2023-11-15 16:19:01 +0200

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -7,64 +7,64 @@ import torch.nn.functional as F
 
 from .conv import Conv, DWConv, GhostConv, LightConv, RepConv
 from .transformer import TransformerBlock
-
-__all__ = ('SpatialAttention','ChannelAttention','Conv_CBAM','DFL', 'HGBlock', 'HGStem', 'SPP', 'SPPF', 'C1', 'C2', 'C3', 'C2f', 'C3x', 'C3TR', 'C3Ghost',
+#('SpatialAttention','ChannelAttention','Conv_CBAM',
+__all__ = ('DFL', 'HGBlock', 'HGStem', 'SPP', 'SPPF', 'C1', 'C2', 'C3', 'C2f', 'C3x', 'C3TR', 'C3Ghost',
            'GhostBottleneck', 'Bottleneck', 'BottleneckCSP', 'Proto', 'RepC3')
-class SpatialAttention(nn.Module):
-    def __init__(self, kernel_size=7):
-        super(SpatialAttention, self).__init__()
+#class SpatialAttention(nn.Module):
+ #   def __init__(self, kernel_size=7):
+  #      super(SpatialAttention, self).__init__()
 
-        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
-        padding = 3 if kernel_size == 7 else 1
+   #     assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+    #    padding = 3 if kernel_size == 7 else 1
 
-        self.conv = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
-        self.sigmoid = nn.Sigmoid()
+     #   self.conv = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+      #  self.sigmoid = nn.Sigmoid()
 
-    def forward(self, x):
-        avg_out = torch.mean(x, dim=1, keepdim=True)
-        max_out, _ = torch.max(x, dim=1, keepdim=True)
-        x = torch.cat([avg_out, max_out], dim=1)
-        x = self.conv(x)
-        return self.sigmoid(x)
-class ChannelAttention(nn.Module):
-    def __init__(self, in_planes, ratio=16):
-        super(ChannelAttention, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.max_pool = nn.AdaptiveMaxPool2d(1)
+    #def forward(self, x):
+     #   avg_out = torch.mean(x, dim=1, keepdim=True)
+      #  max_out, _ = torch.max(x, dim=1, keepdim=True)
+      #  x = torch.cat([avg_out, max_out], dim=1)
+       # x = self.conv(x)
+        #return self.sigmoid(x)
+#class ChannelAttention(nn.Module):
+ #   def __init__(self, in_planes, ratio=16):
+  #      super(ChannelAttention, self).__init__()
+   #     self.avg_pool = nn.AdaptiveAvgPool2d(1)
+    #    self.max_pool = nn.AdaptiveMaxPool2d(1)
 
-        self.f1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
-        self.relu = nn.ReLU()
-        self.f2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+     #   self.f1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+      #  self.relu = nn.ReLU()
+       # self.f2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
         # 写法二,亦可使用顺序容器
         # self.sharedMLP = nn.Sequential(
         # nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False), nn.ReLU(),
         # nn.Conv2d(in_planes // rotio, in_planes, 1, bias=False))
 
-        self.sigmoid = nn.Sigmoid()
+       # self.sigmoid = nn.Sigmoid()
 
-    def forward(self, x):
-        avg_out = self.f2(self.relu(self.f1(self.avg_pool(x))))
-        max_out = self.f2(self.relu(self.f1(self.max_pool(x))))
-        out = self.sigmoid(avg_out + max_out)
-        return out
-class Conv_CBAM(nn.Module):
+   # def forward(self, x):
+    #    avg_out = self.f2(self.relu(self.f1(self.avg_pool(x))))
+     #   max_out = self.f2(self.relu(self.f1(self.max_pool(x))))
+     #   out = self.sigmoid(avg_out + max_out)
+      #  return out
+#class Conv_CBAM(nn.Module):
     # Standard convolution
-    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
-        super(Conv_CBAM, self).__init__()
-        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
-        self.bn = nn.BatchNorm2d(c2)
-        self.act = nn.Hardswish() if act else nn.Identity()
-        self.ca = ChannelAttention(c2)
-        self.sa = SpatialAttention()
+ #   def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+  #      super(Conv_CBAM, self).__init__()
+   #     self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+    #    self.bn = nn.BatchNorm2d(c2)
+     #   self.act = nn.Hardswish() if act else nn.Identity()
+      #  self.ca = ChannelAttention(c2)
+       # self.sa = SpatialAttention()
 
-    def forward(self, x):
-        x = self.act(self.bn(self.conv(x)))
-        x = self.ca(x) * x
-        x = self.sa(x) * x
-        return x
+    #def forward(self, x):
+     #   x = self.act(self.bn(self.conv(x)))
+      #  x = self.ca(x) * x
+       # x = self.sa(x) * x
+        #return x
 
-    def fuseforward(self, x):
-        return self.act(self.conv(x))
+    #def fuseforward(self, x):
+     #   return self.act(self.conv(x))
 class DFL(nn.Module):
     """
     Integral module of Distribution Focal Loss (DFL).

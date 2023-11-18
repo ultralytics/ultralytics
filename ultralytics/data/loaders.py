7f81c2395d3d4e0fa6c257a72adbc97c945f43a7
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-11-18 04:08:26 +0100

    use_pafy=False
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/data/loaders.py
+++ ultralytics/data/loaders.py
@@ -493,7 +493,7 @@ def autocast_list(source):
 LOADERS = LoadStreams, LoadPilAndNumpy, LoadImages, LoadScreenshots  # tuple
 
 
-def get_best_youtube_url(url, use_pafy=True):
+def get_best_youtube_url(url, use_pafy=False):
     """
     Retrieves the URL of the best quality MP4 video stream from a given YouTube video.
 

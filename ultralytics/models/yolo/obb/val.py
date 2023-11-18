e284c7695919584804579332873bbd27112b1cb3
Laughing-q <1185102784@qq.com>
2023-11-18 11:06:46 +0800

    add batch_probiou

--- ultralytics/models/yolo/obb/val.py
+++ ultralytics/models/yolo/obb/val.py
@@ -1,7 +1,7 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
 from ultralytics.models.yolo.detect import DetectionValidator
-from ultralytics.utils.metrics import OBBMetrics
+from ultralytics.utils.metrics import OBBMetrics, batch_probiou
 
 
 class OBBValidator(DetectionValidator):
@@ -25,3 +25,28 @@ class OBBValidator(DetectionValidator):
         self.process = None
         self.args.task = 'obb'
         self.metrics = OBBMetrics(save_dir=self.save_dir, plot=True, on_plot=self.on_plot)
+
+    def _process_batch(self, detections, labels):
+        """
+        Return correct prediction matrix.
+
+        Args:
+            detections (torch.Tensor): Tensor of shape [N, 6] representing detections.
+                Each detection is of the format: x1, y1, x2, y2, conf, class.
+            labels (torch.Tensor): Tensor of shape [M, 5] representing labels.
+                Each label is of the format: class, x1, y1, x2, y2.
+
+        Returns:
+            (torch.Tensor): Correct prediction matrix of shape [N, 10] for 10 IoU levels.
+        """
+        iou = batch_probiou(labels[:, 1:], detections[:, :4])
+        return self.match_predictions(detections[:, 5], labels[:, 0], iou)
+
+    # TODO
+    def update_metrics(self, preds, batch):
+        pass
+
+    # TODO
+    def plot_predictions(self, batch, preds, ni):
+        """Plots predicted bounding boxes on input images and saves the result."""
+        pass
--- ultralytics/utils/metrics.py
+++ ultralytics/utils/metrics.py
@@ -165,6 +165,25 @@ def kpt_iou(kpt1, kpt2, area, sigma, eps=1e-7):
     return (torch.exp(-e) * kpt_mask[:, None]).sum(-1) / (kpt_mask.sum(-1)[:, None] + eps)
 
 
+def _get_covariance_matrix(boxes):
+    """Generating covariance matrix from obbs.
+
+    Args:
+        boxes (torch.Tensor): A tensor of shape (N, 5) representing rotated bounding boxes, with xywhr format.
+
+    Returns:
+        (torch.Tensor): Covariance metrixs corresponding to original rotated bounding boxes.
+    """
+    # Gaussian bounding boxes, ignored the center points(the first two columns) cause it's not needed here.
+    gbbs = torch.cat((torch.pow(boxes[:, 2:4], 2) / 12, boxes[:, 4:]), dim=-1)
+    a, b, c = gbbs.split(1, dim=-1)
+    return (
+        a * torch.cos(c) ** 2 + b * torch.sin(c) ** 2,
+        a * torch.sin(c) ** 2 + b * torch.cos(c) ** 2,
+        a * torch.cos(c) * torch.sin(c) - b * torch.sin(c) * torch.cos(c),
+    )
+
+
 def probiou(obb1, obb2, eps=1e-7):
     """
     Calculate the prob iou between oriented bounding boxes, https://arxiv.org/pdf/2106.06072v1.pdf.
@@ -177,24 +196,39 @@ def probiou(obb1, obb2, eps=1e-7):
     Returns:
         (torch.Tensor): A tensor of shape (N, ) representing obb similarities.
     """
+    x1, y1 = obb1[..., :2].split(1, dim=-1)
+    x2, y2 = obb2[..., :2].split(1, dim=-1)
+    a1, b1, c1 = _get_covariance_matrix(obb1)
+    a2, b2, c2 = _get_covariance_matrix(obb2)
 
-    def _get_gbb(boxes):
-        """Generate Gaussian bounding boxes from obbs."""
-        return torch.cat((boxes[:, :2], torch.pow(boxes[:, 2:4], 2) / 12, boxes[:, 4:]), dim=-1)
+    t1 = (((a1 + a2) * (torch.pow(y1 - y2, 2)) + (b1 + b2) * (torch.pow(x1 - x2, 2))) /
+          ((a1 + a2) * (b1 + b2) - (torch.pow(c1 + c2, 2)) + eps)) * 0.25
+    t2 = (((c1 + c2) * (x2 - x1) * (y1 - y2)) / ((a1 + a2) * (b1 + b2) - (torch.pow(c1 + c2, 2)) + eps)) * 0.5
+    t3 = torch.log(((a1 + a2) * (b1 + b2) - (torch.pow(c1 + c2, 2))) /
+                   (4 * torch.sqrt((a1 * b1 - torch.pow(c1, 2)).clamp_(0) *
+                                   (a2 * b2 - torch.pow(c2, 2)).clamp_(0)) + eps) + eps) * 0.5
+    bd = t1 + t2 + t3
+    bd = torch.clamp(bd, eps, 100.0)
+    hd = torch.sqrt(1.0 - torch.exp(-bd) + eps)
+    return 1 - hd
 
-    def _get_rotated(a, b, r):
-        return (
-            a * torch.cos(r) ** 2 + b * torch.sin(r) ** 2,
-            a * torch.sin(r) ** 2 + b * torch.cos(r) ** 2,
-            a * torch.cos(r) * torch.sin(r) - b * torch.sin(r) * torch.cos(r),
-        )
 
-    gbboxes1 = _get_gbb(obb1)
-    gbboxes2 = _get_gbb(obb2)
-    x1, y1, a1_, b1_, c1_ = gbboxes1.split(1, dim=-1)
-    x2, y2, a2_, b2_, c2_ = gbboxes2.split(1, dim=-1)
-    a1, b1, c1 = _get_rotated(a1_, b1_, c1_)
-    a2, b2, c2 = _get_rotated(a2_, b2_, c2_)
+def batch_probiou(obb1, obb2, eps=1e-7):
+    """
+    Calculate the prob iou between oriented bounding boxes, https://arxiv.org/pdf/2106.06072v1.pdf.
+
+    Args:
+        obb1 (torch.Tensor): A tensor of shape (N, 5) representing ground truth obbs, with xywhr format.
+        obb2 (torch.Tensor): A tensor of shape (M, 5) representing predicted obbs, with xywhr format.
+        eps (float, optional): A small value to avoid division by zero. Defaults to 1e-7.
+
+    Returns:
+        (torch.Tensor): A tensor of shape (N, M) representing obb similarities.
+    """
+    x1, y1 = obb1[..., :2].split(1, dim=-1)
+    x2, y2 = [x.squeeze(-1)[None] for x in obb2[..., :2].split(1, dim=-1)]
+    a1, b1, c1 = _get_covariance_matrix(obb1)
+    a2, b2, c2 = [x.squeeze(-1)[None] for x in _get_covariance_matrix(obb2)]
 
     t1 = (((a1 + a2) * (torch.pow(y1 - y2, 2)) + (b1 + b2) * (torch.pow(x1 - x2, 2))) /
           ((a1 + a2) * (b1 + b2) - (torch.pow(c1 + c2, 2)) + eps)) * 0.25

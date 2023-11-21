9b627ee34189429182f23010293bf771982724da
Laughing-q <1185102784@qq.com>
2023-11-21 19:36:15 +0800

    revert box decode

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -8,7 +8,6 @@ import torch.nn as nn
 from torch.nn.init import constant_, xavier_uniform_
 
 from ultralytics.utils.tal import TORCH_1_10, dist2bbox, make_anchors
-from ultralytics.utils.tal_obb import dist2rbox
 
 from .block import DFL, Proto
 from .conv import Conv
@@ -122,35 +121,11 @@ class OBB(Detect):
     def forward(self, x):
         bs = x[0].shape[0]  # batch size
         angle = torch.cat([self.cv4[i](x[i]).view(bs, self.ne, -1) for i in range(self.nl)], 2)  # OBB theta logits
-        shape = x[0].shape  # BCHW
-        for i in range(self.nl):
-            x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
+        x = self.detect(self, x)
         if self.training:
-            return x
-        elif self.dynamic or self.shape != shape:
-            self.anchors, self.strides = (x.transpose(0, 1) for x in make_anchors(x, self.stride, 0.5))
-            self.shape = shape
-
-        x_cat = torch.cat([xi.view(shape[0], self.no, -1) for xi in x], 2)
-        if self.export and self.format in ('saved_model', 'pb', 'tflite', 'edgetpu', 'tfjs'):  # avoid TF FlexSplitV ops
-            box = x_cat[:, :self.reg_max * 4]
-            cls = x_cat[:, self.reg_max * 4:]
-        else:
-            box, cls = x_cat.split((self.reg_max * 4, self.nc), 1)
-        dbox = dist2rbox(torch.cat([self.dfl(box), angle], dim=1).permute(0, 2, 1), self.anchors.unsqueeze(0).permute(0, 2, 1))
-        dbox = dbox[..., :4].permute(0, 2, 1) * self.strides
-
-        if self.export and self.format in ('tflite', 'edgetpu'):
-            # Normalize xywh with image size to mitigate quantization error of TFLite integer models as done in YOLOv5:
-            # https://github.com/ultralytics/yolov5/blob/0c8de3fca4a702f8ff5c435e67f378d1fce70243/models/tf.py#L307-L309
-            # See this PR for details: https://github.com/ultralytics/ultralytics/pull/1695
-            img_h = shape[2] * self.stride[0]
-            img_w = shape[3] * self.stride[0]
-            img_size = torch.tensor([img_w, img_h, img_w, img_h], device=dbox.device).reshape(1, 4, 1)
-            dbox /= img_size
-
-        y = torch.cat((dbox, cls.sigmoid()), 1)
-        return torch.cat([y, angle], 1) if self.export else (torch.cat([y, angle], 1), (x, angle))
+            return x, angle
+        # degrees = self.logits2degrees(angle)  # 0-1 theta to -180 to 180 degrees
+        return torch.cat([x, angle], 1) if self.export else (torch.cat([x[0], angle], 1), (x[1], angle))
 
     @staticmethod
     def logits2degrees(x):

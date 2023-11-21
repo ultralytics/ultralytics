cfe360f2d3456b91e7fe74355ed4edca23930043
Laughing-q <1185102784@qq.com>
2023-11-21 20:19:19 +0800

    fix precommit

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -13,7 +13,7 @@ from ultralytics.utils import LOGGER, colorstr
 from ultralytics.utils.checks import check_version
 from ultralytics.utils.instance import Instances
 from ultralytics.utils.metrics import bbox_ioa
-from ultralytics.utils.ops import segment2box, xyxyxyxy2xywhr
+from ultralytics.utils.ops import segment2box
 
 from .utils import polygons2masks, polygons2masks_overlap
 

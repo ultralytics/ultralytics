347e3489cc106979016e8ac5ce47521f57299cf9
johnnynunez <johnnynuca14@gmail.com>
2023-11-28 14:51:53 +0100

    testing

--- inference_tools/export_v2.py
+++ inference_tools/export_v2.py
@@ -13,10 +13,12 @@ def export_to_onnx(
     model: torch.nn.Module,
     dummy_input: Tensor,
     output_file: str,
-    opset_version: int = 9,
-    input_names: list = ["input"],
-    output_names: list = ["output"],
+    opset_version: int = 11,
+    input_names: list = ["images"],
+    output_names: list = ["output0"],
+    do_constant_folding: bool = True,
     verbose: bool = False,
+    dynamic_axes: dict = None,
 ) -> None:
     """
     Exports a PyTorch model to ONNX format.
@@ -40,14 +42,16 @@ def export_to_onnx(
         f=output_file,
         verbose=verbose,
         input_names=input_names,
+        do_constant_folding=do_constant_folding,
         opset_version=opset_version,
         output_names=output_names,
+        dynamic_axes=dynamic_axes,
     )
 
 
 if __name__ == "__main__":
     # Load config.json
-    with open("./inference_config.json", "r") as f:
+    with open("/home/johnny/Projects/small-fast-detector/inference_tools/inference_config.json", "r") as f:
         config = json.load(f)
     print("Loaded config: ", config)
     img_height_size = config["img_size"]
@@ -56,11 +60,11 @@ if __name__ == "__main__":
     print("🚀 Initializing model...")
     # Initialize and set up model
     # Load model
-    model = YOLO(model=config["model_path"], task="detect")
+    model = YOLO(model=config["model_path"], task="detect").model
 
     print("🔌 Setting up device...")
     # Device setup
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
     model = model.to(device)
 
     print("📝 Preparing dummy input...")
@@ -69,25 +73,24 @@ if __name__ == "__main__":
 
     print("📦 Exporting to ONNX format...")
     # Export to ONNX
-    onnx_output_file = "./models/detector_best.onnx"
-
-    output_names = ['output0', 'output1'] if isinstance(model, SegmentationModel) else ['output0']
-    export_to_onnx(model, dummy_input, onnx_output_file, output_names=output_names)
+    onnx_output_file = "./models/detector_best"
+    export_to_onnx(model, dummy_input, onnx_output_file + ".onnx")
 
     if config["simplify_onnx"]:
         print("🔧 Simplifying ONNX file...")
         # Simplify ONNX file
-        onnx_model = onnx.load(onnx_output_file)
+        onnx_model = onnx.load(onnx_output_file + ".onnx")
+        onnx.checker.check_model(onnx_model)
         onnx_model_simp, check = simplify(onnx_model)
         assert check, "Simplified ONNX model could not be validated"
-        onnx.save(onnx_model_simp, onnx_output_file + "_simplified")
+        onnx.save(onnx_model_simp, onnx_output_file + "_simplified.onnx")
 
     if config["optimize_onnx"]:
         print("🔧 Optimizing ONNX file...")
         # Optimize ONNX file
-        onnx_model = onnx.load(onnx_output_file)
+        onnx_model = onnx.load(onnx_output_file + "_simplified.onnx")
         passes = ["fuse_bn_into_conv", "fuse_add_bias_into_conv"]
         onnx_model_opt = optimize(onnx_model, passes)
-        onnx.save(onnx_model_opt, onnx_output_file + "_simplified" + "_optimized")
+        onnx.save(onnx_model_opt, onnx_output_file + "_simplified" + "_optimized.onnx")
 
     print("✅ Model exported successfully! 🎉")

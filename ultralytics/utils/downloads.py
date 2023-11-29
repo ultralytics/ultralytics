36ecad8e8a67b47b196933f62eae31d9b81939ad
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-11-29 20:55:54 +0100

    Update
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/utils/downloads.py
+++ ultralytics/utils/downloads.py
@@ -351,7 +351,7 @@ def get_github_assets(repo='ultralytics/assets', version='latest', retry=False):
     return data['tag_name'], [x['name'] for x in data['assets']]  # tag, assets
 
 
-def attempt_download_asset(file, repo='ultralytics/assets', release='v0.0.0'):
+def attempt_download_asset(file, repo='ultralytics/assets', release='v0.0.0', **kwargs):
     """
     Attempt file download from GitHub release assets if not found locally.
 
@@ -370,23 +370,24 @@ def attempt_download_asset(file, repo='ultralytics/assets', release='v0.0.0'):
     else:
         # URL specified
         name = Path(parse.unquote(str(file))).name  # decode '%2F' to '/' etc.
+        download_url = f'https://github.com/{repo}/releases/download'
         if str(file).startswith(('http:/', 'https:/')):  # download
             url = str(file).replace(':/', '://')  # Pathlib turns :// -> :/
             file = url2file(name)  # parse authentication https://url.com/file.txt?auth...
             if Path(file).is_file():
                 LOGGER.info(f'Found {clean_url(url)} locally at {file}')  # file already exists
             else:
-                safe_download(url=url, file=file, min_bytes=1E5)
+                safe_download(url=url, file=file, min_bytes=1E5, **kwargs)
 
         elif repo == GITHUB_ASSETS_REPO and name in GITHUB_ASSETS_NAMES:
-            safe_download(url=f'https://github.com/{repo}/releases/download/{release}/{name}', file=file, min_bytes=1E5)
+            safe_download(url=f'{download_url}/{release}/{name}', file=file, min_bytes=1E5, **kwargs)
 
         else:
             tag, assets = get_github_assets(repo, release)
             if not assets:
                 tag, assets = get_github_assets(repo)  # latest release
             if name in assets:
-                safe_download(url=f'https://github.com/{repo}/releases/download/{tag}/{name}', file=file, min_bytes=1E5)
+                safe_download(url=f'{download_url}/{tag}/{name}', file=file, min_bytes=1E5, **kwargs)
 
         return str(file)
 

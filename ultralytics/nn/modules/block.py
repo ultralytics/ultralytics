888b61849810f0f0bac3a2c55a9c205a499a8a29
Ovide Academy <ovide@staff.uns.ac.id>
2023-11-29 18:41:01 +0700

    update

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -14,7 +14,7 @@ from .transformer import CrossDATransformerBlock
 
 __all__ = ('DFL', 'HGBlock', 'HGStem', 'SPP', 'SPPF', 'C1', 'C2', 'C3', 'C2f', 'C3x', 'C3TR', 'C3Ghost',
            'GhostBottleneck', 'Bottleneck', 'BottleneckCSP', 'Proto', 'RepC3'
-           ,'FusedMBConv','MBConv', 'SABottleneck', 'sa_layer', 'C3SA', 'LightC3x', 'C3xTR', 'C2HG', 'C3xHG', 'C2fx', 'C2TR', 'C3CTR')
+           ,'FusedMBConv','MBConv', 'SABottleneck', 'sa_layer', 'C3SA', 'LightC3x', 'C3xTR', 'C2HG', 'C3xHG', 'C2fx', 'C2TR', 'C3CTR', 'C2DfConv')
 
 class sa_layer(nn.Module):
     """Constructs a Channel Spatial Group module.
@@ -750,3 +750,28 @@ class C3CTR(C3):
         self.m = CrossDATransformerBlock(c_, c_, num_heads, n, n_levels=n_levels, n_points=n_points)
 
 
+class C2DfConv(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
+        expansion.
+        """
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = DfConv(c1, 2 * self.c, 1, 1)  # Replaced Conv with DfConv
+        self.cv2 = DfConv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2), Replaced Conv with DfConv
+        # Here, Bottleneck should be compatible with DfConv if it uses convolution internally
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer using Deformable Convolution."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk() with Deformable Convolution."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))

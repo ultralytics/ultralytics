6e8a71aced43ab9790f84b2cc4d7e7a875b53d12
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-11-29 21:02:21 +0100

    Update
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -666,7 +666,7 @@ class Exporter:
         # Download onnx2tf calibration file https://github.com/PINTO0309/onnx2tf/issues/545
         onnx2tf_file = Path('calibration_image_sample_data_20x128x128x3_float32.npy')
         if not onnx2tf_file.exists():
-            attempt_download_asset(f'{onnx2tf_file}.zip', unzip=True, download=True)
+            attempt_download_asset(f'{onnx2tf_file}.zip', unzip=True, delete=True)
 
         # Export to ONNX
         self.args.simplify = True

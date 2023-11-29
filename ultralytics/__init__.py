9c6989a1b0d0b94c51eacf7cb123c028145b224e
Maksym Shamrai <vortmanmax@gmail.com>
2023-11-29 03:29:15 +0200

    `ultralytics 8.0.220` fix UnboundLocalError in tuner.py (#6614)
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,6 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = '8.0.219'
+__version__ = '8.0.220'
 
 from ultralytics.models import RTDETR, SAM, YOLO
 from ultralytics.models.fastsam import FastSAM

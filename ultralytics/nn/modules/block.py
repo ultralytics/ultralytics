04b7d082b2e10c8cbec191547c19b09b8bf4ce6c
Ovide Academy <ovide@staff.uns.ac.id>
2023-11-30 16:26:51 +0700

    update triplet attention

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -546,99 +546,6 @@ class BottleneckCSP(nn.Module):
         y2 = self.cv2(x)
         return self.cv4(self.act(self.bn(torch.cat((y1, y2), 1))))
 
-### Triplet attention
-class BasicConv(nn.Module):
-    def __init__(
-        self,
-        in_planes,
-        out_planes,
-        kernel_size,
-        stride=1,
-        padding=0,
-        dilation=1,
-        groups=1,
-        relu=True,
-        bn=True,
-        bias=False,
-    ):
-        super(BasicConv, self).__init__()
-        self.out_channels = out_planes
-        self.conv = nn.Conv2d(
-            in_planes,
-            out_planes,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            dilation=dilation,
-            groups=groups,
-            bias=bias,
-        )
-        self.bn = (
-            nn.BatchNorm2d(out_planes, eps=1e-5, momentum=0.01, affine=True)
-            if bn
-            else None
-        )
-        self.relu = nn.ReLU() if relu else None
-
-    def forward(self, x):
-        x = self.conv(x)
-        if self.bn is not None:
-            x = self.bn(x)
-        if self.relu is not None:
-            x = self.relu(x)
-        return x
-
-
-class ChannelPool(nn.Module):
-    def forward(self, x):
-        return torch.cat(
-            (torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1
-        )
-
-
-class SpatialGate(nn.Module):
-    def __init__(self):
-        super(SpatialGate, self).__init__()
-        kernel_size = 7
-        self.compress = ChannelPool()
-        self.spatial = BasicConv(
-            2, 1, kernel_size, stride=1, padding=(kernel_size - 1) // 2, relu=False
-        )
-
-    def forward(self, x):
-        x_compress = self.compress(x)
-        x_out = self.spatial(x_compress)
-        scale = torch.sigmoid_(x_out)
-        return x * scale
-
-
-class TripletAttention(nn.Module):
-    def __init__(
-        self,
-        no_spatial=False,
-    ):
-        super(TripletAttention, self).__init__()
-        self.ChannelGateH = SpatialGate()
-        self.ChannelGateW = SpatialGate()
-        self.no_spatial = no_spatial
-        if not no_spatial:
-            self.SpatialGate = SpatialGate()
-
-    def forward(self, x):
-        x_perm1 = x.permute(0, 2, 1, 3).contiguous()
-        x_out1 = self.ChannelGateH(x_perm1)
-        x_out11 = x_out1.permute(0, 2, 1, 3).contiguous()
-        x_perm2 = x.permute(0, 3, 2, 1).contiguous()
-        x_out2 = self.ChannelGateW(x_perm2)
-        x_out21 = x_out2.permute(0, 3, 2, 1).contiguous()
-        if not self.no_spatial:
-            x_out = self.SpatialGate(x)
-            x_out = (1 / 3) * (x_out + x_out11 + x_out21)
-        else:
-            x_out = (1 / 2) * (x_out11 + x_out21)
-        return x_out
-
-
 class C2SA(nn.Module):
     """Faster Implementation of SABottleneck with 2 convolutions."""
 
@@ -894,6 +801,61 @@ class MBC2f(nn.Module):
 
     # ... (forward dan forward_split tetap sama)
 
+##Triplet Attention
+class BasicConv(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True, bn=True, bias=False):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+        self.bn = nn.BatchNorm2d(out_planes, eps=1e-5, momentum=0.01, affine=True) if bn else None
+        self.relu = nn.ReLU() if relu else None
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+class ChannelPool(nn.Module):
+    def forward(self, x):
+        return torch.cat((torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1)
+
+class SpatialGate(nn.Module):
+    def __init__(self, kernel_size=(3, 3)):
+        super(SpatialGate, self).__init__()
+        self.compress = ChannelPool()
+        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=[(k - 1) // 2 for k in kernel_size], relu=False)
+
+    def forward(self, x):
+        x_compress = self.compress(x)
+        x_out = self.spatial(x_compress)
+        scale = torch.sigmoid(x_out)
+        return x * scale
+
+class TripletAttention(nn.Module):
+    def __init__(self, no_spatial=False):
+        super(TripletAttention, self).__init__()
+        self.ChannelGateH = SpatialGate(kernel_size=(1, 3))
+        self.ChannelGateW = SpatialGate(kernel_size=(3, 1))
+        self.no_spatial = no_spatial
+        if not no_spatial:
+            self.SpatialGate = SpatialGate(kernel_size=(3, 3))
+
+    def forward(self, x):
+        x_perm1 = x.permute(0, 2, 1, 3).contiguous()
+        x_out1 = self.ChannelGateH(x_perm1)
+        x_out11 = x_out1.permute(0, 2, 1, 3).contiguous()
+        x_perm2 = x.permute(0, 3, 2, 1).contiguous()
+        x_out2 = self.ChannelGateW(x_perm2)
+        x_out21 = x_out2.permute(0, 3, 2, 1).contiguous()
+        if not self.no_spatial:
+            x_out = self.SpatialGate(x)
+            x_out = (1 / 3) * (x_out + x_out11 + x_out21)
+        else:
+            x_out = (1 / 2) * (x_out11 + x_out21)
+        return x_out
 
 
 ##C2F with Triplet Attention

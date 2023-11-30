1d2dea5a6624bd650024b93a1625379941dbc86e
Laughing-q <1185102784@qq.com>
2023-11-30 11:03:13 +0800

    fix obb plot

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -693,7 +693,7 @@ def output_to_rotated_target(output, max_det=300):
         j = torch.full((conf.shape[0], 1), i)
         targets.append(torch.cat((j, cls, box, angle, conf), 1))
     targets = torch.cat(targets, 0).numpy()
-    return targets[:, 0], targets[:, 1], ops.xywhr2xyxyxyxy(targets[:, 2:-1]), targets[:, -1]
+    return targets[:, 0], targets[:, 1], targets[:, 2:-1], targets[:, -1]
 
 
 def feature_visualization(x, module_type, stage, n=32, save_dir=Path('runs/detect/exp')):

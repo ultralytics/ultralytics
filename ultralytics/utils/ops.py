19ff2665d1d8b8b555e1968e9abc96856bbdfea4
Laughing-q <1185102784@qq.com>
2023-12-01 10:51:30 +0800

    fix batch_piou

--- ultralytics/utils/ops.py
+++ ultralytics/utils/ops.py
@@ -176,6 +176,7 @@ def nms_rotated(boxes, scores, threshold=0.45):
     if len(boxes) == 0:
         return np.empty((0, ), dtype=np.int8)
     sorted_idx = torch.argsort(scores, descending=True)
+    boxes = boxes[sorted_idx]
     ious = batch_probiou(boxes, boxes).triu_(diagonal=1)
     pick = torch.nonzero(ious.max(dim=0)[0] < threshold).squeeze_(-1)
     return sorted_idx[pick]

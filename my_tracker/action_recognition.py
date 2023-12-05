d2c5f24eeb704f9eee0d17a1789050fc10fec4ed
johnnynunez <johnnynuca14@gmail.com>
2023-12-05 13:16:00 +0100

    osb

--- my_tracker/action_recognition.py
+++ my_tracker/action_recognition.py
@@ -29,6 +29,11 @@ class ActionRecognizer:
         self.sr_enabled = config["suddenly_run"]["enabled"]
         self.sr_acceleration_threshold = config["suddenly_run"]["acceleration_threshold"]
 
+        self.osb_enabled = config["overstep_boundary"]["enabled"]
+        self.osb_line = config["overstep_boundary"]["line"]  # Coords: [x1, y1, x2, y2]
+        self.osb_direction = config["overstep_boundary"]["direction"]  # "up" or "down"
+        self.osb_distance_threshold = config["overstep_boundary"]["distance_threshold"]
+
     def recognize_frame(self, tracks, frame):
         """
         Recognizes actions in a frame.
@@ -46,6 +51,8 @@ class ActionRecognizer:
             ar_results["fast_approach"] = self.recognize_fast_approach(tracks, frame)
         if self.sr_enabled:
             ar_results["suddenly_run"] = self.recognize_suddenly_run(tracks)
+        if self.osb_enabled:
+            ar_results["overstepboundry"] = self.recognize_overstep_boundary(tracks)
 
         # TODO: merge individual actions so that we only loop and annotate once
         return ar_results
@@ -72,6 +79,9 @@ class ActionRecognizer:
         if self.sr_enabled:
             frame = self.annotate_suddenly_run(frame, ar_results["suddenly_run"])
 
+        if self.osb_enabled:
+            frame = self.annotate_overstep_boundary(frame, ar_results["overstepboundry"])
+
         return frame
 
     def recognize_gather(self, tracks):
@@ -97,7 +107,7 @@ class ActionRecognizer:
 
                 # If distance is below threshold and the ratio between the areas is within the threshold add pair
                 if distance <= self.g_distance_threshold and \
-                        (self.g_area_threshold <= a1/a2 <= (1/self.g_area_threshold)):
+                        (self.g_area_threshold <= a1 / a2 <= (1 / self.g_area_threshold)):
                     pairs.append([i, j])
 
         # Find independent chains in the graph
@@ -132,7 +142,7 @@ class ActionRecognizer:
         a1 = det1.tlwh[2] * det1.tlwh[3]
         a2 = det2.tlwh[2] * det2.tlwh[3]
         mean_area = (a1 + a2) / 2
-        return distance/np.sqrt(mean_area), a1, a2
+        return distance / np.sqrt(mean_area), a1, a2
 
     @staticmethod
     def get_independent_chains(pairs):
@@ -176,12 +186,14 @@ class ActionRecognizer:
 
     def recognize_stand_still(self, tracks):
         # TODO: also controlled by sv.STrack.frame_stride
+        if not tracks:  # Verificar si la lista de tracks está vacía
+            return None
         frame_stride = tracks[0].frame_stride
         ss_results = {}
         for track in tracks:
             if track.tracklet_len > frame_stride and track.class_ids == 0:
                 pixel_s, pixel_a, _ = self.get_motion_descriptors(track)
-                if pixel_s < self.ss_speed_threshold:   # TODO: condition on acceleration?
+                if pixel_s < self.ss_speed_threshold:  # TODO: condition on acceleration?
                     ss_results[track.track_id] = track.tlbr
         return ss_results if len(ss_results.keys()) > 0 else None
 
@@ -218,7 +230,7 @@ class ActionRecognizer:
 
         dX = np.diff(X)
         dY = np.diff(Y)
-        distance = np.sqrt(dX**2 + dY**2)
+        distance = np.sqrt(dX ** 2 + dY ** 2)
 
         # Speed
         speeds = distance / track.frame_stride
@@ -229,14 +241,14 @@ class ActionRecognizer:
         avg_acceleration = np.mean(acceleration)
         # Direction of movement in Y axis
         direction = np.mean(np.sign(dY))
-        #angles = np.arctan2(dY, dX)
-        #direction = np.degrees(np.mean(angles))
+        # angles = np.arctan2(dY, dX)
+        # direction = np.degrees(np.mean(angles))
 
         return avg_speed, avg_acceleration, direction
 
     def recognize_fast_approach(self, tracks, frame):
-        valid_classes = [0, 1, 2]   # personnel, car, truck
-        interest_point = np.array([frame.shape[1]//2, frame.shape[0]])  # bottom center of the frame
+        valid_classes = [0, 1, 2]  # personnel, car, truck
+        interest_point = np.array([frame.shape[1] // 2, frame.shape[0]])  # bottom center of the frame
 
         fa_results = {}
         for track in tracks:
@@ -248,10 +260,10 @@ class ActionRecognizer:
                     dy = max(abs(track.mean[1] - interest_point[1]) - track.mean[3] / 2, 0)
                     distace_to_interest_point = np.sqrt(dx ** 2 + dy ** 2)
                     # Threshold determined the distance to the bottom of the frame
-                    if distace_to_interest_point < (interest_point[1]/self.fa_distance_threshold):
-                    # TODO: cars have bigger area, so they are considered to be closer to the interest point
-                    #distace_to_interest_point = np.sqrt(np.sum((track.mean[0:2] - interest_point) ** 2))/np.sqrt(track.tlwh[2] * track.tlwh[3])
-                    #if distace_to_interest_point < self.fa_distance_threshold:
+                    if distace_to_interest_point < (interest_point[1] / self.fa_distance_threshold):
+                        # TODO: cars have bigger area, so they are considered to be closer to the interest point
+                        # distace_to_interest_point = np.sqrt(np.sum((track.mean[0:2] - interest_point) ** 2))/np.sqrt(track.tlwh[2] * track.tlwh[3])
+                        # if distace_to_interest_point < self.fa_distance_threshold:
                         fa_results[track.track_id] = track.tlbr
         return fa_results if len(fa_results.keys()) > 0 else None
 
@@ -288,7 +300,7 @@ class ActionRecognizer:
                     thickness=2,
                 )
 
-                text = f"G #{idx+1}"
+                text = f"G #{idx + 1}"
                 text_width, text_height = cv2.getTextSize(
                     text=text,
                     fontFace=font,
@@ -458,3 +470,99 @@ class ActionRecognizer:
 
         return frame
 
+    def is_overstep_boundary(self, track):
+        bbox = track.tlbr
+        x1, y1, x2, y2 = bbox
+        bx_center, by_center = (x1 + x2) / 2, (y1 + y2) / 2
+
+        # Check if prev_states has at least one element
+        if not track.prev_states or len(track.prev_states) < 1:
+            return False
+
+        _,_, movement_direction = self.get_motion_descriptors(track)
+
+        # Calculate the cross product to determine the side of the line on which the bbox is located.
+        lx1, ly1, lx2, ly2 = self.osb_line
+        line_vec = np.array([lx2 - lx1, ly2 - ly1])
+        bbox_vec = np.array([bx_center - lx1, by_center - ly1])
+        cross_product = np.cross(line_vec, bbox_vec)
+
+        # Verify if the bbox has crossed the line based on the configured address
+        crossed = False
+        if self.osb_direction == "up":
+            crossed = cross_product < 0
+        elif self.osb_direction == "down":
+            crossed = cross_product > 0
+        else:
+            raise ValueError(f"Unknown boundary direction: {self.osb_direction}")
+
+        # Check if the object is moving in the desired direction and is close to the line.
+        if crossed:
+            if ((self.osb_direction == "down" and movement_direction > 0) or
+                    (self.osb_direction == "up" and movement_direction < 0)):
+                """# Calcular distancia al límite y normalizarla
+                height = frame_size[1]  # Altura de la imagen
+                distance_to_line = abs(by_center - self.osb_line[1]) / height
+                if distance_to_line < self.osb_distance_threshold:"""
+                return True
+
+        return False
+
+    def recognize_overstep_boundary(self, tracks):
+        osb_results = {}
+        for track in tracks:
+            # Check if the bbox crosses the boundary line
+            if self.is_overstep_boundary(track):
+                osb_results[track.track_id] = track.tlbr
+        return osb_results if len(osb_results.keys()) > 0 else None
+
+    @staticmethod
+    def annotate_overstep_boundary(frame, osb_results):
+        if osb_results is not None:
+            for idx, bbox in osb_results.items():
+                x1, y1, x2, y2 = bbox.astype(int)
+
+                cv2.rectangle(
+                    img=frame,
+                    pt1=(x1, y1),
+                    pt2=(x2, y2),
+                    color=(0, 0, 255),
+                    thickness=2,
+                )
+
+                text = "OSB"
+                text_width, text_height = cv2.getTextSize(
+                    text=text,
+                    fontFace=font,
+                    fontScale=font_scale,
+                    thickness=font_thickness,
+                )[0]
+
+                text_x = max(x1,
+                             min(x2 - text_width - 3 * text_padding, frame.shape[1] - text_width - 3 * text_padding))
+                text_y = max(y1 + text_height + 3 * text_padding, text_height + 3 * text_padding)
+
+                text_background_x1 = text_x - text_padding
+                text_background_y1 = text_y - text_height - 2 * text_padding
+                text_background_x2 = text_x + text_width + text_padding
+                text_background_y2 = text_y + text_padding
+
+                cv2.rectangle(
+                    img=frame,
+                    pt1=(text_background_x1, text_background_y1),
+                    pt2=(text_background_x2, text_background_y2),
+                    color=background_color,
+                    thickness=cv2.FILLED,
+                )
+                cv2.putText(
+                    img=frame,
+                    text=text,
+                    org=(text_x, text_y - text_padding),
+                    fontFace=font,
+                    fontScale=font_scale,
+                    color=text_color,
+                    thickness=font_thickness,
+                    lineType=cv2.LINE_AA,
+                )
+
+        return frame

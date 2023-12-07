249553932045a9f32dfa7b3d365e6ee0f9a1a060
yermandy <yermaand@gmail.com>
2023-12-07 13:41:37 +0100

    Disentangles detection head by removing additional concatenation

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -41,22 +41,28 @@ class Detect(nn.Module):
 
     def forward(self, x):
         """Concatenates and returns predicted bounding boxes and class probabilities."""
-        shape = x[0].shape  # BCHW
-        for i in range(self.nl):
-            x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
         if self.training:
+            for i in range(self.nl):
+                x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
             return x
-        elif self.dynamic or self.shape != shape:
+        
+        shape = x[0].shape  # BCHW
+        
+        box_head = [self.cv2[i](xi) for i, xi in enumerate(x)]
+        cls_head = [self.cv3[i](xi) for i, xi in enumerate(x)]
+
+        if self.dynamic or self.shape != shape:
+            self.anchors, self.strides = (x.transpose(0, 1) for x in make_anchors(box_head, self.stride, 0.5))
+            self.shape = shape
+
+        box_head = torch.cat([xi.view(shape[0], self.reg_max * 4, -1) for xi in box_head], 2)
+        cls_head = torch.cat([xi.view(shape[0], self.nc, -1) for xi in cls_head], 2)
+        
+        if self.dynamic or self.shape != shape:
             self.anchors, self.strides = (x.transpose(0, 1) for x in make_anchors(x, self.stride, 0.5))
             self.shape = shape
 
-        x_cat = torch.cat([xi.view(shape[0], self.no, -1) for xi in x], 2)
-        if self.export and self.format in ('saved_model', 'pb', 'tflite', 'edgetpu', 'tfjs'):  # avoid TF FlexSplitV ops
-            box = x_cat[:, :self.reg_max * 4]
-            cls = x_cat[:, self.reg_max * 4:]
-        else:
-            box, cls = x_cat.split((self.reg_max * 4, self.nc), 1)
-        dbox = dist2bbox(self.dfl(box), self.anchors.unsqueeze(0), xywh=True, dim=1) * self.strides
+        dbox = dist2bbox(self.dfl(box_head), self.anchors.unsqueeze(0), xywh=True, dim=1) * self.strides
 
         if self.export and self.format in ('tflite', 'edgetpu'):
             # Normalize xywh with image size to mitigate quantization error of TFLite integer models as done in YOLOv5:
@@ -67,7 +73,7 @@ class Detect(nn.Module):
             img_size = torch.tensor([img_w, img_h, img_w, img_h], device=dbox.device).reshape(1, 4, 1)
             dbox /= img_size
 
-        y = torch.cat((dbox, cls.sigmoid()), 1)
+        y = torch.cat((dbox, cls_head.sigmoid()), 1)
         return y if self.export else (y, x)
 
     def bias_init(self):

37b71b0cc62cc5ea112a99d4b58860813475bbdf
Muhammad Rizqi Nur <xstinky12@gmail.com>
2023-12-08 00:35:30 +0700

    Allow override cache and epochs when resume

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -573,7 +573,7 @@ class BaseTrainer:
                 resume = True
                 self.args = get_cfg(ckpt_args)
                 self.args.model = str(last)  # reinstate model
-                for k in 'imgsz', 'batch':  # allow arg updates to reduce memory on resume if crashed due to CUDA OOM
+                for k in 'imgsz', 'batch', 'cache', 'epochs':  # allow arg updates to reduce memory on resume if crashed due to CUDA OOM
                     if k in overrides:
                         setattr(self.args, k, overrides[k])
 

327426e8c82ed61db99275c1459a1f71337c4700
Laughing-q <1185102784@qq.com>
2023-12-13 17:08:07 +0800

    fix

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -553,7 +553,7 @@ def plot_images(images,
     annotator = Annotator(mosaic, line_width=round(fs / 10), font_size=fs, pil=True, example=names)
     for i in range(i + 1):
         x, y = int(w * (i // ns)), int(h * (i % ns))  # block origin
-        # annotator.rectangle([x, y, x + w, y + h], None, (255, 255, 255), width=2)  # borders
+        annotator.rectangle([x, y, x + w, y + h], None, (255, 255, 255), width=2)  # borders
         if paths:
             annotator.text((x + 5, y + 5), text=Path(paths[i]).name[:40], txt_color=(220, 220, 220))  # filenames
         if len(cls) > 0:

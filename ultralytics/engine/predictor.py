b5db6b3d5ca9fab0045d14876ba4ac5f36512b54
Laughing-q <1185102784@qq.com>
2023-12-15 10:58:38 +0800

    update

--- ultralytics/engine/predictor.py
+++ ultralytics/engine/predictor.py
@@ -281,7 +281,6 @@ class BasePredictor:
                                                     im0s) if not self.embed else self.postprocess_embeds(preds)
 
                 self.run_callbacks('on_predict_postprocess_end')
-
                 # Visualize, save, write results if embeddings are not being computed
                 n = len(im0s)
                 for i in range(n):
@@ -321,7 +320,7 @@ class BasePredictor:
             LOGGER.info(f'Speed: %.1fms preprocess, %.1fms inference, %.1fms postprocess per image at shape '
                         f'{(1, 3, *im.shape[2:])}' % t)
 
-        if self.args.save or self.args.save_txt or self.args.save_crop and not self.embed:
+        if (self.args.save or self.args.save_txt or self.args.save_crop) and not self.embed:
             nl = len(list(self.save_dir.glob('labels/*.txt')))  # number of labels
             s = f"\n{nl} label{'s' * (nl > 1)} saved to {self.save_dir / 'labels'}" if self.args.save_txt else ''
             LOGGER.info(f"Results saved to {colorstr('bold', self.save_dir)}{s}")

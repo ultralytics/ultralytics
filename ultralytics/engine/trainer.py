4386acf8547ed2be8c6a5ba0ab231cadcef808d8
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-12-19 22:10:02 +0100

    Update scheduler
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -403,7 +403,7 @@ class BaseTrainer:
             with warnings.catch_warnings():
                 warnings.simplefilter('ignore')  # suppress 'Detected lr_scheduler.step() before optimizer.step()'
                 if self.args.time:
-                    self.epochs = self.args.epochs = round(self.args.time / self.epoch_time)
+                    self.epochs = self.args.epochs = math.ceil(self.args.time * 3600 / self.epoch_time)
                     self._setup_scheduler()
                     self.scheduler.last_epoch = self.epoch  # do not move
                 self.scheduler.step()

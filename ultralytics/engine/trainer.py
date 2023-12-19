21622f8222c7ae1a8feeca2dedefff0687acf74e
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-12-19 22:46:30 +0100

    prevent negative LRs
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -194,7 +194,7 @@ class BaseTrainer:
         if self.args.cos_lr:
             self.lf = one_cycle(1, self.args.lrf, self.epochs)  # cosine 1->hyp['lrf']
         else:
-            self.lf = lambda x: (1 - x / self.epochs) * (1.0 - self.args.lrf) + self.args.lrf  # linear
+            self.lf = lambda x: max(1 - x / self.epochs, 0) * (1.0 - self.args.lrf) + self.args.lrf  # linear
         self.scheduler = optim.lr_scheduler.LambdaLR(self.optimizer, lr_lambda=self.lf)
 
     def _setup_ddp(self, world_size):

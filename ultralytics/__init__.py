1394c55eaa219d6de8e7434fa50e7844165d6bd6
ayush chaurasia <ayush.chaurarsia@gmail.com>
2023-12-28 22:22:39 +0530

    update

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -8,5 +8,6 @@ from ultralytics.models.nas import NAS
 from ultralytics.utils import SETTINGS as settings
 from ultralytics.utils.checks import check_yolo as checks
 from ultralytics.utils.downloads import download
+from ultralytics.data.explorer.explorer import Explorer
 
-__all__ = '__version__', 'YOLO', 'NAS', 'SAM', 'FastSAM', 'RTDETR', 'checks', 'download', 'settings'
+__all__ = '__version__', 'YOLO', 'NAS', 'SAM', 'FastSAM', 'RTDETR', 'checks', 'download', 'settings', 'Explorer'

93c6cc519b532b479dcb8296d197a7ba804c3da5
Ubaydah <64612602+Taoheed-O@users.noreply.github.com>
2024-01-01 18:07:27 +0100

    added two main folders

---
+++ examples/example_detector_app/functions/webcam.py
@@ -0,0 +1,41 @@
+import cv2
+import streamlit as st
+from functions.helper import  display_tracker_options, display_detected_frames
+from functions import settings
+
+
+
+# webcam function
+def play_webcam(conf, model):
+    """
+    Plays a webcam stream. Detects Objects in real-time using the YOLOv8 object detection model.
+
+    Parameters:
+        conf: Confidence of YOLOv8 model.
+        model: An instance of the `YOLOv8` class containing the YOLOv8 model.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    source_webcam = settings.WEBCAM_PATH
+    if st.sidebar.button('Detect Objects'):
+        try:
+            vid_cap = cv2.VideoCapture(source_webcam)
+            st_frame = st.empty()
+            while (vid_cap.isOpened()):
+                success, image = vid_cap.read()
+                if success:
+                    display_detected_frames(conf,
+                                             model,
+                                             st_frame,
+                                             image
+                                             )
+                else:
+                    vid_cap.release()
+                    break
+        except Exception as e:
+            st.sidebar.error("Error loading video: " + str(e))
+

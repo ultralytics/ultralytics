6218b820723b9e8647c582589c536c63c1f473f3
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-01-01 15:40:27 +0100

    `ultralytics 8.0.232` OpenCV 4.9.0 readonly np.array fix (#7256)

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,6 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = '8.0.231'
+__version__ = '8.0.232'
 
 from ultralytics.models import RTDETR, SAM, YOLO
 from ultralytics.models.fastsam import FastSAM

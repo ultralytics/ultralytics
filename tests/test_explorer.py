b785aef45bd0fc86753c5155c1341a39f74646d0
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2024-01-07 00:23:25 +0530

    `ultralytics 8.0.236` dataset semantic & SQL search API (#7136)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: Laughing-q <1182102784@qq.com>

---
+++ tests/test_explorer.py
@@ -0,0 +1,50 @@
+from ultralytics import Explorer
+
+
+def test_similarity():
+    exp = Explorer()
+    exp.create_embeddings_table()
+    similar = exp.get_similar(idx=1)
+    assert len(similar) == 25
+    similar = exp.get_similar(img='https://ultralytics.com/images/zidane.jpg')
+    assert len(similar) == 25
+    similar = exp.get_similar(idx=[1, 2], limit=10)
+    assert len(similar) == 10
+    sim_idx = exp.similarity_index()
+    assert len(sim_idx) > 0
+    sql = exp.sql_query("WHERE labels LIKE '%person%'")
+    len(sql) > 0
+
+
+def test_det():
+    exp = Explorer(data='coco8.yaml', model='yolov8n.pt')
+    exp.create_embeddings_table(force=True)
+    assert len(exp.table.head()['bboxes']) > 0
+    similar = exp.get_similar(idx=[1, 2], limit=10)
+    assert len(similar) > 0
+    # This is a loose test, just checks errors not correctness
+    similar = exp.plot_similar(idx=[1, 2], limit=10)
+    assert similar is not None
+    similar.show()
+
+
+def test_seg():
+    exp = Explorer(data='coco8-seg.yaml', model='yolov8n-seg.pt')
+    exp.create_embeddings_table(force=True)
+    assert len(exp.table.head()['masks']) > 0
+    similar = exp.get_similar(idx=[1, 2], limit=10)
+    assert len(similar) > 0
+    similar = exp.plot_similar(idx=[1, 2], limit=10)
+    assert similar is not None
+    similar.show()
+
+
+def test_pose():
+    exp = Explorer(data='coco8-pose.yaml', model='yolov8n-pose.pt')
+    exp.create_embeddings_table(force=True)
+    assert len(exp.table.head()['keypoints']) > 0
+    similar = exp.get_similar(idx=[1, 2], limit=10)
+    assert len(similar) > 0
+    similar = exp.plot_similar(idx=[1, 2], limit=10)
+    assert similar is not None
+    similar.show()

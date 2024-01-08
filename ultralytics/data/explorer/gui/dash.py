a75a77f7dbcb8dd243ceef851397af730eb81c7f
ayush chaurasia <ayush.chaurarsia@gmail.com>
2024-01-09 00:02:35 +0530

    update

--- ultralytics/data/explorer/gui/dash.py
+++ ultralytics/data/explorer/gui/dash.py
@@ -144,10 +144,10 @@ def utralytics_explorer_docs_callback():
         st.image('https://raw.githubusercontent.com/ultralytics/assets/main/logo/Ultralytics_Logotype_Original.svg',
                  width=100)
         st.markdown(
-            "<p>This demo is built using Ultralytics Explorer API. Visit <a href=''>API docs</a> to try examples & learn more</p>",
+            "<p>This demo is built using Ultralytics Explorer API. Visit <a href='https://docs.ultralytics.com/datasets/explorer/'>API docs</a> to try examples & learn more</p>",
             unsafe_allow_html=True,
             help=None)
-        st.link_button('Ultrlaytics Explorer API', 'https://docs.ultralytics.com/')
+        st.link_button('Ultrlaytics Explorer API', "https://docs.ultralytics.com/datasets/explorer/")
 
 
 def layout():

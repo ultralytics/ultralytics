e1b1c4f5ed31a9a3dc7b34e196abf8d1e9f45a19
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-01-10 02:47:02 +0100

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -302,8 +302,8 @@ class GhostBottleneck(nn.Module):
         self.conv = nn.Sequential(
             GhostConv(c1, c_, 1, 1),  # pw
             DWConv(c_, c_, k, s, act=False) if s == 2 else nn.Identity(),  # dw
-            GhostConv(c_, c2, 1, 1, act=False),
-        )  # pw-linear
+            GhostConv(c_, c2, 1, 1, act=False),  # pw-linear
+        )
         self.shortcut = (
             nn.Sequential(DWConv(c1, c1, k, s, act=False), Conv(c1, c2, 1, 1, act=False)) if s == 2 else nn.Identity()
         )

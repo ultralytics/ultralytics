ea60c7385dc3acb5e3a3dec1053e964725a2d986
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-01-10 02:53:57 +0100

    Update ops.py

--- ultralytics/utils/ops.py
+++ ultralytics/utils/ops.py
@@ -76,10 +76,8 @@ def segment2box(segment, width=640, height=640):
     # Convert 1 segment label to 1 box label, applying inside-image constraint, i.e. (xy1, xy2, ...) to (xyxy)
     x, y = segment.T  # segment xy
     inside = (x >= 0) & (y >= 0) & (x <= width) & (y <= height)
-    (
-        x,
-        y,
-    ) = x[inside], y[inside]
+    x = x[inside]
+    y = y[inside]
     return (
         np.array([x.min(), y.min(), x.max(), y.max()], dtype=segment.dtype)
         if any(x)

643ff2c7c37c4b5b406887c78787584153f4a5c9
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-01-12 15:45:43 +0100

    Docs, Solutions and Export updates

--- ultralytics/utils/benchmarks.py
+++ ultralytics/utils/benchmarks.py
@@ -87,8 +87,8 @@ def benchmark(
             assert i != 9 or LINUX, "Edge TPU export only supported on Linux"
             if i == 10:
                 assert MACOS or LINUX, "TF.js export only supported on macOS and Linux"
-            elif i == 11:
-                assert sys.version_info < (3, 11), "PaddlePaddle export only supported on Python<=3.10"
+            # elif i == 11:
+            #     assert sys.version_info < (3, 11), "PaddlePaddle export only supported on Python<=3.10"
             if "cpu" in device.type:
                 assert cpu, "inference not supported on CPU"
             if "cuda" in device.type:

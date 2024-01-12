b131dd2de10f6623bf64d16f364be04dca31e651
UltralyticsAssistant <web@ultralytics.com>
2024-01-12 12:38:57 +0000

    Auto-format by Ultralytics actions

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -177,17 +177,20 @@ def fuse_nd_conv_and_nd_bn(conv: nn.Module, bn: nn.Module, is_transposed: bool, 
     :param ndim: Number of dim
     :return: Fused convolution layer.
     """
-    fused_conv = getattr(
-        nn, f"ConvTranspose{ndim}d" if is_transposed else f"Conv{ndim}d")(
-        conv.in_channels,
-        conv.out_channels,
-        kernel_size=conv.kernel_size,
-        stride=conv.stride,
-        padding=conv.padding,
-        dilation=conv.dilation,
-        groups=conv.groups,
-        bias=True,
-    ).requires_grad_(False).to(conv.weight.device)
+    fused_conv = (
+        getattr(nn, f"ConvTranspose{ndim}d" if is_transposed else f"Conv{ndim}d")(
+            conv.in_channels,
+            conv.out_channels,
+            kernel_size=conv.kernel_size,
+            stride=conv.stride,
+            padding=conv.padding,
+            dilation=conv.dilation,
+            groups=conv.groups,
+            bias=True,
+        )
+        .requires_grad_(False)
+        .to(conv.weight.device)
+    )
 
     # Prepare filters
     w_conv = conv.weight.clone().view(conv.out_channels, -1)
@@ -202,18 +205,17 @@ def fuse_nd_conv_and_nd_bn(conv: nn.Module, bn: nn.Module, is_transposed: bool, 
     return fused_conv
 
 
-def fuse_conv_and_bn(
-        conv: nn.Module,
-        bn: nn.Module
-) -> nn.Module:
+def fuse_conv_and_bn(conv: nn.Module, bn: nn.Module) -> nn.Module:
     """
     Fuse convolution and batch normalization layers.
+
     :param conv: Convolution layer
     :param bn: Batch normalization layer
     :return: Fused convolution layer.
     """
     if not isinstance(
-            conv, (nn.Conv1d, nn.Conv2d, nn.Conv3d, nn.ConvTranspose1d, nn.ConvTranspose2d, nn.ConvTranspose3d)):
+        conv, (nn.Conv1d, nn.Conv2d, nn.Conv3d, nn.ConvTranspose1d, nn.ConvTranspose2d, nn.ConvTranspose3d)
+    ):
         raise ValueError(
             f"Error: Expected 'conv' to be an instance of nn.ConvNd or nn.ConvTransposeNd, but got {type(conv).__name__}."
         )
@@ -224,8 +226,10 @@ def fuse_conv_and_bn(
     conv_dim = getattr(type(conv), "__name__")[-2]
     bn_dim = getattr(type(bn), "__name__")[-2]
     if conv_dim is None or bn_dim is None or conv_dim != bn_dim:
-        raise ValueError(f"Error: Convolution and Batch Normalization types do not match. "
-                         f"Convolution type: {type(conv).__name__}, BN type: {type(bn).__name__}.")
+        raise ValueError(
+            f"Error: Convolution and Batch Normalization types do not match. "
+            f"Convolution type: {type(conv).__name__}, BN type: {type(bn).__name__}."
+        )
 
     is_transposed = True if "Transpose" in name else False
     fused_conv = fuse_nd_conv_and_nd_bn(conv, bn, is_transposed, int(conv_dim))

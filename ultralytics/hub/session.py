1d09d88d6825da15401c46da59ea79a05ea18a56
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-01-15 21:11:24 +0100

    add version constraint
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/hub/session.py
+++ ultralytics/hub/session.py
@@ -43,7 +43,7 @@ class HUBTrainingSession:
             ValueError: If the provided model identifier is invalid.
             ConnectionError: If connecting with global API key is not supported.
         """
-        checks.check_requirements("hub_sdk")
+        checks.check_requirements("hub-sdk>=0.0.2")
         from hub_sdk import HUBClient
 
         self.rate_limits = {

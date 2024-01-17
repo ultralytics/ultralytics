90d786b1c2250c1a35828c15f9ac19b1b3f199b0
Gudbrand Tandberg <gudbrand@3lc.ai>
2024-01-17 20:23:31 +0100

    Implement build_dataset

--- ultralytics/utils/tlc_trainer.py
+++ ultralytics/utils/tlc_trainer.py
@@ -4,7 +4,8 @@ from ultralytics.utils import (
     DEFAULT_CFG, LOGGER,)
 from ultralytics.utils.tlc_utils import tlc_check_dataset
 from ultralytics.utils.tlc_validator import TLCDetectionValidator
-
+from ultralytics.utils.torch_utils import de_parallel
+from ultralytics.data import build_yolo_dataset
 class TLCDetectionTrainer(DetectionTrainer):
     """A class extending the BaseTrainer class for training a detection model using the 3LC."""
 
@@ -12,15 +13,20 @@ class TLCDetectionTrainer(DetectionTrainer):
         LOGGER.info("Using 3LC Trainer 🌟")
         super().__init__(cfg, overrides, _callbacks)
 
-    @staticmethod
-    def get_dataset(data):
-        """
-        Get train, val path from data dict if it exists.
 
-        Returns None if data format is not recognized.
+    def build_dataset(self, img_path, mode="train", batch=None):
         """
-        tables = tlc_check_dataset(data["yaml_file"], get_splits=["train", "val"])
-        return data["train"], data.get("val") or data.get("test")
+        Build YOLO Dataset.
+
+        Args:
+            img_path (str): Path to the folder containing images.
+            mode (str): `train` mode or `val` mode, users are able to customize different augmentations for each mode.
+            batch (int, optional): Size of batches, this is for `rect`. Defaults to None.
+        """
+        gs = max(int(de_parallel(self.model).stride.max() if self.model else 0), 32)
+        tables = tlc_check_dataset(self.data["yaml_file"], get_splits=["train", "val"])
+        return build_yolo_dataset(self.args, img_path, batch, self.data, mode=mode, rect=mode == "val", stride=gs)
+
 
     def get_validator(self):
         """Returns a DetectionValidator for YOLO model validation."""

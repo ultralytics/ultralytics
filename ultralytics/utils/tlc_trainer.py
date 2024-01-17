42a1112d38666d11b645e6f4eb0ba7e00b285016
Gudbrand Tandberg <gudbrand@3lc.ai>
2024-01-17 19:26:59 +0100

    Add TLCDetectionValidator to TLCDetectionTrainer

--- ultralytics/utils/tlc_trainer.py
+++ ultralytics/utils/tlc_trainer.py
@@ -1,7 +1,8 @@
+import copy
 from ultralytics.models.yolo.detect import DetectionTrainer
 from ultralytics.utils import (
     DEFAULT_CFG, LOGGER,)
-
+from ultralytics.utils.tlc_validator import TLCDetectionValidator
 
 class TLCDetectionTrainer(DetectionTrainer):
     """A class extending the BaseTrainer class for training a detection model using the 3LC."""
@@ -18,3 +19,10 @@ class TLCDetectionTrainer(DetectionTrainer):
         Returns None if data format is not recognized.
         """
         return data["train"], data.get("val") or data.get("test")
+
+    def get_validator(self):
+        """Returns a DetectionValidator for YOLO model validation."""
+        self.loss_names = "box_loss", "cls_loss", "dfl_loss"
+        return TLCDetectionValidator(
+            self.test_loader, save_dir=self.save_dir, args=copy(self.args), _callbacks=self.callbacks
+        )

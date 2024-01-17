fde5212f096917e06122b9e54a1cebcfe37ae756
Markus Rognlien <markus.rognlien@3lc.ai>
2024-01-17 20:53:59 +0100

    fix mistake

--- ultralytics/utils/tlc_dataset.py
+++ ultralytics/utils/tlc_dataset.py
@@ -5,7 +5,7 @@ from tlc import Table, Url
 from tlc.core.builtins.constants.column_names import IMAGE
 import numpy as np
 import tlc
-from ultralytics.data import build_yolo_dataset
+from ultralytics.utils import colorstr
 
 def unpack_box(bbox):
     return [bbox[tlc.LABEL], bbox[tlc.X0], bbox[tlc.Y0], bbox[tlc.X1], bbox[tlc.Y1]]
@@ -17,8 +17,27 @@ def tlc_table_row_to_yolo_label(row):
         arr = arr.reshape(0, 5)
     return arr
 
-def build_tlc_dataset(table, **kwargs):
-    return build_yolo_dataset(table=table, **kwargs)
+def build_tlc_dataset(cfg, img_path, batch, data, mode="train", rect=False, stride=32, table=None):
+    """Build TLC Dataset."""
+    assert table is not None
+    return TLCDataset(
+        img_path=img_path,
+        imgsz=cfg.imgsz,
+        batch_size=batch,
+        augment=mode == "train",  # augmentation
+        hyp=cfg,  # TODO: probably add a get_hyps_from_cfg function
+        rect=cfg.rect or rect,  # rectangular batches
+        cache=cfg.cache or None,
+        single_cls=cfg.single_cls or False,
+        stride=int(stride),
+        pad=0.0 if mode == "train" else 0.5,
+        prefix=colorstr(f"{mode}: "),
+        task=cfg.task,
+        classes=cfg.classes,
+        data=data,
+        fraction=cfg.fraction if mode == "train" else 1.0,
+        table=table,
+    )
 
 class TLCDataset(YOLODataset):
     def __init__(self, *args, data=None, task="detect", table: Table, **kwargs):

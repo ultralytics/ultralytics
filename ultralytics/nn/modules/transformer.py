f67ab0fdd4d0920249305137049b1be45251664b
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-01-21 00:37:22 +0100

    Fix RTDETR warnings
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/nn/modules/transformer.py
+++ ultralytics/nn/modules/transformer.py
@@ -93,18 +93,18 @@ class AIFI(TransformerEncoderLayer):
     def forward(self, x, *args):
         """Forward pass for the AIFI transformer layer."""
         c, h, w = x.shape[1:]
-        assert c % 4 == 0, "Embed dimension must be divisible by 4 for 2D sin-cos position embedding"
         pos_embed = self.build_2d_sincos_position_embedding(w, h, c)
         # Flatten [B, C, H, W] to [B, HxW, C]
         x = super().forward(x.flatten(2).permute(0, 2, 1), pos=pos_embed.to(device=x.device, dtype=x.dtype))
         return x.permute(0, 2, 1).view([-1, c, h, w]).contiguous()
 
     @staticmethod
-    def build_2d_sincos_position_embedding(w, h, embed_dim=256, temperature=10000.0):
+    def build_2d_sincos_position_embedding(w, h, embed_dim: int = 256, temperature: float = 10000.0):
         """Builds 2D sine-cosine position embedding."""
         grid_w = torch.arange(w, dtype=torch.float32)
         grid_h = torch.arange(h, dtype=torch.float32)
         grid_w, grid_h = torch.meshgrid(grid_w, grid_h, indexing="ij")
+        assert embed_dim % 4 == 0, "Embed dimension must be divisible by 4 for 2D sin-cos position embedding"
         pos_dim = embed_dim // 4
         omega = torch.arange(pos_dim, dtype=torch.float32) / pos_dim
         omega = 1.0 / (temperature**omega)

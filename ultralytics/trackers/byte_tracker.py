a19e0f84aac72f5ffbec06459effd0874342f336
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-01-23 00:00:15 +0100

    Add docstring.

--- ultralytics/trackers/byte_tracker.py
+++ ultralytics/trackers/byte_tracker.py
@@ -200,6 +200,7 @@ class STrack(BaseTrack):
 
     @property
     def result(self):
+        """Get current tracking results."""
         coords = self.xyxy if self.angle is None else self.xywha
         return coords.tolist() + [self.track_id, self.score, self.cls, self.idx]
 

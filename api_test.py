56fa12b85151fab5461b8ee4f2f05ee61e81f4ed
UltralyticsAssistant <web@ultralytics.com>
2024-01-26 15:30:30 +0000

    Auto-format by https://ultralytics.com/actions

--- api_test.py
+++ api_test.py
@@ -1,5 +1,6 @@
 from ultralytics.data.explorer.explorer import Explorer
+
 exp = Explorer()
 exp.create_embeddings_table()
 
-print(exp.label_count(["person","dog"]))
+print(exp.label_count(["person", "dog"]))

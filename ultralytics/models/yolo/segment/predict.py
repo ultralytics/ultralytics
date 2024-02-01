374c0d0301dc91a70a22edc1061877403d555c0d
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-02-01 23:15:52 +0100

    Update predict.py

--- ultralytics/models/yolo/segment/predict.py
+++ ultralytics/models/yolo/segment/predict.py
@@ -41,9 +41,7 @@ class SegmentationPredictor(DetectionPredictor):
             orig_imgs = ops.convert_torch2numpy_batch(orig_imgs)
 
         results = []
-        proto = (
-            preds[1][-1] if isinstance(preds[1], tuple) else preds[1]
-        )  # second output is tuple if pt, but an array if exported
+        proto = preds[1][-1] if isinstance(preds[1], tuple) else preds[1]  # second output is tuple if pt, but array if exported
         for i, pred in enumerate(p):
             orig_img = orig_imgs[i]
             img_path = self.batch[0][i]

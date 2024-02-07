12625ed5d0a7ac8239a338dd794b3ccae7e98f90
Frederik Mellbye <frederik.mellbye@3lc.ai>
2024-02-07 13:48:50 +0100

    Remove one main.py

--- ultralytics/main.py
+++
@@ -1,7 +0,0 @@
-print("Running")
-
-from ultralytics.utils.tlc import TLCYOLO  # noqa: E402
-
-model = TLCYOLO('yolov8n.pt')  # initialize
-
-results = model.train(data='coco128.yaml', model='yolov8n.pt', epochs=2, batch=64, imgsz=320)

dc04635d70981087cff30661f5f8fc1ea1fddeba
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-02-09 02:32:51 +0100

    Update __init__.py

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,6 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = "8.1.10"
+__version__ = "8.1.11"
 
 from ultralytics.data.explorer.explorer import Explorer
 from ultralytics.models import RTDETR, SAM, YOLO

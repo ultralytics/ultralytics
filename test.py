5dddfb7d275ae6922bf2deb99e417df327576893
vemundtl <vemund@thallaug.org>
2024-02-09 16:32:59 +0100

    split dataset

---
+++ test.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    import fiftyone as fo
+    from ultralytics.utils.custom_utils.helpers import get_fiftyone_dataset
+
+    dataset, classes = get_fiftyone_dataset(0)
+    sesh = fo.launch_app(dataset)
--- ultralytics/config.py
+++ ultralytics/config.py
@@ -10,106 +10,86 @@ def get_git_root():
         return None
 
 ROOT_DIR = get_git_root()
-DATASET_NAME = "My_dataset"
-DATA_PATH = f'{ROOT_DIR}/data'
-# DATA_PATH = "/media/vemund-sigurd/aa7e1253-187b-48d4-af32-eb9f050db5dd/vemund-sigurd/data/new_data"
-# DATA_PATH = "/cluster/home/vemundtl/ultralytics/data"
+# DATASET_NAME = "My_dataset"
+DATASET_NAME = "My_dataset_new"
+DATA_PATH = f'{ROOT_DIR}/data_new'
 LABELS_PATH = f'{DATA_PATH}/labels/labels.json'
-TRAIN_DATA_PATH = f'{DATA_PATH}/train_split'
 TEST_DATA_PATH = f'{DATA_PATH}/test_split'
-VAL_DATA_PATH = f'{DATA_PATH}/val_split'
 TEST_SET_DESCRIPTION = f'{ROOT_DIR}/test.yaml'
 DATASET_DESCRIPTION = f'{DATA_PATH}/data.yaml'
 PLOTS_PATH = f'{ROOT_DIR}/plots'
 OS = sys.platform
 
-CLASSES_TO_KEEP = [
-    'grandiflora',
-    'Tynn vifteformet, traktformet_svamp',
-    'sjopolse',
-    'sjofaer',
-    'tare',
-    'blomkalkorall',
-    'sjostjerne',
-    'Massiv rund, tjukk skalformet, poros bulkeformet_svamp',
-    'skorpe',
-    'liten piperenser',
-    'anemone',
-    'finger_svamp',
-    'risengrynkorall',
-    'fisk',
-    'vortesvamp'
-]
-
-CLASSES_MAPPING = {
-    "liten piperenser": "liten piperenser",
-    "Massiv rund, tjukk skalformet, poros bulkeformet_svamp": "MRTSPB_svamp",
-    "blomkalkorall": "blomkalkorall",
-    "sjostjerne": "sjostjerne",
-    "anemone": "anemone",
-    "Tynn vifteformet, traktformet_svamp": "TVT_svamp",
-    "vortesvamp": "vortesvamp",
-    "fisk": "fisk",
-    "finger_svamp": "finger_svamp",
-    "risengrynkorall": "risengrynkorall",
-    "tare": "tare",
-    "sjopolse": "sjopolse",
-    "skorpe": "skorpe",
-    "sjofaer": "sjofaer",
-    "grandiflora": "grandiflora",
-}
 
 DATASET_MEAN = (0.5227, 0.5336, 0.3736)
 DATASET_STD = (0.2749, 0.2483, 0.1730)
 
-MAX_BATCH = {
-    "FasterRCNN_resnet50": 3,
-    "FasterRCNN_resnet50_2": 2,
-    "FasterRCNN_vitdet": 2,
-    "FasterRCNN_squeezenet1_1": 8,
-    "FasterRCNN_convnext_tiny": 2,
-    "RetinaNet_resnet50": 4,
-    "MaskRCNN_resnet50": 2,
-    "MaskRCNN_convnext_tiny": 2,
-    "yolo-nas": 8,
-    }
-
-#### EPOCHS ####
-# Resnet50 -      10  min per epoch
-# Squeezenet1_1 - 3   min per epoch 
-# Vitdet -        12  min per epoch
-# Retinanet -     8   min per epoch
-# Convnext Faster -      25  min per epoch
-# YOLOv8 -        3-4 min per epoch
-# RTDETR -        30  min per epoch
-# MASK RCNN -     13  min per epoch
-
 ORIGINAL_CLASSES = [
-    "anemone",
-    "Massiv rund, tjukk skalformet, poros bulkeformet_svamp",
-    "fisk",
-    "blomkalkorall",
-    "vortesvamp",
-    "sjostjerne",
-    "finger_svamp",
-    "risengrynkorall",
-    "Tynn vifteformet, traktformet_svamp",
-    "steinbit",
-    "flyndre",
-    "krabbe",
-    "liten piperenser",
-    "sjofaer",
-    "skorpe",
-    "mysterie",
-    "sjopolse",
-    "krepsdyr",
-    "stor piperenser",
-    "tare",
-    "krakebolle",
-    "snegle",
-    "soppel",
-    "fjarsjolilje",
-    "grandiflora"
+    'anemone',
+    'Massiv rund, tjukk skalformet, poros bulkeformet_svamp',
+    'fisk',
+    'blomkalkorall',
+    'vortesvamp',
+    'sjostjerne',
+    'finger_svamp',
+    'risengrynkorall',
+    'Tynn vifteformet, traktformet_svamp',
+    'steinbit',
+    'flyndre',
+    'krabbe',
+    'liten piperenser',
+    'sjofaer',
+    'skorpe',
+    'mysterie',
+    'sjopolse',
+    'krepsdyr',
+    'stor piperenser',
+    'tare',
+    'krakebolle',
+    'snegle',
+    'soppel',
+    'fjarsjolilje',
+    'grandiflora',
+    'bambuskorall',
+    'Skate',
+    'Sjøtre',
+    'stylocordyla',
+    'sjobusk',
+    'reirskjell',
+    'Ukjent korall',
+    'oyekorall'
+    ]
+
+CLASSES_MAPPING = {
+    "Massiv rund, tjukk skalformet, poros bulkeformet_svamp": "MRTSPB_svamp",
+    "Tynn vifteformet, traktformet_svamp": "TVT_svamp",
+}
+
+CLASSES_TO_KEEP = [
+    'anemone',
+    'Massiv rund, tjukk skalformet, poros bulkeformet_svamp',
+    'fisk',
+    'blomkalkorall',
+    'vortesvamp',
+    'sjostjerne',
+    'finger_svamp',
+    'risengrynkorall',
+    'Tynn vifteformet, traktformet_svamp',
+    'flyndre',
+    'liten piperenser',
+    'sjofaer',
+    'skorpe',
+    'sjopolse',
+    'krepsdyr',
+    'stor piperenser',
+    'tare',
+    'krakebolle',
+    'grandiflora',
+    'bambuskorall',
+    'Sjøtre',
+    'stylocordyla',
+    'sjobusk',
+    'oyekorall'
 ]
 
 def get_yolo_classes():

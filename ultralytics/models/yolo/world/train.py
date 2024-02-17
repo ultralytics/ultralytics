a3a7e9cbe3ff0d80a08f2fdf7c790caf9c11d6b2
Shubham Gupta <shubhamgupto@gmail.com>
2024-02-17 10:21:48 -0500

    recognize world trainer module

--- ultralytics/models/yolo/world/train.py
+++ ultralytics/models/yolo/world/train.py
@@ -92,7 +92,7 @@ class WorldTrainer(BaseTrainer):
 
     def get_validator(self):
         """Returns a DetectionValidator for YOLO model validation."""
-        self.loss_names = "box_loss", "cls_loss", "dfl_loss"
+        self.loss_names = "cov_mse_loss"
         return yolo.detect.DetectionValidator(
             self.test_loader, save_dir=self.save_dir, args=copy(self.args), _callbacks=self.callbacks
         )

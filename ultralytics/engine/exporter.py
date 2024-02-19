dbb5d3d3790cfbb1adad3bc53ceee1141e750a43
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-02-19 22:01:54 +0100

    Update constraint to >= from ==

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -722,7 +722,7 @@ class Exporter:
                 "onnxsim>=0.4.33",
                 "onnx_graphsurgeon>=0.3.26",
                 "tflite_support",
-                "flatbuffers==23.5.26",
+                "flatbuffers>=23.5.26",
                 # if ARM64
                 # else "",  # Fix an issue with flatbuffer version included inside tensorflow package
                 "onnxruntime-gpu" if cuda else "onnxruntime",

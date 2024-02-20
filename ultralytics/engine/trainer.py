fd5f6f0bf5e87b3e723493d57005e8d994fd5893
Alexander Suslov <alexander.suslov@intel.com>
2024-02-20 17:24:57 +0400

    replied to comments

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -252,7 +252,7 @@ class BaseTrainer:
             if any(x in k for x in freeze_layer_names):
                 LOGGER.info(f"Freezing layer '{k}'")
                 v.requires_grad = False
-            elif not v.requires_grad and torch.is_floating_point(v):
+            elif not v.requires_grad and v.dtype.is_floating_point:
                 LOGGER.info(
                     f"WARNING ⚠️ setting 'requires_grad=True' for frozen layer '{k}'. "
                     "See ultralytics.engine.trainer for customization of frozen layers."

6e24e22faa2a1c68c47329590dac96f0bd392c66
Vemund Thallaug Lund <vemund@thallaug.org>
2024-02-20 11:29:15 +0100

    new models + small changes

--- train.py
+++ train.py
@@ -1,21 +1,19 @@
 import subprocess
 import argparse
 import os
-import shutil
+import warnings
+
 from ultralytics.config import DATASET_DESCRIPTION, TEST_DATA_PATH
-# from ultralytics.utils.custom_utils.helpers import get_fiftyone_dataset
-# from predict import run_prediction
+from ultralytics.utils.custom_utils.helpers import copy_model_config
+
 
 def train(epochs, model, imgsz=640, device="0", batch_size=16, patience=30, save_dir="train"):
-
-    model_path = f'ultralytics/cfg/models/v8/{model}'
-
     command = [
         "yolo",
         "detect",
         "train",
         f"data={DATASET_DESCRIPTION}",
-        f"model={model_path}",
+        f"model={model}",
         f"epochs={epochs}",
         f"imgsz={imgsz}",
         f"device={device}",
@@ -30,33 +28,6 @@ def train(epochs, model, imgsz=640, device="0", batch_size=16, patience=30, save
         subprocess.run(command, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
-
-def copy_model_config(model, save_dir):
-    if model[:6] != "rtdetr":
-        model = model.split('-', 1)
-        if len(model) > 1:
-            path_to_yaml = model[0][:-1]+"-"+model[1]
-        else:
-            temp_path_to_yaml = model[0].split("yolov8")
-            path_to_yaml = "yolov8"+temp_path_to_yaml[1][1:]
-    else:
-        path_to_yaml = model
-    
-    source_dir= f'ultralytics/cfg/models/v8/{path_to_yaml}'
-    dest_dir = f'./runs/detect/{save_dir}/{save_dir}'
-
-    if os.path.exists(f'{dest_dir}.yaml'):
-        for i in range(2, 100):
-            if os.path.exists(f'{dest_dir}_doesnt_belong_here_{i}.yaml'):
-                continue
-            else:
-                dest_dir = f'{dest_dir}_doesnt_belong_here_{i}.yaml'
-                break
-    else:
-        dest_dir = f'{dest_dir}.yaml'
-    print(source_dir)
-    print(dest_dir)
-    shutil.copy(source_dir, dest_dir)
 
 if __name__ == "__main__":
 
@@ -108,11 +79,12 @@ if __name__ == "__main__":
             else:
                 save_dir = f'{save_dir}{i}'
                 break
+    
+    warnings.filterwarnings("ignore", message="Variable._execution_engine.run_backward")
+    warnings.filterwarnings("ignore", message="(Triggered internally at ../aten/src/ATen/Context.cpp:79.)")
 
     train(epochs=epochs, model=model, device=device, batch_size=batch_size, save_dir=save_dir)
-
     copy_model_config(model, save_dir)
 
     # dataset, classes = get_fiftyone_dataset(0)
-
     # run_prediction(TEST_DATA_PATH, dataset, save_dir, model[:6])

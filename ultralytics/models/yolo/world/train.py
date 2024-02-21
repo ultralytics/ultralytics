348a3bdbc8171e62b8526678342fed4ee5cbb431
Laughing-q <1185102784@qq.com>
2024-02-21 18:13:55 +0800

    fix validate

--- ultralytics/models/yolo/world/train.py
+++ ultralytics/models/yolo/world/train.py
@@ -2,9 +2,9 @@
 
 from ultralytics.models import yolo
 from ultralytics.nn.tasks import WorldModel
-from ultralytics.utils import DEFAULT_CFG, RANK
-from ultralytics.data import build_yolomultimodal_dataset
-from ultralytics.utils.torch_utils import de_parallel
+from ultralytics.utils import DEFAULT_CFG, RANK, LOGGER
+from ultralytics.data import build_yolomultimodal_dataset, build_yolo_dataset, build_dataloader
+from ultralytics.utils.torch_utils import de_parallel, torch_distributed_zero_first
 from ultralytics.utils.checks import check_requirements
 import itertools
 
@@ -71,9 +71,8 @@ class WorldTrainer(yolo.detect.DetectionTrainer):
             batch (int, optional): Size of batches, this is for `rect`. Defaults to None.
         """
         gs = max(int(de_parallel(self.model).stride.max() if self.model else 0), 32)
-        return build_yolomultimodal_dataset(
-            self.args, img_path, batch, self.data, mode=mode, rect=mode == "val", stride=gs
-        )
+        build_dataset = build_yolomultimodal_dataset if mode == "train" else build_yolo_dataset
+        return build_dataset(self.args, img_path, batch, self.data, mode=mode, rect=mode == "val", stride=gs)
 
     def preprocess_batch(self, batch):
         batch = super().preprocess_batch(batch)

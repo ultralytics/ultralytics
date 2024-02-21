80ebc30d17b1af6d8162764075f1baae6747dff1
UltralyticsAssistant <web@ultralytics.com>
2024-02-21 03:38:26 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/models/yolo/detect/predict.py
+++ ultralytics/models/yolo/detect/predict.py
@@ -5,6 +5,7 @@ from ultralytics.engine.results import Results
 from ultralytics.utils import ops
 import torch
 
+
 class DetectionPredictor(BasePredictor):
     """
     A class extending the BasePredictor class for prediction based on a detection model.
@@ -23,7 +24,7 @@ class DetectionPredictor(BasePredictor):
     def postprocess(self, preds, img, orig_imgs):
         """Post-processes predictions and returns a list of Results objects."""
         if isinstance(preds[0], tuple):
-            for i,pred in enumerate(preds):
+            for i, pred in enumerate(preds):
                 preds[i] = ops.non_max_suppression(
                     pred,
                     self.args.conf,
@@ -44,26 +45,27 @@ class DetectionPredictor(BasePredictor):
 
         if not isinstance(orig_imgs, list):  # input images are a torch.Tensor, not a list
             orig_imgs = ops.convert_torch2numpy_batch(orig_imgs)
-        
+
         import copy
+
         tp = copy.copy(preds)
         # preds = preds[0]
-        delelethead = []#删除头操作
-        preds = []#图片有多少张就有多少张
+        delelethead = []  # 删除头操作
+        preds = []  # 图片有多少张就有多少张
         if isinstance(tp[0], list):
             for i in delelethead:
                 del tp[i]
-            for i, pred in enumerate(tp[0]):#每张图片遍历一次,取第一个head的为初始
-                res = pred#preds coco预测
-                for j, det in enumerate(tp):#遍历每一个头的这张图片下的输出
+            for i, pred in enumerate(tp[0]):  # 每张图片遍历一次,取第一个head的为初始
+                res = pred  # preds coco预测
+                for j, det in enumerate(tp):  # 遍历每一个头的这张图片下的输出
                     if j == 0:
                         continue
-                    det[i][:, -1] += 80*j
-                    res = torch.cat((res, det[i]),dim=0)
+                    det[i][:, -1] += 80 * j
+                    res = torch.cat((res, det[i]), dim=0)
                 preds.append(res)
         else:
             preds = tp
-        
+
         results = []
         for i, pred in enumerate(preds):
             orig_img = orig_imgs[i]

0eeef8d30bed137504d0c50f1eaf5432e46dee2b
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-02-21 14:31:35 +0100

    Add docstrings
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- tests/test_python.py
+++ tests/test_python.py
@@ -555,6 +555,7 @@ def test_hub():
 
 @pytest.fixture
 def image():
+    """Define tests 'image' argument."""
     return cv2.imread(str(SOURCE))
 
 
@@ -568,6 +569,7 @@ def image():
     ],
 )
 def test_classify_transforms_train(image, auto_augment, erasing, force_color_jitter):
+    """Tests classification transforms during training with various augmentation settings."""
     import torchvision.transforms as T
 
     from ultralytics.data.augment import classify_augmentations

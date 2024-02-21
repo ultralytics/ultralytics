43c9f8845b3da2fb87fd6685842965335ee45f0a
Sigurd Bakkerud <sbakkerud1@gmail.com>
2024-02-21 12:29:58 +0100

    Changes in train.py

--- train.py
+++ train.py
@@ -1,20 +1,39 @@
 import subprocess
 import argparse
 import os
+import re
 import shutil
-from ultralytics.config import DATASET_DESCRIPTION, TEST_DATA_PATH
+from ultralytics import YOLO
+from ultralytics.config import DATASET_DESCRIPTION, TEST_DATA_PATH, ROOT_DIR
 # from ultralytics.utils.custom_utils.helpers import get_fiftyone_dataset
 # from predict import run_prediction
 
-def train(epochs, model, imgsz=640, device="0", batch_size=16, patience=30, save_dir="train"):
+def train(epochs, model, imgsz=640, device="0", batch_size=16, patience=30, save_dir="train", data=DATASET_DESCRIPTION):
+    import re
 
-    model_path = f'ultralytics/cfg/models/v8/{model}'
+    model_prefixes = {
+        "yolov8": "ultralytics/cfg/models/v8/",
+        "rtdetr": "ultralytics/cfg/models/rt-detr/"
+    }
+
+    pattern = re.compile(r'^(yolov8|rtdetr)(.*)(\.yaml|\.pt)$')
+    match = pattern.match(model)
+    if match:
+        prefix = match.group(1)
+        extension = match.group(3)
+        model_path = f"{model_prefixes[prefix]}{'weights/' if extension == '.pt' else ''}{model}"
+
+    print(model_path)
+    try:
+        test_model = YOLO(model_path, "detect")
+    except FileNotFoundError:
+        raise FileNotFoundError(f"{model_path} does not exist")
 
     command = [
         "yolo",
         "detect",
         "train",
-        f"data={DATASET_DESCRIPTION}",
+        f"data={data}",
         f"model={model_path}",
         f"epochs={epochs}",
         f"imgsz={imgsz}",
@@ -22,14 +41,17 @@ def train(epochs, model, imgsz=640, device="0", batch_size=16, patience=30, save
         "save_txt=True",
         f"batch={batch_size}",
         f"patience={patience}",
-        "nms=True", 
+        "nms=True",
         f"name={save_dir}",
+        f"cfg=./model_args.yaml"
     ]
 
     try:
         subprocess.run(command, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
+    
+    # return model_path
 
 def copy_model_config(model, save_dir):
     if model[:6] != "rtdetr":
@@ -64,41 +86,43 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--model",
-        # nargs="+",  # '+' allows one or more values
         type=str,   # specify the type of elements in the list
-        help="List of all models",
+        help="Name of model, matching file name",
     )
     parser.add_argument(
         "--device",
-        # nargs="+",  # '+' allows one or more values
+        default="0",
         type=str,   # specify the type of elements in the list
-        help="List of predictions to perform",
+        help="Device to run training on",
     )
     parser.add_argument(
         "--epochs",
         # nargs="+",  # '+' allows one or more values
         type=int,   # specify the type of elements in the list
-        help="List of predictions to perform",
+        help="Number of epochs for the model",
     )
     parser.add_argument(
         "--batch_size",
         # nargs="+",  # '+' allows one or more values
         type=int,   # specify the type of elements in the list
-        help="List of predictions to perform",
+        help="Batch size of the model",
     )
     parser.add_argument(
         "--save_dir",
         # nargs="+",  # '+' allows one or more values
         # default=args.model[:-5],
         type=str,   # specify the type of elements in the list
-        help="List of predictions to perform",
+        help="Directory to save the results",
+    )
+    parser.add_argument(
+        "--data_dir",
+        # nargs="+",  # '+' allows one or more values
+        default=DATASET_DESCRIPTION,
+        type=str,   # specify the type of elements in the list
+        help="Path to data.yaml file for dataset",
     )
 
     args = parser.parse_args()
-    model = args.model
-    device = args.device
-    epochs = args.epochs
-    batch_size = args.batch_size
     save_dir = args.save_dir
 
     if os.path.exists(save_dir):
@@ -109,9 +133,9 @@ if __name__ == "__main__":
                 save_dir = f'{save_dir}{i}'
                 break
 
-    train(epochs=epochs, model=model, device=device, batch_size=batch_size, save_dir=save_dir)
+    train(epochs=args.epochs, model=args.model, device=args.device, batch_size=args.batch_size, save_dir=save_dir, data=args.data_dir)
 
-    copy_model_config(model, save_dir)
+    # copy_model_config(args.model, save_dir)
 
     # dataset, classes = get_fiftyone_dataset(0)
 

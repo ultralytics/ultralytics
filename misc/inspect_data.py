b7bec02257aabb0951eebade6b899c734b1a5d04
inakierregueab <inaki.erregue@gmail.com>
2024-02-22 16:24:07 +0100

    Inspect CDV2 and save generated df

--- misc/inspect_data.py
+++ misc/inspect_data.py
@@ -50,8 +50,8 @@ def inspect_dataset(
  df['class_name'] = df['new_class_id'].map(class_names)
  return df
 
-df_val = inspect_dataset('/data-fast/127-data2/ierregue/datasets/custom_coco', 'val')
-df_train = inspect_dataset('/data-fast/127-data2/ierregue/datasets/custom_coco', 'train')
+df_val = inspect_dataset('/data-fast/127-data2/ierregue/datasets/custom_dataset_v2', 'val')
+df_train = inspect_dataset('/data-fast/127-data2/ierregue/datasets/custom_dataset_v2', 'train')
 
 print(f"The number of objects is {len(df_val)}")
 print(f"The number of images is {len(df_val['img'].unique())}")
@@ -60,7 +60,7 @@ print(f"The number of objects is {len(df_train)}")
 print(f"The number of images is {len(df_train['img'].unique())}")
 
 # Create dir to store plots
-save_dir = './data/dataset_creation/plots/custom_coco'
+save_dir = './data/dataset_creation/plots/v2'
 if not os.path.isdir(save_dir):
     os.makedirs(save_dir)
 
@@ -199,3 +199,7 @@ ax.grid()
 #plt.legend()
 None
 fig.savefig(save_dir+f'/val_objsz_counts.png', bbox_inches = 'tight')
+
+# Save dataframes
+df_train.to_csv('./data/dataset_creation/dataframes/v2/df_train.csv', index=False)
+df_val.to_csv('./data/dataset_creation/dataframes/v2/df_val.csv', index=False)

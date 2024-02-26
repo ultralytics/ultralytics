a56493143e7d092b2e615a72b87233f0ce6978e0
zdreamer1980 <158547928+zdreamer1980@users.noreply.github.com>
2024-02-26 19:58:43 +0800

    Update utils.py
    Keeping the tolerance of 0.2, the obb rotation box will be out of bounds

--- ultralytics/data/utils.py
+++ ultralytics/data/utils.py
@@ -130,7 +130,7 @@ def verify_image_label(args):
                 else:
                     assert lb.shape[1] == 5, f"labels require 5 columns, {lb.shape[1]} columns detected"
                     points = lb[:, 1:]
-                assert points.max() <= 1, f"non-normalized or out of bounds coordinates {points[points > 1]}"
+                assert points.max() <= 1.2, f"non-normalized or out of bounds coordinates {points[points > 1.2]}" # Keeping the tolerance of 0.2, the obb rotation box will be out of bounds
                 assert lb.min() >= 0, f"negative label values {lb[lb < 0]}"
 
                 # All labels

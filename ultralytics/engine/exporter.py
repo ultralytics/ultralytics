bf428778972d7840799555f016da4a505a3dfa56
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-02-26 23:24:58 +0100

    Update exporter.py

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -883,10 +883,7 @@ class Exporter:
 
         quantization = "--quantize_float16" if self.args.half else "--quantize_uint8" if self.args.int8 else ""
         with spaces_in_path(f_pb) as fpb_, spaces_in_path(f) as f_:  # exporter can not handle spaces in path
-            cmd = (
-                f"tensorflowjs_converter --input_format=tf_frozen_model {quantization} --output_node_names="
-                f'{outputs} "{fpb_}" "{f_}"'
-            )
+            cmd = f'tensorflowjs_converter --input_format=tf_frozen_model {quantization} --output_node_names={outputs} "{fpb_}" "{f_}"'
             LOGGER.info(f"{prefix} running '{cmd}'")
             subprocess.run(cmd, shell=True)
 

a355b6ede7a39a271b84d13ec03beb093308322c
saumil <saumil.shah@verdantrobotics.com>
2024-02-28 21:15:13 -0800

    Use the correct variable

--- training_utils/export.py
+++ training_utils/export.py
@@ -10,18 +10,17 @@ import argparse
 
 def Export(checkpoint_file_path):
     model = YOLO(GetModelYaml(training_task))  # Initialize model
-    if os.path.exists(args.model):
-        model = GiveModel(args.load)
+    if os.path.exists(checkpoint_file_path):
+        model = GiveModel(checkpoint_file_path)
     else:
-        print(f"[ERROR] : Model {args.load} does not exists")
+        print(f"[ERROR] : Model {checkpoint_file_path} does not exists")
         exit(1)
 
+    base_path, _ = os.path.split(checkpoint_file_path)
     path = model.export(format="onnx", imgsz=[2144, 768], opset=12)
-    base_path, name = os.path.split(args.model)
     os.system(f"mv {path} {base_path}/best_full_height.onnx")
 
     path = model.export(format="onnx", imgsz=[2144, 4096], opset=12)
-    base_path, name = os.path.split(args.model)
     os.system(f"mv {path} {base_path}/best_full_frame.onnx")
 
     path = model.export(format="onnx", imgsz=[768, 768], opset=12)

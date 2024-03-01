076ff8cc6660575e4afcabb4fa47b70836f80282
johnnynunez <johnnynuca14@gmail.com>
2024-03-01 10:27:04 +0100

    revert config & infer

--- inference_tools/infer.py
+++ inference_tools/infer.py
@@ -1,6 +1,5 @@
 import json
 import time
-import os
 
 from inference_utils import inference_time_csv_writer
 from ultralytics import YOLO
@@ -18,20 +17,6 @@ source = config['input_data_dir']
 # Output directory
 root = config['output_dir']
 experiment_name = time.strftime("%Y%m%d-%H%M%S")
-
-model_directory, model_filename = os.path.split(config['model_path'])
-model_basename, _ = os.path.splitext(model_filename)
-
-# Construir el path del modelo ONNX usando el nombre base
-model_onnx_path = os.path.join(model_directory, model_basename + '.onnx')
-
-# Verificar si el archivo .onnx existe
-if os.path.exists(model_onnx_path):
-    # Si el archivo .onnx existe, usar este path
-    model_path = model_onnx_path
-else:
-    # Si el archivo .onnx no existe, usar el path original de la configuración
-    model_path = config['model_path']
 
 # Load model
 model = YOLO(config['model_path'], task='detect')

2c815325f9f9619f5008d37adb883b893271eb8f
Laughing-q <1185102784@qq.com>
2024-03-04 15:38:22 +0800

    revert RepConv

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -564,7 +564,7 @@ class RepBottleneck(nn.Module):
         """
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv1 = RepConv(c1, c_, k[0], 1)
         self.cv2 = Conv(c_, c2, k[1], 1, g=g)
         self.add = shortcut and c1 == c2
 

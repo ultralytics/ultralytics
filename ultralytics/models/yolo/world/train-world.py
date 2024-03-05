1a236c11951bc73877e064d19875fc521cc1477c
Laughing-q <1185102784@qq.com>
2024-03-05 16:38:34 +0800

    fix path

--- ultralytics/models/yolo/world/train-world.py
+++ ultralytics/models/yolo/world/train-world.py
@@ -26,8 +26,8 @@ class WorldTrainerFromScratch(WorldTrainer):
         super().__init__(cfg, overrides, _callbacks)
         # NOTE: debug
         self.flickr30k_data = dict(
-            img_path="../datasets/flick30k/images",
-            json_file="../datasets/flick30k/final_flickr_separateGT_train.json",
+            img_path="../datasets/flickr30k/images",
+            json_file="../datasets/flickr30k/final_flickr_separateGT_train.json",
         )
         self.gqa_data = dict(
             img_path="../datasets/GQA/images",

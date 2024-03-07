c8561bb7bdb836ae015819cefeb7473127e3301a
UltralyticsAssistant <web@ultralytics.com>
2024-03-07 09:05:41 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/trackers/deep_sort/sort/detection.py
+++ ultralytics/trackers/deep_sort/sort/detection.py
@@ -25,31 +25,27 @@ class Detection(object):
         Detector confidence score.
     feature : ndarray | NoneType
         A feature vector that describes the object contained in this image.
-
     """
-    
+
     def __init__(self, tlwh, confidence, feature):
         self.tlwh = np.asarray(tlwh, dtype=float)
         self.confidence = float(confidence)
-        self.feature = np.asarray(feature, dtype=np.float32)    
-    
+        self.feature = np.asarray(feature, dtype=np.float32)
 
     # def __init__(self, tlwh, confidence, feature):
-        # self.tlwh = np.asarray(tlwh, dtype=np.float)
-        # self.confidence = float(confidence)
-        # self.feature = np.asarray(feature, dtype=np.float32)
+    # self.tlwh = np.asarray(tlwh, dtype=np.float)
+    # self.confidence = float(confidence)
+    # self.feature = np.asarray(feature, dtype=np.float32)
 
     def to_tlbr(self):
-        """Convert bounding box to format `(min x, min y, max x, max y)`, i.e.,
-        `(top left, bottom right)`.
-        """
+        """Convert bounding box to format `(min x, min y, max x, max y)`, i.e., `(top left, bottom right)`."""
         ret = self.tlwh.copy()
         ret[2:] += ret[:2]
         return ret
 
     def to_xyah(self):
-        """Convert bounding box to format `(center x, center y, aspect ratio,
-        height)`, where the aspect ratio is `width / height`.
+        """Convert bounding box to format `(center x, center y, aspect ratio, height)`, where the aspect ratio is `width
+        / height`.
         """
         ret = self.tlwh.copy()
         ret[:2] += ret[2:] / 2

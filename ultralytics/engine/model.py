bb9b72cfc9f1fe5598ad8274a04a44781da078fb
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-03-10 00:27:47 +0100

    set tracking default batch=1
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/engine/model.py
+++ ultralytics/engine/model.py
@@ -474,6 +474,7 @@ class Model(nn.Module):
 
             register_tracker(self, persist)
         kwargs["conf"] = kwargs.get("conf") or 0.1  # ByteTrack-based method needs low confidence predictions as input
+        kwargs["batch"] = kwargs.get("batch") or 1  # batch-size 1 for tracking in videos
         kwargs["mode"] = "track"
         return self.predict(source=source, stream=stream, **kwargs)
 

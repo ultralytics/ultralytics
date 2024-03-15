21219fe44319f58b9e8d8f1b7941cab03ed38431
Laughing-q <1185102784@qq.com>
2024-03-15 16:00:11 +0800

    update

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -250,7 +250,7 @@ class C2fn(nn.Module):
         self.c = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, 2 * self.c, 1, 1)
         self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
-        self.m = nn.ModuleList(C2f(self.c, self.c, 1, shortcut, g) for _ in range(n))
+        self.m = nn.ModuleList(C2f(self.c, self.c, 2, shortcut, g) for _ in range(n))
 
     def forward(self, x):
         """Forward pass through C2f layer."""

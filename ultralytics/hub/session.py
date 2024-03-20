95f256f43f0931d419124ab3697776a1a19b8aac
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-03-20 15:48:43 -0700

    Update session.py

--- ultralytics/hub/session.py
+++ ultralytics/hub/session.py
@@ -174,14 +174,11 @@ class HUBTrainingSession:
         """
         Initializes training arguments and creates a model entry on the Ultralytics HUB.
 
-        This method sets up training arguments based on the model's state and updates
-        them with any additional arguments provided. It handles different states of the
-        model, such as whether it's resumable, pretrained, or requires specific file setup.
+        This method sets up training arguments based on the model's state and updates them with any additional arguments provided. It handles different states of the model, such as whether it's resumable, pretrained, or requires specific file setup.
 
         Raises:
-            ValueError: If the model is already trained, if required dataset information
-                        is missing, or if there are issues with the provided training
-                        arguments.
+            ValueError: If the model is already trained, if required dataset information is missing, 
+                or if there are issues with the provided training arguments.
         """
         if self.model.is_trained():
             raise ValueError(emojis(f"Model is already trained and uploaded to {self.model_url} 🚀"))

2b0a3d5b0ab47b10a2c55bfb6bf67874fef07e40
Burhan <62214284+Burhan-Q@users.noreply.github.com>
2024-03-22 22:56:38 -0400

    relocate `clip_model`
    fixes  #9240

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -561,7 +561,6 @@ class WorldModel(DetectionModel):
     def __init__(self, cfg="yolov8s-world.yaml", ch=3, nc=None, verbose=True):
         """Initialize YOLOv8 world model with given config and parameters."""
         self.txt_feats = torch.randn(1, nc or 80, 512)  # features placeholder
-        self.clip_model = None  # CLIP model placeholder
         super().__init__(cfg=cfg, ch=ch, nc=nc, verbose=verbose)
 
     def set_classes(self, text):
@@ -571,7 +570,8 @@ class WorldModel(DetectionModel):
         except ImportError:
             check_requirements("git+https://github.com/openai/CLIP.git")
             import clip
-
+        
+        self.clip_model = None  # CLIP model placeholder
         if not self.clip_model:
             self.clip_model = clip.load("ViT-B/32")[0]
         device = next(self.clip_model.parameters()).device

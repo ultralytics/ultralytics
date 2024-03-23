c521c340b6e979a385919dc54ff987c5b749519e
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-03-23 15:54:04 +0100

    Simplify solution

--- ultralytics/cfg/__init__.py
+++ ultralytics/cfg/__init__.py
@@ -243,11 +243,9 @@ def check_cfg(cfg, hard=True):
                             f"'{k}={v}' is of invalid type {type(v).__name__}. "
                             f"Valid '{k}' types are int (i.e. '{k}=0') or float (i.e. '{k}=0.5')"
                         )
-                    cfg[k] = float(v)
-                if not (0.0 <= cfg[k] <= 1.0):
-                    raise ValueError(
-                        f"'{k}={cfg[k]}' is an invalid value. " f"Valid '{k}' values are between 0.0 and 1.0."
-                    )
+                    cfg[k] = v = float(v)
+                if not (0.0 <= v <= 1.0):
+                    raise ValueError(f"'{k}={v}' is an invalid value. " f"Valid '{k}' values are between 0.0 and 1.0.")
             elif k in CFG_INT_KEYS and not isinstance(v, int):
                 if hard:
                     raise TypeError(

d1e2f34a920d38a212cb7362887682a0932248c3
Laughing-q <1185102784@qq.com>
2024-03-29 15:27:50 +0800

    update val.py

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -285,11 +285,11 @@ class DetectionValidator(BaseValidator):
                 / ("instances_val2017.json" if self.is_coco else f"lvis_v1_{self.args.split}.json")
             )  # annotations
             pkg = "pycocotools" if self.is_coco else "lvis"
+            LOGGER.info(f"\nEvaluating {pkg} mAP using {pred_json} and {anno_json}...")
             try:  # https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocoEvalDemo.ipynb
                 for x in pred_json, anno_json:
                     assert x.is_file(), f"{x} file not found"
                 check_requirements("pycocotools>=2.0.6" if self.is_coco else "lvis>=0.5.3")
-                LOGGER.info(f"\nEvaluating {pkg} mAP using {pred_json} and {anno_json}...")
                 if self.is_coco:
                     from pycocotools.coco import COCO  # noqa
                     from pycocotools.cocoeval import COCOeval  # noqa

14a74737344691544c5bb603cb8e6d752b41bfbd
UltralyticsAssistant <web@ultralytics.com>
2024-03-29 01:53:43 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -30,7 +30,7 @@ RANK = int(os.getenv("RANK", -1))
 LOCAL_RANK = int(os.getenv("LOCAL_RANK", -1))  # https://pytorch.org/docs/stable/elastic/run.html
 
 # Other Constants
-ARGV = sys.argv or ["", ""]   # sometimes sys.argv = []
+ARGV = sys.argv or ["", ""]  # sometimes sys.argv = []
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[1]  # YOLO
 ASSETS = ROOT / "assets"  # default images

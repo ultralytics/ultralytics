1585655da828eba90b384111559d9c3d3d0c09dc
Laughing-q <1185102784@qq.com>
2024-03-29 15:16:19 +0800

    attempt to fix final_eval

--- ultralytics/models/yolo/world/train_world.py
+++ ultralytics/models/yolo/world/train_world.py
@@ -101,6 +101,8 @@ class WorldTrainerFromScratch(WorldTrainer):
         pass
 
     def final_eval(self):
-        """Performs final evaluation and validation for object detection YOLO model."""
-        self.validator.args.data = self.args.data["val"]
+        """Performs final evaluation and validation for object detection YOLO-World model."""
+        val = self.args.data["val"]["yolo_data"][0]
+        self.validator.args.data = val
+        self.validator.args.split = "minival" if isinstance(val, str) and "lvis" in val else "val"
         return super().final_eval()

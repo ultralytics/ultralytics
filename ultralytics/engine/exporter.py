03d0ffd9f58da3ad3e33908c19445c733a701015
Jiacong Fang <zldrobit@126.com>
2024-03-29 23:22:32 +0800

    Improved float `workspace` arg for TRT exports (#9407)
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -675,9 +675,7 @@ class Exporter:
 
         builder = trt.Builder(logger)
         config = builder.create_builder_config()
-        config.max_workspace_size = self.args.workspace * 1 << 30
-        # config.set_memory_pool_limit(trt.MemoryPoolType.WORKSPACE, workspace << 30)  # fix TRT 8.4 deprecation notice
-
+        config.max_workspace_size = int(self.args.workspace * (1 << 30))
         flag = 1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
         network = builder.create_network(flag)
         parser = trt.OnnxParser(network, logger)

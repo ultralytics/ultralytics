959acf67db9ce3c0fc00d6f02efa42dedd62e5ea
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-04-01 19:46:04 +0200

    `ultralytics 8.1.41` DDP resume untrained-checkpoint fix (#9453)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: UltralyticsAssistant <web@ultralytics.com>
    Co-authored-by: Laughing-q <1185102784@qq.com>

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -513,7 +513,7 @@ def convert_optimizer_state_dict_to_fp16(state_dict):
     """
     for state in state_dict["state"].values():
         for k, v in state.items():
-            if isinstance(v, torch.Tensor) and v.dtype is torch.float32:
+            if k != "step" and isinstance(v, torch.Tensor) and v.dtype is torch.float32:
                 state[k] = v.half()
 
     return state_dict

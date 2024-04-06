f408c64b026c7f1cb459329da94f178874b5efb6
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-04-07 00:16:41 +0200

    Update __init__.py

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -6,11 +6,11 @@ from ultralytics.data.explorer.explorer import Explorer
 from ultralytics.models import RTDETR, SAM, YOLO, YOLOWorld
 from ultralytics.models.fastsam import FastSAM
 from ultralytics.models.nas import NAS
-from ultralytics.utils import ASSETS
-from ultralytics.utils import SETTINGS as settings
+from ultralytics.utils import ASSETS, SETTINGS
 from ultralytics.utils.checks import check_yolo as checks
 from ultralytics.utils.downloads import download
 
+settings = SETTINGS
 __all__ = (
     "__version__",
     "ASSETS",

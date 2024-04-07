dfcb2791282829b3a0752f52c3ef1970b8ab8bd6
UltralyticsAssistant <web@ultralytics.com>
2024-04-07 23:50:19 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -481,7 +481,8 @@ def is_raspberrypi() -> bool:
 
 def is_jetson() -> bool:
     """
-    Determines if the Python environment is running on a Jetson Nano or Jetson Orin device by checking the device model information.
+    Determines if the Python environment is running on a Jetson Nano or Jetson Orin device by checking the device model
+    information.
 
     Returns:
         (bool): True if running on a Jetson Nano or Jetson Orin, False otherwise.

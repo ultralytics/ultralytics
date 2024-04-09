2c03f83a3b9896c71ec69c1ffa1a16efcaf43898
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-04-09 14:46:38 +0200

    Refactor constants out of for loop for speed

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -698,11 +698,11 @@ class Exporter:
             if shape[0] <= 1:
                 LOGGER.warning(f"{prefix} WARNING ⚠️ 'dynamic=True' model requires max batch size, i.e. 'batch=16'")
             profile = builder.create_optimization_profile()
+            min_shape = (1, shape[1], 32, 32)
+            opt_shape = (max(1, shape[0] // 2), *shape[1:])
+            max_shape = (*shape[:2], *(max(1, self.args.workspace) * d for d in shape[2:]))
             for inp in inputs:
-                _min = (1, shape[1], 32, 32)
-                _opt = (max(1, shape[0] // 2), *shape[1:])
-                _max = (*shape[:2], *(max(1, self.args.workspace) * d for d in shape[2:]))
-                profile.set_shape(inp.name, _min, _opt, _max)
+                profile.set_shape(inp.name, min_shape, opt_shape, max_shape)
             config.add_optimization_profile(profile)
 
         half = builder.platform_has_fast_fp16 and self.args.half

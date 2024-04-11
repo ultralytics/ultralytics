6f3c67ba9764b5b6450cb6183e8cd4fa419ca2f9
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-04-11 18:04:54 +0200

    `ultralytics 8.1.47` PNNX release 20240226 (#9954)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,6 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = "8.1.46"
+__version__ = "8.1.47"
 
 from ultralytics.data.explorer.explorer import Explorer
 from ultralytics.models import RTDETR, SAM, YOLO, YOLOWorld

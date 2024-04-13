a015607e58fd4f13249a3eae8313e2cf242b1cc9
UltralyticsAssistant <web@ultralytics.com>
2024-04-13 15:22:24 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/trackers/track.py
+++ ultralytics/trackers/track.py
@@ -7,6 +7,7 @@ import torch
 
 from ultralytics.utils import IterableSimpleNamespace, yaml_load
 from ultralytics.utils.checks import check_yaml
+
 from .bot_sort import BOTSORT
 from .byte_tracker import BYTETracker
 

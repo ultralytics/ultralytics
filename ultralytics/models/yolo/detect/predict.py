28e1447720f0c77c471b2d271d4834d10e919305
UltralyticsAssistant <web@ultralytics.com>
2024-04-14 03:54:39 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/models/yolo/detect/predict.py
+++ ultralytics/models/yolo/detect/predict.py
@@ -1,9 +1,10 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
+import torch
+
 from ultralytics.engine.predictor import BasePredictor
 from ultralytics.engine.results import Results
 from ultralytics.utils import ops
-import torch
 
 
 class DetectionPredictor(BasePredictor):

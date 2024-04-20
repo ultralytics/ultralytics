fdec6c64580f6ce24a6381ac9f51d438df54d2e0
johnnynunez <johnnynuca14@gmail.com>
2024-04-20 16:48:51 +0200

    WIP

--- ultralytics/nn/autobackend.py
+++ ultralytics/nn/autobackend.py
@@ -174,7 +174,7 @@ class AutoBackend(nn.Module):
             check_requirements(("onnx", "onnxruntime-gpu" if cuda else "onnxruntime"))
             import onnxruntime
 
-            providers = ["CUDAExecutionProvider", "CPUExecutionProvider"] if cuda else ["CPUExecutionProvider"]
+            providers = ["TensorrtExecutionProvider", "CUDAExecutionProvider", "CPUExecutionProvider"] if cuda else ["CPUExecutionProvider"]
             session = onnxruntime.InferenceSession(w, providers=providers)
             output_names = [x.name for x in session.get_outputs()]
             metadata = session.get_modelmeta().custom_metadata_map  # metadata

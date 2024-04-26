5d4d9e1838da43e45e62793de2a8fe8d427af9a4
RizwanMunawar <chr043416@gmail.com>
2024-04-26 14:01:01 +0500

    update

--- ultralytics/cfg/__init__.py
+++ ultralytics/cfg/__init__.py
@@ -468,7 +468,6 @@ def entrypoint(debug=""):
         "copy-cfg": copy_default_cfg,
         "explorer": lambda: handle_explorer(),
     }
-
     full_args_dict = {**DEFAULT_CFG_DICT, **{k: None for k in TASKS}, **{k: None for k in MODES}, **special}
 
     # Define common misuses of special commands, i.e. -h, -help, --help

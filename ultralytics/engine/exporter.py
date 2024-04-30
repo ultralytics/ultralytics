1c25914d2bd034e48a4e10a5fd6b27d2cff9a4f0
Burhan <62214284+Burhan-Q@users.noreply.github.com>
2024-04-29 20:54:34 -0400

    change image open limit to 500

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -803,11 +803,11 @@ class Exporter:
             if self.args.task != "classify":
                 dataset = load_inference_source(data["val"], batch=bsize)
             else:
-                files = [
+                files = (
                     f.as_posix()
                     for nf, f in enumerate(Path(data["val"]).rglob("*.*"))
-                    if (nf < 5000 and f.suffix.strip(".").lower() in IMG_FORMATS)
-                ]
+                    if (nf < 500 and f.suffix.strip(".").lower() in IMG_FORMATS)
+                )
                 dataset = load_inference_source(files, batch=bsize)
 
             n = len(dataset) * bsize

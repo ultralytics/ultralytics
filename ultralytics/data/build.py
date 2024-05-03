fce12cb192834ccf380f0dedf400f54b71a37083
Andreas Moldskred <moldskredandreas1@gmail.com>
2024-05-03 12:58:15 -0700

    Avoid AttributeError on windows

--- ultralytics/data/build.py
+++ ultralytics/data/build.py
@@ -81,7 +81,11 @@ def seed_worker(worker_id):  # noqa
     worker_seed = torch.initial_seed() % 2**32
     np.random.seed(worker_seed)
     random.seed(worker_seed)
-    os.sched_setaffinity(0, range(NUM_THREADS))  # fix https://github.com/ultralytics/ultralytics/pull/11195
+    try:
+        # fix https://github.com/ultralytics/ultralytics/pull/11195
+        os.sched_setaffinity(0, range(NUM_THREADS))
+    except AttributeError:
+        pass
 
 
 def build_yolo_dataset(cfg, img_path, batch, data, mode="train", rect=False, stride=32, multi_modal=False):

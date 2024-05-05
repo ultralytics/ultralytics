878c132bc8b36dadec1012af43e078faf6cb60e8
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-05-05 16:17:30 +0200

    Update test_exports.py

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -86,7 +86,7 @@ def test_export_onnx_matrix(task, dynamic, int8, half, batch):
         batch=batch,
     )
     YOLO(file)([SOURCE] * batch, imgsz=64 if dynamic else 32)  # exported model inference
-    shutil.rmtree(file)
+    Path(file).unlink()
 
 
 # @pytest.mark.slow
@@ -102,7 +102,7 @@ def test_export_torchscript_matrix(task, dynamic, int8, half, batch):
         batch=batch,
     )
     YOLO(file)([SOURCE] * 3, imgsz=64 if dynamic else 32)  # exported model inference at batch=3
-    shutil.rmtree(file)
+    Path(file).unlink()
 
 
 @pytest.mark.skipif(not TORCH_1_9, reason="CoreML>=7.2 not supported with PyTorch<=1.8")

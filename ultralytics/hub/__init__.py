f0b667ca5d692399ea216f311143abb511367831
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-05-12 19:06:40 +0200

    Update docstring with all examples

--- ultralytics/hub/__init__.py
+++ ultralytics/hub/__init__.py
@@ -109,7 +109,7 @@ def get_export(model_id="", format="torchscript"):
 def check_dataset(path: str, task: str) -> None:
     """
     Function for error-checking HUB dataset Zip file before upload. It checks a dataset for errors before it is uploaded
-    to the HUB. Usage examples are given below.
+    to the HUB. Usage examples are given below. All example datasets are available at https://github.com/ultralytics/hub, i.e. https://github.com/ultralytics/hub/blob/main/example_datasets/coco8.zip.
 
     Args:
         path (str): Path to data.zip (with data.yaml inside data.zip).
@@ -122,6 +122,8 @@ def check_dataset(path: str, task: str) -> None:
         check_dataset('path/to/coco8.zip', task='detect')  # detect dataset
         check_dataset('path/to/coco8-seg.zip', task='segment')  # segment dataset
         check_dataset('path/to/coco8-pose.zip', task='pose')  # pose dataset
+        check_dataset('path/to/dota8.zip', task='obb')  # OBB dataset
+        check_dataset('path/to/imagenet10.zip', task='classify')  # classification dataset
         ```
     """
     HUBDatasetStats(path=path, task=task).get_json()

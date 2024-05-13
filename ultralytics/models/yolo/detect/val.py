44ae7e46977cc03199fff953c256546597729e7a
sunmooncode <zt7128331@gmail.com>
2024-05-13 11:56:13 +0800

    Better evaluation results show.

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -31,6 +31,7 @@ class DetectionValidator(BaseValidator):
     def __init__(self, dataloader=None, save_dir=None, pbar=None, args=None, _callbacks=None):
         """Initialize detection model with necessary variables and settings."""
         super().__init__(dataloader, save_dir, pbar, args, _callbacks)
+        self.nt_per_image = None
         self.nt_per_class = None
         self.is_coco = False
         self.is_lvis = False
@@ -77,7 +78,8 @@ class DetectionValidator(BaseValidator):
         self.confusion_matrix = ConfusionMatrix(nc=self.nc, conf=self.args.conf)
         self.seen = 0
         self.jdict = []
-        self.stats = dict(tp=[], conf=[], pred_cls=[], target_cls=[])
+        self.stats = dict(tp=[], conf=[], pred_cls=[], target_cls=[], target_img=[]) \
+            if self.args.per_class_img else dict(tp=[], conf=[], pred_cls=[], target_cls=[])
 
     def get_desc(self):
         """Return a formatted string summarizing class metrics of YOLO model."""
@@ -130,6 +132,8 @@ class DetectionValidator(BaseValidator):
             cls, bbox = pbatch.pop("cls"), pbatch.pop("bbox")
             nl = len(cls)
             stat["target_cls"] = cls
+            if self.args.per_class_img:
+                stat["target_img"] = torch.unique(cls)
             if npr == 0:
                 if nl:
                     for k in self.stats.keys():
@@ -168,6 +172,11 @@ class DetectionValidator(BaseValidator):
     def get_stats(self):
         """Returns metrics statistics and results dictionary."""
         stats = {k: torch.cat(v, 0).cpu().numpy() for k, v in self.stats.items()}  # to numpy
+        if self.args.per_class_img:
+            self.nt_per_image = np.bincount(
+                stats["target_img"].astype(int), minlength=self.nc
+            )  # number of targets per image
+            stats.pop("target_img", None)
         if len(stats) and stats["tp"].any():
             self.metrics.process(**stats)
         self.nt_per_class = np.bincount(
@@ -185,7 +194,8 @@ class DetectionValidator(BaseValidator):
         # Print results per class
         if self.args.verbose and not self.training and self.nc > 1 and len(self.stats):
             for i, c in enumerate(self.metrics.ap_class_index):
-                LOGGER.info(pf % (self.names[c], self.seen, self.nt_per_class[c], *self.metrics.class_result(i)))
+                LOGGER.info(pf % (self.names[c], self.nt_per_image[c] if self.args.per_class_img else self.seen,
+                                  self.nt_per_class[c], *self.metrics.class_result(i)))
 
         if self.args.plots:
             for normalize in True, False:

26b1ffb0508477717cbd9db8959d9fcf3cf273db
lixiang <lixiang@netranking.cn>
2024-05-14 17:57:33 +0800

    旋转识别

--- rotate_panorama.py
+++ rotate_panorama.py
@@ -63,7 +63,7 @@ def screen_to_equirectangular(x, y, screen_width, screen_height, fov, yaw, pitch
     ny = (y / screen_height) * 2 - 1
 
     # FOV的一半的切线值，用于计算z坐标
-    t = 1
+    t = np.tan(np.radians(fov / 2))
 
     # 逆向计算出对应的方向向量
     direction = np.array([t * nx, t * ny, 1])
@@ -98,7 +98,7 @@ def screen_to_equirectangular(x, y, screen_width, screen_height, fov, yaw, pitch
     return [int(ex), int(ey)]
 
 # Load a model
-model = YOLO('runs/detect/train55/weights/best.pt')  # pretrained YOLOv8n model
+model = YOLO('runs/detect/train33/weights/best.pt')  # pretrained YOLOv8n model
 inferencer = MMSegInferencer(model='deeplabv3plus_r18-d8_4xb2-80k_cityscapes-512x1024')
 
 def predict_result(image):
@@ -126,17 +126,17 @@ images = [
     # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/6176dca744e64bd6bf8a02dd92f466eb.jpg',
     # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/057c166dcc8648e69cba69009a8535b4.jpg',
     # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/0b3f8b79969940528ab1cf6a43ca83a9.jpg',
-    # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/9cf26a02681f44f68b718762cd0f5494.jpg',
-    'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/aa00a97db0a34c0e90eea6d393cedde2.jpg',
-    'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/7868ef382f84429fb79047fba3978b67.jpg',
-    'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/723fbb77ae71495e8407265cb3d8f7db.jpg',
-    'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/33fa8f17405f415195055f6a714f4c09.jpg',
-    'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/809d5a96fee24fc294fb4a8d5445412a.jpg',
-    'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/d5c8a742f7464f96979633f0dc2f10aa.jpg',
-    'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/e1e599f026834cd0ae7f0a714123175d.jpg',
-    'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/d92593ffbcf74d80aa12afaa43a26584.jpg',
-    'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/74483c83efb84cf7a41f55c314bffcb8.jpg',
-    'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/61523fb326234be194e5d41afa06b17b.jpg'
+    'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/9cf26a02681f44f68b718762cd0f5494.jpg',
+    # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/aa00a97db0a34c0e90eea6d393cedde2.jpg',
+    # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/7868ef382f84429fb79047fba3978b67.jpg',
+    # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/723fbb77ae71495e8407265cb3d8f7db.jpg',
+    # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/33fa8f17405f415195055f6a714f4c09.jpg',
+    # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/809d5a96fee24fc294fb4a8d5445412a.jpg',
+    # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/d5c8a742f7464f96979633f0dc2f10aa.jpg',
+    # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/e1e599f026834cd0ae7f0a714123175d.jpg',
+    # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/d92593ffbcf74d80aa12afaa43a26584.jpg',
+    # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/74483c83efb84cf7a41f55c314bffcb8.jpg',
+    # 'https://ow-prod-cdn.survey.work/platform_id_1/app_id_null/roled_user_id_null/type_1/61523fb326234be194e5d41afa06b17b.jpg'
 ]
 
 img_names = [x.split('/')[-1].split('.')[0] for x in images]
@@ -177,8 +177,8 @@ for idx_conf in confs:
         (height, width, depth) = img.shape
         equi_img = np.transpose(img, (2, 0, 1))
         equi_img = torch.tensor(equi_img)
-        pers_should_height = height / 4
-        pers_should_width = width / 4
+        pers_should_height = int(height / 4)
+        pers_should_width = int(width / 4)
         pers_height = 480
         pers_width = 640
         pitch = math.radians(0)
@@ -187,7 +187,7 @@ for idx_conf in confs:
         img_PIL = Image.fromarray(img[..., ::-1])  # 转成 PIL 格式
         draw = ImageDraw.Draw(img_PIL)  # 创建绘制对象
         # 俯仰角从0到30度，每次转动5度
-        for j in range(7):
+        for j in range(1):
             now = time.time()
             yaw = math.pi
             pers_imgs = []
@@ -210,6 +210,8 @@ for idx_conf in confs:
                 fov_deg = 90.0
                 pers_height = 480
                 pers_width = 640
+                # pers_height = pers_should_height
+                # pers_width = pers_should_width
                 pers_img = equi2pers(
                     equi=equi_img,
                     rots=rots,
@@ -219,6 +221,8 @@ for idx_conf in confs:
                     mode="bilinear",
                 )
                 cube_result = np.ascontiguousarray(np.transpose(pers_img, (1, 2, 0)))
+                cv2.imshow('img', cube_result)
+                cv2.waitKey(0)
                 pers_imgs.append(PersImage(pitch, yaw, cube_result))
                 _u, _v = cvt_coord(yaw, pitch)
                 center = np.array([int(pers_width / 2), int(pers_height / 2)])
@@ -235,12 +239,12 @@ for idx_conf in confs:
             pitch += math.radians(5)
             print(f'转动视角耗时{time.time() - now}秒')
 
-            results = model.predict([elem.pers_img for elem in pers_imgs], conf=0.5, imgsz=640)
+            results = model.predict([elem.pers_img for elem in pers_imgs], conf=0.3, imgsz=640)
             for idx, result in enumerate(results):
                 pers_image = pers_imgs[idx]
                 orig_image = Image.fromarray(pers_image.pers_img[..., ::-1])  # 转成 PIL 格式
                 orig_draw = ImageDraw.Draw(orig_image)
-                cv2.imwrite(f'{dir}/原图/origin_img_{idx}.jpg', pers_image.pers_img)
+                # cv2.imwrite(f'{dir}/原图/origin_img_{idx}.jpg', pers_image.pers_img)
                 for cls, box, conf in zip(result.boxes.cls, result.boxes.xyxy, result.boxes.conf):
                     cls_np = int(cls.cpu().detach().numpy().item())
                     if cls_np not in name_dict.keys():
@@ -249,8 +253,8 @@ for idx_conf in confs:
                     conf_np = conf.cpu().detach().numpy().item()
                     p1 = (box_np[0], box_np[1])
                     p2 = (box_np[2], box_np[3])
-                    if (p2[0] - p1[0]) * (p2[1] - p1[1]) < 100:
-                        continue
+                    # if (p2[0] - p1[0]) * (p2[1] - p1[1]) < 100:
+                    #     continue
                     left_top = screen_to_equirectangular(box_np[0], box_np[1], pers_width, pers_height, fov_deg,
                                                          math.degrees(pers_image.yaw), math.degrees(pers_image.pitch), width, height)
                     right_bottom = screen_to_equirectangular(box_np[2], box_np[3], pers_width, pers_height, fov_deg,
@@ -274,15 +278,15 @@ for idx_conf in confs:
                     name = f'{name_dict[cls_np]} {conf_np:.2f}'
                     orig_draw.text(xy=(p1[0], p1[1] - font.size - 10), text=name, font=font,
                                    fill=(255, 255, 255))
-                    # cv2.imshow('img', cv2.cvtColor(np.asarray(orig_image), cv2.COLOR_RGB2BGR))
-                    # cv2.waitKey(0)
+                    cv2.imshow('img', cv2.cvtColor(np.asarray(orig_image), cv2.COLOR_RGB2BGR))
+                    cv2.waitKey(0)
                     r = Rectangle(left_top, right_bottom, cls_np, conf_np, pers_image.pitch, pers_image.yaw)
                     r.img_idx = idx
                     r.pers_p1 = [box_np[0], box_np[1]]
                     r.pers_p2 = [box_np[2], box_np[3]]
                     rectangles.append(r)
-                print(f'保存标注图第{idx}张')
-                cv2.imwrite(f'{dir}/标注图/labeled_img_{idx}.jpg', cv2.cvtColor(np.asarray(orig_image), cv2.COLOR_RGB2BGR))
+                # print(f'保存标注图第{idx}张')
+                # cv2.imwrite(f'{dir}/标注图/labeled_img_{idx}.jpg', cv2.cvtColor(np.asarray(orig_image), cv2.COLOR_RGB2BGR))
         # merged = []
         # for rect1 in rectangles:
         #     # 如果该矩形已被其他矩形合并过则不需要再处理

f65568eac83909cdf54923448de9bba18e03e66b
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-05-17 19:17:15 +0200

    Update val.py

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -170,12 +170,8 @@ class DetectionValidator(BaseValidator):
     def get_stats(self):
         """Returns metrics statistics and results dictionary."""
         stats = {k: torch.cat(v, 0).cpu().numpy() for k, v in self.stats.items()}  # to numpy
-        self.nt_per_image = np.bincount(
-            stats["target_img"].astype(int), minlength=self.nc
-        )  # number of targets per image
-        self.nt_per_class = np.bincount(
-            stats["target_cls"].astype(int), minlength=self.nc
-        )  # number of targets per class
+        self.nt_per_image = np.bincount(stats["target_img"].astype(int), minlength=self.nc)
+        self.nt_per_class = np.bincount(stats["target_cls"].astype(int), minlength=self.nc)
         stats.pop("target_img", None)
         if len(stats) and stats["tp"].any():
             self.metrics.process(**stats)

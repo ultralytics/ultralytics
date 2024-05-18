03f546fdb381c2719bf5220a30845fbd21010c6e
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-05-18 13:16:50 +0200

    Import all classes to solutions level
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/solutions/__init__.py
+++ ultralytics/solutions/__init__.py
@@ -1,1 +1,19 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
+
+from distance_calculation import DistanceCalculation
+from heatmap import Heatmap
+from parking_management import ParkingManagement
+from queue_management import QueueManager
+from speed_estimation import SpeedEstimator
+from .ai_gym import AIGym
+from .object_counter import ObjectCounter
+
+__all__ = (
+    "DistanceCalculation",
+    "Heatmap",
+    "ParkingManagement",
+    "QueueManager",
+    "SpeedEstimator",
+    "AIGym",
+    "ObjectCounter",
+)

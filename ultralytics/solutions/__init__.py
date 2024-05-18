62a7584dca2efe388742c3175e0a08804f3a262a
UltralyticsAssistant <web@ultralytics.com>
2024-05-18 12:40:37 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/solutions/__init__.py
+++ ultralytics/solutions/__init__.py
@@ -1,12 +1,12 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
+from .ai_gym import AIGym
 from .distance_calculation import DistanceCalculation
 from .heatmap import Heatmap
+from .object_counter import ObjectCounter
 from .parking_management import ParkingManagement
 from .queue_management import QueueManager
 from .speed_estimation import SpeedEstimator
-from .ai_gym import AIGym
-from .object_counter import ObjectCounter
 
 __all__ = (
     "DistanceCalculation",

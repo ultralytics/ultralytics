cd5224192b604acfd933a320ddcc57d0788b7bee
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-05-18 14:40:06 +0200

    Fix solutions imports
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/solutions/__init__.py
+++ ultralytics/solutions/__init__.py
@@ -1,11 +1,10 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-from distance_calculation import DistanceCalculation
-from heatmap import Heatmap
-from parking_management import ParkingManagement
-from queue_management import QueueManager
-from speed_estimation import SpeedEstimator
-
+from .distance_calculation import DistanceCalculation
+from .heatmap import Heatmap
+from .parking_management import ParkingManagement
+from .queue_management import QueueManager
+from .speed_estimation import SpeedEstimator
 from .ai_gym import AIGym
 from .object_counter import ObjectCounter
 

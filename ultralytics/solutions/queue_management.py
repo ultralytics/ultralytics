5437725d8356478bd0a23854a6994e36478c6777
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-05-18 17:46:31 +0200

    Reformat
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/solutions/queue_management.py
+++ ultralytics/solutions/queue_management.py
@@ -3,12 +3,13 @@
 from collections import defaultdict
 
 import cv2
-from shapely.geometry import Point, Polygon
 
 from ultralytics.utils.checks import check_imshow, check_requirements
 from ultralytics.utils.plotting import Annotator, colors
 
 check_requirements("shapely>=2.0.0")
+
+from shapely.geometry import Point, Polygon
 
 
 class QueueManager:

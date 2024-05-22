4700a75501be4f89a5a1905f2b2f274af61e5303
Burhan <62214284+Burhan-Q@users.noreply.github.com>
2024-05-22 07:19:05 -0400

    `ultralytics 8.2.19` fix Conda CI relative import bug (#13006)
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -18,8 +18,7 @@ from ultralytics.utils import (
     checks,
 )
 from ultralytics.utils.torch_utils import TORCH_1_9, TORCH_1_13
-
-from . import MODEL, SOURCE
+from tests import MODEL, SOURCE
 
 
 def test_export_torchscript():

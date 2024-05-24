0628791daa1d38d94a12bc26e0dd3fc8a5ac2e06
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-05-24 19:35:15 +0200

    fix
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -873,54 +873,56 @@ class Albumentations:
         """Initialize the transform object for YOLO bbox formatted params."""
         self.p = p
         self.transform = None
-        self.spatial_transforms = {
-            "Affine",
-            "BBoxSafeRandomCrop",
-            "CenterCrop",
-            "CoarseDropout",
-            "Crop",
-            "CropAndPad",
-            "CropNonEmptyMaskIfExists",
-            "D4",
-            "ElasticTransform",
-            "Flip",
-            "GridDistortion",
-            "GridDropout",
-            "HorizontalFlip",
-            "Lambda",
-            "LongestMaxSize",
-            "MaskDropout",
-            "MixUp",
-            "Morphological",
-            "NoOp",
-            "OpticalDistortion",
-            "PadIfNeeded",
-            "Perspective",
-            "PiecewiseAffine",
-            "PixelDropout",
-            "RandomCrop",
-            "RandomCropFromBorders",
-            "RandomGridShuffle",
-            "RandomResizedCrop",
-            "RandomRotate90",
-            "RandomScale",
-            "RandomSizedBBoxSafeCrop",
-            "RandomSizedCrop",
-            "Resize",
-            "Rotate",
-            "SafeRotate",
-            "ShiftScaleRotate",
-            "SmallestMaxSize",
-            "Transpose",
-            "VerticalFlip",
-            "XYMasking",
-        }  # from https://albumentations.ai/docs/getting_started/transforms_and_targets/#spatial-level-transforms
-
         prefix = colorstr("albumentations: ")
+
         try:
             import albumentations as A
 
             check_version(A.__version__, "1.0.3", hard=True)  # version requirement
+
+            # List of possible spatial transforms
+            spatial_transforms = {
+                "Affine",
+                "BBoxSafeRandomCrop",
+                "CenterCrop",
+                "CoarseDropout",
+                "Crop",
+                "CropAndPad",
+                "CropNonEmptyMaskIfExists",
+                "D4",
+                "ElasticTransform",
+                "Flip",
+                "GridDistortion",
+                "GridDropout",
+                "HorizontalFlip",
+                "Lambda",
+                "LongestMaxSize",
+                "MaskDropout",
+                "MixUp",
+                "Morphological",
+                "NoOp",
+                "OpticalDistortion",
+                "PadIfNeeded",
+                "Perspective",
+                "PiecewiseAffine",
+                "PixelDropout",
+                "RandomCrop",
+                "RandomCropFromBorders",
+                "RandomGridShuffle",
+                "RandomResizedCrop",
+                "RandomRotate90",
+                "RandomScale",
+                "RandomSizedBBoxSafeCrop",
+                "RandomSizedCrop",
+                "Resize",
+                "Rotate",
+                "SafeRotate",
+                "ShiftScaleRotate",
+                "SmallestMaxSize",
+                "Transpose",
+                "VerticalFlip",
+                "XYMasking",
+            }  # from https://albumentations.ai/docs/getting_started/transforms_and_targets/#spatial-level-transforms
 
             # Transforms
             T = [
@@ -933,14 +935,13 @@ class Albumentations:
                 A.ImageCompression(quality_lower=75, p=0.0),
             ]
 
-            # Check if any of the transforms are spatial
-            self.contains_spatial = any(transform.__class__.__name__ in self.spatial_transforms for transform in T)
+            # Compose transforms
+            self.contains_spatial = any(transform.__class__.__name__ in spatial_transforms for transform in T)
             self.transform = (
                 A.Compose(T, bbox_params=A.BboxParams(format="yolo", label_fields=["class_labels"]))
                 if self.contains_spatial
                 else A.Compose(T)
             )
-
             LOGGER.info(prefix + ", ".join(f"{x}".replace("always_apply=False, ", "") for x in T if x.p))
         except ImportError:  # package not installed, skip
             pass

f7e67e13497e6d00cbd399445fc435ea52f5ad5d
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-05-24 13:44:58 +0200

    Update test_exports.py

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -7,7 +7,6 @@ from pathlib import Path
 
 import pytest
 
-from tests import MODEL, SOURCE
 from ultralytics import YOLO
 from ultralytics.cfg import TASK2DATA, TASK2MODEL, TASKS
 from ultralytics.utils import (
@@ -19,6 +18,7 @@ from ultralytics.utils import (
     checks,
 )
 from ultralytics.utils.torch_utils import TORCH_1_9, TORCH_1_13
+from tests import MODEL, SOURCE
 
 
 def test_export_torchscript():

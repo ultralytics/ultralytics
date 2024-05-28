50cb7fce78158c432cfcef8fd9a6f075ac8f539a
UltralyticsAssistant <web@ultralytics.com>
2024-05-28 09:07:37 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -7,7 +7,7 @@ import os
 os.environ["OMP_NUM_THREADS"] = "1"  # reduce CPU utilization during training
 
 from ultralytics.data.explorer.explorer import Explorer
-from ultralytics.models import RTDETR, SAM, YOLO, YOLOWorld, FastSAM, NAS
+from ultralytics.models import NAS, RTDETR, SAM, YOLO, FastSAM, YOLOWorld
 from ultralytics.utils import ASSETS, SETTINGS
 from ultralytics.utils.checks import check_yolo as checks
 from ultralytics.utils.downloads import download
--- ultralytics/models/__init__.py
+++ ultralytics/models/__init__.py
@@ -1,9 +1,9 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
+from .fastsam import FastSAM
+from .nas import NAS
 from .rtdetr import RTDETR
 from .sam import SAM
 from .yolo import YOLO, YOLOWorld
-from .nas import NAS
-from .fastsam import FastSAM
 
 __all__ = "YOLO", "RTDETR", "SAM", "FastSAM", "NAS", "YOLOWorld"  # allow simpler import

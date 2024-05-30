af6e2a2cc187386ed610738f3cbecf7c29dfc148
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-05-30 18:39:39 +0200

    Add human to TASKS

--- ultralytics/cfg/__init__.py
+++ ultralytics/cfg/__init__.py
@@ -31,7 +31,7 @@ from ultralytics.utils import (
 
 # Define valid tasks and modes
 MODES = {"train", "val", "predict", "export", "track", "benchmark"}
-TASKS = {"detect", "segment", "classify", "pose", "obb"}
+TASKS = {"detect", "segment", "classify", "pose", "obb", "human"}
 TASK2DATA = {
     "detect": "coco8.yaml",
     "segment": "coco8-seg.yaml",

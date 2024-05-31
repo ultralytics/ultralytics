dcfc614f39bb8422928848c0d17f5a1af6109be4
UltralyticsAssistant <web@ultralytics.com>
2024-05-31 15:28:12 +0000

    Auto-format by https://ultralytics.com/actions

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -73,7 +73,7 @@ def test_export_openvino_matrix(task, dynamic, int8, half, batch):
 
 @pytest.mark.slow
 @pytest.mark.parametrize(
-    "task, dynamic, int8, half, batch, simplify",  product(TASKS, [True, False], [False], [False], [1, 2], [True, False])
+    "task, dynamic, int8, half, batch, simplify", product(TASKS, [True, False], [False], [False], [1, 2], [True, False])
 )
 def test_export_onnx_matrix(task, dynamic, int8, half, batch, simplify):
     """Test YOLO exports to ONNX format."""

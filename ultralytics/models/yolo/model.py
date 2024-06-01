ef3cb367daba2dbaf02231e986dc6cefc0940994
Niels <niels.rogge1@gmail.com>
2024-06-01 10:52:17 +0200

    Add mixin

--- ultralytics/models/yolo/model.py
+++ ultralytics/models/yolo/model.py
@@ -7,8 +7,10 @@ from ultralytics.models import yolo
 from ultralytics.nn.tasks import ClassificationModel, DetectionModel, OBBModel, PoseModel, SegmentationModel, WorldModel
 from ultralytics.utils import ROOT, yaml_load
 
+from huggingface_hub import PyTorchModelHubMixin
 
-class YOLO(Model):
+
+class YOLO(Model, PyTorchModelHubMixin):
     """YOLO (You Only Look Once) object detection model."""
 
     def __init__(self, model="yolov8n.pt", task=None, verbose=False):

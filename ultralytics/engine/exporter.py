d9a1f220ab02117d18ef3c2d7e5c5949df80a05e
Burhan <62214284+Burhan-Q@users.noreply.github.com>
2024-06-06 19:51:07 -0400

    add cmake and onnxsim to check requirements for saved_model export, required for YOLOv10

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -610,7 +610,7 @@ class Exporter:
         LOGGER.info(f"\n{prefix} starting export with coremltools {ct.__version__}...")
         assert not WINDOWS, "CoreML export is not supported on Windows, please run on macOS or Linux."
         assert self.args.batch == 1, "CoreML batch sizes > 1 are not supported. Please retry at 'batch=1'."
-        assert not self.end2end, "CoreML export is not supported for end2end models."
+        assert not self.end2end, f"{prefix.strip(':')} export is not supported for end-2-end models."
         f = self.file.with_suffix(".mlmodel" if mlmodel else ".mlpackage")
         if f.is_dir():
             shutil.rmtree(f)
@@ -832,7 +832,7 @@ class Exporter:
                 "tflite_support<=0.4.3" if IS_JETSON else "tflite_support",  # fix ImportError 'GLIBCXX_3.4.29'
                 "flatbuffers>=23.5.26,<100",  # update old 'flatbuffers' included inside tensorflow package
                 "onnxruntime-gpu" if cuda else "onnxruntime",
-            ),
+            ) + ("cmake", "onnxsim>=0.4.33") if self.end2end else (),
             cmds="--extra-index-url https://pypi.ngc.nvidia.com",  # onnx_graphsurgeon only on NVIDIA
         )
 

a5fc01933b0267366bb0448d2c2fbf64971e113e
UltralyticsAssistant <web@ultralytics.com>
2024-06-07 00:59:10 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -833,9 +833,7 @@ class Exporter:
                 "flatbuffers>=23.5.26,<100",  # update old 'flatbuffers' included inside tensorflow package
                 "onnxruntime-gpu" if cuda else "onnxruntime",
             )
-            + (("cmake", "onnxsim>=0.4.33")
-            if self.end2end
-            else ()),
+            + (("cmake", "onnxsim>=0.4.33") if self.end2end else ()),
             cmds="--extra-index-url https://pypi.ngc.nvidia.com",  # onnx_graphsurgeon only on NVIDIA
         )
 

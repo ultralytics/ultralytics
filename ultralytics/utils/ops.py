a1cbda0e26c634745e9d3521f608d6dc27c26b7a
宋慧青 <shq2976@163.com>
2024-06-18 02:41:46 +0000

    fix issue 13587

--- ultralytics/utils/ops.py
+++ ultralytics/utils/ops.py
@@ -210,6 +210,10 @@ def non_max_suppression(
     # Checks
     assert 0 <= conf_thres <= 1, f"Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0"
     assert 0 <= iou_thres <= 1, f"Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0"
+
+    if isinstance(prediction, dict):  # model.pt, output = {"one2many": torch.Tensor, "one2one": torch.Tensor }
+        prediction = prediction.get("one2one", prediction)
+        
     if isinstance(prediction, (list, tuple)):  # YOLOv8 model in validation model, output = (inference_out, loss_out)
         prediction = prediction[0]  # select only inference output
 

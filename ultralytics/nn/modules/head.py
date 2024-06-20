54dcf454d49d1cf280514b1a4804ca246cd50d23
Laughing-q <1185102784@qq.com>
2024-06-20 18:56:08 +0800

    update head

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -37,15 +37,15 @@ class Detect(nn.Module):
         self.cv2 = nn.ModuleList(
             nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch
         )
-        self.cv3 = nn.ModuleList(nn.Sequential(Conv(x, c3, 3), Conv(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
-        # self.cv3 = nn.ModuleList(
-        #     nn.Sequential(
-        #         nn.Sequential(DWConv(x, x, 3), Conv(x, c3, 1)),
-        #         nn.Sequential(DWConv(c3, c3, 3), Conv(c3, c3, 1)),
-        #         nn.Conv2d(c3, self.nc, 1),
-        #     )
-        #     for x in ch
-        # )
+        # self.cv3 = nn.ModuleList(nn.Sequential(Conv(x, c3, 3), Conv(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
+        self.cv3 = nn.ModuleList(
+            nn.Sequential(
+                nn.Sequential(DWConv(x, x, 3), Conv(x, c3, 1)),
+                nn.Sequential(DWConv(c3, c3, 3), Conv(c3, c3, 1)),
+                nn.Conv2d(c3, self.nc, 1),
+            )
+            for x in ch
+        )
         self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
 
     def forward(self, x):

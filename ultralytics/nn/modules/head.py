69e0a583455fe3c2e5f5e7673727e00e2bca80ac
Laughing-q <1185102784@qq.com>
2024-06-21 11:10:11 +0800

    update head

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -336,7 +336,7 @@ class WorldDetect(Detect):
 class HumanDetect(Detect):
     def __init__(self, nc=80, ch=()):
         super().__init__(nc, ch)
-        c4, c5 = max((16, ch[0] // 4, self.reg_max)), 32
+        c4 = max((16, ch[0] // 8, self.reg_max))
         # height
         self.cv4 = nn.ModuleList(
             nn.Sequential(Conv(x, c4, 3), Conv(c4, c4, 3), nn.Conv2d(c4, self.reg_max, 1)) for x in ch
@@ -350,9 +350,9 @@ class HumanDetect(Detect):
             nn.Sequential(Conv(x, c4, 3), Conv(c4, c4, 3), nn.Conv2d(c4, self.reg_max, 1)) for x in ch
         )
         # gender, 2 classes
-        self.cv7 = nn.ModuleList(nn.Sequential(Conv(x, c5, 3), Conv(c5, c5, 3), nn.Conv2d(c5, 2, 1)) for x in ch)
+        self.cv7 = nn.ModuleList(nn.Sequential(Conv(x, c4, 3), Conv(c4, c4, 3), nn.Conv2d(c4, 2, 1)) for x in ch)
         # ethnicity, 6 classes
-        self.cv8 = nn.ModuleList(nn.Sequential(Conv(x, c5, 3), Conv(c5, c5, 3), nn.Conv2d(c5, 6, 1)) for x in ch)
+        self.cv8 = nn.ModuleList(nn.Sequential(Conv(x, c4, 3), Conv(c4, c4, 3), nn.Conv2d(c4, 6, 1)) for x in ch)
 
     def forward(self, x):
         bs = x[0].shape[0]  # batch size

9e56569e79229ff70dc7e78fcf8c9af787160c06
Laughing-q <1185102784@qq.com>
2024-06-21 11:20:42 +0800

    fix

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -168,7 +168,7 @@ class v8DetectionLoss:
         self.device = device
         self.use_dfl = m.reg_max > 1
         self.assigner = TaskAlignedAssigner(topk=tal_topk, num_classes=self.nc, alpha=0.5, beta=6.0)
-        self.bbox_loss = BboxLoss(m.reg_max, use_dfl=self.use_dfl).to(device)
+        self.bbox_loss = BboxLoss(m.reg_max).to(device)
         self.proj = torch.arange(m.reg_max, dtype=torch.float, device=device)
 
     def preprocess(self, targets, batch_size, scale_tensor):

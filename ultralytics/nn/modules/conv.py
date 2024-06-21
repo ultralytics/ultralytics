9bdf5f9194d471f1f236945413d383ecd9b83dc3
UltralyticsAssistant <web@ultralytics.com>
2024-06-21 14:32:56 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -106,7 +106,9 @@ class LightConv(nn.Module):
 class DWConv(Conv):
     """Depth-wise convolution."""
 
-    def __init__(self, c1, c2, k=1, s=1, d=1, act=True, norm_type="none"):  # ch_in, ch_out, kernel, stride, dilation, activation
+    def __init__(
+        self, c1, c2, k=1, s=1, d=1, act=True, norm_type="none"
+    ):  # ch_in, ch_out, kernel, stride, dilation, activation
         """Initialize Depth-wise convolution with given parameters."""
         super().__init__(c1, c2, k, s, g=math.gcd(c1, c2), d=d, act=act, norm_type=norm_type)
 

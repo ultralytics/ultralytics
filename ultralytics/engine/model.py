797d1bcc7f7a702edc9883155ebafe0cebf9d443
UltralyticsAssistant <web@ultralytics.com>
2024-06-23 00:08:16 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/engine/model.py
+++ ultralytics/engine/model.py
@@ -9,8 +9,8 @@ import torch
 
 from ultralytics.cfg import TASK2DATA, get_cfg, get_save_dir
 from ultralytics.engine.results import Results
-from ultralytics.hub.utils import HUB_WEB_ROOT
 from ultralytics.hub.session import get_hub_session
+from ultralytics.hub.utils import HUB_WEB_ROOT
 from ultralytics.nn.tasks import attempt_load_one_weight, guess_model_task, nn, yaml_model_load
 from ultralytics.utils import (
     ARGV,

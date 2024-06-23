08ceeafcdc7864560e7aa623f7a273c9a7ab8538
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-06-23 18:50:47 +0200

    Refactor create_session
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/utils/callbacks/hub.py
+++ ultralytics/utils/callbacks/hub.py
@@ -4,12 +4,12 @@ import json
 from time import time
 
 from ultralytics.hub import HUB_WEB_ROOT, PREFIX, HUBTrainingSession, events
-from ultralytics.utils import LOGGER, SETTINGS
+from ultralytics.utils import LOGGER, RANK, SETTINGS
 
 
 def on_pretrain_routine_start(trainer):
     """Create a remote Ultralytics HUB session to log local model training."""
-    if SETTINGS["hub"] is True and SETTINGS["api_key"] and not getattr(trainer, "hub_session", None):
+    if RANK in {-1, 0} and SETTINGS["hub"] is True and SETTINGS["api_key"] and trainer.hub_session is None:
         trainer.hub_session = HUBTrainingSession.create_session(trainer.args.model, trainer.args)
 
 

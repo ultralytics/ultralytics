913464372ddd90c8c93f46e3f151c337c1446a4e
takipipo <ktphap.ncu@gmail.com>
2024-06-28 16:11:49 +0700

    fix: utilize Neuron AutoBackend

--- ultralytics/models/yolo/detect/__init__.py
+++ ultralytics/models/yolo/detect/__init__.py
@@ -1,7 +1,13 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
 from .predict import DetectionPredictor
+from .neuron_predict import NeuronDetectionPredictor
 from .train import DetectionTrainer
 from .val import DetectionValidator
 
-__all__ = "DetectionPredictor", "DetectionTrainer", "DetectionValidator"
+__all__ = (
+    "DetectionPredictor",
+    "DetectionTrainer",
+    "DetectionValidator",
+    "NeuronDetectionPredictor",
+)
--- ultralytics/models/yolo/neuron_model.py
+++ ultralytics/models/yolo/neuron_model.py
@@ -1,5 +1,16 @@
 from .model import YOLO
+from ultralytics.models import yolo
+
 
 class NeuronYOLO(YOLO):
     def __init__(self, model="yolov8n.pt", task=None, verbose=False):
         super().__init__(model, task, verbose)
+
+    @property
+    def task_map(self):
+        """Map head to model, trainer, validator, and predictor classes."""
+        return {
+            "detect": {
+                "predictor": yolo.detect.NeuronDetectionPredictor,
+            },
+        }

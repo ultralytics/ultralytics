e426b7f4a79c18b359e6a37480e98e8e3d76a2f4
Laughing-q <1185102784@qq.com>
2024-06-29 09:57:06 +0800

    add humanvalidator

--- ultralytics/models/yolo/human/val.py
+++ ultralytics/models/yolo/human/val.py
@@ -4,7 +4,9 @@ from ultralytics.data.dataset import HumanDataset
 from ultralytics.engine.results import Human, Results
 from ultralytics.engine.validator import BaseValidator
 from ultralytics.utils import colorstr
-from ultralytics.utils.metrics import HumanMetrics, box_iou
+from ultralytics.utils.metrics import HumanMetrics
+from pathlib import Path
+import numpy as np
 
 
 class HumanValidator(BaseValidator):
@@ -24,7 +26,7 @@ class HumanValidator(BaseValidator):
     def __init__(self, dataloader=None, save_dir=None, pbar=None, args=None, _callbacks=None):
         super().__init__(dataloader, save_dir, pbar, args, _callbacks)
         self.args.task = "human"
-        self.metrics = HumanMetrics(save_dir=self.save_dir, plot=True, on_plot=self.on_plot)
+        self.metrics = HumanMetrics()
 
     def build_dataset(self, img_path, mode="val", batch=None):
         return HumanDataset(
@@ -42,31 +44,28 @@ class HumanValidator(BaseValidator):
         return batch
 
     def update_metrics(self, preds, batch):
-        """Metrics."""
-        pass
-
-    def _process_attributes(self, pred_attrs, gt_attrs):
         """
         Process Human Attributes and compute the accuracy.
 
         Args:
-            predn_attrs (torch.Tensor): The predictions of attributes with shape [M, 11].
-            gt_attrs (torch.Tensor): The grounding truth of attributes with shape [N, 5].
+            predn (torch.Tensor): The predictions of attributes with shape [M, 11].
+            batch (Dict): A dict contains grounding truth of attributes with shape [N, 5].
 
         Returns:
             The accuracy for each human attribute.
         """
-        pred_attrs = Human(pred_attrs)
+        preds = Human(preds)
+        gt_attrs = batch["attributes"]
         weight = gt_attrs[:, 0]
         height = gt_attrs[:, 1]
         gender = gt_attrs[:, 2]
         age = gt_attrs[:, 3]
         ethnicity = gt_attrs[:, 4]
-        acc_w = 1 - (pred_attrs.weight - weight).abs() / weight
-        acc_h = 1 - (pred_attrs.height - height).abs() / height
-        acc_g = (pred_attrs.cls_gender == gender).float()
-        acc_a = 1 - (pred_attrs.age - age).abs() / age
-        acc_e = (pred_attrs.cls_ethnicity == ethnicity).float()
+        acc_w = 1 - (preds.weight - weight).abs() / weight
+        acc_h = 1 - (preds.height - height).abs() / height
+        acc_g = (preds.cls_gender == gender).float()
+        acc_a = 1 - (preds.age - age).abs() / age
+        acc_e = (preds.cls_ethnicity == ethnicity).float()
 
         self.metrics.attrs_stats["weight"].append(acc_w.clip(0, 1))
         self.metrics.attrs_stats["height"].append(acc_h.clip(0, 1))
@@ -74,12 +73,16 @@ class HumanValidator(BaseValidator):
         self.metrics.attrs_stats["age"].append(acc_a.clip(0, 1))
         self.metrics.attrs_stats["ethnicity"].append(acc_e)
 
+        if self.args.save_txt:
+            for i, pred in enumerate(preds):
+                file = self.save_dir / "labels" / f'{Path(batch["im_file"][i]).stem}.txt'
+                self.save_one_txt(pred, self.args.save_conf, batch["ori_shape"], file)
+
     def save_one_txt(self, predn, save_conf, shape, file):
         """Save YOLO detections to a txt file in normalized coordinates in a specific format."""
-        # im = np.zeros((shape[0], shape[1]), dtype=np.uint8)
-        # result = Results(im, path=None, names=self.names, boxes=predn[:, :6], human=predn[:, 6:])
-        # result.save_txt(file, save_conf=save_conf)
-        pass
+        im = np.zeros((shape[0], shape[1]), dtype=np.uint8)
+        result = Results(im, path=None, names=self.names, human=predn)
+        result.save_txt(file, save_conf=save_conf)
 
     def get_desc(self):
         """Return a formatted description of evaluation metrics."""

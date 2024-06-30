e871408284ad57e5aca175121c906ecd6f3f88bf
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-06-30 20:12:03 +0200

    Update
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/data/explorer/explorer.py
+++ ultralytics/data/explorer/explorer.py
@@ -60,6 +60,7 @@ class Explorer:
         model: str = "yolov8n.pt",
         uri: str = USER_CONFIG_DIR / "explorer",
     ) -> None:
+        """Initializes the Explorer class with dataset path, model, and URI for database connection."""
         # Note duckdb==0.10.0 bug https://github.com/ultralytics/ultralytics/pull/8181
         checks.check_requirements(["lancedb>=0.4.3", "duckdb<=0.9.2"])
         import lancedb
@@ -417,6 +418,7 @@ class Explorer:
     def _check_imgs_or_idxs(
         self, img: Union[str, np.ndarray, List[str], List[np.ndarray], None], idx: Union[None, int, List[int]]
     ) -> List[np.ndarray]:
+        """Determines whether to fetch images or indexes based on provided arguments and returns image paths."""
         if img is None and idx is None:
             raise ValueError("Either img or idx must be provided.")
         if img is not None and idx is not None:

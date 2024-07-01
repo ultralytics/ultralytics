ea093ab79e4dd13d5687fa52ea4ca094457de834
Laughing-q <1185102784@qq.com>
2024-07-01 13:56:52 +0800

    revert

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -1243,8 +1243,7 @@ def classify_transforms(
         tfl = [T.Resize(scale_size[0], interpolation=interpolation)]
     else:
         # Resize the shortest edge to matching target dim for non-square target
-        # tfl = [T.Resize(scale_size, interpolation=interpolation)]
-        tfl = [ClassifyLetterBox(size)]
+        tfl = [T.Resize(scale_size, interpolation=interpolation)]
     tfl.extend(
         [
             T.CenterCrop(size),
@@ -1303,8 +1302,7 @@ def classify_augmentations(
     scale = tuple(scale or (0.08, 1.0))  # default imagenet scale range
     ratio = tuple(ratio or (3.0 / 4.0, 4.0 / 3.0))  # default imagenet ratio range
     primary_tfl = [
-        # T.Resize(size) if resize else T.RandomResizedCrop(size, scale=scale, ratio=ratio, interpolation=interpolation)
-        ClassifyLetterBox(size) if resize else T.RandomResizedCrop(size, scale=scale, ratio=ratio, interpolation=interpolation)
+        T.Resize(size) if resize else T.RandomResizedCrop(size, scale=scale, ratio=ratio, interpolation=interpolation)
     ]
     if hflip > 0.0:
         primary_tfl.append(T.RandomHorizontalFlip(p=hflip))
@@ -1321,7 +1319,8 @@ def classify_augmentations(
 
         if auto_augment == "randaugment":
             if TORCHVISION_0_11:
-                secondary_tfl.append(RandAugment(interpolation=interpolation))
+                secondary_tfl.append(T.RandAugment(interpolation=interpolation))
+                # secondary_tfl.append(RandAugment(interpolation=interpolation))
             else:
                 LOGGER.warning('"auto_augment=randaugment" requires torchvision >= 0.11.0. Disabling it.')
 

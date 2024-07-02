6d94204a6cfbd7704f608918bd43f4288395376d
takipipo <ktphap.ncu@gmail.com>
2024-07-02 11:04:02 +0700

    fix: NeuronYOLO base class

--- ultralytics/models/__init__.py
+++ ultralytics/models/__init__.py
@@ -6,4 +6,11 @@ from .rtdetr import RTDETR
 from .sam import SAM
 from .yolo import YOLO, YOLOWorld, NeuronYOLO
 
-__all__ = "YOLO", "RTDETR", "SAM", "FastSAM", "NAS", "YOLOWorld"  # allow simpler import
+__all__ = (
+    "YOLO",
+    "RTDETR",
+    "SAM",
+    "FastSAM",
+    "NAS",
+    "YOLOWorld, NeuronYOLO",
+)  # allow simpler import

7c8cb38084b977a74208dcdee0820a8bcfbb2e8b
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-07-05 20:13:41 +0200

    Update results.py

--- ultralytics/engine/results.py
+++ ultralytics/engine/results.py
@@ -42,9 +42,7 @@ class BaseTensor(SimpleClass):
             base_tensor = BaseTensor(data, orig_shape)
             ```
         """
-        assert isinstance(
-            data, (torch.Tensor, np.ndarray)
-        ), f"Provided data should be torch.Tensor or np.ndarray, not type {type(data)}"
+        assert isinstance(data, (torch.Tensor, np.ndarray)), f"data must be torch.Tensor or np.ndarray"
         self.data = data
         self.orig_shape = orig_shape
 

9460152300228f98cade09a7a583591f165a1f6a
Dan Phan <danphan2884@gmail.com>
2024-07-06 13:27:58 -0700

    Simplify probiou calculation

--- ultralytics/utils/metrics.py
+++ ultralytics/utils/metrics.py
@@ -205,26 +205,32 @@ def probiou(obb1, obb2, CIoU=False, eps=1e-7):
     Returns:
         (torch.Tensor): A tensor of shape (N, ) representing obb similarities.
     """
-    x1, y1 = obb1[..., :2].split(1, dim=-1)
-    x2, y2 = obb2[..., :2].split(1, dim=-1)
+    x1, y1, w1, h1, theta1 = obb1.split(1, dim=-1)
+    x2, y2, w2, h2, theta2 = obb2.split(1, dim=-1)
     a1, b1, c1 = _get_covariance_matrix(obb1)
     a2, b2, c2 = _get_covariance_matrix(obb2)
 
+    sqrt_det1 = w1 * h1 / 12
+    sqrt_det2 = w2 * h2 / 12
+
+    det_sum = (
+        sqrt_det1.pow(2) + sqrt_det2.pow(2)
+        + (w1.pow(2) * h2.pow(2) + w2.pow(2) * h1.pow(2)) * torch.cos(theta2 - theta1).pow(2) / 144
+        + (w1.pow(2) * h1.pow(2) + w2.pow(2) * h2.pow(2))  * torch.sin(theta2 - theta1).pow(2) / 144
+    )
     t1 = (
-        ((a1 + a2) * (y1 - y2).pow(2) + (b1 + b2) * (x1 - x2).pow(2)) / ((a1 + a2) * (b1 + b2) - (c1 + c2).pow(2) + eps)
+        ((a1 + a2) * (y1 - y2).pow(2) + (b1 + b2) * (x1 - x2).pow(2)) / (det_sum + eps)
     ) * 0.25
-    t2 = (((c1 + c2) * (x2 - x1) * (y1 - y2)) / ((a1 + a2) * (b1 + b2) - (c1 + c2).pow(2) + eps)) * 0.5
+    t2 = (((c1 + c2) * (x2 - x1) * (y1 - y2)) / (det_sum + eps)) * 0.5
     t3 = (
-        ((a1 + a2) * (b1 + b2) - (c1 + c2).pow(2))
-        / (4 * ((a1 * b1 - c1.pow(2)).clamp_(0) * (a2 * b2 - c2.pow(2)).clamp_(0)).sqrt() + eps)
+        det_sum
+        / (4 * sqrt_det1 * sqrt_det2 + eps)
         + eps
     ).log() * 0.5
     bd = (t1 + t2 + t3).clamp(eps, 100.0)
     hd = (1.0 - (-bd).exp() + eps).sqrt()
     iou = 1 - hd
     if CIoU:  # only include the wh aspect ratio part
-        w1, h1 = obb1[..., 2:4].split(1, dim=-1)
-        w2, h2 = obb2[..., 2:4].split(1, dim=-1)
         v = (4 / math.pi**2) * ((w2 / h2).atan() - (w1 / h1).atan()).pow(2)
         with torch.no_grad():
             alpha = v / (v - iou + (1 + eps))
@@ -247,18 +253,27 @@ def batch_probiou(obb1, obb2, eps=1e-7):
     obb1 = torch.from_numpy(obb1) if isinstance(obb1, np.ndarray) else obb1
     obb2 = torch.from_numpy(obb2) if isinstance(obb2, np.ndarray) else obb2
 
-    x1, y1 = obb1[..., :2].split(1, dim=-1)
-    x2, y2 = (x.squeeze(-1)[None] for x in obb2[..., :2].split(1, dim=-1))
+    x1, y1, w1, h1, theta1 = obb1.split(1, dim=-1)
+    x2, y2, w2, h2, theta2 = (x.squeeze(-1)[None] for x in obb2.split(1, dim=-1))
     a1, b1, c1 = _get_covariance_matrix(obb1)
     a2, b2, c2 = (x.squeeze(-1)[None] for x in _get_covariance_matrix(obb2))
 
+    sqrt_det1 = w1 * h1 / 12
+    sqrt_det2 = w2 * h2 / 12
+
+    det_sum = (
+        sqrt_det1.pow(2) + sqrt_det2.pow(2)
+        + (w1.pow(2) * h2.pow(2) + w2.pow(2) * h1.pow(2)) * torch.cos(theta2 - theta1).pow(2) / 144
+        + (w1.pow(2) * h1.pow(2) + w2.pow(2) * h2.pow(2))  * torch.sin(theta2 - theta1).pow(2) / 144
+    )
+
     t1 = (
-        ((a1 + a2) * (y1 - y2).pow(2) + (b1 + b2) * (x1 - x2).pow(2)) / ((a1 + a2) * (b1 + b2) - (c1 + c2).pow(2) + eps)
+        ((a1 + a2) * (y1 - y2).pow(2) + (b1 + b2) * (x1 - x2).pow(2)) / (det_sum + eps)
     ) * 0.25
-    t2 = (((c1 + c2) * (x2 - x1) * (y1 - y2)) / ((a1 + a2) * (b1 + b2) - (c1 + c2).pow(2) + eps)) * 0.5
+    t2 = (((c1 + c2) * (x2 - x1) * (y1 - y2)) / (det_sum + eps)) * 0.5
     t3 = (
-        ((a1 + a2) * (b1 + b2) - (c1 + c2).pow(2))
-        / (4 * ((a1 * b1 - c1.pow(2)).clamp_(0) * (a2 * b2 - c2.pow(2)).clamp_(0)).sqrt() + eps)
+        det_sum
+        / (4 * sqrt_det1 * sqrt_det2 + eps)
         + eps
     ).log() * 0.5
     bd = (t1 + t2 + t3).clamp(eps, 100.0)

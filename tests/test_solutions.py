23c596c35a623959c570da326e2e5df88ec40544
RizwanMunawar <chr043416@gmail.com>
2024-07-11 18:27:20 +0500

    update

--- tests/test_solutions.py
+++ tests/test_solutions.py
@@ -43,38 +43,3 @@ def test_major_solutions():
     cap.release()
     cv2.destroyAllWindows()
 
-@pytest.mark.slow
-def test_det():
-    """Test detection functionalities and verify embedding table includes bounding boxes."""
-    exp = Explorer(data="coco8.yaml", model="yolov8n.pt")
-    exp.create_embeddings_table(force=True)
-    assert len(exp.table.head()["bboxes"]) > 0
-    similar = exp.get_similar(idx=[1, 2], limit=10)
-    assert len(similar) > 0
-    # This is a loose test, just checks errors not correctness
-    similar = exp.plot_similar(idx=[1, 2], limit=10)
-    assert isinstance(similar, PIL.Image.Image)
-
-
-@pytest.mark.slow
-def test_seg():
-    """Test segmentation functionalities and ensure the embedding table includes segmentation masks."""
-    exp = Explorer(data="coco8-seg.yaml", model="yolov8n-seg.pt")
-    exp.create_embeddings_table(force=True)
-    assert len(exp.table.head()["masks"]) > 0
-    similar = exp.get_similar(idx=[1, 2], limit=10)
-    assert len(similar) > 0
-    similar = exp.plot_similar(idx=[1, 2], limit=10)
-    assert isinstance(similar, PIL.Image.Image)
-
-
-@pytest.mark.slow
-def test_pose():
-    """Test pose estimation functionality and verify the embedding table includes keypoints."""
-    exp = Explorer(data="coco8-pose.yaml", model="yolov8n-pose.pt")
-    exp.create_embeddings_table(force=True)
-    assert len(exp.table.head()["keypoints"]) > 0
-    similar = exp.get_similar(idx=[1, 2], limit=10)
-    assert len(similar) > 0
-    similar = exp.plot_similar(idx=[1, 2], limit=10)
-    assert isinstance(similar, PIL.Image.Image)

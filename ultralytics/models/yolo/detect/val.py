9c1a89a629d821e12e482eb5024ee6029a37141b
Laughing-q <1185102784@qq.com>
2024-07-17 13:34:24 +0800

    Update val.py

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -159,8 +159,7 @@ class DetectionValidator(BaseValidator):
             if self.args.save_json:
                 self.pred_to_json(predn, batch["im_file"][si])
             if self.args.save_txt:
-                file = self.save_dir / "labels" / f'{Path(batch["im_file"][si]).stem}.txt'
-                self.save_one_txt(predn, self.args.save_conf, pbatch["ori_shape"], file)
+                self.save_one_txt(predn, self.args.save_conf, pbatch["ori_shape"], self.save_dir / "labels" / f'{Path(batch["im_file"][si]).stem}.txt')
 
     def finalize_metrics(self, *args, **kwargs):
         """Set final values for metrics speed and confusion matrix."""

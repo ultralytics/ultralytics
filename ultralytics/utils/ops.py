9e97531cdb14fa6326d384d28ad666ab3bad89ef
Laughing-q <1185102784@qq.com>
2024-07-18 11:16:05 +0800

    remove process_mask_upsample

--- ultralytics/utils/ops.py
+++ ultralytics/utils/ops.py
@@ -652,26 +652,6 @@ def crop_mask(masks, boxes):
     return masks * ((r >= x1) * (r < x2) * (c >= y1) * (c < y2))
 
 
-def process_mask_upsample(protos, masks_in, bboxes, shape):
-    """
-    Takes the output of the mask head, and applies the mask to the bounding boxes. This produces masks of higher quality
-    but is slower.
-
-    Args:
-        protos (torch.Tensor): [mask_dim, mask_h, mask_w]
-        masks_in (torch.Tensor): [n, mask_dim], n is number of masks after nms
-        bboxes (torch.Tensor): [n, 4], n is number of masks after nms
-        shape (tuple): the size of the input image (h,w)
-
-    Returns:
-        (torch.Tensor): The upsampled masks.
-    """
-    from ultralytics.utils import deprecation_warn
-
-    deprecation_warn("process_mask_upsample", "process_mask_native")
-    return process_mask_native(protos, masks_in, bboxes, shape)
-
-
 def process_mask(protos, masks_in, bboxes, shape, upsample=False):
     """
     Apply masks to bounding boxes using the output of the mask head.

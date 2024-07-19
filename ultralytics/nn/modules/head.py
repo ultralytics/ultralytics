b83ab03c220ed083812e0a521ec8d882f7a344c5
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-07-19 20:16:00 +0200

    Update head.py

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -281,11 +281,6 @@ class Classify(nn.Module):
 
 
 class WorldDetect(Detect):
-    """
-    Text-conditioned object detection using contrastive learning.
-
-    [Ref](https://arxiv.org/abs/2109.10355)
-    """
 
     def __init__(self, nc=80, embed=512, with_bn=False, ch=()):
         """Initialize YOLOv8 detection layer with nc classes and layer channels ch."""

6bb30c4589143f5403bfbd665da5a2659c4b625f
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-07-23 18:38:58 +0200

    Fix `torch.amp.autocast('cuda')`` warnings
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -264,7 +264,7 @@ class BaseTrainer:
         if RANK > -1 and world_size > 1:  # DDP
             dist.broadcast(self.amp, src=0)  # broadcast the tensor from rank 0 to all other ranks (returns None)
         self.amp = bool(self.amp)  # as boolean
-        self.scaler = torch.cuda.amp.GradScaler(enabled=self.amp)
+        self.scaler = torch.amp.GradScaler("cuda", enabled=self.amp)
         if world_size > 1:
             self.model = nn.parallel.DistributedDataParallel(self.model, device_ids=[RANK], find_unused_parameters=True)
 

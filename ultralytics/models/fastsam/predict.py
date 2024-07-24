91883dfbd8d3c98b8be6c5e3de4197778184d010
Laughing-q <1185102784@qq.com>
2024-07-24 17:34:08 +0800

    Update predict.py

--- ultralytics/models/fastsam/predict.py
+++ ultralytics/models/fastsam/predict.py
@@ -3,6 +3,7 @@ import torch
 
 from ultralytics.models.yolo.segment import SegmentationPredictor
 from ultralytics.utils.metrics import box_iou
+from ultralytics.utils import DEFAULT_CFG
 
 from .utils import adjust_bboxes_to_image_border
 
@@ -17,10 +18,17 @@ class FastSAMPredictor(SegmentationPredictor):
     class segmentation.
     """
 
+    def __init__(self, cfg=DEFAULT_CFG, overrides=None, _callbacks=None):
+        super().__init__(cfg, overrides, _callbacks)
+        self.prompts = {}
+
     def postprocess(self, preds, img, orig_imgs):
         """Applies box postprocess for FastSAM predictions."""
+        bboxes = self.prompts.pop("bboxes", None)
+        points = self.prompts.pop("points", None)
+        texts = self.prompts.pop("texts", None)
         results = super().postprocess(preds, img, orig_imgs)
-        for result in results:
+        for i, result in enumerate(results):
             full_box = torch.tensor(
                 [0, 0, result.orig_shape[1], result.orig_shape[0]], device=preds[0].device, dtype=torch.float32
             )
@@ -28,23 +36,21 @@ class FastSAMPredictor(SegmentationPredictor):
             idx = torch.nonzero(box_iou(full_box[None], boxes) > 0.9).flatten()
             if idx.numel() != 0:
                 result.boxes.xyxy[idx] = full_box
+
+            # bboxes prompt
+            if bboxes is not None:
+                bboxes = torch.as_tensor(bboxes, dtype=torch.int32, device=self.device)
+                bboxes = bboxes[None] if bboxes.ndim == 1 else bboxes
+                masks = result.masks.data
+                bbox_areas = (bboxes[:, 3] - bboxes[:, 1]) * (bboxes[:, 2] - bboxes[:, 0])
+                mask_areas = torch.stack([masks[:, b[1] : b[3], b[0] : b[2]].sum(dim=(1, 2)) for b in bboxes])
+                full_mask_areas = torch.sum(masks, dim=(1, 2))
+
+                union = bbox_areas[:, None] + full_mask_areas - mask_areas
+                idx = torch.argmax(mask_areas / union, dim=1)
+                results[i] = result[idx]
         return results
 
-    def inference(self, im, bboxes=None, points=None, texts=None):
-        """
-        Perform image segmentation inference based on the given input cues, using the currently loaded image. This
-        method leverages FastSAM's (Based on Ultralytics YOLOv8) architecture and CLIP model for real-time and 
-        promptable segmentation tasks.
-
-        Args:
-            im (torch.Tensor): The preprocessed input image in tensor format, with shape (N, C, H, W).
-            bboxes (np.ndarray | List, optional): Bounding boxes with shape (N, 4), in XYXY format.
-            points (np.ndarray | List, optional): Points indicating object locations with shape (N, 2), in pixels.
-            texts (List, optional): Textual prompts.
-        """
-        results = super().inference(im)
-        if bboxes is not None:
-            bboxes = torch.as_tensor(bboxes, dtype=torch.float32, device=self.device)
-            bboxes = bboxes[None] if bboxes.ndim == 1 else bboxes
-            for result in results:
-                pass
+    def set_prompts(self, prompts):
+        """Set prompts in advance."""
+        self.prompts = prompts

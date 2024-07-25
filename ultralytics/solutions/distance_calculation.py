f77add07a39eca0334cd6a4c96b37d5a922dbe4f
RizwanMunawar <chr043416@gmail.com>
2024-07-25 16:48:44 +0500

    update distance_calculation.py

--- ultralytics/solutions/distance_calculation.py
+++ ultralytics/solutions/distance_calculation.py
@@ -3,58 +3,46 @@
 import math
 
 import cv2
+from pathlib import Path
 
-from ultralytics.utils.checks import check_imshow
+from ultralytics.cfg import get_cfg
+from ultralytics.solutions.cfg import extract_cfg_data
 from ultralytics.utils.plotting import Annotator, colors
+from ultralytics.utils.checks import check_imshow
+
+FILE = Path(__file__).resolve()     # get path of file
 
 
 class DistanceCalculation:
     """A class to calculate distance between two objects in a real-time video stream based on their tracks."""
 
-    def __init__(
-        self,
-        names,
-        pixels_per_meter=10,
-        view_img=False,
-        line_thickness=2,
-        line_color=(255, 255, 0),
-        centroid_color=(255, 0, 255),
-    ):
-        """
-        Initializes the DistanceCalculation class with the given parameters.
-
-        Args:
-            names (dict): Dictionary of classes names.
-            pixels_per_meter (int, optional): Conversion factor from pixels to meters. Defaults to 10.
-            view_img (bool, optional): Flag to indicate if the video stream should be displayed. Defaults to False.
-            line_thickness (int, optional): Thickness of the lines drawn on the image. Defaults to 2.
-            line_color (tuple, optional): Color of the lines drawn on the image (BGR format). Defaults to (255, 255, 0).
-            centroid_color (tuple, optional): Color of the centroids drawn (BGR format). Defaults to (255, 0, 255).
-        """
-        # Visual & image information
+    def __init__(self, **kwargs):
+        """Initializes the DistanceCalculation class with the kwargs arguments."""
+        import ast
+        self.args = get_cfg(extract_cfg_data(FILE))
+        if 'names' not in kwargs:
+            raise ValueError("Error: Classes names 'names' argument is required")
+        for key, value in kwargs.items():
+            if hasattr(self.args, key):
+                setattr(self.args, key, value)
+            else:
+                print(f"Warning: Unknown argument Skipping!!! {key}")
         self.im0 = None
         self.annotator = None
-        self.view_img = view_img
-        self.line_color = line_color
-        self.centroid_color = centroid_color
 
         # Prediction & tracking information
         self.clss = None
-        self.names = names
         self.boxes = None
-        self.line_thickness = line_thickness
         self.trk_ids = None
-
-        # Distance calculation information
         self.centroids = []
-        self.pixel_per_meter = pixels_per_meter
 
         # Mouse event information
         self.left_mouse_count = 0
         self.selected_boxes = {}
 
-        # Check if environment supports imshow
-        self.env_check = check_imshow(warn=True)
+        self.env_check = check_imshow(warn=True)    # Check if environment supports imshow
+        self.args.line_color = ast.literal_eval(self.args.line_color)
+        self.args.centroid_color = ast.literal_eval(self.args.centroid_color)
 
     def mouse_event_for_distance(self, event, x, y, flags, param):
         """
@@ -114,7 +102,7 @@ class DistanceCalculation:
             (tuple): Distance in meters and millimeters.
         """
         pixel_distance = math.sqrt((centroid1[0] - centroid2[0]) ** 2 + (centroid1[1] - centroid2[1]) ** 2)
-        distance_m = pixel_distance / self.pixel_per_meter
+        distance_m = pixel_distance / self.args.pixels_per_meter
         distance_mm = distance_m * 1000
         return distance_m, distance_mm
 
@@ -131,15 +119,15 @@ class DistanceCalculation:
         """
         self.im0 = im0
         if tracks[0].boxes.id is None:
-            if self.view_img:
+            if self.args.view_img:
                 self.display_frames()
             return im0
 
         self.extract_tracks(tracks)
-        self.annotator = Annotator(self.im0, line_width=self.line_thickness)
+        self.annotator = Annotator(self.im0, line_width=self.args.line_thickness)
 
         for box, cls, track_id in zip(self.boxes, self.clss, self.trk_ids):
-            self.annotator.box_label(box, color=colors(int(cls), True), label=self.names[int(cls)])
+            self.annotator.box_label(box, color=colors(int(cls), True), label=self.args.names[int(cls)])
 
             if len(self.selected_boxes) == 2:
                 for trk_id in self.selected_boxes.keys():
@@ -151,12 +139,12 @@ class DistanceCalculation:
 
             distance_m, distance_mm = self.calculate_distance(self.centroids[0], self.centroids[1])
             self.annotator.plot_distance_and_line(
-                distance_m, distance_mm, self.centroids, self.line_color, self.centroid_color
+                distance_m, distance_mm, self.centroids, self.args.line_color, self.args.centroid_color
             )
 
         self.centroids = []
 
-        if self.view_img and self.env_check:
+        if self.args.view_img and self.env_check:
             self.display_frames()
 
         return im0
@@ -173,4 +161,4 @@ class DistanceCalculation:
 
 if __name__ == "__main__":
     names = {0: "person", 1: "car"}  # example class names
-    distance_calculation = DistanceCalculation(names)
+    distance_calculation = DistanceCalculation(names=names)

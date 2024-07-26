2f776b231228b01a71c2442fde419ccf3d5e15d5
pabloi-dsense <piturralde@digitalsense.ai>
2024-07-26 19:19:10 -0300

    Change plotting to use last three channels of images

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -984,7 +984,7 @@ def plot_images(
     mosaic = np.full((int(ns * h), int(ns * w), 3), 255, dtype=np.uint8)  # init
     for i in range(bs):
         x, y = int(w * (i // ns)), int(h * (i % ns))  # block origin
-        mosaic[y : y + h, x : x + w, :] = images[i].transpose(1, 2, 0)
+        mosaic[y : y + h, x : x + w, :] = images[i][-3:,:,:].transpose(1, 2, 0) ##OVERRIDE, USING LAST THREE CHANNELS FOR PLOTTING
 
     # Resize (optional)
     scale = max_size / ns / max(h, w)

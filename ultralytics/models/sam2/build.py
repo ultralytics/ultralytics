9590bbd4674123d4c96965d5c5e045ad4d7c4192
Laughing-q <1185102784@qq.com>
2024-07-30 14:01:09 +0800

    Update build.py

--- ultralytics/models/sam2/build.py
+++ ultralytics/models/sam2/build.py
@@ -1,5 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
-
+from .modules.sam2 import SAM2Base
+from .modules.encoders import ImageEncoder, MemoryEncoder
 
 def _build_sam2(
     encoder_embed_dim=1280,

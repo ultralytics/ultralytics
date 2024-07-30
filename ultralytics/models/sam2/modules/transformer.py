2f7d5f59241dbb05996db7929a166b9d75aa6683
Laughing-q <1185102784@qq.com>
2024-07-30 13:43:07 +0800

    Update transformer.py

--- ultralytics/models/sam2/modules/transformer.py
+++ ultralytics/models/sam2/modules/transformer.py
@@ -1,10 +1,17 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-from ultralytics.models.sam.modules.transformer import TwoWayAttentionBlock as SAMTwoWayAttentionBlock
-from ultralytics.models.sam.modules.transformer import TwoWayTransformer as SAMTwoWayTransformer
+from ultralytics.models.sam.modules.transformer import (
+    TwoWayAttentionBlock as SAMTwoWayAttentionBlock,
+    TwoWayTransformer as SAMTwoWayTransformer,
+    Attention,
+)
 from ultralytics.nn.modules import MLP
+from .utils import apply_rotary_enc, compute_axial_cis
+from functools import partial
 from typing import Type
-import torch.nn as nn
+from torch import Tensor, nn
+import torch
+import math
 
 
 class TwoWayAttentionBlock(SAMTwoWayAttentionBlock):
@@ -100,3 +107,65 @@ class TwoWayTransformer(SAMTwoWayTransformer):
                     skip_first_layer_pe=(i == 0),
                 )
             )
+
+
+class RoPEAttention(Attention):
+    """Attention with rotary position encoding."""
+
+    def __init__(
+        self,
+        *args,
+        rope_theta=10000.0,
+        # whether to repeat q rope to match k length
+        # this is needed for cross-attention to memories
+        rope_k_repeat=False,
+        feat_sizes=(32, 32),  # [w, h] for stride 16 feats at 512 resolution
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+
+        self.compute_cis = partial(compute_axial_cis, dim=self.internal_dim // self.num_heads, theta=rope_theta)
+        freqs_cis = self.compute_cis(end_x=feat_sizes[0], end_y=feat_sizes[1])
+        self.freqs_cis = freqs_cis
+        self.rope_k_repeat = rope_k_repeat
+
+    def forward(self, q: Tensor, k: Tensor, v: Tensor, num_k_exclude_rope: int = 0) -> Tensor:
+        # Input projections
+        q = self.q_proj(q)
+        k = self.k_proj(k)
+        v = self.v_proj(v)
+
+        # Separate into heads
+        q = self._separate_heads(q, self.num_heads)
+        k = self._separate_heads(k, self.num_heads)
+        v = self._separate_heads(v, self.num_heads)
+
+        # Apply rotary position encoding
+        w = h = math.sqrt(q.shape[-2])
+        self.freqs_cis = self.freqs_cis.to(q.device)
+        if self.freqs_cis.shape[0] != q.shape[-2]:
+            self.freqs_cis = self.compute_cis(end_x=w, end_y=h).to(q.device)
+        if q.shape[-2] != k.shape[-2]:
+            assert self.rope_k_repeat
+
+        num_k_rope = k.size(-2) - num_k_exclude_rope
+        q, k[:, :, :num_k_rope] = apply_rotary_enc(
+            q,
+            k[:, :, :num_k_rope],
+            freqs_cis=self.freqs_cis,
+            repeat_freqs_k=self.rope_k_repeat,
+        )
+
+        # Attention
+        _, _, _, c_per_head = q.shape
+        attn = q @ k.permute(0, 1, 3, 2)  # B x N_heads x N_tokens x N_tokens
+        attn = attn / math.sqrt(c_per_head)
+        attn = torch.softmax(attn, dim=-1)
+
+        # Get output
+        out = attn @ v
+
+        out = self._recombine_heads(out)
+        out = self.out_proj(out)
+
+        return out

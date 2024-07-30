5d37f539bb7d8d98ae877b8e3f21ccf7f7f04288
Laughing-q <1185102784@qq.com>
2024-07-30 13:12:43 +0800

    Update transformer.py

--- ultralytics/models/sam/modules/transformer.py
+++ ultralytics/models/sam/modules/transformer.py
@@ -86,7 +86,6 @@ class TwoWayTransformer(nn.Module):
           (torch.Tensor): the processed image_embedding
         """
         # BxCxHxW -> BxHWxC == B x N_image_tokens x C
-        bs, c, h, w = image_embedding.shape
         image_embedding = image_embedding.flatten(2).permute(0, 2, 1)
         image_pe = image_pe.flatten(2).permute(0, 2, 1)
 

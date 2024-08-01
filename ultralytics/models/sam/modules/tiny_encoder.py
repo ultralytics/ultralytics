0f8094cbc7cfbaa3110ef41fd3a3d2ddabd07af9
UltralyticsAssistant <web@ultralytics.com>
2024-08-01 07:40:16 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/models/sam/modules/tiny_encoder.py
+++ ultralytics/models/sam/modules/tiny_encoder.py
@@ -17,8 +17,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.checkpoint as checkpoint
 
-from ultralytics.utils.instance import to_2tuple
 from ultralytics.nn.modules import LayerNorm2d
+from ultralytics.utils.instance import to_2tuple
 
 
 class Conv2d_BN(torch.nn.Sequential):

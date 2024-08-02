811d1963c855268e3fe13ff8aa5fc1c34580eba1
Francesco Mattioli <Francesco.mttl@gmail.com>
2024-08-02 15:51:37 +0200

    updated memory test

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,9 +1,7 @@
 import os
 import sys
-
 import psutil
 import pytest
-
 from ultralytics import YOLO
 
 
@@ -47,38 +45,31 @@ def test_export_engine():
 
 
 # Load and inference
-
-
 def test_pytorch():
     model = YOLO("yolov8n.pt")
-    for i in range(10):
-        model.predict()
+    model.predict()
     return model
 
 
 def test_openvino():
     model = YOLO("yolov8n_openvino_model")
-    for i in range(10):
-        model.predict()
+    model.predict()
     return model
 
 
 def test_openvino_int8():
     model = YOLO("yolov8n_int8_openvino_model")
-    for i in range(10):
-        model.predict()
+    model.predict()
     return model
 
 
 def test_engine():
     model = YOLO("yolov8n.engine")
-    for i in range(10):
-        model.predict()
+    model.predict()
     return model
 
 
 def test_engine_int8():
     model = YOLO("yolov8n_int8.engine")
-    for i in range(10):
-        model.predict()
+    model.predict()
     return model

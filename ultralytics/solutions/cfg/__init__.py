9b6dd45ad32ec779929256dd0b1b86e7cdc695dc
RizwanMunawar <chr043416@gmail.com>
2024-08-03 17:07:30 +0500

    update __init__.py

--- ultralytics/solutions/cfg/__init__.py
+++ ultralytics/solutions/cfg/__init__.py
@@ -1,13 +1,1 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
-
-from ultralytics.utils import IterableSimpleNamespace, yaml_load
-
-
-def extract_cfg_data(FILE):
-    """Extract configuration data."""
-    DEFAULT_CFG_DICT = yaml_load(FILE.parents[0] / "cfg/default.yaml")  # solutions
-    for k, v in DEFAULT_CFG_DICT.items():
-        if isinstance(v, str) and v.lower() == "none":
-            DEFAULT_CFG_DICT[k] = None
-    DEFAULT_CFG = IterableSimpleNamespace(**DEFAULT_CFG_DICT)
-    return DEFAULT_CFG

087f5a642b10a47d7a7edc56e4b51b2869c09cf8
Francesco Mattioli <Francesco.mttl@gmail.com>
2024-08-05 11:04:22 +0200

    fixed error handling

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -138,7 +138,7 @@ def try_export(inner_func):
             LOGGER.info(f"{prefix} export success ✅ {dt.t:.1f}s, saved as '{f}' ({file_size(f):.1f} MB)")
             return f, model
         except Exception as e:
-            LOGGER.fatal(f"{prefix} FATAL ⚠️ export failure ❌ {dt.t:.1f}s: {e}")
+            LOGGER.error(f"{prefix} ERROR export failure ❌ {dt.t:.1f}s: {e}")
             raise e
 
     return outer_func

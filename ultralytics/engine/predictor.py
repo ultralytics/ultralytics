9bc6cc02ab3f7a97df246068c2e19410172c5cdc
UltralyticsAssistant <web@ultralytics.com>
2024-08-14 09:28:43 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/engine/predictor.py
+++ ultralytics/engine/predictor.py
@@ -393,7 +393,7 @@ class BasePredictor:
         cv2.imshow(p, im)
         cv2.waitKey(300 if self.dataset.mode == "image" else 1)  # 1 millisecond
         if self.dataset.mode == "image":
-            cv2.destroyAllWindows() #Prevents opening multiple windows in image mode
+            cv2.destroyAllWindows()  # Prevents opening multiple windows in image mode
 
     def run_callbacks(self, event: str):
         """Runs all registered callbacks for a specific event."""

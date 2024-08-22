273b8185410f2ea3175ae0d975ec16f6d3bdd751
Laughing-q <1185102784@qq.com>
2024-08-22 16:50:39 +0800

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -1039,6 +1039,6 @@ class C3PSA(C3):
         c_ = int(c2 * e)  # hidden channels
         self.m = nn.Sequential(*(PSABlock(c_, attn_ratio=0.5, num_heads=c_ // 64) for _ in range(n)))
 
-    # def forward(self, x):
-    #     """Forward pass through the CSP bottleneck with 2 convolutions."""
-    #     return self.cv3(torch.cat((self.cv1(x), self.m(self.cv2(x))), 1))
+    def forward(self, x):
+        """Forward pass through the CSP bottleneck with 2 convolutions."""
+        return self.cv3(torch.cat((self.cv1(x), self.m(self.cv2(x))), 1))

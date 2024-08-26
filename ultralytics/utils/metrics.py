20bc74b899468fca2bf0bd88e86912d412848c2f
UltralyticsAssistant <web@ultralytics.com>
2024-08-26 02:41:36 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/utils/metrics.py
+++ ultralytics/utils/metrics.py
@@ -143,7 +143,7 @@ def mask_iou(mask1, mask2, eps=1e-7):
     Returns:
         (torch.Tensor): A tensor of shape (N, M) representing masks IoU.
     """
-    intersection = torch.sum(torch.mul(mask1, mask2),dim=1)[:, None]
+    intersection = torch.sum(torch.mul(mask1, mask2), dim=1)[:, None]
     union = (mask1.sum(1)[:, None] + mask2.sum(1)[None]) - intersection  # (area1 + area2) - intersection
     return intersection / (union + eps)
 

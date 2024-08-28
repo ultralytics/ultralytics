a810887f0aed01b50b209a487a3972ea3e5cf032
RizwanMunawar <chr043416@gmail.com>
2024-08-28 14:17:57 +0500

    update solutions/object_counter.py & object-counting.md

--- tests/test_solutions.py
+++ tests/test_solutions.py
@@ -10,47 +10,53 @@ MAJOR_SOLUTIONS_DEMO = "https://github.com/ultralytics/assets/releases/download/
 WORKOUTS_SOLUTION_DEMO = "https://github.com/ultralytics/assets/releases/download/v0.0.0/solution_ci_pose_demo.mp4"
 
 
-# @pytest.mark.slow
-# def test_major_solutions():
-#     """Test the object counting, heatmap, speed estimation and queue management solution."""
-#     safe_download(url=MAJOR_SOLUTIONS_DEMO)
-#     model = YOLO("yolov8n.pt")
-#     names = model.names
-#     cap = cv2.VideoCapture("solutions_ci_demo.mp4")
-#     assert cap.isOpened(), "Error reading video file"
-#     region_points = [(20, 400), (1080, 404), (1080, 360), (20, 360)]
-#     counter = solutions.ObjectCounter(reg_pts=region_points, names=names, view_img=False)
-#     heatmap = solutions.Heatmap(colormap=cv2.COLORMAP_PARULA, names=names, view_img=False)
-#     speed = solutions.SpeedEstimator(reg_pts=region_points, names=names, view_img=False)
-#     queue = solutions.QueueManager(names=names, reg_pts=region_points, view_img=False)
-#     while cap.isOpened():
-#         success, im0 = cap.read()
-#         if not success:
-#             break
-#         original_im0 = im0.copy()
-#         tracks = model.track(im0, persist=True, show=False)
-#         _ = counter.start_counting(original_im0.copy(), tracks)
-#         _ = heatmap.generate_heatmap(original_im0.copy(), tracks)
-#         _ = speed.estimate_speed(original_im0.copy(), tracks)
-#         _ = queue.process_queue(original_im0.copy(), tracks)
-#     cap.release()
-#     cv2.destroyAllWindows()
-
-
 @pytest.mark.slow
-def test_aigym():
-    """Test the workouts monitoring solution."""
-    safe_download(url=WORKOUTS_SOLUTION_DEMO)
-    cap = cv2.VideoCapture("solution_ci_pose_demo.mp4")
+def test_major_solutions():
+    """Test the object counting, heatmap, speed estimation and queue management solution."""
+    safe_download(url=MAJOR_SOLUTIONS_DEMO)
+    cap = cv2.VideoCapture("solutions_ci_demo.mp4")
     assert cap.isOpened(), "Error reading video file"
-    gym_object = solutions.AIGym(pose_type="squat", kpts_to_check=[5, 11, 13], model="yolov8n-pose.pt")
+    region_points = [(20, 400), (1080, 404), (1080, 360), (20, 360)]
+    line_points = [(20, 400), (1080, 400)]
+    polygon_points = [(20, 400), (1080, 404), (1080, 360), (20, 360), (20, 400)]
+    counter_reg = solutions.ObjectCounter(reg_pts=region_points, show=True, model="yolov8n.pt")
+    counter_line = solutions.ObjectCounter(reg_pts=line_points, show=True, model="yolov8n.pt")
+    counter_polygon = solutions.ObjectCounter(reg_pts=polygon_points, show=True, model="yolov8n.pt")
+    counter_cls = solutions.ObjectCounter(reg_pts=line_points, show=True, model="yolov8n.pt", classes=0)
+
+    # heatmap = solutions.Heatmap(colormap=cv2.COLORMAP_PARULA, names=names, view_img=False)
+    # speed = solutions.SpeedEstimator(reg_pts=region_points, names=names, view_img=False)
+    # queue = solutions.QueueManager(names=names, reg_pts=region_points, view_img=False)
     while cap.isOpened():
         success, im0 = cap.read()
         if not success:
             break
-        _ = gym_object.start_counting(im0)
+        original_im0 = im0.copy()
+        _ = counter_reg.start_counting(original_im0.copy())
+        _ = counter_line.start_counting(original_im0.copy())
+        _ = counter_polygon.start_counting(original_im0.copy())
+        _ = counter_cls.start_counting(original_im0.copy())
+        # _ = heatmap.generate_heatmap(original_im0.copy(), tracks)
+        # _ = speed.estimate_speed(original_im0.copy(), tracks)
+        # _ = queue.process_queue(original_im0.copy(), tracks)
     cap.release()
     cv2.destroyAllWindows()
+
+
+# @pytest.mark.slow
+# def test_aigym():
+#     """Test the workouts monitoring solution."""
+#     safe_download(url=WORKOUTS_SOLUTION_DEMO)
+#     cap = cv2.VideoCapture("solution_ci_pose_demo.mp4")
+#     assert cap.isOpened(), "Error reading video file"
+#     gym_object = solutions.AIGym(pose_type="squat", kpts_to_check=[5, 11, 13], model="yolov8n-pose.pt")
+#     while cap.isOpened():
+#         success, im0 = cap.read()
+#         if not success:
+#             break
+#         _ = gym_object.start_counting(im0)
+#     cap.release()
+#     cv2.destroyAllWindows()
 
 
 #

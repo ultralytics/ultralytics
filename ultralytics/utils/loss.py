3d81d9bba563aba073b9c5572a0e2384311dc397
Laughing-q <1185102784@qq.com>
2024-08-28 15:27:24 +0800

    Update loss.py

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -221,7 +221,7 @@ class v8DetectionLoss:
         dfl_conf = (dfl_conf.amax(-1).mean(-1) + dfl_conf.amax(-1).amin(-1)) / 2
 
         _, target_bboxes, target_scores, fg_mask, _ = self.assigner(
-            pred_scores.detach().sigmoid() * 0.7 + dfl_conf.unsqueeze(-1) * 0.3,
+            pred_scores.detach().sigmoid() * 0.8 + dfl_conf.unsqueeze(-1) * 0.2,
             # pred_scores.detach().sigmoid(),
             (pred_bboxes.detach() * stride_tensor).type(gt_bboxes.dtype),
             anchor_points * stride_tensor,

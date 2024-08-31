1b29b9e9413f10ae0a8a16d588a0d6afef03f9d1
Burhan <62214284+Burhan-Q@users.noreply.github.com>
2024-08-31 11:39:45 -0400

    Includes warning for enforced `dynamic` during INT8 exports (#15874)
    Co-authored-by: UltralyticsAssistant <web@ultralytics.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -204,8 +204,9 @@ class Exporter:
             self.args.half = False
             assert not self.args.dynamic, "half=True not compatible with dynamic=True, i.e. use only one."
         self.imgsz = check_imgsz(self.args.imgsz, stride=model.stride, min_dim=2)  # check image size
-        if self.args.int8 and (engine or xml):
+        if self.args.int8 and not self.args.dynamic and (engine or xml):
             self.args.dynamic = True  # enforce dynamic to export TensorRT INT8; ensures ONNX is dynamic
+            LOGGER.warning("WARNING ⚠️ INT8 export requires dynamic image sizes, setting dynamic=True.")
         if self.args.optimize:
             assert not ncnn, "optimize=True not compatible with format='ncnn', i.e. use optimize=False"
             assert self.device.type == "cpu", "optimize=True not compatible with cuda devices, i.e. use device='cpu'"

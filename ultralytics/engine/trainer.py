d21baef033c968793f20a856ec3e1423e7e9a28b
UltralyticsAssistant <web@ultralytics.com>
2024-09-03 13:21:22 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -103,7 +103,7 @@ class BaseTrainer:
         self.validator = None
         self.metrics = None
         self.plots = {}
-       
+
         init_seeds(self.args.seed + 1 + RANK, deterministic=self.args.deterministic)
         # Dirs
         self.save_dir = get_save_dir(self.args)

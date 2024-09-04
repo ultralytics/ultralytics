d610eccb3cfa282236764742e577ee4f8ca2465c
Anand <anand@flappiedoors.com>
2024-09-04 13:58:46 +0200

    Implemented build multi label dataset functionality

--- ultralytics/data/build.py
+++ ultralytics/data/build.py
@@ -9,7 +9,7 @@ import torch
 from PIL import Image
 from torch.utils.data import dataloader, distributed
 
-from ultralytics.data.dataset import GroundingDataset, YOLODataset, YOLOMultiModalDataset
+from ultralytics.data.dataset import GroundingDataset, YOLODataset, YOLOMultiModalDataset, YOLOMultiLabelDataset
 from ultralytics.data.loaders import (
     LOADERS,
     LoadImagesAndVideos,
@@ -85,6 +85,25 @@ def build_yolo_dataset(cfg, img_path, batch, data, mode="train", rect=False, str
     """Build YOLO Dataset."""
     dataset = YOLOMultiModalDataset if multi_modal else YOLODataset
     return dataset(
+        img_path=img_path,
+        imgsz=cfg.imgsz,
+        batch_size=batch,
+        augment=mode == "train",  # augmentation
+        hyp=cfg,  # TODO: probably add a get_hyps_from_cfg function
+        rect=cfg.rect or rect,  # rectangular batches
+        cache=cfg.cache or None,
+        single_cls=cfg.single_cls or False,
+        stride=int(stride),
+        pad=0.0 if mode == "train" else 0.5,
+        prefix=colorstr(f"{mode}: "),
+        task=cfg.task,
+        classes=cfg.classes,
+        data=data,
+        fraction=cfg.fraction if mode == "train" else 1.0,
+    )
+
+def build_multilabel_dataset(cfg, img_path, batch, data, mode="train", rect=False, stride=32):
+        return YOLOMultiLabelDataset(
         img_path=img_path,
         imgsz=cfg.imgsz,
         batch_size=batch,

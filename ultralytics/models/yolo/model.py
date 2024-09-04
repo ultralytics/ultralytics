8a5702e2f077e42fc6f5fc5d57f0e40cf8f8ee2e
Anand <anand@flappiedoors.com>
2024-09-04 14:21:43 +0200

    Created task mapping for multi label classification

--- ultralytics/models/yolo/model.py
+++ ultralytics/models/yolo/model.py
@@ -32,6 +32,12 @@ class YOLO(Model):
                 "validator": yolo.classify.ClassificationValidator,
                 "predictor": yolo.classify.ClassificationPredictor,
             },
+            "multi_label_classify": {
+                "model": ClassificationModel,
+                "trainer": yolo.classify.MultiLabelClassificationTrainer,
+                "validator": yolo.classify.MultiLabelClassificationValidator,
+                "predictor": yolo.classify.MultiLabelClassificationPredictor,
+            },
             "detect": {
                 "model": DetectionModel,
                 "trainer": yolo.detect.DetectionTrainer,
@@ -64,14 +70,10 @@ class YOLOWorld(Model):
 
     def __init__(self, model="yolov8s-world.pt", verbose=False) -> None:
         """
-        Initialize YOLOv8-World model with a pre-trained model file.
-
-        Loads a YOLOv8-World model for object detection. If no custom class names are provided, it assigns default
-        COCO class names.
+        Initializes the YOLOv8-World model with the given pre-trained model file. Supports *.pt and *.yaml formats.
 
         Args:
-            model (str | Path): Path to the pre-trained model file. Supports *.pt and *.yaml formats.
-            verbose (bool): If True, prints additional information during initialization.
+            model (str | Path): Path to the pre-trained model. Defaults to 'yolov8s-world.pt'.
         """
         super().__init__(model=model, task="detect", verbose=verbose)
 

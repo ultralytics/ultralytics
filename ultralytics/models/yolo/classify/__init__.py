82e51945acc3b1410fcf0aa29ca333afab2cc062
Anand <anand@flappiedoors.com>
2024-09-04 14:18:56 +0200

    Allow import of Multilabel classification trainer, predictor and validator

--- ultralytics/models/yolo/classify/__init__.py
+++ ultralytics/models/yolo/classify/__init__.py
@@ -1,7 +1,7 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-from ultralytics.models.yolo.classify.predict import ClassificationPredictor
-from ultralytics.models.yolo.classify.train import ClassificationTrainer
-from ultralytics.models.yolo.classify.val import ClassificationValidator
+from ultralytics.models.yolo.classify.predict import ClassificationPredictor, MultiLabelClassificationPredictor
+from ultralytics.models.yolo.classify.train import ClassificationTrainer, MultiLabelClassificationTrainer
+from ultralytics.models.yolo.classify.val import ClassificationValidator, MultiLabelClassificationValidator
 
-__all__ = "ClassificationPredictor", "ClassificationTrainer", "ClassificationValidator"
+__all__ = "ClassificationPredictor", "ClassificationTrainer", "ClassificationValidator", "MultiLabelClassificationValidator", "MultiLabelClassificationPredictor", "MultiLabelClassificationTrainer"

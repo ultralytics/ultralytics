95d4e89c98f06ffb3fa73036c4b4fc64597ece9e
Burhan <62214284+Burhan-Q@users.noreply.github.com>
2024-09-04 20:42:27 -0400

    revise for hard 2.4.0 check

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -45,7 +45,7 @@ TORCHVISION_0_10 = check_version(TORCHVISION_VERSION, "0.10.0")
 TORCHVISION_0_11 = check_version(TORCHVISION_VERSION, "0.11.0")
 TORCHVISION_0_13 = check_version(TORCHVISION_VERSION, "0.13.0")
 TORCHVISION_0_18 = check_version(TORCHVISION_VERSION, "0.18.0")
-if WINDOWS and TORCH_2_4:  # reject version 2.4.0 on Windows
+if WINDOWS and check_version(torch.__version__, "==2.4.0"):  # reject version 2.4.0 on Windows
     LOGGER.warning(
         "WARNING ⚠️ Known issue with torch==2.4.0 on Windows with CPU, recommend upgrading to torch>=2.4.1 to resolve "
         "https://github.com/ultralytics/ultralytics/issues/15049"

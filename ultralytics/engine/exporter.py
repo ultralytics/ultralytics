a9eb0b1af99656efc1c1210a8633da9c98e20d24
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-09-06 15:49:11 +0200

    cleanup

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -610,12 +610,9 @@ class Exporter:
         f = self.file.with_suffix(".mlmodel" if mlmodel else ".mlpackage")
         if f.is_dir():
             shutil.rmtree(f)
-        if self.args.nms and hasattr(self.model, "end2end"):
-            if self.model.end2end:
-                LOGGER.warning(
-                    f"{prefix} WARNING ⚠️ 'nms=True' is not available for end2end models forcing 'nms=False'."
-                )
-                self.args.nms = False
+        if self.args.nms and getattr(self.model, "end2end", False)
+            LOGGER.warning(f"{prefix} WARNING ⚠️ 'nms=True' is not available for end2end models. Forcing 'nms=False'.")
+            self.args.nms = False
 
         bias = [0.0, 0.0, 0.0]
         scale = 1 / 255

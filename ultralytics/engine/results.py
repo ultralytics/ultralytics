950829402bf369e4a7b2ef06604b914b72d2b377
RizwanMunawar <chr043416@gmail.com>
2024-09-07 21:43:11 +0500

    update results.py

--- ultralytics/engine/results.py
+++ ultralytics/engine/results.py
@@ -535,7 +535,7 @@ class Results(SimpleClass):
                 annotator.box_label(
                     box,
                     label,
-                    color=colors((id if id is not None else (i if color_mode == "instance" else c)), True),
+                    color=colors(i if color_mode=="instance" else (id if id is not None else c), True),
                     rotated=is_obb,
                 )
 

0e08184db64981bccc755e52e91aa7e02dbfefe8
RizwanMunawar <chr043416@gmail.com>
2024-09-10 17:42:12 +0500

    remove tracking Id in bbox label

--- ultralytics/solutions/object_counter.py
+++ ultralytics/solutions/object_counter.py
@@ -136,7 +136,7 @@ class ObjectCounter:
             # Extract tracks
             for box, track_id, cls in zip(boxes, track_ids, clss):
                 # Draw bounding box
-                annotator.box_label(box, label=f"{self.names[cls]}#{track_id}", color=colors(int(track_id), True))
+                annotator.box_label(box, label=self.names[cls], color=colors(int(track_id), True))
 
                 # Store class info
                 if self.names[cls] not in self.class_wise_count:

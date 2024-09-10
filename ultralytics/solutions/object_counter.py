3a68c3a104156d7244a0794ecb3526156f9aa0c0
RizwanMunawar <chr043416@gmail.com>
2024-09-10 17:36:21 +0500

    remove unnecessary direction argument introduced + revert the comments + update line counter for vertical direction

--- ultralytics/solutions/object_counter.py
+++ ultralytics/solutions/object_counter.py
@@ -9,7 +9,7 @@ from ultralytics.utils.plotting import Annotator, colors
 
 check_requirements("shapely>=2.0.0")
 
-from shapely.geometry import LineString, Polygon
+from shapely.geometry import LineString, Point, Polygon
 
 
 class ObjectCounter:
@@ -24,7 +24,6 @@ class ObjectCounter:
         view_in_counts=True,
         view_out_counts=True,
         draw_tracks=False,
-        direction="left_to_right",  # New parameter
     ):
         """
         Initializes the ObjectCounter with various tracking and counting parameters.
@@ -37,7 +36,6 @@ class ObjectCounter:
             view_in_counts (bool): Flag to control whether to display the in counts on the video stream.
             view_out_counts (bool): Flag to control whether to display the out counts on the video stream.
             draw_tracks (bool): Flag to control whether to draw the object tracks.
-            direction (str): Direction of counting, "left_to_right" or "right_to_left".
         """
         # Mouse events
         self.is_drawing = False
@@ -70,9 +68,6 @@ class ObjectCounter:
 
         # Check if environment supports imshow
         self.env_check = check_imshow(warn=True)
-
-        # Direction of counting
-        self.direction = direction  # New direction attribute
 
         # Initialize counting region
         if len(self.reg_pts) == 2:
@@ -127,6 +122,7 @@ class ObjectCounter:
 
     def extract_and_process_tracks(self, tracks):
         """Extracts and processes tracks for object counting in a video stream."""
+        # Annotator Init and region drawing
         annotator = Annotator(self.im0, self.tf, self.names)
 
         # Draw region or line
@@ -137,44 +133,70 @@ class ObjectCounter:
             clss = tracks[0].boxes.cls.cpu().tolist()
             track_ids = tracks[0].boxes.id.int().cpu().tolist()
 
+            # Extract tracks
             for box, track_id, cls in zip(boxes, track_ids, clss):
+                # Draw bounding box
                 annotator.box_label(box, label=f"{self.names[cls]}#{track_id}", color=colors(int(track_id), True))
 
+                # Store class info
                 if self.names[cls] not in self.class_wise_count:
                     self.class_wise_count[self.names[cls]] = {"IN": 0, "OUT": 0}
 
+                # Draw Tracks
                 track_line = self.track_history[track_id]
                 track_line.append((float((box[0] + box[2]) / 2), float((box[1] + box[3]) / 2)))
                 if len(track_line) > 30:
                     track_line.pop(0)
 
+                # Draw track trails
                 if self.draw_tracks:
                     annotator.draw_centroid_and_tracks(
-                        track_line, color=colors(int(track_id), True), track_thickness=self.tf
+                        track_line,
+                        color=colors(int(track_id), True),
+                        track_thickness=self.tf,
                     )
 
                 prev_position = self.track_history[track_id][-2] if len(self.track_history[track_id]) > 1 else None
 
-                if len(self.reg_pts) == 2 and prev_position is not None and track_id not in self.count_ids:
-                    if LineString([(prev_position[0], prev_position[1]), (box[0], box[1])]).intersects(
-                        self.counting_line_segment
-                    ):
+                # Count objects in any polygon
+                if len(self.reg_pts) >= 3:
+                    is_inside = self.counting_region.contains(Point(track_line[-1]))
+
+                    if prev_position is not None and is_inside and track_id not in self.count_ids:
                         self.count_ids.append(track_id)
 
-                        # Determine the direction of movement
-                        if self.direction == "left_to_right":
-                            direction = box[0] - prev_position[0]
-                        else:  # right_to_left
-                            direction = prev_position[0] - box[0]
-
-                        if direction > 0:
+                        if (box[0] - prev_position[0]) * (self.counting_region.centroid.x - prev_position[0]) > 0:
                             self.in_counts += 1
                             self.class_wise_count[self.names[cls]]["IN"] += 1
                         else:
                             self.out_counts += 1
                             self.class_wise_count[self.names[cls]]["OUT"] += 1
 
+                # Count objects using line
+                elif len(self.reg_pts) == 2:
+                    if prev_position is not None and track_id not in self.count_ids:
+                        # Check if the object's movement segment intersects the counting line
+                        if LineString([(prev_position[0], prev_position[1]), (box[0], box[1])]).intersects(
+                            self.counting_line_segment
+                        ):
+                            self.count_ids.append(track_id)
+
+                            # Determine the direction of movement (IN or OUT)
+                            direction_x = (box[0] - prev_position[0]) * (
+                                self.counting_region.centroid.x - prev_position[0]
+                            )
+                            direction_y = (box[1] - prev_position[1]) * (
+                                        self.counting_region.centroid.y - prev_position[1])
+
+                            if direction_x > 0 and direction_y > 0:
+                                self.in_counts += 1
+                                self.class_wise_count[self.names[cls]]["IN"] += 1
+                            else:
+                                self.out_counts += 1
+                                self.class_wise_count[self.names[cls]]["OUT"] += 1
+
         labels_dict = {}
+
         for key, value in self.class_wise_count.items():
             if value["IN"] != 0 or value["OUT"] != 0:
                 if not self.view_in_counts and not self.view_out_counts:

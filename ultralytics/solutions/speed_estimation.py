a3ae3af7f8b63c170c520064cc1032696e9fbce8
RizwanMunawar <chr043416@gmail.com>
2024-09-12 22:32:02 +0500

    update

--- ultralytics/solutions/speed_estimation.py
+++ ultralytics/solutions/speed_estimation.py
@@ -24,9 +24,6 @@ class SpeedEstimator:
             line_thickness (int, optional): Thickness of the lines for drawing boxes and tracks. Defaults to 2.
             spdl_dist_thresh (int, optional): Distance threshold for speed calculation. Defaults to 10.
         """
-        self.view_img = view_img    # bool for displaying inference
-        self.tf = line_thickness    # line thickness for annotator
-
         # Region information
         self.reg_pts = reg_pts if reg_pts is not None else [(20, 400), (1260, 400)]
 
@@ -35,7 +32,8 @@ class SpeedEstimator:
         # Tracking information
         self.trk_history = defaultdict(list)
 
-        # Speed estimation information
+        self.view_img = view_img  # bool for displaying inference
+        self.tf = line_thickness  # line thickness for annotator
         self.spd = {}       # set for speed data
         self.trkd_ids = []  # list for already speed_estimated and tracked ID's
         self.spdl = spdl_dist_thresh    # Speed line distance threshold
@@ -57,18 +55,16 @@ class SpeedEstimator:
             (ndarray): The image with annotated boxes and tracks.
         """
         if tracks[0].boxes.id is None:
-            if self.view_img and self.env_check:
-                self.display_frames()
             return im0
 
         boxes = tracks[0].boxes.xyxy.cpu()
         clss = tracks[0].boxes.cls.cpu().tolist()
-        trk_ids = tracks[0].boxes.id.int().cpu().tolist()
+        t_ids = tracks[0].boxes.id.int().cpu().tolist()
         annotator = Annotator(im0, line_width=self.tf)
         annotator.draw_region(reg_pts=self.reg_pts, color=(255, 0, 255), thickness=self.tf * 2)
 
-        for box, trk_id, cls in zip(boxes, trk_ids, clss):
-            track = self.trk_history[track_id]
+        for box, t_id, cls in zip(boxes, t_ids, clss):
+            track = self.trk_history[t_id]
             bbox_center = (float((box[0] + box[2]) / 2), float((box[1] + box[3]) / 2))
             track.append(bbox_center)
 
@@ -77,14 +73,14 @@ class SpeedEstimator:
 
             trk_pts = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
 
-            if trk_id not in self.trk_pt:
-                self.trk_pt[trk_id] = 0
+            if t_id not in self.trk_pt:
+                self.trk_pt[t_id] = 0
 
-            speed_label = f"{int(self.spd[trk_id])} km/h" if track_id in self.spd else self.names[int(cls)]
-            bbox_color = colors(int(trk_id)) if trk_id in self.spd else (255, 0, 255)
+            speed_label = f"{int(self.spd[t_id])} km/h" if t_id in self.spd else self.names[int(cls)]
+            bbox_color = colors(int(t_id), True)
 
             annotator.box_label(box, speed_label, bbox_color)
-            cv2.polylines(im0, [trk_pts], isClosed=False, color=(0, 255, 0), thickness=self.tf)
+            cv2.polylines(im0, [trk_pts], isClosed=False, color=bbox_color, thickness=self.tf)
             cv2.circle(im0, (int(track[-1][0]), int(track[-1][1])), self.tf*2, bbox_color, -1)
 
             # Calculation of object speed
@@ -97,15 +93,15 @@ class SpeedEstimator:
             else:
                 direction = "unknown"
 
-            if self.trk_pt.get(trk_id) != 0 and direction != "unknown" and trk_id not in self.trkd_ids:
-                self.trkd_ids.append(trk_id)
+            if self.trk_pt.get(t_id) != 0 and direction != "unknown" and t_id not in self.trkd_ids:
+                self.trkd_ids.append(t_id)
 
-                time_difference = time() - self.trk_pt[trk_id]
+                time_difference = time() - self.trk_pt[t_id]
                 if time_difference > 0:
-                    self.spd[trk_id] = np.abs(track[-1][1] - self.trk_pp[trk_id][1]) / time_difference
+                    self.spd[t_id] = np.abs(track[-1][1] - self.trk_pp[t_id][1]) / time_difference
 
-            self.trk_pt[trk_id] = time()
-            self.trk_pp[trk_id] = track[-1]
+            self.trk_pt[t_id] = time()
+            self.trk_pp[t_id] = track[-1]
 
         if self.view_img and self.env_check:
             cv2.imshow("Ultralytics Speed Estimation", im0)

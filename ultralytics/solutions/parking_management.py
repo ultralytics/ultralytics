1a8962e7d29ce10caf1edfb973345efb3f59fd65
RizwanMunawar <chr043416@gmail.com>
2024-09-14 00:11:13 +0500

    update

--- ultralytics/solutions/parking_management.py
+++ ultralytics/solutions/parking_management.py
@@ -113,8 +113,7 @@ class ParkingPtsSelection:
             box (list): Bounding box data
         """
         for i in range(4):
-            x1, y1 = box[i]
-            x2, y2 = box[(i + 1) % 4]
+            [x1, y1, x2, y2] = box[i], box[(i + 1) % 4]
             self.canvas.create_line(x1, y1, x2, y2, fill="blue", width=2)
 
     def remove_last_bounding_box(self):

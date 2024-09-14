176685896f09c482524d306cdda7a497c38b5c9d
MatthewNoyce <matthewnoyce@icloud.com>
2024-09-14 12:49:00 +0100

    added tocsv() and toxml() methods utilising the todataframe() method
    Added tocsv() and toxml() methods using the todataframe() method and the in built methods with pandas

--- ultralytics/engine/results.py
+++ ultralytics/engine/results.py
@@ -822,7 +822,7 @@ class Results(SimpleClass):
         """
         Converts detection results to a Pandas Dataframe
         
-        This method converts teh detection results into a pandas dataframe 
+        This method converts the detection results into a pandas dataframe 
         Args:
             normalize (bool): Whether to normalize the bounding box coordinates by the image dimensions.
                 If True, coordinates will be returned as float values between 0 and 1. Defaults to False.
@@ -832,13 +832,57 @@ class Results(SimpleClass):
             
         Examples:
             >>> results = model("path/to/image.jpg")
-            >>> df_result = results[0].todf()
+            >>> df_result = results[0].todataframe()
             >>> print(df_result)
         """
         
         import pandas as pd
         
         return  pd.DataFrame(self.summary(normalize=normalize, decimals=decimals))
+    
+    def tocsv(self, normalize=False, decimals=5):
+        """
+        Converts detection results to a csv format
+        
+        This method converts the detection results into a csv format 
+        Args:
+            normalize (bool): Whether to normalize the bounding box coordinates by the image dimensions.
+                If True, coordinates will be returned as float values between 0 and 1. Defaults to False.
+            decimals (int): Number of decimal places to round the output values to. Defaults to 5.
+        Returns:
+            (str): A csv containing all the information in results in an organized way.
+            
+        Examples:
+            >>> results = model("path/to/image.jpg")
+            >>> csv_result = results[0].tocsv()
+            >>> print(csv_result)
+        """
+        
+        
+        
+        return Results.todataframe(self.summary(normalize=normalize, decimals=decimals)).to_csv()
+    
+    def toxml(self, normalize=False, decimals=5):
+        """
+        Converts detection results to a xml format
+        
+        This method converts the detection results into a xml format 
+        Args:
+            normalize (bool): Whether to normalize the bounding box coordinates by the image dimensions.
+                If True, coordinates will be returned as float values between 0 and 1. Defaults to False.
+            decimals (int): Number of decimal places to round the output values to. Defaults to 5.
+        Returns:
+            (str): A xml containing all the information in results in an organized way.
+            
+        Examples:
+            >>> results = model("path/to/image.jpg")
+            >>> xml_result = results[0].toxml()
+            >>> print(xml_result)
+        """
+        
+        
+        
+        return Results.todataframe(self.summary(normalize=normalize, decimals=decimals)).to_xml()
 
     def tojson(self, normalize=False, decimals=5):
         """

99276842b8e32102e846f63083b5ef21e2c6cf9a
Y-T-G <yaseensinbox@gmail.com>
2024-09-16 21:56:06 +0800

    Disable FP16 validation when AMP fails and improve AMP checks

--- ultralytics/utils/checks.py
+++ ultralytics/utils/checks.py
@@ -655,12 +655,14 @@ def check_amp(model):
         return False  # AMP only used on CUDA devices
 
     def amp_allclose(m, im):
-        """All close FP32 vs AMP results."""
+        """All close FP32 vs AMP vs Half results."""
         a = m(im, device=device, verbose=False)[0].boxes.data  # FP32 inference
         with autocast(enabled=True):
             b = m(im, device=device, verbose=False)[0].boxes.data  # AMP inference
+        c = m(im, device=device, verbose=False, half=True)[0].boxes.data # Half inference
         del m
-        return a.shape == b.shape and torch.allclose(a, b.float(), atol=0.5)  # close to 0.5 absolute tolerance
+        allclose = torch.allclose(a, b.float(), atol=0.5) and torch.allclose(a, c.float(), atol=0.5)
+        return a.shape == b.shape == c.shape and allclose  # close to 0.5 absolute tolerance
 
     im = ASSETS / "bus.jpg"  # image to check
     prefix = colorstr("AMP: ")

fd09d7b393e27ed58a078ea1c72a76022a25eaab
RizwanMunawar <chr043416@gmail.com>
2024-09-19 21:27:30 +0500

    update

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -273,7 +273,7 @@ class Annotator:
                 lineType=cv2.LINE_AA,
             )
 
-    def text_label(self, box, label="", color=(128, 128, 128), txt_color=(255, 255, 255), margin=5):
+    def text_label(self, box, label="Ultralytics", color=(128, 128, 128), txt_color=(255, 255, 255)):
         """
         Draws a label with a background rectangle centered within a given bounding box.
 
@@ -282,22 +282,18 @@ class Annotator:
             label (str): The text label to be displayed.
             color (tuple, optional): The background color of the rectangle (B, G, R).
             txt_color (tuple, optional): The color of the text (R, G, B).
-            margin (int, optional): The margin between the text and the rectangle border.
         """
-        # Calculate the center of the bounding box
+        # Calculate the center of the box and get the text size
         x_center, y_center = int((box[0] + box[2]) / 2), int((box[1] + box[3]) / 2)
-        # Get the size of the text
         text_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, self.sf - 0.1, self.tf)[0]
+
         # Calculate the top-left corner of the text (to center it)
         text_x = x_center - text_size[0] // 2
         text_y = y_center + text_size[1] // 2
-        # Calculate the coordinates of the background rectangle
-        rect_x1 = text_x - margin
-        rect_y1 = text_y - text_size[1] - margin
-        rect_x2 = text_x + text_size[0] + margin
-        rect_y2 = text_y + margin
+
         # Draw the background rectangle
-        cv2.rectangle(self.im, (rect_x1, rect_y1), (rect_x2, rect_y2), color, -1)
+        cv2.rectangle(self.im, (text_x - self.tf * 2, text_y - text_size[1] - self.tf * 2), (text_x + + text_size[0]+ self.tf * 2, text_y + self.tf * 2), color, -1)
+
         # Draw the text on top of the rectangle
         cv2.putText(
             self.im,

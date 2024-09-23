6969667509a56eab705ac285180daf5a1522b276
RizwanMunawar <chr043416@gmail.com>
2024-09-23 17:25:41 +0500

    update

--- ultralytics/solutions/object_counter.py
+++ ultralytics/solutions/object_counter.py
@@ -127,74 +127,68 @@ class ObjectCounter:
         # Draw region or line
         annotator.draw_region(reg_pts=self.reg_pts, color=(104, 0, 123), thickness=self.tf * 2)
 
-        # Extract tracks for OBB or object detection
-        track_data = tracks[0].obb or tracks[0].boxes
+        if tracks[0].boxes.id is not None:
+            boxes = tracks[0].boxes.xyxy.cpu()
+            clss = tracks[0].boxes.cls.cpu().tolist()
+            track_ids = tracks[0].boxes.id.int().cpu().tolist()
 
-        if track_data and track_data.id is not None:
-            track_ids = track_data.id.int().cpu().tolist()
-        else:
-            return self.im0
+            # Extract tracks
+            for box, track_id, cls in zip(boxes, track_ids, clss):
+                # Draw bounding box
+                annotator.box_label(box, label=self.names[cls], color=colors(int(track_id), True))
 
-        boxes = track_data.xyxy.cpu()
-        clss = track_data.cls.cpu().tolist()
+                # Store class info
+                if self.names[cls] not in self.class_wise_count:
+                    self.class_wise_count[self.names[cls]] = {"IN": 0, "OUT": 0}
 
-        # Extract tracks
-        for box, track_id, cls in zip(boxes, track_ids, clss):
-            # Draw bounding box
-            annotator.box_label(box, label=self.names[cls], color=colors(int(track_id), True))
+                # Draw Tracks
+                track_line = self.track_history[track_id]
+                track_line.append((float((box[0] + box[2]) / 2), float((box[1] + box[3]) / 2)))
+                if len(track_line) > 30:
+                    track_line.pop(0)
 
-            # Store class info
-            if self.names[cls] not in self.class_wise_count:
-                self.class_wise_count[self.names[cls]] = {"IN": 0, "OUT": 0}
+                # Draw track trails
+                if self.draw_tracks:
+                    annotator.draw_centroid_and_tracks(
+                        track_line,
+                        color=colors(int(track_id), True),
+                        track_thickness=self.tf,
+                    )
 
-            # Draw Tracks
-            track_line = self.track_history[track_id]
-            track_line.append((float((box[0] + box[2]) / 2), float((box[1] + box[3]) / 2)))
-            if len(track_line) > 30:
-                track_line.pop(0)
+                prev_position = self.track_history[track_id][-2] if len(self.track_history[track_id]) > 1 else None
 
-            # Draw track trails
-            if self.draw_tracks:
-                annotator.draw_centroid_and_tracks(
-                    track_line,
-                    color=colors(int(track_id), True),
-                    track_thickness=self.tf,
-                )
+                # Count objects in any polygon
+                if len(self.reg_pts) >= 3:
+                    is_inside = self.counting_region.contains(Point(track_line[-1]))
 
-            prev_position = self.track_history[track_id][-2] if len(self.track_history[track_id]) > 1 else None
-
-            # Count objects in any polygon
-            if len(self.reg_pts) >= 3:
-                is_inside = self.counting_region.contains(Point(track_line[-1]))
-
-                if prev_position is not None and is_inside and track_id not in self.count_ids:
-                    self.count_ids.append(track_id)
-
-                    if (box[0] - prev_position[0]) * (self.counting_region.centroid.x - prev_position[0]) > 0:
-                        self.in_counts += 1
-                        self.class_wise_count[self.names[cls]]["IN"] += 1
-                    else:
-                        self.out_counts += 1
-                        self.class_wise_count[self.names[cls]]["OUT"] += 1
-
-            # Count objects using line
-            elif len(self.reg_pts) == 2:
-                if prev_position is not None and track_id not in self.count_ids:
-                    # Check if the object's movement segment intersects the counting line
-                    if LineString([(prev_position[0], prev_position[1]), (box[0], box[1])]).intersects(
-                        self.counting_line_segment
-                    ):
+                    if prev_position is not None and is_inside and track_id not in self.count_ids:
                         self.count_ids.append(track_id)
 
-                        # Determine the direction of movement (IN or OUT)
-                        dx = (box[0] - prev_position[0]) * (self.counting_region.centroid.x - prev_position[0])
-                        dy = (box[1] - prev_position[1]) * (self.counting_region.centroid.y - prev_position[1])
-                        if dx > 0 and dy > 0:
+                        if (box[0] - prev_position[0]) * (self.counting_region.centroid.x - prev_position[0]) > 0:
                             self.in_counts += 1
                             self.class_wise_count[self.names[cls]]["IN"] += 1
                         else:
                             self.out_counts += 1
                             self.class_wise_count[self.names[cls]]["OUT"] += 1
+
+                # Count objects using line
+                elif len(self.reg_pts) == 2:
+                    if prev_position is not None and track_id not in self.count_ids:
+                        # Check if the object's movement segment intersects the counting line
+                        if LineString([(prev_position[0], prev_position[1]), (box[0], box[1])]).intersects(
+                            self.counting_line_segment
+                        ):
+                            self.count_ids.append(track_id)
+
+                            # Determine the direction of movement (IN or OUT)
+                            dx = (box[0] - prev_position[0]) * (self.counting_region.centroid.x - prev_position[0])
+                            dy = (box[1] - prev_position[1]) * (self.counting_region.centroid.y - prev_position[1])
+                            if dx > 0 and dy > 0:
+                                self.in_counts += 1
+                                self.class_wise_count[self.names[cls]]["IN"] += 1
+                            else:
+                                self.out_counts += 1
+                                self.class_wise_count[self.names[cls]]["OUT"] += 1
 
         labels_dict = {}
 

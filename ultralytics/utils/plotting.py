4dcc109b651d422711aeaa689f6e4f4c828d7c24
RizwanMunawar <chr043416@gmail.com>
2024-09-29 16:23:55 +0500

    update queue_counts_display function

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -573,39 +573,36 @@ class Annotator:
         cv2.polylines(self.im, [points], isClosed=False, color=color, thickness=track_thickness)
         cv2.circle(self.im, (int(track[-1][0]), int(track[-1][1])), track_thickness * 2, color, -1)
 
-    def queue_counts_display(self, label, points=None, region_color=(255, 255, 255), txt_color=(0, 0, 0)):
+    def queue_counts_display(self, label, points=None, region_color=(255, 255, 255), txt_color=(104, 31, 17)):
         """
         Displays queue counts on an image centered at the points with customizable font size and colors.
 
         Args:
             label (str): queue counts label
             points (tuple): region points for center point calculation to display text
-            region_color (RGB): queue region color
-            txt_color (RGB): text display color
+            region_color (tuple, optional): queue region color
+            txt_color (tuple, optional): text display color
         """
-        x_values = [point[0] for point in points]
-        y_values = [point[1] for point in points]
-        center_x = sum(x_values) // len(points)
-        center_y = sum(y_values) // len(points)
+        center_x = sum([point[0] for point in points]) // len(points)
+        center_y = sum([point[1] for point in points]) // len(points)
 
         text_size = cv2.getTextSize(label, 0, fontScale=self.sf, thickness=self.tf)[0]
-        text_width = text_size[0]
-        text_height = text_size[1]
 
-        rect_width = text_width + 20
-        rect_height = text_height + 20
-        rect_top_left = (center_x - rect_width // 2, center_y - rect_height // 2)
-        rect_bottom_right = (center_x + rect_width // 2, center_y + rect_height // 2)
-        cv2.rectangle(self.im, rect_top_left, rect_bottom_right, region_color, -1)
-
-        text_x = center_x - text_width // 2
-        text_y = center_y + text_height // 2
+        rect_width = text_size[0] + 20
+        rect_height = text_size[1] + 20
+        cv2.rectangle(
+            self.im,
+            (center_x - rect_width // 2, center_y - rect_height // 2),
+            (center_x + rect_width // 2, center_y + rect_height // 2),
+            region_color,
+            -1,
+        )
 
         # Draw text
         cv2.putText(
             self.im,
             label,
-            (text_x, text_y),
+            (center_x - text_size[0] // 2, center_y + text_size[1] // 2),
             0,
             fontScale=self.sf,
             color=txt_color,

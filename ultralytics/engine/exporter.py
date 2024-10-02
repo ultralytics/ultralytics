b452a16a2960a8ca053fdaefd8dbace6247003ee
UltralyticsAssistant <web@ultralytics.com>
2024-10-02 09:39:45 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -182,7 +182,9 @@ class Exporter:
         fmts = tuple(export_formats()["Argument"][1:])  # available export formats
         if fmt not in fmts:
             # Get the closest match if format is invalid
-            closest_match = difflib.get_close_matches(fmt, fmts, n=1, cutoff=0.6)   # cutoff 0.6 mean a 60% match required for auto-selection.
+            closest_match = difflib.get_close_matches(
+                fmt, fmts, n=1, cutoff=0.6
+            )  # cutoff 0.6 mean a 60% match required for auto-selection.
             if closest_match:
                 LOGGER.warning(
                     f"WARNING ⚠️ Invalid export format='{fmt}'. Automatically updating to nearest valid format: '{closest_match[0]}'"

b10d48d2ecf404cba676fd03dbd8be0de80425d7
RizwanMunawar <chr043416@gmail.com>
2024-10-03 20:43:34 +0500

    update

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -182,8 +182,10 @@ def select_device(device="", batch=0, newline=False, verbose=True):
                 if torch.cuda.device_count() == 0
                 else ""
             )
-            raise ValueError(
-                f"Invalid CUDA 'device={device}' requested."
+
+            LOGGER.warning(
+            # raise ValueError(
+                f"Invalid CUDA 'device={device}' requested. Using 'device=cpu'"
                 f" Use 'device=cpu' or pass valid CUDA device(s) if available,"
                 f" i.e. 'device=0' or 'device=0,1,2,3' for Multi-GPU.\n"
                 f"\ntorch.cuda.is_available(): {torch.cuda.is_available()}"

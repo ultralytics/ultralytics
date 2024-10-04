Fixes uint8 overflow when number of segmentation objects is greater than 255. dtype of *masks* is set to int32 to allow promotion of an individual mask to uint32.

ultralytics/data/utils.py
@@ -210,13 +210,13 @@ def polygons2masks_overlap(imgsz, segments, downsample_ratio=1):
     """Return a (640, 640) overlap mask."""
     masks = np.zeros(
         (imgsz[0] // downsample_ratio, imgsz[1] // downsample_ratio),
-        dtype=np.int32 if len(segments) > 255 else np.uint8,
+        dtype=np.int32,
     )
     areas = []
     ms = []
     for si in range(len(segments)):
         mask = polygon2mask(imgsz, [segments[si].reshape(-1)], downsample_ratio=downsample_ratio, color=1)
-        ms.append(mask)
+        ms.append(mask.astype(np.uint32))
         areas.append(mask.sum())
     areas = np.asarray(areas)
     index = np.argsort(-areas)
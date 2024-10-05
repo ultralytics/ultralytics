73b4f1c314c82b2293bc23be0d2ad14916862cd4
RizwanMunawar <chr043416@gmail.com>
2024-10-05 16:37:51 +0500

    update

--- ultralytics/solutions/ai_gym.py
+++ ultralytics/solutions/ai_gym.py
@@ -23,6 +23,7 @@ class AIGym(BaseSolution):
         self.stage = []  # List for stage, necessary where there are multiple objects in frame
 
         # Extract details from CFG single time for usage later
+        self.initial_stage = None
         self.up_angle = float(self.CFG["up_angle"])  # Pose up predefined angle to consider up pose
         self.down_angle = float(self.CFG["down_angle"])  # Pose down predefined angle to consider down pose
         self.kpts = self.CFG["kpts"]  # User selected kpts of workouts storage for further usage
@@ -59,12 +60,12 @@ class AIGym(BaseSolution):
                 im0 = self.annotator.draw_specific_points(k, self.kpts, radius=self.lw * 3)
 
                 # Determine stage and count logic based on angle thresholds
-                if self.angle[ind] > self.up_angle:
-                    self.stage[ind] = "up"
-                elif self.angle[ind] < self.down_angle:
+                if self.angle[ind] < self.down_angle:
                     if self.stage[ind] == "up":
                         self.count[ind] += 1
                     self.stage[ind] = "down"
+                elif self.angle[ind] > self.up_angle:
+                    self.stage[ind] = "up"
 
                 # Display angle, count, and stage text
                 self.annotator.plot_angle_and_count_and_stage(

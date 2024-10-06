5d399b22b01581a9f45e8cfc61bda8da72dbe36f
RizwanMunawar <chr043416@gmail.com>
2024-10-06 14:49:59 +0500

    update

--- ultralytics/cfg/__init__.py
+++ ultralytics/cfg/__init__.py
@@ -82,9 +82,11 @@ CLI_HELP_MSG = f"""
 
     5. Explore your datasets using semantic search and SQL with a simple GUI powered by Ultralytics Explorer API
         yolo explorer data=data.yaml model=yolo11n.pt
+        
 
     6. Streamlit real-time webcam inference GUI
         yolo streamlit-predict
+
 
     7. Run special commands:
         yolo help

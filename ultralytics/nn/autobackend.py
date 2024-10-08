9d97283efb221d8b7cb859e4c2da3d263c8ba5b6
UltralyticsAssistant <web@ultralytics.com>
2024-10-08 17:57:13 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/autobackend.py
+++ ultralytics/nn/autobackend.py
@@ -322,7 +322,7 @@ class AutoBackend(nn.Module):
             with open(w, "rb") as f:
                 gd.ParseFromString(f.read())
             frozen_func = wrap_frozen_graph(gd, inputs="x:0", outputs=gd_outputs(gd))
-            try: # find metadata in SavedModel alongside GraphDef
+            try:  # find metadata in SavedModel alongside GraphDef
                 metadata = next(Path(w).resolve().parent.rglob(f"{Path(w).stem}_saved_model*/metadata.yaml"))
             except StopIteration:
                 pass

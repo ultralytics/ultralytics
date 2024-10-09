91722a04fdd67cf3fdacd0778713620939a9c1ba
UltralyticsAssistant <web@ultralytics.com>
2024-10-09 15:43:16 +0200

    CSV time column
    Signed-off-by: UltralyticsAssistant <web@ultralytics.com>

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -469,11 +469,9 @@ class BaseTrainer:
 
         if RANK in {-1, 0}:
             # Do final val with best.pt
-            epochs = epoch - self.start_epoch + 1  # total training epochs
-            seconds = time.time() - self.train_time_start  # total training seconds
-            LOGGER.info(f"\n{epochs} epochs completed in {seconds / 3600:.3f} hours.")
+            seconds = time.time() - self.train_time_start
+            LOGGER.info(f"\n{epoch - self.start_epoch + 1} epochs completed in {seconds / 3600:.3f} hours.")
             self.final_eval()
-            self.validator.metrics.training = {"epochs": epochs, "seconds": seconds}  # add training speed
             if self.args.plots:
                 self.plot_metrics()
             self.run_callbacks("on_train_end")

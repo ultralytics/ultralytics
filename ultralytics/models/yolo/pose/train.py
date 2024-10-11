ab382ba9db38c2f607afbd678e20634c673f3ea5
sjhpark <sampark.jh@gmail.com>
2024-10-11 02:59:03 +0000

    monitor triplet loss  during training

--- ultralytics/models/yolo/pose/train.py
+++ ultralytics/models/yolo/pose/train.py
@@ -48,7 +48,7 @@ class PoseTrainer(yolo.detect.DetectionTrainer):
 
     def get_validator(self):
         """Returns an instance of the PoseValidator class for validation."""
-        self.loss_names = 'box_loss', 'pose_loss', 'kobj_loss', 'cls_loss', 'dfl_loss'
+        self.loss_names = 'box_loss', 'pose_loss', 'kobj_loss', 'cls_loss', 'dfl_loss', 'triplet_loss'
         return yolo.pose.PoseValidator(self.test_loader, save_dir=self.save_dir, args=copy(self.args))
 
     def plot_training_samples(self, batch, ni):

ed49333d6892726b2c3f07e8fe5ad3ecbe386923
a <fish78991@gmail.com>
2024-10-11 23:39:07 +0800

    1. add hyperparameter "rad" in the "default.yaml" 2. add "rad loss" in train.py for displaying on the tensorboard 3. add "rad loss" in the loss.py

--- ultralytics/models/yolo/obb/train.py
+++ ultralytics/models/yolo/obb/train.py
@@ -38,5 +38,5 @@ class OBBTrainer(yolo.detect.DetectionTrainer):
 
     def get_validator(self):
         """Return an instance of OBBValidator for validation of YOLO model."""
-        self.loss_names = "box_loss", "cls_loss", "dfl_loss"
+        self.loss_names = "box_loss", "cls_loss", "dfl_loss", "rad_loss"
         return yolo.obb.OBBValidator(self.test_loader, save_dir=self.save_dir, args=copy(self.args))

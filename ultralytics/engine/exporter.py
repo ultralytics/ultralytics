bd29570e0790b091fd3c3ebba2bfca1ae45b7af7
Laughing-q <1185102784@qq.com>
2024-10-15 14:29:53 +0800

    Update exporter.py

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -546,7 +546,7 @@ class Exporter:
     @try_export
     def export_mnn(self, prefix=colorstr("MNN:")):
         """YOLOv8 MNN export using MNN https://github.com/alibaba/MNN."""
-        check_requirements("MNN")
+        check_requirements("MNN>=2.9.6")
         import MNN  # noqa
         from MNN.tools import mnnconvert
 

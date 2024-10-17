965583a5c95ba3e4f95df5f9d4e2a02e1b02ad1d
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-10-17 02:55:41 +0200

    Update autobackend.py

--- ultralytics/nn/autobackend.py
+++ ultralytics/nn/autobackend.py
@@ -342,7 +342,8 @@ class AutoBackend(nn.Module):
                     platform.system()
                 ]
                 interpreter = Interpreter(
-                    model_path=w, experimental_delegates=[load_delegate(delegate, options={"device": device})]
+                    model_path=w, 
+                    experimental_delegates=[load_delegate(delegate, options={"device": device})],
                 )
             else:  # TFLite
                 LOGGER.info(f"Loading {w} for TensorFlow Lite inference...")

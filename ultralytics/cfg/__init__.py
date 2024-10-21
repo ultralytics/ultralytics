eefbb22c8746e6d9056097ccface07c117ccc4fd
RizwanMunawar <chr043416@gmail.com>
2024-10-21 14:30:34 +0500

    remove task==obb check in __init__.py

--- ultralytics/cfg/__init__.py
+++ ultralytics/cfg/__init__.py
@@ -810,8 +810,6 @@ def entrypoint(debug=""):
     # Mode
     if mode in {"predict", "track"} and "source" not in overrides:
         overrides["source"] = DEFAULT_CFG.source or ASSETS
-        if task == "obb":  # use oriented bounding box assets
-            overrides["source"] = overrides["source"] / "obb"
         LOGGER.warning(f"WARNING ⚠️ 'source' argument is missing. Using default 'source={overrides['source']}'.")
     elif mode in {"train", "val"}:
         if "data" not in overrides and "resume" not in overrides:

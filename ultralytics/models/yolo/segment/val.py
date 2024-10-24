4d154c3957ce578cd7e56edbf000edf3d60adeb4
MiXaiLL76 <mike.milos@yandex.ru>
2024-10-24 23:49:03 +0300

    fix pycocotools error

--- ultralytics/models/yolo/segment/val.py
+++ ultralytics/models/yolo/segment/val.py
@@ -46,7 +46,10 @@ class SegmentationValidator(DetectionValidator):
         """Initialize metrics and select mask processing function based on save_json flag."""
         super().init_metrics(model)
         self.plot_masks = []
-
+        
+        if self.args.save_json:
+            check_requirements("pycocotools>=2.0.6")
+        
         # more accurate vs faster
         self.process = ops.process_mask_native if self.args.save_json or self.args.save_txt else ops.process_mask
         self.stats = dict(tp_m=[], tp=[], conf=[], pred_cls=[], target_cls=[], target_img=[])

e1570c55d8c2ec687a457a71c945570154f0823c
UltralyticsAssistant <web@ultralytics.com>
2024-10-25 07:30:58 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -11,7 +11,7 @@ import numpy as np
 import torch
 
 from ultralytics.utils import IS_COLAB, IS_KAGGLE, LOGGER, TryExcept, ops, plt_settings, threaded
-from ultralytics.utils.checks import check_font, check_version, is_ascii
+from ultralytics.utils.checks import is_ascii
 from ultralytics.utils.files import increment_path
 
 
@@ -478,7 +478,7 @@ class Annotator:
         """Show the annotated image."""
         drawIm = self.im
         if drawIm.dtype == np.uint16:
-            drawIm = (drawIm / 65535.0)*255.0 # Converts uint16 to uint8 for plotting purposes
+            drawIm = (drawIm / 65535.0) * 255.0  # Converts uint16 to uint8 for plotting purposes
         im = Image.fromarray(np.asarray(drawIm)[..., ::-1])  # Convert numpy array to PIL Image with RGB to BGR
         if IS_COLAB or IS_KAGGLE:  # can not use IS_JUPYTER as will run for all ipython environments
             try:
@@ -1126,7 +1126,7 @@ def plot_images(
                         except Exception:
                             pass
                         annotator.fromarray(im)
-                        
+
     # After annotations, scale the annotated image back to original dtype if necessary
     annotated_image = np.asarray(annotator.im)
     # if img_dtype == np.uint16:
@@ -1134,8 +1134,6 @@ def plot_images(
     # else:
     #     annotated_image = (annotated_image * max_pixel_value).astype(img_dtype)
 
-                        
-                
     if not save:
         return annotated_image
 

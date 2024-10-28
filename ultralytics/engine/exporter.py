92a846ffb096c5f17d79a98f84841b806da290b5
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-10-28 19:36:43 +0100

    Update exporter.py

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -457,7 +457,7 @@ class Exporter:
     @try_export
     def export_openvino(self, prefix=colorstr("OpenVINO:")):
         """YOLO OpenVINO export."""
-        check_requirements("openvino>=2024.0.0")  # fix OpenVINO issue on ARM64
+        check_requirements(("openvino>=2024.0.0", "numpy<2.0.0")  # fix numpy>=2.0.0 issue with OpenVINO
         import openvino as ov
 
         LOGGER.info(f"\n{prefix} starting export with openvino {ov.__version__}...")

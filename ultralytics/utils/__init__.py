214cc4235971e097789e911af9f1a8090afd82f3
RizwanMunawar <chr043416@gmail.com>
2024-10-29 16:38:22 +0500

    update

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -569,10 +569,14 @@ def is_kaggle():
 
 def is_jupyter():
     """
-    Check if the current script is running inside a Jupyter Notebook. Verified on Colab, Jupyterlab, Kaggle, Paperspace.
+    Check if the current script is running inside a Jupyter Notebook.
 
     Returns:
         (bool): True if running inside a Jupyter Notebook, False otherwise.
+
+    Note:
+        - Only works on Colab and Kaggle, other environments like Jupyterlab and Paperspace are not reliably detectable.
+        - "get_ipython" in globals() method suffers false positives when IPython package installed manually.
     """
     return IS_COLAB or IS_KAGGLE
 

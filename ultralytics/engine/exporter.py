83e0cb5bc163a796bb93290bdcc7d99505fbb072
Francesco Mattioli <francesco.mttl@gmail.com>
2024-10-29 17:43:02 +0100

    updated mct version

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -1075,7 +1075,7 @@ class Exporter:
 
     @try_export
     def export_mct(self, prefix=colorstr("Sony MCT:")):
-        check_requirements(["mct-nightly", "sony-custom-layers[torch]"])
+        check_requirements(["model-compression-toolkit==2.2.1", "sony-custom-layers[torch]"])
         import subprocess
 
         import model_compression_toolkit as mct

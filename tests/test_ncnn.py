9e41fb890d4851980017c2291426426b9f7138a2
Laughing-q <1185102784@qq.com>
2024-10-30 18:55:16 +0800

    Update test_ncnn.py

--- tests/test_ncnn.py
+++ tests/test_ncnn.py
@@ -19,6 +19,6 @@ def test_ncnn():
     # then export onnx
 
 
-def test_onnx():
-    model = YOLO("yolo11n.pt")
-    model.export(imgsz=160, format="onnx", half=False, int8=False, device="cpu", verbose=False)
+# def test_onnx():
+#     model = YOLO("yolo11n.pt")
+#     model.export(imgsz=160, format="onnx", half=False, int8=False, device="cpu", verbose=False)

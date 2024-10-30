472b1ced11058a6d786913f49dd469e243ae10b3
Laughing-q <1185102784@qq.com>
2024-10-30 13:45:54 +0800

    update

--- tests/test_ncnn.py
+++ tests/test_ncnn.py
@@ -5,17 +5,17 @@ def test_ncnn():
     # export ncnn and do inference
     model = YOLO("yolo11n.pt")
     filename = model.export(imgsz=160, format="ncnn", half=False, int8=False, device="cpu", verbose=False)
-    exported_model = YOLO(filename, task=model.task)
-    exported_model.val(
-        data="coco8.yaml",
-        batch=1,
-        imgsz=160,
-        plots=False,
-        device="cpu",
-        half=False,
-        int8=False,
-        verbose=False,
-    )
+    # exported_model = YOLO(filename, task=model.task)
+    # exported_model.val(
+    #     data="coco8.yaml",
+    #     batch=1,
+    #     imgsz=160,
+    #     plots=False,
+    #     device="cpu",
+    #     half=False,
+    #     int8=False,
+    #     verbose=False,
+    # )
     # then export onnx
     model = YOLO("yolo11n.pt")
     filename = model.export(imgsz=160, format="onnx", half=False, int8=False, device="cpu", verbose=False)

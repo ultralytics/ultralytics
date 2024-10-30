845ee8d4ab584a69373667310151f21d0b9dde9d
UltralyticsAssistant <web@ultralytics.com>
2024-10-30 05:38:03 +0000

    Auto-format by https://ultralytics.com/actions

--- tests/test_ncnn.py
+++ tests/test_ncnn.py
@@ -6,7 +6,7 @@ def test_ncnn():
     model = YOLO("yolo11n.pt")
     filename = model.export(imgsz=160, format="ncnn", half=False, int8=False, device="cpu", verbose=False)
     exported_model = YOLO(filename, task=model.task)
-    results = exported_model.val(
+    exported_model.val(
         data="coco8.yaml",
         batch=1,
         imgsz=160,

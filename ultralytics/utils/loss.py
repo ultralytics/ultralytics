dfc4d74c4411d841e6d867c884d75df90124ec9c
ItayCoifman <itaycoifman@gmail.com>
2024-10-30 16:08:37 +0200

    updated

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -4,7 +4,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from ultralytics.utils.metrics import OKS_SIGMA
 from ultralytics.utils.ops import crop_mask, xywh2xyxy, xyxy2xywh
 from ultralytics.utils.tal import RotatedTaskAlignedAssigner, TaskAlignedAssigner, dist2bbox, dist2rbox, make_anchors
 from ultralytics.utils.torch_utils import autocast

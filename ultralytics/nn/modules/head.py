954001673c2d7386249f966ced81739b11a112a1
Laughing-q <1185102784@qq.com>
2024-10-31 13:41:42 +0800

    Update head.py

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -248,7 +248,10 @@ class Pose(Detect):
         ndim = self.kpt_shape[1]
         if self.export:  # required for TFLite export to avoid 'PLACEHOLDER_FOR_GREATER_OP_CODES' bug
             y = kpts.view(bs, *self.kpt_shape, -1)
-            a = (y[:, :, :2] * 2.0 + (self.anchors - 0.5)) * self.strides
+            grid_h, grid_w = self.shape[2], self.shape[3]
+            grid_size = torch.tensor([grid_w, grid_h], device=y.device).reshape(1, 2, 1)
+            norm = self.strides / (self.stride[0] * grid_size)
+            a = (y[:, :, :2] * 2.0 + (self.anchors - 0.5)) * norm
             if ndim == 3:
                 a = torch.cat((a, y[:, :, 2:3].sigmoid()), 2)
             return a.view(bs, self.nk, -1)

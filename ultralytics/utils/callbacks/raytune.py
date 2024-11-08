35737f081575acbacbb1e0d1c59aa2a6d10df4c4
Y-T-G <yaseensinbox@gmail.com>
2024-11-08 10:03:02 +0000

    Correct epoch number

--- ultralytics/utils/callbacks/raytune.py
+++ ultralytics/utils/callbacks/raytune.py
@@ -16,7 +16,7 @@ def on_fit_epoch_end(trainer):
     """Sends training metrics to Ray Tune at end of each epoch."""
     if ray.train._internal.session._get_session():  # replacement for deprecated ray.tune.is_session_enabled()
         metrics = trainer.metrics
-        session.report({**metrics, **{"epoch": trainer.epoch}})
+        session.report({**metrics, **{"epoch": trainer.epoch + 2}})
 
 
 callbacks = (

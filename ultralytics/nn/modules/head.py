50aba1e908dbcd2e5d77ccd45f45e70b71a7c97a
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-11-11 00:05:00 +0100

    Update head.py

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -102,7 +102,7 @@ class Detect(nn.Module):
         # Inference path
         shape = x[0].shape  # BCHW
         x_cat = torch.cat([xi.view(shape[0], self.no, -1) for xi in x], 2)
-        if self.format != "imx" and (self.dynamic or self.shape != shape):
+        if (self.dynamic or self.shape != shape) and self.format != "imx":
             self.anchors, self.strides = (x.transpose(0, 1) for x in make_anchors(x, self.stride, 0.5))
             self.shape = shape
 

0c3d180c033ed8687884dbf202d5cbffba4dce46
Laughing-q <1185102784@qq.com>
2024-11-13 15:35:59 +0800

    Update tal.py

--- ultralytics/utils/tal.py
+++ ultralytics/utils/tal.py
@@ -69,6 +69,55 @@ class TaskAlignedAssigner(nn.Module):
                 torch.zeros_like(pd_scores[..., 0]).to(device),
             )
 
+        try:
+            target_labels, target_bboxes, target_scores, fg_mask, target_gt_idx = self._forward(
+                pd_scores,
+                pd_bboxes,
+                anc_points,
+                gt_labels,
+                gt_bboxes,
+                mask_gt,
+            )
+        except torch.OutOfMemoryError:
+            device = gt_bboxes.device
+            target_labels, target_bboxes, target_scores, fg_mask, target_gt_idx = self._forward(
+                pd_scores.cpu(),
+                pd_bboxes.cpu(),
+                anc_points.cpu(),
+                gt_labels.cpu(),
+                gt_bboxes.cpu(),
+                mask_gt.cpu(),
+            )
+            target_labels, target_bboxes, target_scores, fg_mask, target_gt_idx = (
+                target_labels.to(device),
+                target_bboxes.to(device),
+                target_scores.to(device),
+                fg_mask.to(device),
+                target_gt_idx.to(device),
+            )
+
+        return target_labels, target_bboxes, target_scores, fg_mask, target_gt_idx
+
+    def _forward(self, pd_scores, pd_bboxes, anc_points, gt_labels, gt_bboxes, mask_gt):
+        """
+        Compute the task-aligned assignment. Reference code is available at
+        https://github.com/Nioolek/PPYOLOE_pytorch/blob/master/ppyoloe/assigner/tal_assigner.py.
+
+        Args:
+            pd_scores (Tensor): shape(bs, num_total_anchors, num_classes)
+            pd_bboxes (Tensor): shape(bs, num_total_anchors, 4)
+            anc_points (Tensor): shape(num_total_anchors, 2)
+            gt_labels (Tensor): shape(bs, n_max_boxes, 1)
+            gt_bboxes (Tensor): shape(bs, n_max_boxes, 4)
+            mask_gt (Tensor): shape(bs, n_max_boxes, 1)
+
+        Returns:
+            target_labels (Tensor): shape(bs, num_total_anchors)
+            target_bboxes (Tensor): shape(bs, num_total_anchors, 4)
+            target_scores (Tensor): shape(bs, num_total_anchors, num_classes)
+            fg_mask (Tensor): shape(bs, num_total_anchors)
+            target_gt_idx (Tensor): shape(bs, num_total_anchors)
+        """
         mask_pos, align_metric, overlaps = self.get_pos_mask(
             pd_scores, pd_bboxes, gt_labels, gt_bboxes, anc_points, mask_gt
         )

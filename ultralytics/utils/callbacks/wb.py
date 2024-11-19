ad0ec167da8d60172129e02c90a671e6b2e308e6
Kyre <shinkyre@163.com>
2024-11-19 06:55:42 +0000

    [fix] fix bug in wb project name

--- ultralytics/utils/callbacks/wb.py
+++ ultralytics/utils/callbacks/wb.py
@@ -109,7 +109,9 @@ def _log_plots(plots, step):
 
 def on_pretrain_routine_start(trainer):
     """Initiate and start project if module is present."""
-    wb.run or wb.init(project=trainer.args.project or "Ultralytics", name=trainer.args.name, config=vars(trainer.args))
+    project = trainer.args.project.replace("/", "-")
+    name = trainer.args.name.replace("/", "-")
+    wb.run or wb.init(project=project or "Ultralytics", name=name, config=vars(trainer.args))
 
 
 def on_fit_epoch_end(trainer):

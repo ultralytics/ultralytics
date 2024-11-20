1a5af153420f0aeb34509aca2eaf97c1ba23458a
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-11-20 16:12:35 +0100

    Update extra.js

--- docs/overrides/javascript/extra.js
+++ docs/overrides/javascript/extra.js
@@ -70,7 +70,7 @@ window.onhashchange = () => {
 // Add Inkeep button
 document.addEventListener("DOMContentLoaded", () => {
   const inkeepScript = document.createElement("script");
-  inkeepScript.src = "https://unpkg.com/@inkeep/uikit-js@0.3.11/dist/embed.js";
+  inkeepScript.src = "https://unpkg.com/@inkeep/uikit-js@0.3.18/dist/embed.js";
   inkeepScript.type = "module";
   inkeepScript.defer = true;
   document.head.appendChild(inkeepScript);

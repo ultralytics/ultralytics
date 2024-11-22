99e993c8b6ccae3411f5d264285c4706f1f04c60
UltralyticsAssistant <web@ultralytics.com>
2024-11-22 12:09:21 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/models/yolo/detect/train.py
+++ ultralytics/models/yolo/detect/train.py
@@ -144,9 +144,9 @@ class DetectionTrainer(BaseTrainer):
         cls = np.concatenate([lb["cls"] for lb in self.train_loader.dataset.labels], 0)
         plot_labels(boxes, cls.squeeze(), names=self.data["names"], save_dir=self.save_dir, on_plot=self.on_plot)
 
-    def auto_batch(self,ch=3):
+    def auto_batch(self, ch=3):
         """Get batch size by calculating memory occupation of model."""
         train_dataset = self.build_dataset(self.trainset, mode="train", batch=16)
         # 4 for mosaic augmentation
         max_num_obj = max(len(l["cls"]) for l in train_dataset.labels) * 4
-        return super().auto_batch(ch,max_num_obj)
+        return super().auto_batch(ch, max_num_obj)

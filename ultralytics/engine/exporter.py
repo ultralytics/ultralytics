426879d80d49d0180b525c4fc2484772f9f6f8cc
Adrian Boguszewski <adrian.boguszewski@intel.com>
2024-11-22 12:25:42 +0100

    `ultralytics 8.3.36` unpin OpenVINO ARM install version (#16600)
    Co-authored-by: UltralyticsAssistant <web@ultralytics.com>
    Co-authored-by: Lakshantha Dissanayake <lakshanthad@yahoo.com>
    Co-authored-by: Lakshantha <lakshantha@ultralytics.com>
    Co-authored-by: Francesco Mattioli <Francesco.mttl@gmail.com>
    Co-authored-by: Ultralytics Assistant <135830346+UltralyticsAssistant@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -501,8 +501,7 @@ class Exporter:
     @try_export
     def export_openvino(self, prefix=colorstr("OpenVINO:")):
         """YOLO OpenVINO export."""
-        # WARNING: numpy>=2.0.0 issue with OpenVINO on macOS https://github.com/ultralytics/ultralytics/pull/17221
-        check_requirements(f'openvino{"<=2024.0.0" if ARM64 else ">=2024.0.0"}')  # fix OpenVINO issue on ARM64
+        check_requirements("openvino>=2024.5.0")
         import openvino as ov
 
         LOGGER.info(f"\n{prefix} starting export with openvino {ov.__version__}...")
@@ -530,7 +529,7 @@ class Exporter:
         if self.args.int8:
             fq = str(self.file).replace(self.file.suffix, f"_int8_openvino_model{os.sep}")
             fq_ov = str(Path(fq) / self.file.with_suffix(".xml").name)
-            check_requirements("nncf>=2.8.0")
+            check_requirements("nncf>=2.14.0")
             import nncf
 
             def transform_fn(data_item) -> np.ndarray:

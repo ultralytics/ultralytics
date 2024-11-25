b0e1cc7087966f0310c37f0c66bf078838129763
Lakshantha Dissanayake <lakshantha@ultralytics.com>
2024-11-25 02:39:29 -0800

    Improve `is_jetson` to support more Jetson devices

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -613,7 +613,8 @@ def is_jetson() -> bool:
     Returns:
         (bool): True if running on a Jetson Nano or Jetson Orin, False otherwise.
     """
-    return "NVIDIA" in PROC_DEVICE_MODEL  # i.e. "NVIDIA Jetson Nano" or "NVIDIA Orin NX"
+    # return "NVIDIA" in PROC_DEVICE_MODEL  # i.e. "NVIDIA Jetson Nano" or "NVIDIA Orin NX"
+    return any(keyword in PROC_DEVICE_MODEL.lower() for keyword in ("nvidia", "jetson"))
 
 
 def is_online() -> bool:

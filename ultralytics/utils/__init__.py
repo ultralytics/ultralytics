e8eb70c889552f846b6e2362ce5f06e46dc7837a
Lakshantha Dissanayake <lakshantha@ultralytics.com>
2024-11-25 02:54:44 -0800

    Improve docstring

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -607,11 +607,11 @@ def is_raspberrypi() -> bool:
 
 def is_jetson() -> bool:
     """
-    Determines if the Python environment is running on a Jetson Nano or Jetson Orin device by checking the device model
+    Determines if the Python environment is running on an NVIDIA Jetson device by checking the device model
     information.
 
     Returns:
-        (bool): True if running on a Jetson Nano or Jetson Orin, False otherwise.
+        (bool): True if running on an NVIDIA Jetson device, False otherwise.
     """
     return any(keyword in PROC_DEVICE_MODEL.lower() for keyword in ("nvidia", "jetson"))
 

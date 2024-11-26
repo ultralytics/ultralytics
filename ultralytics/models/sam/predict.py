ee6b36888168382b38827e69a0f902a5f03fc6e9
UltralyticsAssistant <web@ultralytics.com>
2024-11-26 02:55:38 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/models/sam/predict.py
+++ ultralytics/models/sam/predict.py
@@ -943,7 +943,7 @@ class SAM2VideoPredictor(SAM2Predictor):
         self._add_output_per_object(frame, current_out, storage_key)
         self.inference_state["frames_already_tracked"].append(frame)
         pred_masks = current_out["pred_masks"].flatten(0, 1)
-        pred_masks = pred_masks[(pred_masks > self.model.mask_threshold).sum((1, 2)) > 0] # filter blank masks
+        pred_masks = pred_masks[(pred_masks > self.model.mask_threshold).sum((1, 2)) > 0]  # filter blank masks
 
         return pred_masks, torch.ones(len(pred_masks), dtype=pred_masks.dtype, device=pred_masks.device)
 

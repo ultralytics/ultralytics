dba29115979bbc5a38e94f22f2abdd12e4c985fe
WangQvQ <1579093407@qq.com>
2024-12-03 16:08:52 +0800

    Update __init__.py

--- ultralytics/nn/modules/__init__.py
+++ ultralytics/nn/modules/__init__.py
@@ -72,7 +72,7 @@ from .conv import (
     RepConv,
     SpatialAttention,
 )
-from .head import OBB, Classify, Detect, Pose, RTDETRDecoder, Segment, WorldDetect, v10Detect, v11Detect
+from .head import OBB, Classify, Detect, Pose, RTDETRDecoder, Segment, WorldDetect, v10Detect
 from .transformer import (
     AIFI,
     MLP,
@@ -141,7 +141,6 @@ __all__ = (
     "OBB",
     "WorldDetect",
     "v10Detect",
-    "v11Detect",
     "ImagePoolingAttn",
     "ContrastiveHead",
     "BNContrastiveHead",

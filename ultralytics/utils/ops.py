c3916fbfc96cd840a3e96140e70481b17277c2df
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2024-12-05 20:55:00 +0800

    Preserve original points in resample_segments

--- ultralytics/utils/ops.py
+++ ultralytics/utils/ops.py
@@ -625,8 +625,9 @@ def resample_segments(segments, n=1000):
     """
     for i, s in enumerate(segments):
         s = np.concatenate((s, s[0:1, :]), axis=0)
-        x = np.linspace(0, len(s) - 1, n)
+        x = np.linspace(0, len(s) - 1, n - len(s))
         xp = np.arange(len(s))
+        x = np.insert(x, np.searchsorted(x, xp), xp)
         segments[i] = (
             np.concatenate([np.interp(x, xp, s[:, i]) for i in range(2)], dtype=np.float32).reshape(2, -1).T
         )  # segment xy

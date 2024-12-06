32058086c6589db29030283debfedae7820e37d2
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2024-12-06 19:16:33 +0000

    Add custom callback support for DDP training

--- ultralytics/utils/dist.py
+++ ultralytics/utils/dist.py
@@ -57,8 +57,6 @@ def generate_ddp_command(world_size, trainer):
     """Generates and returns command for distributed training."""
     import __main__  # noqa local import to avoid https://github.com/Lightning-AI/lightning/issues/15218
 
-    if not trainer.resume:
-        shutil.rmtree(trainer.save_dir)  # remove the save_dir
     file = generate_ddp_file(trainer)
     dist_cmd = "torch.distributed.run" if TORCH_1_9 else "torch.distributed.launch"
     port = find_free_network_port()

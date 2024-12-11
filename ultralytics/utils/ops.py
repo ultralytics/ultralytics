b6a1ea4618d91586c2d9adaccc736871a6723e82
Laughing-q <1185102784@qq.com>
2024-12-11 19:51:17 +0800

    Update ops.py

--- ultralytics/utils/ops.py
+++ ultralytics/utils/ops.py
@@ -623,16 +623,16 @@ def resample_segments(segments, n=1000):
     Returns:
         segments (list): the resampled segments.
     """
-    for i, s in enumerate(segments):
-        s = np.asarray(s)
+    resampled_segments = []
+    for s in segments:
         s = np.concatenate((s, s[0:1, :]), axis=0)
         x = np.linspace(0, len(s) - 1, n - len(s) if len(s) < n else n)
         xp = np.arange(len(s))
         x = np.insert(x, np.searchsorted(x, xp), xp) if len(s) < n else x
-        segments[i] = (
+        resampled_segments.append(
             np.concatenate([np.interp(x, xp, s[:, i]) for i in range(2)], dtype=np.float32).reshape(2, -1).T
         )  # segment xy
-    return segments
+    return resampled_segments
 
 
 def crop_mask(masks, boxes):

69d770761b86fa8b73add15262cb53eb520cae59
Laughing-q <1185102784@qq.com>
2024-12-13 20:00:12 +0800

    revert lqe

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -87,32 +87,32 @@ class Detect(nn.Module):
         #     )
         #     for _ in ch
         # )
-        self.cv4 = nn.ModuleList(nn.Sequential(Conv(4 * 5, 64, 1), nn.Conv2d(64, 1, 1), nn.Sigmoid()) for _ in ch)
+        # self.cv4 = nn.ModuleList(nn.Sequential(Conv(4 * 5, 64, 1), nn.Conv2d(64, 1, 1), nn.Sigmoid()) for _ in ch)
 
     def forward(self, x):
         """Concatenates and returns predicted bounding boxes and class probabilities."""
         if self.end2end:
             return self.forward_end2end(x)
 
-        # for i in range(self.nl):
-        #     x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
         for i in range(self.nl):
-            box = self.cv2[i](x[i])
-            cls = self.cv3[i](x[i])
-            # conf = self.cv4[i](box)
-            N, _, H, W = box.size()
-            prob = box.view(N, 4, self.reg_max, H, W).softmax(dim=2)
-            prob_topk = prob.topk(4, dim=2)[0]
-            prob_topk = torch.cat([prob_topk, prob_topk.mean(dim=2, keepdim=True)], dim=2).view(N, -1, H, W)
-            conf = self.cv4[i](prob_topk)
-
-            # N, C, H, W = box.size()
-            # prob = box.view(N, 4, self.reg_max, H, W).softmax(dim=2)
-            # prob_max = prob.amax(dim=2)
-            # prob_mean = prob.mean(dim=2)
-            # conf = self.cv4[i](torch.cat([prob_max, prob_mean], dim=1))
-            # # conf = self.cv4[i](prob_max)
-            x[i] = torch.cat((box, conf * cls), 1)
+            x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
+        # for i in range(self.nl):
+        #     box = self.cv2[i](x[i])
+        #     cls = self.cv3[i](x[i])
+        #     # conf = self.cv4[i](box)
+        #     N, _, H, W = box.size()
+        #     prob = box.view(N, 4, self.reg_max, H, W).softmax(dim=2)
+        #     prob_topk = prob.topk(4, dim=2)[0]
+        #     prob_topk = torch.cat([prob_topk, prob_topk.mean(dim=2, keepdim=True)], dim=2).view(N, -1, H, W)
+        #     conf = self.cv4[i](prob_topk)
+        #
+        #     # N, C, H, W = box.size()
+        #     # prob = box.view(N, 4, self.reg_max, H, W).softmax(dim=2)
+        #     # prob_max = prob.amax(dim=2)
+        #     # prob_mean = prob.mean(dim=2)
+        #     # conf = self.cv4[i](torch.cat([prob_max, prob_mean], dim=1))
+        #     # # conf = self.cv4[i](prob_max)
+        #     x[i] = torch.cat((box, conf * cls), 1)
         if self.training:  # Training path
             return x
         y = self._inference(x)

d3679f3a3c6d3000a0534cb2f4d8218f787e89ff
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-12-16 11:27:54 +0100

    ImageNet mean stride-computation forward pass
    @Y-T-G @Laughing-q simpler alternative to https://github.com/ultralytics/ultralytics/pull/18149
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -327,13 +327,14 @@ class DetectionModel(BaseModel):
             s = 256  # 2x min stride
             m.inplace = self.inplace
 
+            @torch.no_grad()
             def _forward(x):
                 """Performs a forward pass through the model, handling different Detect subclass types accordingly."""
                 if self.end2end:
                     return self.forward(x)["one2many"]
                 return self.forward(x)[0] if isinstance(m, (Segment, Pose, OBB)) else self.forward(x)
 
-            m.stride = torch.tensor([s / x.shape[-2] for x in _forward(torch.zeros(1, ch, s, s))])  # forward
+            m.stride = torch.tensor([s / x.shape[-2] for x in _forward(torch.full(1, ch, s, s), 0.447)])  # forward
             self.stride = m.stride
             m.bias_init()  # only run once
         else:

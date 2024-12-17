e269d4f9e754f986141bc8e93e507865df666387
RizwanMunawar <chr043416@gmail.com>
2024-12-17 16:55:31 +0500

    Update

--- ultralytics/solutions/security_alarm.py
+++ ultralytics/solutions/security_alarm.py
@@ -8,35 +8,48 @@ from email.mime.text import MIMEText
 
 
 class SecurityAlarm(BaseSolution):
-    def __init__(self):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.start_time = 0
-        self.end_time = 0
+        self.email_sent = False
+        self.records = self.CFG["records"]
 
-    def send_email(self, to_email, from_email, object_detected=1):
-        """Sends an email notification indicating the number of objects detected; defaults to 1 object."""
+    def authenticate(self, from_email, password, to_email):
+        # Authenticate server for email
+        import smtplib
+        server = smtplib.SMTP("smtp.gmail.com: 587")
+        server.starttls()
+        server.login(from_email, password)
+        self.to_email = to_email
+        self.from_email = from_email
+
+    def send_email(self, object_detected=5):
+        """Sends an email notification indicating the number of objects detected; defaults to 5 object."""
         message = MIMEMultipart()
-        message["From"] = from_email
-        message["To"] = to_email
+        message["From"] = self.from_email
+        message["To"] = self.to_email
         message["Subject"] = "Security Alert"
         # Add in the message body
-        message_body = f"ALERT - {object_detected} objects has been detected!!"
+        message_body = (f"Ultralytics ALERT!!! "
+                        f"{object_detected} objects has been detected!!")
 
         message.attach(MIMEText(message_body, "plain"))
         server.sendmail(from_email, to_email, message.as_string())
 
-    def display_fps(self, im0):
-        """Displays the FPS on an image `im0` by calculating and overlaying as white text on a black rectangle."""
-        self.end_time = time()
-        fps = 1 / round(self.end_time - self.start_time, 2)
-        text = f"FPS: {int(fps)}"
-        text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1.0, 2)[0]
-        gap = 10
-        cv2.rectangle(
-            im0,
-            (20 - gap, 70 - text_size[1] - gap),
-            (20 + text_size[0] + gap, 70 + gap),
-            (255, 255, 255),
-            -1,
-        )
-        cv2.putText(im0, text, (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 2)\
+    def monitor(self, im0):
+        self.annotator = Annotator(im0, line_width=self.line_width)  # Initialize annotator
+        self.extract_tracks(im0)  # Extract tracks
+
+        # Iterate over bounding boxes, track ids and classes index
+        for box, track_id, cls in zip(self.boxes, self.track_ids, self.clss):
+            # Draw bounding box and counting region
+            self.annotator.box_label(box, label=self.names[cls], color=colors(cls, True))
+
+        total_det = len(self.clss)
+        if total_det > self.records:  # Only send email If not sent before
+            if not self.email_sent:
+                send_email(total_det)
+                self.email_sent = True
+
+        self.display_output(im0)  # display output with base class function
+
+        return im0  # return output image for more usage

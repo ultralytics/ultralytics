be095be7fa7efc95c315baece37c86b9f8ee6faf
Laughing-q <1185102784@qq.com>
2024-12-17 19:38:10 +0800

    revert c2f2

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -721,20 +721,46 @@ class C3f(nn.Module):
         return self.cv3(torch.cat(y, 1))
 
 
-class C3k2(C2f):
+class C2f2(nn.Module):
     """Faster Implementation of CSP Bottleneck with 2 convolutions."""
 
-    def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True, chattn=False):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
+        expansion.
+        """
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(C2f(self.c, self.c, 2, shortcut, g) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+
+class C3k2(C2f2):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True):
         """Initializes the C3k2 module, a faster CSP Bottleneck with 2 convolutions and optional C3k blocks."""
         super().__init__(c1, c2, n, shortcut, g, e)
         self.m = nn.ModuleList(
             C3k(self.c, self.c, 2, shortcut, g) if c3k else Bottleneck(self.c, self.c, shortcut, g) for _ in range(n)
         )
-        self.chattn = ChannelAttention(c1) if chattn else None
+        # self.chattn = ChannelAttention(c1) if chattn else None
 
     def forward(self, x):
-        if self.chattn is not None:
-            x = self.chattn(x)
+        # if self.chattn is not None:
+        #     x = self.chattn(x)
         return super().forward(x)
 
 

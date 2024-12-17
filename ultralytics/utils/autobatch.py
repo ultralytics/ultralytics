e81f2987ae01ee231876e4ce8e9f7c286ad60561
UltralyticsAssistant <web@ultralytics.com>
2024-12-17 19:25:20 +0100

    Update
    Signed-off-by: UltralyticsAssistant <web@ultralytics.com>

--- ultralytics/utils/autobatch.py
+++ ultralytics/utils/autobatch.py
@@ -77,8 +77,14 @@ def autobatch(model, imgsz=640, fraction=0.60, batch_size=DEFAULT_CFG.batch, max
         results = profile(img, model, n=1, device=device, max_num_obj=max_num_obj)
 
         # Fit a solution
-        xy = [[x, y[2]] for x, y in zip(batch_sizes, results) if y]  # memory [2]
-        fit_x, fit_y = zip(*xy)
+        xy = [
+            [x, y[2]]
+            for i, (x, y) in enumerate(zip(batch_sizes, results))
+            if y  # valid result
+            and 0 < y[2] < t  # between 0 and GPU limit
+            and (i == 0 or not results[i-1] or y[2] > results[i - 1][2])  # first item or increasing memory
+        ]
+        fit_x, fit_y = zip(*xy) if xy else ([], [])
         p = np.polyfit(np.log(fit_x), np.log(fit_y), deg=1)  # first-degree polynomial fit in log space
         b = int(round(np.exp((np.log(f * fraction) - p[1]) / p[0])))  # y intercept (optimal batch size)
         if None in results:  # some sizes failed

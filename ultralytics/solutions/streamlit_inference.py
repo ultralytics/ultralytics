334e6d9f038bfcd7c4192710eb9b47dd612fdaf1
RizwanMunawar <chr043416@gmail.com>
2024-12-19 15:27:46 +0500

    update

--- ultralytics/solutions/streamlit_inference.py
+++ ultralytics/solutions/streamlit_inference.py
@@ -12,16 +12,15 @@ from ultralytics.utils.downloads import GITHUB_ASSETS_STEMS
 
 class LiveInference(BaseSolution):
     def __init__(self, **kwargs):
-        """Initializes the ObjectCounter class for real-time object counting in video streams."""
         super().__init__(**kwargs)
         check_requirements("streamlit>=1.29.0")  # scope imports for faster ultralytics package load speeds
         import streamlit as st
 
     def web_ui(self):
-        self.menu_style_cfg = """<style>MainMenu {visibility: hidden;}</style>"""  # Hide main menu style
+        menu_style_cfg = """<style>MainMenu {visibility: hidden;}</style>"""  # Hide main menu style
 
         # Main title of streamlit application
-        self.main_title_cfg = """  
+        main_title_cfg = """  
         <div>
             <h1 style="color:#FF64DA; text-align:center; font-size:40px; font-family: 'Archivo', sans-serif; 
                         margin-top:-50px;margin-bottom:20px;">
@@ -31,7 +30,7 @@ class LiveInference(BaseSolution):
         """
 
         # Subtitle of streamlit application
-        self.sub_title_cfg = """
+        sub_title_cfg = """
         <div>
             <h4 style="color:#042AFF; text-align:center; font-family: 'Archivo', sans-serif; margin-top:-15px; 
                         margin-bottom:50px;">
@@ -40,123 +39,113 @@ class LiveInference(BaseSolution):
         </div>
         """
 
+        st.set_page_config(page_title="Ultralytics Streamlit App",  # Set html page configuration
+                           layout="wide", initial_sidebar_state="auto")
+        # Append the custom HTML
+        st.markdown(menu_style_cfg, unsafe_allow_html=True)
+        st.markdown(main_title_cfg, unsafe_allow_html=True)
+        st.markdown(sub_title_cfg, unsafe_allow_html=True)
 
-    def inference(model=None):
-        """Performs real-time object detection on video input using YOLO in a Streamlit web application."""
-        from ultralytics import YOLO
+    def sidebar(self):
+        with st.sidebar:        # Add Ultralytics LOGO
+            logo = "https://raw.githubusercontent.com/ultralytics/assets/main/logo/Ultralytics_Logotype_Original.svg"
+            st.image(logo, width=250)
 
+        st.sidebar.title("User Configuration")  # Add elements to vertical setting menu
+        self.source = st.sidebar.selectbox("Video", ("webcam", "video"),)  # Add source selection dropdown
+        self.enable_trk = st.sidebar.radio("Enable Tracking", ("Yes", "No"))
+        self.conf = float(st.sidebar.slider("Confidence Threshold", 0.0, 1.0, 0.25, 0.01))
+        self.iou = float(st.sidebar.slider("IoU Threshold", 0.0, 1.0, 0.45, 0.01))
 
-    # Set html page configuration
-    st.set_page_config(page_title="Ultralytics Streamlit App", layout="wide", initial_sidebar_state="auto")
+        col1, col2 = st.columns(2)
+        self.org_frame = col1.empty()
+        self.ann_frame = col2.empty()
+        self.fps_display = st.sidebar.empty()  # Placeholder for FPS display
 
-    # Append the custom HTML
-    st.markdown(menu_style_cfg, unsafe_allow_html=True)
-    st.markdown(main_title_cfg, unsafe_allow_html=True)
-    st.markdown(sub_title_cfg, unsafe_allow_html=True)
+    def source_upload(self):
+        self.vid_file_name = ""
+        if self.source == "video":
+            vid_file = st.sidebar.file_uploader("Upload Video File", type=["mp4", "mov", "avi", "mkv"])
+            if vid_file is not None:
+                g = io.BytesIO(vid_file.read())  # BytesIO Object
+                vid_location = "ultralytics.mp4"
+                with open(vid_location, "wb") as out:  # Open temporary file as bytes
+                    out.write(g.read())  # Read bytes into file
+                self.vid_file_name = "ultralytics.mp4"
+        elif source == "webcam":
+            self.vid_file_name = 0
 
-    # Add ultralytics logo in sidebar
-    with st.sidebar:
-        logo = "https://raw.githubusercontent.com/ultralytics/assets/main/logo/Ultralytics_Logotype_Original.svg"
-        st.image(logo, width=250)
+    def configure(self):
+        # Add dropdown menu for model selection
+        available_models = [x.replace("yolo", "YOLO") for x in GITHUB_ASSETS_STEMS if x.startswith("yolo11")]
+        if model:
+            # insert model without suffix as *.pt is added later
+            available_models.insert(0, model.split(".pt")[0])
+        self.selected_model = st.sidebar.selectbox("Model", available_models)
 
-    # Add elements to vertical setting menu
-    st.sidebar.title("User Configuration")
+        with st.spinner("Model is downloading..."):
+            model = YOLO(f"{selected_model.lower()}.pt")  # Load the YOLO model
+            class_names = list(model.names.values())  # Convert dictionary to list of class names
+        st.success("Model loaded successfully!")
 
-    # Add video source selection dropdown
-    source = st.sidebar.selectbox(
-        "Video",
-        ("webcam", "video"),
-    )
+        # Multiselect box with class names and get indices of selected classes
+        selected_classes = st.sidebar.multiselect("Classes", class_names, default=class_names[:3])
+        selected_ind = [class_names.index(option) for option in selected_classes]
 
-    vid_file_name = ""
-    if source == "video":
-        vid_file = st.sidebar.file_uploader("Upload Video File", type=["mp4", "mov", "avi", "mkv"])
-        if vid_file is not None:
-            g = io.BytesIO(vid_file.read())  # BytesIO Object
-            vid_location = "ultralytics.mp4"
-            with open(vid_location, "wb") as out:  # Open temporary file as bytes
-                out.write(g.read())  # Read bytes into file
-            vid_file_name = "ultralytics.mp4"
-    elif source == "webcam":
-        vid_file_name = 0
+        if not isinstance(selected_ind, list):  # Ensure selected_options is a list
+            selected_ind = list(selected_ind)
 
-    # Add dropdown menu for model selection
-    available_models = [x.replace("yolo", "YOLO") for x in GITHUB_ASSETS_STEMS if x.startswith("yolo11")]
-    if model:
-        available_models.insert(0, model.split(".pt")[0])  # insert model without suffix as *.pt is added later
+    def inference(self, model=None):
+        if st.sidebar.button("Start"):
+            videocapture = cv2.VideoCapture(vid_file_name)  # Capture the video
 
-    selected_model = st.sidebar.selectbox("Model", available_models)
-    with st.spinner("Model is downloading..."):
-        model = YOLO(f"{selected_model.lower()}.pt")  # Load the YOLO model
-        class_names = list(model.names.values())  # Convert dictionary to list of class names
-    st.success("Model loaded successfully!")
+            if not videocapture.isOpened():
+                st.error("Could not open webcam.")
 
-    # Multiselect box with class names and get indices of selected classes
-    selected_classes = st.sidebar.multiselect("Classes", class_names, default=class_names[:3])
-    selected_ind = [class_names.index(option) for option in selected_classes]
+            stop_button = st.button("Stop")  # Button to stop the inference
 
-    if not isinstance(selected_ind, list):  # Ensure selected_options is a list
-        selected_ind = list(selected_ind)
+            while videocapture.isOpened():
+                success, frame = videocapture.read()
+                if not success:
+                    st.warning("Failed to read frame from webcam. Please make sure the webcam is connected properly.")
+                    break
 
-    enable_trk = st.sidebar.radio("Enable Tracking", ("Yes", "No"))
-    conf = float(st.sidebar.slider("Confidence Threshold", 0.0, 1.0, 0.25, 0.01))
-    iou = float(st.sidebar.slider("IoU Threshold", 0.0, 1.0, 0.45, 0.01))
+                prev_time = time.time()  # Store initial time for FPS calculation
 
-    col1, col2 = st.columns(2)
-    org_frame = col1.empty()
-    ann_frame = col2.empty()
+                # Store model predictions
+                if enable_trk == "Yes":
+                    results = model.track(frame, conf=conf, iou=iou, classes=selected_ind, persist=True)
+                else:
+                    results = model(frame, conf=conf, iou=iou, classes=selected_ind)
+                annotated_frame = results[0].plot()  # Add annotations on frame
 
-    fps_display = st.sidebar.empty()  # Placeholder for FPS display
+                # Calculate model FPS
+                curr_time = time.time()
+                fps = 1 / (curr_time - prev_time)
 
-    if st.sidebar.button("Start"):
-        videocapture = cv2.VideoCapture(vid_file_name)  # Capture the video
+                # display frame
+                org_frame.image(frame, channels="BGR")
+                ann_frame.image(annotated_frame, channels="BGR")
 
-        if not videocapture.isOpened():
-            st.error("Could not open webcam.")
+                if stop_button:
+                    videocapture.release()  # Release the capture
+                    torch.cuda.empty_cache()  # Clear CUDA memory
+                    st.stop()  # Stop streamlit app
 
-        stop_button = st.button("Stop")  # Button to stop the inference
+                # Display FPS in sidebar
+                fps_display.metric("FPS", f"{fps:.2f}")
 
-        while videocapture.isOpened():
-            success, frame = videocapture.read()
-            if not success:
-                st.warning("Failed to read frame from webcam. Please make sure the webcam is connected properly.")
-                break
+            # Release the capture
+            videocapture.release()
 
-            prev_time = time.time()  # Store initial time for FPS calculation
+        # Clear CUDA memory
+        torch.cuda.empty_cache()
 
-            # Store model predictions
-            if enable_trk == "Yes":
-                results = model.track(frame, conf=conf, iou=iou, classes=selected_ind, persist=True)
-            else:
-                results = model(frame, conf=conf, iou=iou, classes=selected_ind)
-            annotated_frame = results[0].plot()  # Add annotations on frame
-
-            # Calculate model FPS
-            curr_time = time.time()
-            fps = 1 / (curr_time - prev_time)
-
-            # display frame
-            org_frame.image(frame, channels="BGR")
-            ann_frame.image(annotated_frame, channels="BGR")
-
-            if stop_button:
-                videocapture.release()  # Release the capture
-                torch.cuda.empty_cache()  # Clear CUDA memory
-                st.stop()  # Stop streamlit app
-
-            # Display FPS in sidebar
-            fps_display.metric("FPS", f"{fps:.2f}")
-
-        # Release the capture
-        videocapture.release()
-
-    # Clear CUDA memory
-    torch.cuda.empty_cache()
-
-    # Destroy window
-    cv2.destroyAllWindows()
+        # Destroy window
+        cv2.destroyAllWindows()
 
 
 # Main function call
 if __name__ == "__main__":
-    inference()
+    LiveInference.inference(model="yolo11n.pt")
+

5eb222d6cc8d528811930eb7f596d6cb1dec3b98
UltralyticsAssistant <web@ultralytics.com>
2024-12-21 23:21:39 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -1241,7 +1241,8 @@ class SettingsManager(JSONDict):
     def update(self, *args, **kwargs):
         """Updates settings, validating keys and types."""
         for arg in args:
-            if isinstance(arg, dict): kwargs.update(arg)
+            if isinstance(arg, dict):
+                kwargs.update(arg)
         for k, v in kwargs.items():
             if k not in self.defaults:
                 raise KeyError(f"No Ultralytics setting '{k}'. {self.help_msg}")

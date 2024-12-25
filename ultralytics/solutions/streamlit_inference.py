f367b470748f9341dbf01accfd8e0e8ced73bdc0
RizwanMunawar <chr043416@gmail.com>
2024-12-25 20:27:01 +0500

    update

--- ultralytics/solutions/streamlit_inference.py
+++ ultralytics/solutions/streamlit_inference.py
@@ -186,8 +186,10 @@ if __name__ == "__main__":
 
     # Check if a model name is provided as a command-line argument
     args = len(sys.argv)
-    if args > 1:
-        model = sys.argv[1]  # Assign the first argument as the model name
+    # if args > 1:
+    #     model = sys.argv[1]  # Assign the first argument as the model name
+
+    model = args if args > 1 else None
 
     # Create an instance of the Inference class and run inference
     Inference(model=model).inference()

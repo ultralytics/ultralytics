b5eee3088d054ebe127072ab94c34540345ae779
Laughing-q <1185102784@qq.com>
2024-12-26 20:11:37 +0800

    Update dataset.py

--- ultralytics/data/dataset.py
+++ ultralytics/data/dataset.py
@@ -324,7 +324,7 @@ class GroundingDataset(YOLODataset):
                     continue
 
                 caption = img["caption"]
-                cat_name = " ".join([caption[t[0]: t[1]] for t in ann["tokens_positive"]])
+                cat_name = " ".join([caption[t[0] : t[1]] for t in ann["tokens_positive"]])
                 if cat_name not in cat2id:
                     cat2id[cat_name] = len(cat2id)
                     texts.append([cat_name])

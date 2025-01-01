d57a331a5b71c91e95a282e0e7ba57bd6769c96e
UltralyticsAssistant <web@ultralytics.com>
2025-01-01 12:51:24 +0000

    Auto-format by https://ultralytics.com/actions

--- docs/overrides/javascript/installer.js
+++ docs/overrides/javascript/installer.js
@@ -2,15 +2,18 @@ const options = { build: "latest", package: "Pip" };
 
 function updateOption(key, value) {
   options[key] = value;
-  document.querySelectorAll(`[data-key="${key}"]`).forEach((btn) =>
-    btn.classList.toggle("active", btn.dataset.value === value)
-  );
+  document
+    .querySelectorAll(`[data-key="${key}"]`)
+    .forEach((btn) =>
+      btn.classList.toggle("active", btn.dataset.value === value),
+    );
   updateCommand();
 }
 
 function updateCommand() {
   const { build, package } = options;
-  const version = build !== "latest" ? (package === "Pip" ? `==${build}` : `=${build}`) : "";
+  const version =
+    build !== "latest" ? (package === "Pip" ? `==${build}` : `=${build}`) : "";
   document.getElementById("command").innerText =
     `${package === "Pip" ? "pip3 install ultralytics" : "conda install -c conda-forge ultralytics"}${version}`;
 }

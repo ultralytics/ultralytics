ded7f5edfdd32b676945609c60d955c262519357
UltralyticsAssistant <web@ultralytics.com>
2025-01-08 17:37:27 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/engine/predictor.py
+++ ultralytics/engine/predictor.py
@@ -393,8 +393,9 @@ class BasePredictor:
         im = self.plotted_img
         if IS_COLAB or IS_KAGGLE:
             try:
-                from PIL import Image
                 import matplotlib.pyplot as plt
+                from PIL import Image
+
                 # Convert NumPy array (BGR) to PIL Image (RGB)
                 im = Image.fromarray(im[..., ::-1])
 
@@ -405,7 +406,7 @@ class BasePredictor:
                 # Display the saved image inline using Matplotlib
                 img = plt.imread(save_path)
                 plt.imshow(img)
-                plt.axis('off')  # Hide axes for better visualization
+                plt.axis("off")  # Hide axes for better visualization
                 plt.show()
 
                 # Optionally remove the file if cleanup is desired

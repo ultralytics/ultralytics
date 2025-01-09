2f189cd017cc154183d373388d7ccc1649565740
UltralyticsAssistant <web@ultralytics.com>
2025-01-09 08:07:09 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -453,7 +453,9 @@ class Exporter:
         )
         n = len(dataset)
         if n < self.args.batch:
-            raise ValueError(f"The calibration dataset ({n} images) must have at least as many images as the batch size ('batch={self.args.batch}').")
+            raise ValueError(
+                f"The calibration dataset ({n} images) must have at least as many images as the batch size ('batch={self.args.batch}')."
+            )
         elif n < 300:
             LOGGER.warning(f"{prefix} WARNING ⚠️ >300 images recommended for INT8 calibration, found {n} images.")
         return build_dataloader(dataset, batch=batch, workers=0)  # required for batch loading

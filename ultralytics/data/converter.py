fbb90f87253b26be35977f6a19c2e823c5bd0fc6
UltralyticsAssistant <web@ultralytics.com>
2025-01-09 19:28:53 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/data/converter.py
+++ ultralytics/data/converter.py
@@ -242,7 +242,9 @@ def convert_coco(
         from ultralytics.data.converter import convert_coco
 
         convert_coco("../datasets/coco/annotations/", use_segments=True, use_keypoints=False, cls91to80=False)
-        convert_coco("../datasets/lvis/annotations/", use_segments=True, use_keypoints=False, cls91to80=False, lvis=True)
+        convert_coco(
+            "../datasets/lvis/annotations/", use_segments=True, use_keypoints=False, cls91to80=False, lvis=True
+        )
         ```
 
     Output:
@@ -270,7 +272,7 @@ def convert_coco(
             data = json.load(f)
 
         # Create image dict
-        images = {f'{x["id"]:d}': x for x in data["images"]}
+        images = {f"{x['id']:d}": x for x in data["images"]}
         # Create image-annotations dict
         imgToAnns = defaultdict(list)
         for ann in data["annotations"]:

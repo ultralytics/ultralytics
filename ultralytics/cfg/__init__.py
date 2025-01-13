9a89186b52ceaa3b99292ebac0b71eb992631259
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-01-13 15:53:42 +0500

    Add warning for `task=classify` with `mode=track` (#18621)
    Co-authored-by: UltralyticsAssistant <web@ultralytics.com>
    Co-authored-by: Ultralytics Assistant <135830346+UltralyticsAssistant@users.noreply.github.com>

--- ultralytics/cfg/__init__.py
+++ ultralytics/cfg/__init__.py
@@ -913,7 +913,12 @@ def entrypoint(debug=""):
     # Task
     task = overrides.pop("task", None)
     if task:
-        if task not in TASKS:
+        if task == "classify" and mode == "track":
+            raise ValueError(
+                f"❌ Classification doesn't support 'mode=track'. Valid modes for classification are"
+                f" {MODES - {'track'}}.\n{CLI_HELP_MSG}"
+            )
+        elif task not in TASKS:
             raise ValueError(f"Invalid 'task={task}'. Valid tasks are {TASKS}.\n{CLI_HELP_MSG}")
         if "model" not in overrides:
             overrides["model"] = TASK2MODEL[task]

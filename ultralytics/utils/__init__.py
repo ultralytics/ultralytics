8abc75b1968447fbadc9f65cbe8b55ce108b7b91
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-01-19 17:07:27 +0100

    Update __init__.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -1052,7 +1052,7 @@ def set_sentry():
         auto_enabling_integrations=False,
         traces_sample_rate=1.0,
         release=__version__,
-        environment="production",  # 'dev' or 'production'
+        environment="runpod" if is_runpod() else "production",
         before_send=before_send,
         ignore_errors=[KeyboardInterrupt, FileNotFoundError],
     )

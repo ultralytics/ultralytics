09017e0fa1cdb8ba8572f7126b4f3e0f5c598101
UltralyticsAssistant <web@ultralytics.com>
2025-01-20 22:18:10 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/utils/benchmarks.py
+++ ultralytics/utils/benchmarks.py
@@ -40,7 +40,7 @@ import yaml
 from ultralytics import YOLO, YOLOWorld
 from ultralytics.cfg import TASK2DATA, TASK2METRIC
 from ultralytics.engine.exporter import export_formats
-from ultralytics.utils import ARM64, ASSETS, IS_JETSON, IS_RASPBERRYPI, LINUX, LOGGER, MACOS, TQDM, WEIGHTS_DIR
+from ultralytics.utils import ARM64, ASSETS, LINUX, LOGGER, MACOS, TQDM, WEIGHTS_DIR
 from ultralytics.utils.checks import IS_PYTHON_3_12, check_requirements, check_yolo
 from ultralytics.utils.downloads import safe_download
 from ultralytics.utils.files import file_size

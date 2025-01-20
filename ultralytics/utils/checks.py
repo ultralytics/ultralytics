174b175ee67e9fcc785c8fe13fce2e349134ccbc
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-01-21 00:07:16 +0100

    Update checks.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/utils/checks.py
+++ ultralytics/utils/checks.py
@@ -815,6 +815,7 @@ def is_sudo_available() -> bool:
 # Run checks and define constants
 check_python("3.8", hard=False, verbose=True)  # check python version
 check_torchvision()  # check torch-torchvision compatibility
+
 # Define constants
 IS_PYTHON_MINIMUM_3_10 = check_python("3.10", hard=False)
 IS_PYTHON_3_12 = PYTHON_VERSION.startswith("3.12")

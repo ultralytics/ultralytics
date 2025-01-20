d7af53b198cd2cf039cbd9139f1c1708b78d0db8
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-01-21 00:45:08 +0100

    Fix link redirects

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -124,7 +124,7 @@ def export_formats():
         ["MNN", "mnn", ".mnn", True, True, ["batch", "half", "int8"]],
         ["NCNN", "ncnn", "_ncnn_model", True, True, ["batch", "half"]],
         ["IMX", "imx", "_imx_model", True, True, ["int8"]],
-        ["RKNN", "rknn", "_rknn_model", False, False, ["name"]],
+        ["RKNN", "rknn", "_rknn_model", False, False, ["batch", "name"]],
     ]
     return dict(zip(["Format", "Argument", "Suffix", "CPU", "GPU", "Arguments"], zip(*x)))
 

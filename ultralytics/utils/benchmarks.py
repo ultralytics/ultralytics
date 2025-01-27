b6023a07160f8285939b8d64accd39a4bd3dc6a5
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-01-27 09:43:58 -0600

    Fix

--- ultralytics/utils/benchmarks.py
+++ ultralytics/utils/benchmarks.py
@@ -81,7 +81,7 @@ def benchmark(
         >>> benchmark(model="yolo11n.pt", imgsz=640)
     """
     imgsz = check_imgsz(imgsz)
-    assert imgsz[0] == imgsz[1] if isinstance(imgsz, list) else None, "benchmark() only supports square imgsz."
+    assert (imgsz[0] == imgsz[1] if isinstance(imgsz, list) else True), "benchmark() only supports square imgsz."
 
     import pandas as pd  # scope for faster 'import ultralytics'
 

84d06d0010c5e527078452a1aaa79df5578683c5
RizwanMunawar <chr043416@gmail.com>
2025-01-30 13:43:41 +0500

    update

--- ultralytics/solutions/__init__.py
+++ ultralytics/solutions/__init__.py
@@ -1,38 +1,38 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
-from .ai_gym import AIGym
-from .analytics import Analytics
-from .distance_calculation import DistanceCalculation
-from .heatmap import Heatmap
-from .instance_segmentation import InstanceSegmentation
-from .object_blurrer import ObjectBlurrer
 from .object_counter import ObjectCounter
 from .object_cropper import ObjectCropper
-from .parking_management import ParkingManagement, ParkingPtsSelection
-from .queue_management import QueueManager
+from .object_blurrer import ObjectBlurrer
+from .ai_gym import AIGym
 from .region_counter import RegionCounter
 from .security_alarm import SecurityAlarm
+from .heatmap import Heatmap
+from .instance_segmentation import InstanceSegmentation
+from .vision_eye import VisionEye
 from .speed_estimation import SpeedEstimator
+from .distance_calculation import DistanceCalculation
+from .queue_management import QueueManager
+from .parking_management import ParkingManagement, ParkingPtsSelection
+from .analytics import Analytics
 from .streamlit_inference import Inference
 from .trackzone import TrackZone
-from .vision_eye import VisionEye
 
 __all__ = (
-    "AIGym",
-    "DistanceCalculation",
-    "Heatmap",
     "ObjectCounter",
-    "ParkingManagement",
-    "ParkingPtsSelection",
-    "QueueManager",
-    "SpeedEstimator",
-    "Analytics",
-    "Inference",
-    "RegionCounter",
-    "TrackZone",
-    "SecurityAlarm",
     "ObjectCropper",
     "ObjectBlurrer",
+    "AIGym",
+    "RegionCounter",
+    "SecurityAlarm",
+    "Heatmap",
     "InstanceSegmentation",
     "VisionEye",
+    "SpeedEstimator",
+    "DistanceCalculation",
+    "QueueManager",
+    "ParkingManagement",
+    "ParkingPtsSelection",
+    "Analytics",
+    "Inference",
+    "TrackZone",
 )

700ad2102737f0cd60f1fc9c32e92801bade6748
RizwanMunawar <chr043416@gmail.com>
2025-01-30 10:57:54 +0500

    update heatmap.py

--- ultralytics/solutions/heatmap.py
+++ ultralytics/solutions/heatmap.py
@@ -78,9 +78,10 @@ class Heatmap(ObjectCounter):
             im0 (np.ndarray): Input image array for processing.
 
         Returns:
-            results (dict): Contains processed image `im0`, 'in_count' (int, count of objects entering the region),
-            'out_count' (int, count of objects exiting the region),
-            'classwise_count' (dict, per-class object count), and 'total_tracks' (int, total number of tracked objects).
+            results (dict): Contains processed image `im0`,
+                'in_count' (int, count of objects entering the region),
+                'out_count' (int, count of objects exiting the region),
+                'classwise_count' (dict, per-class object count), and 'total_tracks' (int, total number of tracked objects).
 
         Examples:
             >>> heatmap = Heatmap()

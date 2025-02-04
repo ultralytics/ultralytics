dcfd4b8fe5ff8ec0519b51ad14b33424f8b15ff6
UltralyticsAssistant <web@ultralytics.com>
2025-02-04 22:28:08 +0000

    Auto-format by https://ultralytics.com

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# RGB-IR-Ultralytics 
+# RGB-IR-Ultralytics
 
-## YOLO with Dual-modal Imput
+## YOLO with Dual-modal Input
 
 This project extends version 8.3.70 of [Ultralytics](https://github.com/ultralytics/ultralytics) by incorporating support for RGB+IR (visible light + infrared) dual-modal input for object detection tasks. Additionally, it provides an adjusted and adapted version of the [LLVIP](https://github.com/bupt-ai-cz/LLVIP) dataset, named [LLVIP-For-Ultralytics](https://github.com/Tdzdele/LLVIP-For-Ultralytics), for training and testing. Users are encouraged to cite the original work if utilized in research or applications.
 
--- train.py
+++ train.py
@@ -1,18 +1,19 @@
 from ultralytics import YOLO
 
-if __name__ == '__main__':
-    model = YOLO('ultralytics/cfg/models/11/yolo11-RGBIR.yaml')
-    model.train(data='ultralytics/cfg/datasets/LLVIP.yaml',
-                cache=False,
-                imgsz=640,
-                epochs=200,
-                batch=8,
-                close_mosaic=0,
-                workers=64,
-                device='0',
-                optimizer='SGD',
-                patience=0,
-                amp=True, # Bypass the AMP check during RGB+IR training or set to False.
-                project='runs/train',
-                name='RGB-IR',
-                )
+if __name__ == "__main__":
+    model = YOLO("ultralytics/cfg/models/11/yolo11-RGBIR.yaml")
+    model.train(
+        data="ultralytics/cfg/datasets/LLVIP.yaml",
+        cache=False,
+        imgsz=640,
+        epochs=200,
+        batch=8,
+        close_mosaic=0,
+        workers=64,
+        device="0",
+        optimizer="SGD",
+        patience=0,
+        amp=True,  # Bypass the AMP check during RGB+IR training or set to False.
+        project="runs/train",
+        name="RGB-IR",
+    )

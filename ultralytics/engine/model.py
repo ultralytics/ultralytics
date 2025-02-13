f4114b031915bafdb94d95f310ce6f4ccca259bd
Love and hope <33247520+LoveAndHope-dev@users.noreply.github.com>
2025-02-13 20:57:36 +0800

    Initialize `model_name` attribute (#19224)

--- ultralytics/engine/model.py
+++ ultralytics/engine/model.py
@@ -122,6 +122,7 @@ class Model(torch.nn.Module):
         self.metrics = None  # validation/training metrics
         self.session = None  # HUB session
         self.task = task  # task type
+        self.model_name = None  # model name
         model = str(model).strip()
 
         # Check if Ultralytics HUB model from https://hub.ultralytics.com

e15835573c097f420416c6b3670af61a8235375b
RizwanMunawar <chr043416@gmail.com>
2025-02-13 14:50:20 +0500

    remove self from annotator in trackzone.py

--- ultralytics/solutions/trackzone.py
+++ ultralytics/solutions/trackzone.py
@@ -53,7 +53,7 @@ class TrackZone(BaseSolution):
             >>> tracker.trackzone(frame)
         """
         plot_im = im0  # For plotting the results
-        self.annotator = SolutionAnnotator(plot_im, line_width=self.line_width)  # Initialize annotator
+        annotator = SolutionAnnotator(plot_im, line_width=self.line_width)  # Initialize annotator
         # Create a mask for the region and extract tracks from the masked image
         masked_frame = cv2.bitwise_and(
             plot_im, plot_im, mask=cv2.fillPoly(np.zeros_like(plot_im[:, :, 0]), [self.region], 255)
@@ -64,7 +64,7 @@ class TrackZone(BaseSolution):
 
         # Iterate over boxes, track ids, classes indexes list and draw bounding boxes
         for box, track_id, cls in zip(self.boxes, self.track_ids, self.clss):
-            self.annotator.box_label(box, label=f"{self.names[cls]}:{track_id}", color=colors(track_id, True))
+            annotator.box_label(box, label=f"{self.names[cls]}:{track_id}", color=colors(track_id, True))
 
         self.display_output(plot_im)  # display output with base class function
 

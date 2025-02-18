e425b93d1b372c0b479566545ec85a82f1df8002
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-02-18 20:17:00 +0800

    Update exporter.py
    Use `bs` in more places
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -1571,8 +1571,8 @@ class NMSModel(torch.nn.Module):
         scores, classes = scores.max(dim=-1)
         self.args.max_det = min(pred.shape[1], self.args.max_det)  # in case num_anchors < max_det
         # (N, max_det, 4 coords + 1 class score + 1 class label + extra_shape).
-        out = torch.zeros(boxes.shape[0], self.args.max_det, boxes.shape[-1] + 2 + extra_shape, **kwargs)
-        for i in range(pred.shape[0]):
+        out = torch.zeros(bs, self.args.max_det, boxes.shape[-1] + 2 + extra_shape, **kwargs)
+        for i in range(bs):
             box, cls, score, extra = boxes[i], classes[i], scores[i], extras[i]
             mask = score > self.args.conf
             if self.is_tf:

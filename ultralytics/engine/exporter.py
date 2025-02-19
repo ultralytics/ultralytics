b94dd87f9d90a9e1463241e52320a29da46ba5f4
Laughing <61612323+Laughing-q@users.noreply.github.com>
2025-02-19 20:02:54 +0800

    Revert "Fix dynamic export with YOLO World" (#19308)

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -520,7 +520,7 @@ class Exporter:
             if isinstance(self.model, SegmentationModel):
                 dynamic["output0"] = {0: "batch", 2: "anchors"}  # shape(1, 116, 8400)
                 dynamic["output1"] = {0: "batch", 2: "mask_height", 3: "mask_width"}  # shape(1,32,160,160)
-            elif isinstance(self.model, (DetectionModel, WorldModel)):
+            elif isinstance(self.model, DetectionModel):
                 dynamic["output0"] = {0: "batch", 2: "anchors"}  # shape(1, 84, 8400)
             if self.args.nms:  # only batch size is dynamic with NMS
                 dynamic["output0"].pop(2)

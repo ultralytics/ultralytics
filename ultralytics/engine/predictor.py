7cd300944b52578cca9951840281e7a3e596e20b
RizwanMunawar <chr043416@gmail.com>
2025-02-25 15:13:03 +0500

    update predictor.py

--- ultralytics/engine/predictor.py
+++ ultralytics/engine/predictor.py
@@ -393,21 +393,12 @@ class BasePredictor:
     def show(self, p=""):
         """Display an image in a window using the OpenCV imshow function."""
         im = self.plotted_img
-        if IS_COLAB or IS_KAGGLE:
-            cv2.imwrite("temp.png", im)
-            from ultralytics.data.utils import display_media_in_colab
-
-            try:
-                display_media_in_colab("temp.png")
-            except Exception as e:
-                print(f"Error displaying image: {e}")
-        else:
-            if platform.system() == "Linux" and p not in self.windows:
-                self.windows.append(p)
-                cv2.namedWindow(p, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
-                cv2.resizeWindow(p, im.shape[1], im.shape[0])  # (width, height)
-            cv2.imshow(p, im)
-            cv2.waitKey(300 if self.dataset.mode == "image" else 1)  # 1 millisecond
+        if platform.system() == "Linux" and p not in self.windows:
+            self.windows.append(p)
+            cv2.namedWindow(p, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
+            cv2.resizeWindow(p, im.shape[1], im.shape[0])  # (width, height)
+        cv2.imshow(p, im)
+        cv2.waitKey(300 if self.dataset.mode == "image" else 1)  # 1 millisecond
 
     def run_callbacks(self, event: str):
         """Runs all registered callbacks for a specific event."""

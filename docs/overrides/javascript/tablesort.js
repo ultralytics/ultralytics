da26e8e89cf70cdb7e0aee2b05aeb14a729ffb56
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-02-26 00:39:56 +0800

    Update tablesort.js

--- docs/overrides/javascript/tablesort.js
+++ docs/overrides/javascript/tablesort.js
@@ -1,5 +1,5 @@
 document$.subscribe(function () {
-  var tables = document.querySelectorAll("article div.tip table");
+  var tables = document.querySelectorAll("article table:not([class])");
   tables.forEach(function (table) {
     new Tablesort(table);
   });

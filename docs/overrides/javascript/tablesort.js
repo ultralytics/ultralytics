20d0ed8a4792a52ec41756f01bf3b5c40fd1c8bb
UltralyticsAssistant <web@ultralytics.com>
2025-02-27 08:24:32 +0000

    Auto-format by https://ultralytics.com/actions

--- docs/overrides/javascript/tablesort.js
+++ docs/overrides/javascript/tablesort.js
@@ -66,35 +66,39 @@ Tablesort.extend(
 );
 
 // tablesort.number.min.js
-(function() {
-  var cleanNumber = function(i) {
-    // Remove everything after ± symbol if present
-    i = i.split('±')[0].trim();
-    return i.replace(/[^\-?0-9.]/g, '');
-  },
+(function () {
+  var cleanNumber = function (i) {
+      // Remove everything after ± symbol if present
+      i = i.split("±")[0].trim();
+      return i.replace(/[^\-?0-9.]/g, "");
+    },
+    compareNumber = function (a, b) {
+      a = parseFloat(a);
+      b = parseFloat(b);
 
-  compareNumber = function(a, b) {
-    a = parseFloat(a);
-    b = parseFloat(b);
+      a = isNaN(a) ? 0 : a;
+      b = isNaN(b) ? 0 : b;
 
-    a = isNaN(a) ? 0 : a;
-    b = isNaN(b) ? 0 : b;
+      return a - b;
+    };
 
-    return a - b;
-  };
+  Tablesort.extend(
+    "number",
+    function (item) {
+      return (
+        item.match(/^[-+]?[£\x24Û¢´€]?\d+\s*([,\.]\d{0,2})/) || // Prefixed currency
+        item.match(/^[-+]?\d+\s*([,\.]\d{0,2})?[£\x24Û¢´€]/) || // Suffixed currency
+        item.match(/^[-+]?(\d)*-?([,\.]){0,1}-?(\d)+([E,e][\-+][\d]+)?%?$/)
+      ); // Number
+    },
+    function (a, b) {
+      a = cleanNumber(a);
+      b = cleanNumber(b);
 
-  Tablesort.extend('number', function(item) {
-    return item.match(/^[-+]?[£\x24Û¢´€]?\d+\s*([,\.]\d{0,2})/) || // Prefixed currency
-      item.match(/^[-+]?\d+\s*([,\.]\d{0,2})?[£\x24Û¢´€]/) || // Suffixed currency
-      item.match(/^[-+]?(\d)*-?([,\.]){0,1}-?(\d)+([E,e][\-+][\d]+)?%?$/); // Number
-  }, function(a, b) {
-    a = cleanNumber(a);
-    b = cleanNumber(b);
-
-    return compareNumber(b, a);
-  });
-
-}());
+      return compareNumber(b, a);
+    },
+  );
+})();
 
 // tablesort.monthname.min.js
 Tablesort.extend(

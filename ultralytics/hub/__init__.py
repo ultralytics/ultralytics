e1ee8ee44c0df851039f364a1dc6d98b060f029a
RizwanMunawar <chr043416@gmail.com>
2025-03-03 09:36:55 +0500

    update __init__.py

--- ultralytics/hub/__init__.py
+++ ultralytics/hub/__init__.py
@@ -126,18 +126,16 @@ def check_dataset(path: str, task: str) -> None:
         path (str): Path to data.zip (with data.yaml inside data.zip).
         task (str): Dataset task. Options are 'detect', 'segment', 'pose', 'classify', 'obb'.
 
-    Example:
+    Examples:
         Download *.zip files from https://github.com/ultralytics/hub/tree/main/example_datasets
             i.e. https://github.com/ultralytics/hub/raw/main/example_datasets/coco8.zip for coco8.zip.
-        ```python
-        from ultralytics.hub import check_dataset
+        >>> from ultralytics.hub import check_dataset
 
-        check_dataset("path/to/coco8.zip", task="detect")  # detect dataset
-        check_dataset("path/to/coco8-seg.zip", task="segment")  # segment dataset
-        check_dataset("path/to/coco8-pose.zip", task="pose")  # pose dataset
-        check_dataset("path/to/dota8.zip", task="obb")  # OBB dataset
-        check_dataset("path/to/imagenet10.zip", task="classify")  # classification dataset
-        ```
+        >>> check_dataset("path/to/coco8.zip", task="detect")  # detect dataset
+        >>> check_dataset("path/to/coco8-seg.zip", task="segment")  # segment dataset
+        >>> check_dataset("path/to/coco8-pose.zip", task="pose")  # pose dataset
+        >>> check_dataset("path/to/dota8.zip", task="obb")  # OBB dataset
+        >>> check_dataset("path/to/imagenet10.zip", task="classify")  # classification dataset
     """
     HUBDatasetStats(path=path, task=task).get_json()
     LOGGER.info(f"Checks completed correctly ✅. Upload this dataset to {HUB_WEB_ROOT}/datasets/.")

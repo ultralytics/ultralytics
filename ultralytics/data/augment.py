13c1b5e180c511cba0e6086aabf8130b5acfcaae
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-03-04 19:58:29 +0800

    Revert saturation and value augmentation to relative scaling
    Signed-off-by: Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -1376,11 +1376,11 @@ class RandomHSV:
             # lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
 
             # Fixed implementation in https://github.com/ultralytics/ultralytics/pull/19311
-            r = np.random.uniform(-1, 1, 3) * (self.hgain, self.sgain, self.vgain) * (180, 255, 255)  # random gains
+            r = np.random.uniform(-1, 1, 3) * (self.hgain, self.sgain, self.vgain) * (180, 1, 1)  # random gains
             x = np.arange(0, 256, dtype=r.dtype)
             lut_hue = ((x + r[0]) % 180).astype(dtype)
-            lut_sat = np.clip(x + r[1], 0, 255).astype(dtype)
-            lut_val = np.clip(x + r[2], 0, 255).astype(dtype)
+            lut_sat = np.clip(x * (1 + r[1]), 0, 255).astype(dtype)
+            lut_val = np.clip(x * (1 + r[2]), 0, 255).astype(dtype)
             lut_sat[0] = 0  # prevent pure white changing color
 
             hue, sat, val = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))

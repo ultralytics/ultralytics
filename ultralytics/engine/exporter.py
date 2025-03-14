f15206e76be9349120bd6ad8e017a67b751024bf
Lakshantha Dissanayake <lakshantha@ultralytics.com>
2025-03-14 13:10:49 -0700

    Revert "Experiment with moving requirements to `pyproject.toml`"
    This reverts commit bf23474a1db56eb2e4bc3627dfac91ac87b071d3.

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -1222,8 +1222,8 @@ class Exporter:
             raise ValueError("IMX export is not supported for end2end models.")
         if "C2f" not in self.model.__str__():
             raise ValueError("IMX export is only supported for YOLOv8n detection models")
-        check_requirements(("model-compression-toolkit<=2.1.1", "sony-custom-layers<=0.2.0", "tensorflow<=2.15.0"))
-        check_requirements("imx500-converter[pt]<=3.14.3")  # Separate requirements for imx500-converter
+        check_requirements(("model-compression-toolkit==2.1.1", "sony-custom-layers==0.2.0", "tensorflow==2.12.0"))
+        check_requirements("imx500-converter[pt]==3.14.3")  # Separate requirements for imx500-converter
 
         import model_compression_toolkit as mct
         import onnx

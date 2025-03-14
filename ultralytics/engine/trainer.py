137b853c3dfb1a931dd543433da0162956aa3c69
CMD <80255379+iCMDdev@users.noreply.github.com>
2025-03-14 20:33:25 +0200

    Update trainer.py
    Signed-off-by: CMD <80255379+iCMDdev@users.noreply.github.com>

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -495,7 +495,10 @@ class BaseTrainer:
         if self.device.type == "mps":
             memory = torch.mps.driver_allocated_memory()
             if fraction:
-                total = torch.mps.recommended_max_memory()
+                if hasattr(torch.mps, "recommended_max_memory"):
+                    total = torch.mps.recommended_max_memory()
+                else:
+                    return __import__("psutil").virtual_memory().percent / 100
         elif self.device.type == "cpu":
             pass
         else:

b3a1e45719de01e5669010202e6ad1379e3303c4
Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>
2025-03-16 00:42:48 +0800

    Update exporter.py
    Signed-off-by: Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -1007,7 +1007,7 @@ class Exporter:
                 "sng4onnx>=1.0.1",  # required by 'onnx2tf' package
                 "onnx_graphsurgeon>=0.3.26",  # required by 'onnx2tf' package
                 "onnx>=1.12.0",
-                "onnx2tf>=1.26.3",
+                "onnx2tf==1.26.3",
                 "onnxslim>=0.1.31",
                 "tflite_support<=0.4.3" if IS_JETSON else "tflite_support",  # fix ImportError 'GLIBCXX_3.4.29'
                 "flatbuffers>=23.5.26,<100",  # update old 'flatbuffers' included inside tensorflow package

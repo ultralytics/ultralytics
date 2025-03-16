ca487e056f3ef2919695457e7be62af0475f996b
Keshav Sharan <keshavsharan97@gmail.com>
2025-03-16 11:43:36 -0500

    Fix `Autobackend` Python version check (#19722)
    Signed-off-by: Keshav Sharan <keshavsharan97@gmail.com>
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -947,7 +947,7 @@ class TryExcept(contextlib.ContextDecorator):
     def __exit__(self, exc_type, value, traceback):
         """Defines behavior when exiting a 'with' block, prints error message if necessary."""
         if self.verbose and value:
-            print(emojis(f"{self.msg}{': ' if self.msg else ''}{value}"))
+            LOGGER.warning(f"{self.msg}{': ' if self.msg else ''}{value}")
         return True
 
 

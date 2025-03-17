9f504c36f7b818b6ac643c6578d4de58b3000cc5
shr4git <123702656+shr4git@users.noreply.github.com>
2025-03-17 03:26:19 -0700

    Update tasks.py
    Signed-off-by: shr4git <123702656+shr4git@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -64,6 +64,7 @@ from ultralytics.nn.modules import (
     WorldDetect,
     v10Detect,
     BiFPN,
+    WeightedAdd
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml

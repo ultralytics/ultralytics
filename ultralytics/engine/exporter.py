4cd832e0812ad7c2afafc19b9de4e29f1a9239a1
Lakshantha Dissanayake <lakshantha@ultralytics.com>
2025-03-18 15:30:18 -0700

    Update exporter.py

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -1013,8 +1013,7 @@ class Exporter:
                 "tflite_support<=0.4.3" if IS_JETSON else "tflite_support",  # fix ImportError 'GLIBCXX_3.4.29'
                 "flatbuffers>=23.5.26,<100",  # update old 'flatbuffers' included inside tensorflow package
                 "onnxruntime-gpu" if cuda else "onnxruntime",
-                "protobuf>=5,<6",  # tflite_support pins <=4 but >=5 works
-                # add protobuf pin for imx500 export
+                "protobuf<=4",  # tflite_support pins <=4 but >=5 works, temporal pin for imx500 export
             ),
             cmds="--extra-index-url https://pypi.ngc.nvidia.com",  # onnx_graphsurgeon only on NVIDIA
         )

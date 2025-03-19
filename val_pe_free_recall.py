fbad5d164b39c7e3a2f787d46c7821a3d6e25d5a
Laughing-q <1185102784@qq.com>
2025-03-19 20:34:07 +0800

    clean up

--- val_pe_free_recall.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLOE
-from ultralytics.models.yolo.detect import DetectionValidator
-
-model = YOLOE("pretrain/yolov8l-vl-seg-pf.pt")
-
-filename = "ultralytics/cfg/datasets/lvis.yaml"
-
-model.val(data=filename, batch=1, split="minival", rect=False, validator=DetectionValidator, single_cls=True)

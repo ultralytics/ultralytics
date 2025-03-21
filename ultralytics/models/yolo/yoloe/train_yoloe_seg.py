be871c731f393732896404ab1f21dfc7985d67af
Laughing-q <1185102784@qq.com>
2025-03-21 14:38:58 +0800

    Update train_yoloe_seg.py

--- ultralytics/models/yolo/yoloe/train_yoloe_seg.py
+++ ultralytics/models/yolo/yoloe/train_yoloe_seg.py
@@ -1,8 +1,11 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
 from ultralytics.models.yolo.yoloe.train_seg import YOLOESegTrainer
-from ultralytics.models.yolo.yoloe.train_yoloe import YOLOETrainerFromScratch
+from ultralytics.models.yolo.yoloe.train_yoloe import YOLOETrainerFromScratch, YOLOEVPTrainer
 
 
 class YOLOESegTrainerFromScratch(YOLOETrainerFromScratch, YOLOESegTrainer):
+    pass
+
+class YOLOESegVPTrainer(YOLOEVPTrainer, YOLOESegTrainerFromScratch):
     pass

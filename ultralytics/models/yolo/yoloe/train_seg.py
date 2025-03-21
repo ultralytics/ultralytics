8d2692779b953ac229276575966a6590c7852b16
Laughing-q <1185102784@qq.com>
2025-03-21 14:26:41 +0800

    Update train_seg.py

--- ultralytics/models/yolo/yoloe/train_seg.py
+++ ultralytics/models/yolo/yoloe/train_seg.py
@@ -1,17 +1,18 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
 
-from copy import copy
+from copy import copy, deepcopy
 
-from ultralytics.models import yolo
+from ultralytics.models.yolo.segment import SegmentationTrainer
 from ultralytics.nn.tasks import YOLOESegModel
 from ultralytics.utils import DEFAULT_CFG, RANK
 
 from .train import YOLOETrainer
 from .val import YOLOESegValidator
+import torch
 
 
-class YOLOESegTrainer(YOLOETrainer, yolo.segment.SegmentationTrainer):
+class YOLOESegTrainer(YOLOETrainer, SegmentationTrainer):
     def __init__(self, cfg=DEFAULT_CFG, overrides=None, _callbacks=None):
         if overrides is None:
             overrides = {}
@@ -38,3 +39,36 @@ class YOLOESegTrainer(YOLOETrainer, yolo.segment.SegmentationTrainer):
         return YOLOESegValidator(
             self.test_loader, save_dir=self.save_dir, args=copy(self.args), _callbacks=self.callbacks
         )
+
+
+class YOLOEPESegTrainer(SegmentationTrainer):
+    """Fine-tune YOLOESeg model in linear probing way."""
+
+    def get_model(self, cfg=None, weights=None, verbose=True):
+        """Return YOLOEModel initialized with specified config and weights."""
+        # NOTE: This `nc` here is the max number of different text samples in one image, rather than the actual `nc`.
+        # NOTE: Following the official config, nc hard-coded to 80 for now.
+        model = YOLOESegModel(
+            cfg["yaml_file"] if isinstance(cfg, dict) else cfg,
+            ch=3,
+            nc=self.data["nc"],
+            verbose=verbose and RANK == -1,
+        )
+
+        del model.model[-1].savpe
+
+        if weights:
+            model.load(weights)
+
+        model.eval()
+        # TODO: removed `train_pe_path`
+        pe_state = torch.load(self.args.train_pe_path)
+        model.set_classes(pe_state["names"], pe_state["pe"])
+        model.model[-1].fuse(model.pe)
+        model.model[-1].cv3[0][2] = deepcopy(model.model[-1].cv3[0][2]).requires_grad_(True)
+        model.model[-1].cv3[1][2] = deepcopy(model.model[-1].cv3[1][2]).requires_grad_(True)
+        model.model[-1].cv3[2][2] = deepcopy(model.model[-1].cv3[2][2]).requires_grad_(True)
+        del model.pe
+        model.train()
+
+        return model

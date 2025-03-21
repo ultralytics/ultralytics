d12398901fca18b291c5ded2d22c95ea8f4efb5a
Laughing-q <1185102784@qq.com>
2025-03-21 14:39:11 +0800

    remove train files

--- ultralytics/models/yolo/yoloe/__init__.py
+++ ultralytics/models/yolo/yoloe/__init__.py
@@ -1,7 +1,7 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-from .train import YOLOETrainer
+from .train import YOLOETrainer, YOLOEPETrainer
 from .train_seg import YOLOESegTrainer
 from .val import YOLOEDetectValidator, YOLOESegValidator
 
-__all__ = ["YOLOETrainer", "YOLOESegTrainer", "YOLOEDetectValidator", "YOLOESegValidator"]
+__all__ = ["YOLOETrainer", "YOLOEPETrainer", "YOLOESegTrainer", "YOLOEDetectValidator", "YOLOESegValidator"]

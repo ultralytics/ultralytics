dcebbd69df33efcbb9c3c7d887429c6abfb54002
Laughing-q <1185102784@qq.com>
2025-03-21 14:43:14 +0800

    Update train_yoloe_seg.py

--- ultralytics/models/yolo/yoloe/train_yoloe_seg.py
+++ ultralytics/models/yolo/yoloe/train_yoloe_seg.py
@@ -3,9 +3,10 @@
 from ultralytics.models.yolo.yoloe.train_seg import YOLOESegTrainer
 from ultralytics.models.yolo.yoloe.train_yoloe import YOLOETrainerFromScratch, YOLOEVPTrainer
 
-
 class YOLOESegTrainerFromScratch(YOLOETrainerFromScratch, YOLOESegTrainer):
+    """Trainer for YOLOE segmentation from scratch."""
     pass
 
 class YOLOESegVPTrainer(YOLOEVPTrainer, YOLOESegTrainerFromScratch):
+    """Trainer for YOLOE segmentation with VP."""
     pass

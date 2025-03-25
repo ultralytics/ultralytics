c775baf043964ab611edb6af56944c8a8bcfee12
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-03-25 11:36:10 +0100

    Create yolo11-frelu.yaml

--- ultralytics/nn/modules/activation.py
+++ ultralytics/nn/modules/activation.py
@@ -34,16 +34,16 @@ class FReLU(nn.Module):
     def __init__(self, c1, k=3):  # ch_in, kernel
         super().__init__()
         self.conv = nn.Conv2d(c1, c1, k, 1, padding=1, groups=c1, bias=False)
-        self.bn = nn.Identity()
+        self.bn = nn.BatchNorm2d(c1)
 
     def forward(self, x):
-        return torch.max(x, self.conv(x))
+        return torch.max(x, self.bn(self.conv(x)))
 
 
 # class FReLU(nn.Module):
 #     def __init__(self, c1, k=3):  # ch_in, kernel
 #         super().__init__()
-#         self.conv = nn.Conv2d(c1, c1, k, 1, 1, groups=c1, bias=False)
+#         self.conv = nn.Conv2d(c1, c1, k, 1, padding=1, groups=c1, bias=False)
 #         self.bn = nn.BatchNorm2d(c1)
 #
 #     def forward(self, x):

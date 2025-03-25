025e648c4a93ee2fee9ddb7626b83b08a7fd8081
Laughing-q <1185102784@qq.com>
2025-03-25 16:30:15 +0800

    Update torch_utils.py

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -403,15 +403,15 @@ def get_flops(model, imgsz=640):
         p = next(model.parameters())
         if not isinstance(imgsz, list):
             imgsz = [imgsz, imgsz]  # expand if int/float
-        # try:
-        #     # Use stride size for input tensor
-        #     stride = max(int(model.stride.max()), 32) if hasattr(model, "stride") else 32  # max stride
-        #     im = torch.empty((1, p.shape[1], stride, stride), device=p.device)  # input image in BCHW format
-        #     flops = thop.profile(deepcopy(model), inputs=[im], verbose=False)[0] / 1e9 * 2  # stride GFLOPs
-        #     return flops * imgsz[0] / stride * imgsz[1] / stride  # imgsz GFLOPs
-        # except Exception:
+        try:
+            # Use stride size for input tensor
+            stride = max(int(model.stride.max()), 32) if hasattr(model, "stride") else 32  # max stride
+            im = torch.empty((1, p.shape[1], stride, stride), device=p.device)  # input image in BCHW format
+            flops = thop.profile(deepcopy(model), inputs=[im], verbose=False)[0] / 1e9 * 2  # stride GFLOPs
+            return flops * imgsz[0] / stride * imgsz[1] / stride  # imgsz GFLOPs
+        except Exception:
         # Use actual image size for input tensor (i.e. required for RTDETR models)
-        im = torch.empty((1, p.shape[1], *imgsz), device=p.device)  # input image in BCHW format
+            im = torch.empty((1, p.shape[1], *imgsz), device=p.device)  # input image in BCHW format
         return thop.profile(deepcopy(model), inputs=[im], verbose=False)[0] / 1e9 * 2  # imgsz GFLOPs
     except Exception:
         return 0.0

1ad1cc89e42b267769ed995c73ac30b9ba8f2e2a
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-03-25 01:15:09 +0100

    Update

--- ultralytics/nn/modules/activation.py
+++ ultralytics/nn/modules/activation.py
@@ -33,7 +33,7 @@ class AGLU(nn.Module):
 class FReLU(nn.Module):
     def __init__(self, c1, k=1):  # ch_in, kernel
         super().__init__()
-        self.conv = nn.Conv2d(c1, c1, k, 1, 1, groups=c1, bias=False)
+        self.conv = nn.Conv2d(c1, c1, k, 1, padding=0, groups=c1, bias=False)
         self.bn = nn.BatchNorm2d(c1)
 
     def forward(self, x):

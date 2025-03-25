f583c909fac38370b840e1b73155b28ffd152820
UltralyticsAssistant <web@ultralytics.com>
2025-03-25 00:11:16 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -1222,9 +1222,7 @@ class Exporter:
             raise ValueError("IMX export is not supported for end2end models.")
         if "C2f" not in self.model.__str__():
             raise ValueError("IMX export is only supported for YOLOv8n detection models")
-        check_requirements(
-            ("model-compression-toolkit<=2.3.0", "sony-custom-layers<=0.2.0")
-        )
+        check_requirements(("model-compression-toolkit<=2.3.0", "sony-custom-layers<=0.2.0"))
         check_requirements("imx500-converter[pt]<=3.16.1")  # Separate requirements for imx500-converter
 
         import model_compression_toolkit as mct

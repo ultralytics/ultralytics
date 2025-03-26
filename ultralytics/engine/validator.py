d1d7d5447a8f551e399e11613f0427de97984da6
Laughing-q <1185102784@qq.com>
2025-03-26 10:21:28 +0800

    revert print_results()

--- ultralytics/engine/validator.py
+++ ultralytics/engine/validator.py
@@ -232,9 +232,7 @@ class BaseValidator:
         self.check_stats(stats)
         self.speed = dict(zip(self.speed.keys(), (x.t / len(self.dataloader.dataset) * 1e3 for x in dt)))
         self.finalize_metrics()
-        if not (self.args.save_json and self.jdict):
-            LOGGER.info("\n")
-            self.print_results()
+        self.print_results()
         self.run_callbacks("on_val_end")
         if self.training:
             model.float()

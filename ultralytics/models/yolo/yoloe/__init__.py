ea68183aa09027f9a80b98a66353566a03d44357
Laughing-q <1185102784@qq.com>
2025-03-26 15:12:05 +0800

    Update __init__.py

--- ultralytics/models/yolo/yoloe/__init__.py
+++ ultralytics/models/yolo/yoloe/__init__.py
@@ -1,7 +1,16 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
 from .train import YOLOEPETrainer, YOLOETrainer
-from .train_seg import YOLOESegTrainer
+from .train_seg import YOLOESegTrainer, YOLOEPESegTrainer, YOLOESegTrainerFromScratch, YOLOESegVPTrainer
 from .val import YOLOEDetectValidator, YOLOESegValidator
 
-__all__ = ["YOLOETrainer", "YOLOEPETrainer", "YOLOESegTrainer", "YOLOEDetectValidator", "YOLOESegValidator"]
+__all__ = [
+    "YOLOETrainer",
+    "YOLOEPETrainer",
+    "YOLOESegTrainer",
+    "YOLOEDetectValidator",
+    "YOLOESegValidator",
+    "YOLOEPESegTrainer",
+    "YOLOESegTrainerFromScratch",
+    "YOLOESegVPTrainer",
+]

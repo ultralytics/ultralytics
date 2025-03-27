dec8249dc0d2b9ac8aa827a609c1feb65362cfef
shr4git <123702656+shr4git@users.noreply.github.com>
2025-03-27 12:57:36 -0700

    Update conv.py
    Signed-off-by: shr4git <123702656+shr4git@users.noreply.github.com>

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -373,3 +373,31 @@ class Index(nn.Module):
         Expects a list of tensors as input.
         """
         return x[self.index]
+
+##########################################
+class BiFPN_Concat2(nn.Module):
+    def __init__(self, dimension=1):
+        super(BiFPN_Concat2, self).__init__()
+        self.d = dimension
+        self.w = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
+        self.epsilon = 0.0001
+
+    def forward(self, x):
+        w = self.w
+        weight = w / (torch.sum(w, dim=0) + self.epsilon)
+        x = [weight[0] * x[0], weight[1] *x [1]]
+        return torch.cat(x, self.d)
+
+
+class BiFPN_Concat3(nn.Module):
+    def __init__(self, dimension=1):
+        super(BiFPN_Concat3, self).__init__()
+        self.d = dimension
+        self.w = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
+        self.epsilon = 0.0001
+
+    def forward(self, x):
+        w = self.w
+        weight = w / (torch.sum(w, dim=0) + self.epsilon)
+        x = [weight[0] * x[0], weight[1] * x[1], weight[2] * x[2]]
+        return torch.cat(x, self.d)

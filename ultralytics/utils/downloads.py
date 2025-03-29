7cd9338c3df41a557522f089df98bdd2da7d9282
UltralyticsAssistant <web@ultralytics.com>
2025-03-29 13:38:10 +0000

    Auto-format by https://ultralytics.com

--- ultralytics/utils/downloads.py
+++ ultralytics/utils/downloads.py
@@ -16,23 +16,23 @@ from ultralytics.utils import LOGGER, TQDM, checks, clean_url, emojis, is_online
 # Define Ultralytics GitHub assets maintained at https://github.com/ultralytics/assets
 GITHUB_ASSETS_REPO = "ultralytics/assets"
 GITHUB_ASSETS_NAMES = frozenset(
-        [f"yolov8{k}{suffix}.pt" for k in "nsmlx" for suffix in ("", "-cls", "-seg", "-pose", "-obb", "-oiv7")]
-        + [f"yolo11{k}{suffix}.pt" for k in "nsmlx" for suffix in ("", "-cls", "-seg", "-pose", "-obb")]
-        + [f"yolo12{k}{suffix}.pt" for k in "nsmlx" for suffix in ("",)]  # detect models only currently
-        + [f"yolov5{k}{resolution}u.pt" for k in "nsmlx" for resolution in ("", "6")]
-        + [f"yolov3{k}u.pt" for k in ("", "-spp", "-tiny")]
-        + [f"yolov8{k}-world.pt" for k in "smlx"]
-        + [f"yolov8{k}-worldv2.pt" for k in "smlx"]
-        + [f"yoloe-v8{k}-seg.pt" for k in "smlx"]
-        + [f"yoloe-11{k}-seg.pt" for k in "smlx"]
-        + [f"yolov9{k}.pt" for k in "tsmce"]
-        + [f"yolov10{k}.pt" for k in "nsmblx"]
-        + [f"yolo_nas_{k}.pt" for k in "sml"]
-        + [f"sam_{k}.pt" for k in "bl"]
-        + [f"FastSAM-{k}.pt" for k in "sx"]
-        + [f"rtdetr-{k}.pt" for k in "lx"]
-        + ["mobile_sam.pt"]
-        + ["calibration_image_sample_data_20x128x128x3_float32.npy.zip"]
+    [f"yolov8{k}{suffix}.pt" for k in "nsmlx" for suffix in ("", "-cls", "-seg", "-pose", "-obb", "-oiv7")]
+    + [f"yolo11{k}{suffix}.pt" for k in "nsmlx" for suffix in ("", "-cls", "-seg", "-pose", "-obb")]
+    + [f"yolo12{k}{suffix}.pt" for k in "nsmlx" for suffix in ("",)]  # detect models only currently
+    + [f"yolov5{k}{resolution}u.pt" for k in "nsmlx" for resolution in ("", "6")]
+    + [f"yolov3{k}u.pt" for k in ("", "-spp", "-tiny")]
+    + [f"yolov8{k}-world.pt" for k in "smlx"]
+    + [f"yolov8{k}-worldv2.pt" for k in "smlx"]
+    + [f"yoloe-v8{k}-seg.pt" for k in "smlx"]
+    + [f"yoloe-11{k}-seg.pt" for k in "smlx"]
+    + [f"yolov9{k}.pt" for k in "tsmce"]
+    + [f"yolov10{k}.pt" for k in "nsmblx"]
+    + [f"yolo_nas_{k}.pt" for k in "sml"]
+    + [f"sam_{k}.pt" for k in "bl"]
+    + [f"FastSAM-{k}.pt" for k in "sx"]
+    + [f"rtdetr-{k}.pt" for k in "lx"]
+    + ["mobile_sam.pt"]
+    + ["calibration_image_sample_data_20x128x128x3_float32.npy.zip"]
 )
 GITHUB_ASSETS_STEMS = frozenset(Path(k).stem for k in GITHUB_ASSETS_NAMES)
 

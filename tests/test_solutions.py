91d5d2b13dd94979668a888674b199aaecfcdf28
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-04-01 14:24:40 +0500

    Update test_solutions.py
    Signed-off-by: Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>

--- tests/test_solutions.py
+++ tests/test_solutions.py
@@ -17,6 +17,7 @@ PARKING_VIDEO = "solution_ci_parking_demo.mp4"  # only for parking management so
 PARKING_AREAS_JSON = "solution_ci_parking_areas.json"  # only for parking management solution
 PARKING_MODEL = "solutions_ci_parking_model.pt"  # only for parking management solution
 
+@pytest.mark.slow
 @pytest.mark.skipif(sys.platform == "linux", reason="Skipping tests for Linux based OS")
 def test_major_solutions():
     """Test the object counting, heatmap, speed estimation, trackzone and queue management solution."""
@@ -95,7 +96,7 @@ def test_major_solutions():
         _ = parkingmanager(im0)
     cap.release()
 
-
+@pytest.mark.slow
 @pytest.mark.skipif(sys.platform == "linux", reason="Skipping tests for Linux based OS")
 def test_streamlit_predict():
     """Test streamlit predict live inference solution."""

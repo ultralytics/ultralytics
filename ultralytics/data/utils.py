5033012eeaa268f861acdeeccc1314b0c0b72107
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-04-02 00:53:10 +0200

    Update utils.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/data/utils.py
+++ ultralytics/data/utils.py
@@ -39,7 +39,6 @@ IMG_FORMATS = {"bmp", "dng", "jpeg", "jpg", "mpo", "png", "tif", "tiff", "webp",
 VID_FORMATS = {"asf", "avi", "gif", "m4v", "mkv", "mov", "mp4", "mpeg", "mpg", "ts", "wmv", "webm"}  # video suffixes
 PIN_MEMORY = str(os.getenv("PIN_MEMORY", True)).lower() == "true"  # global pin_memory for dataloaders
 FORMATS_HELP_MSG = f"Supported formats are:\nimages: {IMG_FORMATS}\nvideos: {VID_FORMATS}"
-DATASET_THRESHOLD = os.getenv("ULTRALYTICS_DATASET_THRESHOLD", 1000)
 
 
 def img2label_paths(img_paths):

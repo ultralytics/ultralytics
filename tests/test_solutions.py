373e40e314f610f69d67e71ee05d935c0dad67ba
UltralyticsAssistant <web@ultralytics.com>
2025-04-01 09:30:33 +0000

    Auto-update Ultralytics Docs Reference by https://ultralytics.com/actions

--- tests/test_solutions.py
+++ tests/test_solutions.py
@@ -8,9 +8,8 @@ import pytest
 
 from tests import TMP
 from ultralytics import solutions
-from ultralytics.utils import ASSETS_URL
+from ultralytics.utils import ASSETS_URL, IS_RASPBERRYPI, LINUX
 from ultralytics.utils.downloads import safe_download
-from ultralytics.utils import IS_RASPBERRYPI, LINUX
 
 DEMO_VIDEO = "solutions_ci_demo.mp4"  # for all the solutions, except workout and parking
 POSE_VIDEO = "solution_ci_pose_demo.mp4"  # only for workouts monitoring solution

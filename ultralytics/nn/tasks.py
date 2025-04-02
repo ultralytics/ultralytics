4cf4a4525f7deb8f45093d3a18dc7352e0bea07b
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-04-02 13:39:15 -0500

    Fix simplify with dynamic export

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -972,7 +972,7 @@ class YOLOEModel(DetectionModel):
                     assert not self.training
                     return vpe
                 cls_pe = self.get_cls_pe(m.get_tpe(tpe), vpe).to(device=x[0].device, dtype=x[0].dtype)
-                if len(cls_pe) != b:
+                if cls_pe.shape[0] != b or m.export:
                     cls_pe = cls_pe.repeat(b, 1, 1)
                 x = m(x, cls_pe)
             else:

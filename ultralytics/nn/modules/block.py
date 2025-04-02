e8a09fe4aa43df426a14ac276dcd5fb160dfbcba
Laughing-q <1185102784@qq.com>
2025-04-02 15:48:22 +0800

    Simplify C2fAttn && RepNCSPELAN4

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -656,8 +656,9 @@ class C2fAttn(nn.Module):
         Returns:
             (torch.Tensor): Output tensor after processing.
         """
-        y = list(self.cv1(x).chunk(2, 1))
-        y.extend(m(y[-1]) for m in self.m)
+        y = [self.cv1(x)]
+        split = y[0].chunk(2, 1)[1]
+        y.extend(m(y[-1] if i else split) for i, m in enumerate(self.m))
         y.append(self.attn(y[-1], guide))
         return self.cv2(torch.cat(y, 1))
 
@@ -672,8 +673,9 @@ class C2fAttn(nn.Module):
         Returns:
             (torch.Tensor): Output tensor after processing.
         """
-        y = list(self.cv1(x).split((self.c, self.c), 1))
-        y.extend(m(y[-1]) for m in self.m)
+        y = [self.cv1(x)]
+        split = y[0].split((self.c, self.c), 1)[1]
+        y.extend(m(y[-1] if i else split) for i, m in enumerate(self.m))
         y.append(self.attn(y[-1], guide))
         return self.cv2(torch.cat(y, 1))
 
@@ -888,14 +890,16 @@ class RepNCSPELAN4(nn.Module):
 
     def forward(self, x):
         """Forward pass through RepNCSPELAN4 layer."""
-        y = list(self.cv1(x).chunk(2, 1))
-        y.extend((m(y[-1])) for m in [self.cv2, self.cv3])
+        y = [self.cv1(x)]
+        split = y[0].chunk(2, 1)[1]
+        y.extend(m(y[-1] if i else split) for i, m in enumerate([self.cv2, self.cv3]))
         return self.cv4(torch.cat(y, 1))
 
     def forward_split(self, x):
         """Forward pass using split() instead of chunk()."""
-        y = list(self.cv1(x).split((self.c, self.c), 1))
-        y.extend(m(y[-1]) for m in [self.cv2, self.cv3])
+        y = [self.cv1(x)]
+        split = y[0].split((self.c, self.c), 1)[1]
+        y.extend(m(y[-1] if i else split) for i, m in enumerate([self.cv2, self.cv3]))
         return self.cv4(torch.cat(y, 1))
 
 

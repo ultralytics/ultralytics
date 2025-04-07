9f2458353cc6034c20e6736f01786752352a23fc
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-04-07 22:41:34 +0500

    Update test_python.py
    Signed-off-by: Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>

--- tests/test_python.py
+++ tests/test_python.py
@@ -1,14 +1,34 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
-
+import contextlib
+import csv
+import urllib
+from copy import copy
+from pathlib import Path
+import cv2
+import numpy as np
 import pytest
-
-from tests import SOURCE, TMP
+import torch
+import yaml
+from PIL import Image
+from tests import CFG, MODEL, SOURCE, SOURCES_LIST, TMP
+from ultralytics import RTDETR, YOLO
+from ultralytics.cfg import MODELS, TASK2DATA, TASKS
+from ultralytics.data.build import load_inference_source
 from ultralytics.utils import (
+    ASSETS,
+    DEFAULT_CFG,
+    DEFAULT_CFG_PATH,
+    LOGGER,
+    ONLINE,
+    ROOT,
     WEIGHTS_DIR,
+    WINDOWS,
     checks,
     is_dir_writeable,
+    is_github_action_running,
 )
+from ultralytics.utils.downloads import download
 from ultralytics.utils.torch_utils import TORCH_1_9
 
 IS_TMP_WRITEABLE = is_dir_writeable(TMP)  # WARNING: must be run once tests start as TMP does not exist on tests/init

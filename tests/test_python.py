c10ed6469b9fa05af5102842f2f52e01f338c944
Laughing-q <1185102784@qq.com>
2025-04-09 15:57:38 +0800

    update solution tests

--- tests/test_python.py
+++ tests/test_python.py
@@ -657,11 +657,11 @@ def test_yoloe():
 
     # # prompt-free
     # predict
-    # model = YOLOE(WEIGHTS_DIR / "yoloe-11s-seg-pf.pt")
-    # model.predict(SOURCE)
+    model = YOLOE(WEIGHTS_DIR / "yoloe-11s-seg-pf.pt")
+    model.predict(SOURCE)
     # # val
-    # model = YOLOE("yoloe-11s-seg.pt")  # or select yoloe-m/l-seg.pt for different sizes
-    # model.val(data="coco128-seg.yaml", imgsz=32)
+    model = YOLOE("yoloe-11s-seg.pt")  # or select yoloe-m/l-seg.pt for different sizes
+    model.val(data="coco128-seg.yaml", imgsz=32)
 
 
 def test_yolov10():

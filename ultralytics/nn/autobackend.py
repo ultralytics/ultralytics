d3c6e7ae665370bcf1ab426c5650d1f64aa802bd
UltralyticsAssistant <web@ultralytics.com>
2025-04-10 12:23:01 +0000

    Auto-update Ultralytics Docs Reference by https://ultralytics.com/actions

--- ultralytics/nn/autobackend.py
+++ ultralytics/nn/autobackend.py
@@ -14,7 +14,7 @@ import torch
 import torch.nn as nn
 from PIL import Image
 
-from ultralytics.utils import ARM64, IS_JETSON, IS_RASPBERRYPI, LINUX, MACOS, LOGGER, PYTHON_VERSION, ROOT, yaml_load
+from ultralytics.utils import ARM64, IS_JETSON, IS_RASPBERRYPI, LINUX, LOGGER, MACOS, PYTHON_VERSION, ROOT, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_version, check_yaml, is_rockchip
 from ultralytics.utils.downloads import attempt_download_asset, is_url
 

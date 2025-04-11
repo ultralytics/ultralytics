007ef48140d87191b61899bcb45e67070f2a8441
UltralyticsAssistant <web@ultralytics.com>
2025-04-11 05:06:02 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/solutions/analytics.py
+++ ultralytics/solutions/analytics.py
@@ -61,7 +61,7 @@ class Analytics(BaseSolution):
         self.title = "Ultralytics Solutions"  # window name
         self.max_points = 45  # maximum points to be drawn on window
         self.fontsize = 25  # text font size for display
-        figsize = kwargs.get("figsize", (12.8, 7.2)) # Fixed 1280x720 output; figsize arg only to speeds up CLI tests
+        figsize = kwargs.get("figsize", (12.8, 7.2))  # Fixed 1280x720 output; figsize arg only to speeds up CLI tests
         self.color_cycle = cycle(["#DD00BA", "#042AFF", "#FF4447", "#7D24FF", "#BD00FF"])
 
         self.total_counts = 0  # count variable for storing total counts i.e. for line

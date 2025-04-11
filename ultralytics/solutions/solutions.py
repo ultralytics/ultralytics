8b679ffafa41b083c5ea8f09440e665bbf5156d7
UltralyticsAssistant <web@ultralytics.com>
2025-04-11 03:07:36 +0200

    Don't redefine default model

--- ultralytics/solutions/solutions.py
+++ ultralytics/solutions/solutions.py
@@ -348,7 +348,7 @@ class SolutionAnnotator(Annotator):
             (float): The angle in degrees between the three points.
         """
         radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
-        angle = np.abs(radians * 180.0 / np.pi)
+        angle = abs(radians * 180.0 / np.pi)
         if angle > 180.0:
             angle = 360 - angle
         return angle

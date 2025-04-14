1df88e226c4dd47111d28886e5de567ab10ee6f4
UltralyticsAssistant <web@ultralytics.com>
2025-04-14 12:10:57 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -1153,9 +1153,9 @@ class Exporter:
             java_version = int(version_match.group(1)) if version_match else 0
             assert java_version >= 17, "Java version too old"
         except (FileNotFoundError, subprocess.CalledProcessError, AssertionError):
-            sudo = (["sudo"] if is_sudo_available() else [])
+            sudo = ["sudo"] if is_sudo_available() else []
             subprocess.run(sudo + ["apt", "update"], check=True)
-            cmd =  sudo + ["apt", "install", "-y", "default-jre"]
+            cmd = sudo + ["apt", "install", "-y", "default-jre"]
             subprocess.run(cmd, check=True)
 
         def representative_dataset_gen(dataloader=self.get_int8_calibration_dataloader(prefix)):

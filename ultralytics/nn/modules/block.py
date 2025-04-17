43c20efc9d155631cf4781dee5fe62c0d4e825f6
laugh12321 <laugh12321@vip.qq.com>
2025-04-17 18:48:53 +0800

    Fix shape constraint error when converting dynamic YOLO-World to TensorRT engine (#13171)
      * Modify guide view to use dynamic shape calculation instead of -1

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -603,7 +603,7 @@ class MaxSigmoidAttnBlock(nn.Module):
         bs, _, h, w = x.shape
 
         guide = self.gl(guide)
-        guide = guide.view(bs, -1, self.nh, self.hc)
+        guide = guide.view(bs, guide.shape[1], self.nh, self.hc)
         embed = self.ec(x) if self.ec is not None else x
         embed = embed.view(bs, self.nh, self.hc, h, w)
 

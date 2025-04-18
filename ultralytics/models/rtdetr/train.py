c08e057578658375bf2f8c83b52e85513fdade3b
Laughing-q <1185102784@qq.com>
2025-04-18 15:46:30 +0800

    Update train.py

--- ultralytics/models/rtdetr/train.py
+++ ultralytics/models/rtdetr/train.py
@@ -85,22 +85,3 @@ class RTDETRTrainer(DetectionTrainer):
         """Returns a DetectionValidator suitable for RT-DETR model validation."""
         self.loss_names = "giou_loss", "cls_loss", "l1_loss"
         return RTDETRValidator(self.test_loader, save_dir=self.save_dir, args=copy(self.args))
-
-    def preprocess_batch(self, batch):
-        """
-        Preprocess a batch of images by scaling and converting to float format.
-
-        Args:
-            batch (dict): Dictionary containing a batch of images, bboxes, and labels.
-
-        Returns:
-            (dict): Preprocessed batch with ground truth bounding boxes and classes separated by batch index.
-        """
-        batch = super().preprocess_batch(batch)
-        bs = len(batch["img"])
-        batch_idx = batch["batch_idx"]
-        gt_bbox, gt_class = [], []
-        for i in range(bs):
-            gt_bbox.append(batch["bboxes"][batch_idx == i].to(batch_idx.device))
-            gt_class.append(batch["cls"][batch_idx == i].to(device=batch_idx.device, dtype=torch.long))
-        return batch

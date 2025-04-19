f7b4527b5f41b7a379e5d573a17c36a88d2d6114
Laughing-q <1185102784@qq.com>
2025-04-19 17:07:04 +0800

    simplify c2f(rolled back to original version)

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -292,22 +292,21 @@ class C2f(nn.Module):
         super().__init__()
         self.c = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, 2 * self.c, 1, 1)
-        self.cv2 = Conv((1 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
         self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
 
     def forward(self, x):
         """Forward pass through C2f layer."""
-        y = list(self.cv1(x).chunk(2, 1))
-        y.extend(m(y[-1]) for m in self.m)
-        y.pop(1)
+        y = [self.cv1(x)]
+        split = y[0].chunk(2, 1)[1]
+        y.extend(m(y[-1] if i else split) for i, m in enumerate(self.m))
         return self.cv2(torch.cat(y, 1))
 
     def forward_split(self, x):
         """Forward pass using split() instead of chunk()."""
-        y = self.cv1(x).split((self.c, self.c), 1)
-        y = [y[0], y[1]]
-        y.extend(m(y[-1]) for m in self.m)
-        y.pop(1)
+        y = [self.cv1(x)]
+        split = y[0].split((self.c, self.c), 1)[1]
+        y.extend(m(y[-1] if i else split) for i, m in enumerate(self.m))
         return self.cv2(torch.cat(y, 1))
 
 
@@ -1096,7 +1095,7 @@ class C3k2(C2f):
         self.m = nn.ModuleList(
             C3k(self.c, self.c, 2, shortcut, g) if c3k else Bottleneck(self.c, self.c, shortcut, g) for _ in range(n)
         )
-        self.cv2 = Conv((1 + n) * self.c, c2, 1, act=act)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1, act=act)
 
 
 class C3k(C3):

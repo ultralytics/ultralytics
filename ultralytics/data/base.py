e61b9d6ee41d079135d36189424072543d8afa24
RizwanMunawar <chr043416@gmail.com>
2025-04-22 10:34:38 +0500

    update base.py

--- ultralytics/data/base.py
+++ ultralytics/data/base.py
@@ -208,9 +208,9 @@ class BaseDataset(Dataset):
             rect_mode (bool, optional): Whether to use rectangular resizing.
 
         Returns:
-            (np.ndarray): Loaded image.
-            (tuple): Original image dimensions (h, w).
-            (tuple): Resized image dimensions (h, w).
+            image (np.ndarray): Loaded image as a NumPy array.
+            original_im_dim (tuple): Original image dimensions in (height, width) format.
+            resized_im_dim (tuple): Resized image dimensions in (height, width) format.
 
         Raises:
             FileNotFoundError: If the image file is not found.

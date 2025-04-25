9917384945b1bb23035b2f9f090d0835a10dfefa
Laughing-q <1185102784@qq.com>
2025-04-25 16:31:04 +0800

    Update dataset.py

--- ultralytics/data/dataset.py
+++ ultralytics/data/dataset.py
@@ -13,6 +13,7 @@ from PIL import Image
 from torch.utils.data import ConcatDataset
 
 from ultralytics.utils import LOCAL_RANK, LOGGER, NUM_THREADS, TQDM, colorstr
+from ultralytics.utils.instance import Instances
 from ultralytics.utils.ops import resample_segments, segments2boxes
 from ultralytics.utils.torch_utils import TORCHVISION_0_18
 
@@ -29,7 +30,6 @@ from .base import BaseDataset
 from .converter import merge_multi_segment
 from .utils import (
     HELP_URL,
-    Instances,
     check_file_speeds,
     get_hash,
     img2label_paths,

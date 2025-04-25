3229eccd5b81133fb4aff75379a199c6fee68bd6
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-04-25 14:45:36 +0600

    Update torch_utils.py
    Signed-off-by: Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -55,7 +55,7 @@ if WINDOWS and check_version(torch.__version__, "==2.4.0"):  # reject version 2.
 def torch_distributed_zero_first(local_rank: int):
     """Ensures all processes in distributed training wait for the local master (rank 0) to complete a task first."""
     initialized = dist.is_available() and dist.is_initialized()
-    use_ids = dist.get_backend() == "nccl"
+    use_ids = initialized and dist.get_backend() == "nccl"
 
     if initialized and local_rank not in {-1, 0}:
         dist.barrier(device_ids=[local_rank]) if use_ids else dist.barrier()

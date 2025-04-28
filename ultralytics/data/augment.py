0888a542e43b5a79113462f20665885d6981f33a
Laughing-q <1185102784@qq.com>
2025-04-28 15:30:53 +0800

    Update augment.py

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -1050,16 +1050,18 @@ class CutMix(BaseMixTransform):
         x1, y1, x2, y2 = self._rand_bbox(w, h, lam)
 
         # Apply CutMix
+        # Remove boxes from original image that overlap with the cut region
         labels["img"][y1:y2, x1:x2] = img2[y1:y2, x1:x2]
         ioa1 = bbox_ioa(np.asarray([[x1, y1, x2, y2]], dtype=np.float32), labels["instances"].bboxes).squeeze(0)
-        indexes1 = np.nonzero((ioa1 < 0.30))[0]  # (N, )
+        indexes1 = np.nonzero((ioa1 < 0.30))[0]
 
+        # Add boxes from additional image that overlap with the cut region
         ioa2 = bbox_ioa(np.asarray([[x1, y1, x2, y2]], dtype=np.float32), labels2["instances"].bboxes).squeeze(0)
-        indexes2 = np.nonzero((ioa2 >= 0.30))[0]  # (N, )
+        indexes2 = np.nonzero((ioa2 >= 0.30))[0]
         instances2 = labels2["instances"][indexes2]
         instances2.convert_bbox("xyxy")
         instances2.denormalize(w, h)
-        # restrain instances2 to the random bounding border
+        # Restrain instances2 to the random bounding border
         instances2.add_padding(-x1, -y1)
         instances2.clip(x2 - x1, y2 - y1)
         instances2.add_padding(x1, y1)

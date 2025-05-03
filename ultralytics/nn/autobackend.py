2d298f1290e52a1f812ad81043b9772e051ede80
dianyo <b03902059@ntu.edu.tw>
2025-05-03 02:59:55 +0000

    fuse on cpu then move to gpu

--- ultralytics/nn/autobackend.py
+++ ultralytics/nn/autobackend.py
@@ -159,9 +159,9 @@ class AutoBackend(nn.Module):
 
         # In-memory PyTorch model
         if nn_module:
-            model = weights.to(device)
             if fuse:
-                model = model.fuse(verbose=verbose)
+                model = weights.fuse(verbose=verbose)
+            model = model.to(device)
             if hasattr(model, "kpt_shape"):
                 kpt_shape = model.kpt_shape  # pose-only
             stride = max(int(model.stride.max()), 32)  # model stride

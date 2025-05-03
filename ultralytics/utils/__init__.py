89f2a8d88aa5947ebc45c6166ed8e974a2cd1dc6
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-05-02 19:39:13 -0500

    Fix

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -541,7 +541,8 @@ def yaml_print(yaml_file: Union[str, Path, dict]) -> None:
         (None)
     """
     yaml_dict = yaml_load(yaml_file) if isinstance(yaml_file, (str, Path)) else yaml_file
-    dump = yaml.dump(yaml_dict, sort_keys=False, allow_unicode=True, width=float("inf"))
+    # dump = yaml.dump(yaml_dict, sort_keys=False, allow_unicode=True, width=float("inf"))
+    dump = yaml.dumps(yaml_dict).replace("~", "None")
     LOGGER.info(f"Printing '{colorstr('bold', 'black', yaml_file)}'\n\n{dump}")
 
 

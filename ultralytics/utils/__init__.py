8277df0737ca623093d4bda18662cd83f499731b
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-05-03 14:21:41 +0200

    Update __init__.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -538,7 +538,7 @@ def yaml_print(yaml_file: Union[str, Path, dict]) -> None:
         (None)
     """
     yaml_dict = yaml_load(yaml_file) if isinstance(yaml_file, (str, Path)) else yaml_file
-    dump = yaml.dump(yaml_dict, sort_keys=False, allow_unicode=True, width=float("inf"), Dumper=yaml.CDumper)
+    dump = yaml.dump(yaml_dict, sort_keys=False, allow_unicode=True, width=float("inf"), Dumper=yaml.CSafeDumper)
     LOGGER.info(f"Printing '{colorstr('bold', 'black', yaml_file)}'\n\n{dump}")
 
 

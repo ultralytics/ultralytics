491565ae6c5429540a7676fc205ff45fddb79676
RizwanMunawar <chr043416@gmail.com>
2025-05-03 15:52:29 +0500

    update

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -521,7 +521,7 @@ def yaml_load(file="data.yaml", append_filename=False):
             s = re.sub(r"[^\x09\x0A\x0D\x20-\x7E\x85\xA0-\uD7FF\uE000-\uFFFD\U00010000-\U0010ffff]+", "", s)
 
         # Add YAML filename to dict and return
-        data = yaml.safe_load(s) or {}  # always return a dict (yaml.safe_load() may return None for empty files)
+        data = yaml.load(s, loader=yaml.CSafeLoader) or {}  # always return a dict (yaml.safe_load() may return None for empty files)
         if append_filename:
             data["yaml_file"] = str(file)
         return data

facb5357de3a14496228a828d2c497ee2de2843d
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-05-06 12:53:06 -0500

    Updated args to selected GPUs

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -105,7 +105,7 @@ class BaseTrainer:
         self.args = get_cfg(cfg, overrides)
         self.check_resume(overrides)
         self.device = select_device(self.args.device, self.args.batch)
-        self.args.device = str(self.device)  # update "-1" devices so post-training val does not repeat search
+        self.args.device = os.getenv("CUDA_VISIBLE_DEVICES")  # update "-1" devices so post-training val does not repeat search
         self.validator = None
         self.metrics = None
         self.plots = {}

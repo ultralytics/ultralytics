069037b0702e4db1750491c0c3a3d9d07c51843f
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-05-08 10:12:59 +0200

    Update train.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/models/yolo/pose/train.py
+++ ultralytics/models/yolo/pose/train.py
@@ -80,10 +80,8 @@ class PoseTrainer(yolo.detect.DetectionTrainer):
         Returns:
             (PoseModel): Initialized pose estimation model.
         """
-        assert self.data.get("kpt_shape"), (
-            f"No `kpt_shape` found in the {self.args.data} file.\n"
-            f"Correct example: https://github.com/ultralytics/ultralytics/blob/main/ultralytics/cfg/datasets/coco8-pose.yaml"
-        )
+        if "kpt_shape" not in self.data:
+            raise KeyError(f"No `kpt_shape` in the {self.args.data}. See https://docs.ultralytics.com/datasets/pose/")
         model = PoseModel(
             cfg, nc=self.data["nc"], ch=self.data["channels"], data_kpt_shape=self.data["kpt_shape"], verbose=verbose
         )

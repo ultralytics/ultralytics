c44cdfc6d790c9dcdf1662e2f8173775c3a1acb8
UltralyticsAssistant <web@ultralytics.com>
2025-05-11 12:35:18 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -622,8 +622,17 @@ def save_one_box(xyxy, im, file=Path("im.jpg"), gain=1.02, pad=10, square=False,
         o = np.stack([pts[s.argmin()], pts[d.argmin()], pts[s.argmax()], pts[d.argmax()]])  # ordering
         w = int(np.hypot(*(o[0] - o[1]) if np.sum((o[0] - o[1]) ** 2) > np.sum((o[2] - o[3]) ** 2) else (o[2] - o[3])))
         h = int(np.hypot(*(o[0] - o[3]) if np.sum((o[0] - o[3]) ** 2) > np.sum((o[1] - o[2]) ** 2) else (o[1] - o[2])))
-        crop = cv2.warpPerspective(im, cv2.getPerspectiveTransform(o, np.array(  # Compute perspective matrix and warp
-            [[0, 0], [w - 1, 0], [w - 1, h - 1], [0, h - 1]], np.float32)), (w, h), flags=cv2.INTER_LINEAR)
+        crop = cv2.warpPerspective(
+            im,
+            cv2.getPerspectiveTransform(
+                o,
+                np.array(  # Compute perspective matrix and warp
+                    [[0, 0], [w - 1, 0], [w - 1, h - 1], [0, h - 1]], np.float32
+                ),
+            ),
+            (w, h),
+            flags=cv2.INTER_LINEAR,
+        )
     else:
         if not isinstance(xyxy, torch.Tensor):  # may be list
             xyxy = torch.stack(xyxy)

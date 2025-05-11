6bd782cc81fc6c91d754b39220a0db8249728c85
RizwanMunawar <chr043416@gmail.com>
2025-05-11 16:44:44 +0500

    update

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -588,7 +588,7 @@ def plot_labels(boxes, cls, names=(), save_dir=Path(""), on_plot=None):
         on_plot(fname)
 
 
-def save_one_box(xyxy, im, file=Path("im.jpg"), gain=1.02, pad=10, square=False, BGR=False, save=True):
+def save_one_box(xyxy, im, file=Path("im.jpg"), gain=1.02, pad=10, square=False, BGR=False, save=True, is_obb=False):
     """
     Save image crop as {file} with crop size multiple {gain} and {pad} pixels. Save and/or return crop.
 
@@ -617,13 +617,30 @@ def save_one_box(xyxy, im, file=Path("im.jpg"), gain=1.02, pad=10, square=False,
     """
     if not isinstance(xyxy, torch.Tensor):  # may be list
         xyxy = torch.stack(xyxy)
-    b = ops.xyxy2xywh(xyxy.view(-1, 4))  # boxes
-    if square:
-        b[:, 2:] = b[:, 2:].max(1)[0].unsqueeze(1)  # attempt rectangle to square
-    b[:, 2:] = b[:, 2:] * gain + pad  # box wh * gain + pad
-    xyxy = ops.xywh2xyxy(b).long()
-    xyxy = ops.clip_boxes(xyxy, im.shape)
-    crop = im[int(xyxy[0, 1]) : int(xyxy[0, 3]), int(xyxy[0, 0]) : int(xyxy[0, 2]), :: (1 if BGR else -1)]
+    if is_obb:
+        polygon = xyxy.view(-1, 4, 2).cpu().numpy()[0].astype(np.float32)
+
+        # Perspective transform
+        (tl, tr, br, bl) = polygon
+        widthA = np.linalg.norm(br - bl)
+        widthB = np.linalg.norm(tr - tl)
+        heightA = np.linalg.norm(tr - br)
+        heightB = np.linalg.norm(tl - bl)
+
+        width = int(max(widthA, widthB))
+        height = int(max(heightA, heightB))
+
+        dst = np.array([[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]], dtype=np.float32)
+        M = cv2.getPerspectiveTransform(polygon, dst)
+        crop = cv2.warpPerspective(im, M, (width, height))
+    else:
+        b = ops.xyxy2xywh(xyxy.view(-1, 4))  # boxes
+        if square:
+            b[:, 2:] = b[:, 2:].max(1)[0].unsqueeze(1)  # attempt rectangle to square
+        b[:, 2:] = b[:, 2:] * gain + pad  # box wh * gain + pad
+        xyxy = ops.xywh2xyxy(b).long()
+        xyxy = ops.clip_boxes(xyxy, im.shape)
+        crop = im[int(xyxy[0, 1]) : int(xyxy[0, 3]), int(xyxy[0, 0]) : int(xyxy[0, 2]), :: (1 if BGR else -1)]
     if save:
         file.parent.mkdir(parents=True, exist_ok=True)  # make directory
         f = str(increment_path(file).with_suffix(".jpg"))

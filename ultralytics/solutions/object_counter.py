e2b4aba8b53ae575b417685fcf06388e3b8f662b
RizwanMunawar <chr043416@gmail.com>
2025-05-11 15:58:08 +0500

    remove extra imports

--- ultralytics/solutions/object_counter.py
+++ ultralytics/solutions/object_counter.py
@@ -1,6 +1,4 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
-import cv2
-import numpy as np
 
 from ultralytics.solutions.solutions import BaseSolution, SolutionAnnotator, SolutionResults
 from ultralytics.utils.plotting import colors

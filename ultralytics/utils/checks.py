c603c8e6ab3eef63fbcae490ea86794cf2ddccd6
Lakshantha Dissanayake <lakshantha@ultralytics.com>
2025-05-11 03:41:19 -0700

    Fix typo

--- ultralytics/utils/checks.py
+++ ultralytics/utils/checks.py
@@ -822,7 +822,7 @@ def cuda_device_count() -> int:
         (int): The number of NVIDIA GPUs available.
     """
     if IS_JETSON:
-        # NVIDIA JETSON does not fully support nvidia-smi; use PyTorch instead
+        # NVIDIA Jetson does not fully support nvidia-smi and therefore use PyTorch instead
         return torch.cuda.device_count()
     else:
         try:

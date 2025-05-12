477b5f8f1f7ca9aaab27884105f1d811efbfd295
Radiuson <runqianzhang@imzrq.cn>
2025-05-12 10:58:35 +0900

    update

--- ultralytics/models/yolo/__init__.py
+++ ultralytics/models/yolo/__init__.py
@@ -1,15 +1,7 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-<<<<<<< Updated upstream
-from ultralytics.models.yolo import classify, detect, obb, pose, segment, world
-=======
-from ultralytics.models.yolo import classify, detect, obb_depth, obb, pose, segment, world, yoloe
->>>>>>> Stashed changes
+from ultralytics.models.yolo import classify, detect, obb_depth, obb, pose, segment, world
 
 from .model import YOLO, YOLOWorld
 
-<<<<<<< Updated upstream
-__all__ = "classify", "segment", "detect", "pose", "obb", "world", "YOLO", "YOLOWorld"
-=======
-__all__ = "classify", "segment", "detect", "pose", "obb_depth", "obb", "world", "yoloe", "YOLO", "YOLOWorld", "YOLOE"
->>>>>>> Stashed changes
+__all__ = "classify", "segment", "detect", "pose", "obb_depth", "obb", "world", "YOLO", "YOLOWorld"

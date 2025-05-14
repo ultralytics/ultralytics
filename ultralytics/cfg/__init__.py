bb5fef184f5b7ca8f4484588d4e7b452d3a94ad3
RizwanMunawar <chr043416@gmail.com>
2025-05-14 17:20:28 +0500

    update __init__.py

--- ultralytics/cfg/__init__.py
+++ ultralytics/cfg/__init__.py
@@ -723,6 +723,7 @@ def handle_yolo_solutions(args: List[str]) -> None:
             save_dir.mkdir(parents=True)  # create the output directory i.e. runs/solutions/exp
             vw = cv2.VideoWriter(str(save_dir / f"{solution_name}.avi"), cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
 
+        show = solution.CFG["show"]
         try:  # Process video frames
             f_n = 0  # frame number, required for analytical graphs
             while cap.isOpened():
@@ -732,7 +733,7 @@ def handle_yolo_solutions(args: List[str]) -> None:
                 results = solution(frame, f_n := f_n + 1) if solution_name == "analytics" else solution(frame)
                 if solution_name != "crop":
                     vw.write(results.plot_im)
-                if solution.CFG["show"]:
+                if show:
                     if cv2.waitKey(1) & 0xFF == ord("q"):
                         break
         finally:

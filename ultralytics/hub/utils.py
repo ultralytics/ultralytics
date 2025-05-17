38acbb96ce57fd1970fe1b128c6413ba0d951a14
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-05-17 12:43:32 +0200

    Update utils.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/hub/utils.py
+++ ultralytics/hub/utils.py
@@ -208,13 +208,13 @@ class Events:
             and (IS_PIP_PACKAGE or get_git_origin_url() == "https://github.com/ultralytics/ultralytics.git")
         )
 
-    def __call__(self, cfg, device):
+    def __call__(self, cfg, device=None):
         """
         Attempt to add a new event to the events list and send events if the rate limit is reached.
 
         Args:
             cfg (IterableSimpleNamespace): The configuration object containing mode and task information.
-            device (torch.device or str): The device type (e.g., 'cpu', 'cuda').
+            device (torch.device | str): The device type (e.g., 'cpu', 'cuda').
         """
         if not self.enabled:
             # Events disabled, do nothing

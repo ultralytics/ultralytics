3090610b4e6c7e70e5a98e1982a06c31aefe6432
dwil618 <danielgwilson618@gmail.com>
2025-05-20 00:48:25 +0100

    fix: bytetracker strack removal logic
    Signed-off-by: dwil618 <danielgwilson618@gmail.com>

--- ultralytics/trackers/byte_tracker.py
+++ ultralytics/trackers/byte_tracker.py
@@ -400,7 +400,7 @@ class BYTETracker:
         self.tracked_stracks = self.joint_stracks(self.tracked_stracks, refind_stracks)
         self.lost_stracks = self.sub_stracks(self.lost_stracks, self.tracked_stracks)
         self.lost_stracks.extend(lost_stracks)
-        self.lost_stracks = self.sub_stracks(self.lost_stracks, self.removed_stracks)
+        self.lost_stracks = self.sub_stracks(self.lost_stracks, removed_stracks)
         self.tracked_stracks, self.lost_stracks = self.remove_duplicate_stracks(self.tracked_stracks, self.lost_stracks)
         self.removed_stracks.extend(removed_stracks)
         if len(self.removed_stracks) > 1000:

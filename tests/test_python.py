eedd682bb4080e5a61dbc091643d8e38f4427c89
RizwanMunawar <chr043416@gmail.com>
2025-05-19 15:40:38 +0500

    update test_python.py

--- tests/test_python.py
+++ tests/test_python.py
@@ -270,9 +270,8 @@ def test_predict_callback_and_setup():
 @pytest.mark.parametrize("model", MODELS)
 def test_results(model):
     """Test YOLO model results processing and output in various formats."""
-    if model == "yolo11n-obb.pt":
-        SOURCE = "https://ultralytics.com/images/boats.jpg"
-    results = YOLO(WEIGHTS_DIR / model)([SOURCE, SOURCE], imgsz=160)
+    temp_s = "https://ultralytics.com/images/boats.jpg" if model == "yolo11n-obb.pt" else SOURCE
+    results = YOLO(WEIGHTS_DIR / model)([temp_s, temp_s], imgsz=160)
     for r in results:
         r = r.cpu().numpy()
         print(r, len(r), r.path)  # print numpy attributes

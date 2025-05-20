7708b12133abb607e37c169a8d9070303b19de17
Laughing-q <1185102784@qq.com>
2025-05-20 20:41:09 +0800

    Update tal.py

--- ultralytics/utils/tal.py
+++ ultralytics/utils/tal.py
@@ -191,7 +191,7 @@ class TaskAlignedAssigner(nn.Module):
         """
         return bbox_iou(gt_bboxes, pd_bboxes, xywh=False, CIoU=True).squeeze(-1).clamp_(0)
 
-    def select_topk_candidates(self, metrics, largest=True, topk_mask=None):
+    def select_topk_candidates(self, metrics, topk_mask=None):
         """
         Select the top-k candidates based on the given metrics.
 
@@ -199,7 +199,6 @@ class TaskAlignedAssigner(nn.Module):
             metrics (torch.Tensor): A tensor of shape (b, max_num_obj, h*w), where b is the batch size,
                               max_num_obj is the maximum number of objects, and h*w represents the
                               total number of anchor points.
-            largest (bool): If True, select the largest values; otherwise, select the smallest values.
             topk_mask (torch.Tensor): An optional boolean tensor of shape (b, max_num_obj, topk), where
                                 topk is the number of top candidates to consider. If not provided,
                                 the top-k values are automatically computed based on the given metrics.
@@ -208,7 +207,7 @@ class TaskAlignedAssigner(nn.Module):
             (torch.Tensor): A tensor of shape (b, max_num_obj, h*w) containing the selected top-k candidates.
         """
         # (b, max_num_obj, topk)
-        topk_metrics, topk_idxs = torch.topk(metrics, self.topk, dim=-1, largest=largest)
+        topk_metrics, topk_idxs = torch.topk(metrics, self.topk, dim=-1, largest=True)
         if topk_mask is None:
             topk_mask = (topk_metrics.max(-1, keepdim=True)[0] > self.eps).expand_as(topk_idxs)
         # (b, max_num_obj, topk)

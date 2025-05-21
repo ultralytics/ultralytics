9e8b427336faff462b7d3adafd9d658575b1c095
RizwanMunawar <chr043416@gmail.com>
2025-05-21 11:16:21 +0500

    update

--- ultralytics/solutions/solutions.py
+++ ultralytics/solutions/solutions.py
@@ -222,11 +222,12 @@ class BaseSolution:
 
     def __call__(self, *args, **kwargs):
         """Allow instances to be called like a function with flexible arguments."""
-        result = self.process(*args, **kwargs)  # Call the subclass-specific process method
+        with self.profilers[1]:
+            result = self.process(*args, **kwargs)  # Call the subclass-specific process method
         if self.CFG["verbose"]:  # extract verbose value to display the output logs if True
             LOGGER.info(
                 f"{self.profilers[0].dt * 1e3:.1f}ms track, "
-                f"{self.profilers[1].dt * 1e3:.1f}ms solution, "
+                f"{((self.profilers[1].dt -self.profilers[0].dt) - self.profilers[2].dt) * 1e3:.1f}ms solution, "
                 f"{self.profilers[2].dt * 1e3:.1f}ms inference processing per image at shape "
                 f"(1, {getattr(self.model, 'ch', 3)}, {self.h}, {self.w})",
             )

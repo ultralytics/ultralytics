b7b6802751fffc52308c7b30c233dd8b431eb586
Lakshantha Dissanayake <lakshantha@ultralytics.com>
2025-05-24 16:16:09 -0700

    Add torch version check for RKNN export

--- ultralytics/utils/benchmarks.py
+++ ultralytics/utils/benchmarks.py
@@ -42,11 +42,11 @@ import torch.cuda
 from ultralytics import YOLO, YOLOWorld
 from ultralytics.cfg import TASK2DATA, TASK2METRIC
 from ultralytics.engine.exporter import export_formats
-from ultralytics.utils import ARM64, ASSETS, IS_JETSON, LINUX, LOGGER, MACOS, TQDM, WEIGHTS_DIR, YAML
-from ultralytics.utils.checks import IS_PYTHON_3_13, check_imgsz, check_requirements, check_yolo, is_rockchip
+from ultralytics.utils import ARM64, ASSETS, IS_JETSON, LINUX, LOGGER, MACOS, TQDM, WEIGHTS_DIR, YAML, TORCH_VERSION
+from ultralytics.utils.checks import IS_PYTHON_3_13, check_imgsz, check_requirements, check_yolo, is_rockchip, check_version
 from ultralytics.utils.downloads import safe_download
 from ultralytics.utils.files import file_size
-from ultralytics.utils.torch_utils import TORCH_2_4, get_cpu_info, select_device
+from ultralytics.utils.torch_utils import get_cpu_info, select_device
 
 
 def benchmark(
@@ -142,7 +142,7 @@ def benchmark(
                 assert not isinstance(model, YOLOWorld), "YOLOWorldv2 RKNN exports not supported yet"
                 assert not is_end2end, "End-to-end models not supported by RKNN yet"
                 assert LINUX and not ARM64, "RKNN export only supported on non-aarch64 Linux"
-                assert TORCH_2_4, "RKNN export requires torch<=2.4"
+                assert check_version(TORCH_VERSION, "<=2.4.0"), "RKNN export requires torch<=2.4.0"
             if "cpu" in device.type:
                 assert cpu, "inference not supported on CPU"
             if "cuda" in device.type:

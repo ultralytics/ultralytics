ae3c3268b292a409bb6172534e607955c436f8a8
UltralyticsAssistant <web@ultralytics.com>
2025-05-26 06:06:18 +0000

    Auto-format by https://ultralytics.com/actions

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -29,6 +29,7 @@ def test_special_modes() -> None:
     run("yolo cfg")
     run("yolo benchmark model=yolo11n.pt data=coco8.yaml format=onnx")  # For functioonality check only benchmark onnx.
 
+
 @pytest.mark.parametrize("task,model,data", TASK_MODEL_DATA)
 def test_train(task: str, model: str, data: str) -> None:
     """Test YOLO training for different tasks, models, and datasets."""

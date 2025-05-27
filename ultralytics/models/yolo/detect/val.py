db9441415fa6692020b3dd65a4208293bbd69bd0
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-05-27 19:54:58 +0500

    Update val.py
    Signed-off-by: Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -102,7 +102,7 @@ class DetectionValidator(BaseValidator):
         self.end2end = getattr(model, "end2end", False)
         self.metrics.names = self.names
         self.metrics.plot = self.args.plots
-        self.confusion_matrix = ConfusionMatrix(nc=self.nc, conf=self.args.conf)
+        self.confusion_matrix = ConfusionMatrix(nc=self.nc, conf=self.args.conf, names=self.names)
         self.seen = 0
         self.jdict = []
         self.stats = dict(tp=[], conf=[], pred_cls=[], target_cls=[], target_img=[])

97a27773c37ec91d54943e538b509400bc2d51af
Laughing-q <1185102784@qq.com>
2025-05-27 19:43:40 +0800

    Update plotting.py

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -207,7 +207,7 @@ class Annotator:
             elif im.shape[2] > 3:  # multispectral
                 im = np.ascontiguousarray(im[..., :3])
         if self.pil:  # use PIL
-            self.im = im if input_is_pil else Image.fromarray(im)  # eliminate
+            self.im = im if input_is_pil else Image.fromarray(im)
             if self.im.mode not in {"RGB", "RGBA"}:  # multispectral
                 self.im = self.im.convert("RGB")
             self.draw = ImageDraw.Draw(self.im, "RGBA")

1a29631f3c9e6bf480787f77f07ac95bdcc55bc1
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-05-27 19:53:13 +0500

    Update val.py
    Signed-off-by: Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>

--- ultralytics/models/yolo/classify/val.py
+++ ultralytics/models/yolo/classify/val.py
@@ -79,7 +79,7 @@ class ClassificationValidator(BaseValidator):
         """Initialize confusion matrix, class names, and tracking containers for predictions and targets."""
         self.names = model.names
         self.nc = len(model.names)
-        self.confusion_matrix = ConfusionMatrix(nc=self.nc, conf=self.args.conf, task="classify")
+        self.confusion_matrix = ConfusionMatrix(nc=self.nc, conf=self.args.conf, names=self.names, task="classify")
         self.pred = []
         self.targets = []
 

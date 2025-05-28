586284f708ca3c5e573cb45e22436ce6f0f85433
RizwanMunawar <chr043416@gmail.com>
2025-05-28 15:36:23 +0500

    update

--- tests/test_python.py
+++ tests/test_python.py
@@ -199,6 +199,9 @@ def test_track_stream(model):
         YAML.save(custom_yaml, {**default_args, "gmc_method": gmc, "with_reid": True, "model": reidm})
         model.track(video_url, imgsz=160, tracker=custom_yaml)
 
+def test_count_stream():
+    _ = YOLO("yolo11n.pt").count()
+
 
 @pytest.mark.parametrize("task,model,data", TASK_MODEL_DATA)
 def test_val(task: str, model: str, data: str) -> None:

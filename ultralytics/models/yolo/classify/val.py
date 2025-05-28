86d711ffdfe26daec5ba6bd038d070c55e144884
UltralyticsAssistant <web@ultralytics.com>
2025-05-28 04:34:41 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/models/yolo/classify/val.py
+++ ultralytics/models/yolo/classify/val.py
@@ -126,9 +126,7 @@ class ClassificationValidator(BaseValidator):
         self.confusion_matrix.process_cls_preds(self.pred, self.targets)
         if self.args.plots:
             for normalize in True, False:
-                self.confusion_matrix.plot(
-                    save_dir=self.save_dir, normalize=normalize, on_plot=self.on_plot
-                )
+                self.confusion_matrix.plot(save_dir=self.save_dir, normalize=normalize, on_plot=self.on_plot)
         self.metrics.speed = self.speed
         self.metrics.confusion_matrix = self.confusion_matrix
         self.metrics.save_dir = self.save_dir

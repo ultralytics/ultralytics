a090be222c57004e2847704514286e13255e6edb
AlissaVe <alyssavelia2980@gmail.com>
2025-05-29 10:38:42 +0700

    change ema

--- ultralytics/nn/modules/__init__.py
+++ ultralytics/nn/modules/__init__.py
@@ -17,6 +17,7 @@ Examples:
     >>> os.system(f"onnxslim {f} {f} && open {f}")  # pip install onnxslim
 """
 
+from .ema import EMA
 from .bifpn import BiFPN
 from .block import (
     C1,
@@ -78,7 +79,6 @@ from .conv import (
     RepConv,
     SpatialAttention,
 )
-from .ema import EMA
 from .head import (
     OBB,
     Classify,

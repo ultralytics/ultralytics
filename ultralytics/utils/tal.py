6accf921edbd61e30c06258f0a567681ff5251dc
Laughing-q <1185102784@qq.com>
2025-06-04 20:50:05 +0800

    align metric

--- ultralytics/utils/tal.py
+++ ultralytics/utils/tal.py
@@ -387,7 +387,8 @@ class TaskAlignedAssigner(nn.Module):
         #             # exit()
         if fg_mask.max() > 1:  # one anchor is assigned to multiple gt_bboxes
             mask_multi_gts = (fg_mask.unsqueeze(1) > 1).expand(-1, n_max_boxes, -1)  # (b, n_max_boxes, h*w)
-            max_overlaps_idx = overlaps.argmax(1)  # (b, h*w)
+            # max_overlaps_idx = overlaps.argmax(1)  # (b, h*w)
+            max_overlaps_idx = align_metric.argmax(1)  # (b, h*w)
             is_max_overlaps = torch.zeros(mask_pos.shape, dtype=mask_pos.dtype, device=mask_pos.device)
             is_max_overlaps.scatter_(1, max_overlaps_idx.unsqueeze(1), 1)
             mask_pos = torch.where(mask_multi_gts, is_max_overlaps, mask_pos).float()  # (b, n_max_boxes, h*w)

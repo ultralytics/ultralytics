ac63a2dea0cf516e15c3eb272021c702446529a4
cgm <501704164@qq.com>
2025-06-05 12:03:20 +0800

    Upload test code

---
+++ tests/test_multi_gpu.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+from ultralytics.utils import LOCAL_RANK
+import os
+
+os.environ["NCCL_DEBUG"] = "INFO"
+os.environ["NCCL_SOCKET_NTHREADS"] = "2"
+os.environ["NCCL_NSOCKS_PERTHREAD"] = "4"
+os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+
+if __name__ == '__main__':
+    model = YOLO("yolo11s.pt")  # Load a pretrained model
+    results = model.train(data="skinning.yaml", epochs=100, imgsz=640, batch=24, mosaic=0,
+                          device=[LOCAL_RANK],
+                          amp=False,
+                          translate=0.12431,
+                          scale=0.07643)
+
+# for node = 0
+# torchrun --nproc_per_node=2 --nnodes=2 --node_rank=0 --master_addr=192.168.0.60 --master_port=23456 main.py
+
+# for node = 1
+# torchrun --nproc_per_node=2 --nnodes=2 --node_rank=1 --master_addr=192.168.0.60 --master_port=23456 main.py
+
+

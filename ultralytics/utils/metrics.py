9c6f9aa5f44afd445d38d36da54e054aad1e3d6d
Malte Ihlefeld <37628763+mihlefeld@users.noreply.github.com>
2025-06-07 06:25:52 +0200

    Improve text coloring across `normalize` modes for confusion matrix (≤30 classes) (#20955)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>

--- ultralytics/utils/metrics.py
+++ ultralytics/utils/metrics.py
@@ -463,6 +463,7 @@ class ConfusionMatrix(DataExportMixin):
             im = ax.imshow(array, cmap="Blues", vmin=0.0, interpolation="none")
             ax.xaxis.set_label_position("bottom")
             if nc < 30:  # Add score for each cell of confusion matrix
+                color_threshold = 0.45 * (1 if normalize else np.nanmax(array))  # text color threshold
                 for i, row in enumerate(array[:nc]):
                     for j, val in enumerate(row[:nc]):
                         val = array[i, j]
@@ -475,7 +476,7 @@ class ConfusionMatrix(DataExportMixin):
                             ha="center",
                             va="center",
                             fontsize=10,
-                            color="white" if val > (0.7 if normalize else 2) else "black",
+                            color="white" if val > color_threshold else "black",
                         )
             cbar = fig.colorbar(im, ax=ax, fraction=0.046, pad=0.05)
         title = "Confusion Matrix" + " Normalized" * normalize

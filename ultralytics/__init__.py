71e9af6465c41907ff7c67921dfb94631ed5f924
Sijie Hu <shu@deeprap>
2025-06-12 22:31:16 +0200

    update nn/tasks.py to support call DetectionModel_MultiView model

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -8,7 +8,8 @@ import os
 if not os.environ.get("OMP_NUM_THREADS"):
     os.environ["OMP_NUM_THREADS"] = "1"  # default for reduced CPU utilization during training
 
-from ultralytics.models import NAS, RTDETR, SAM, YOLO, YOLOE, FastSAM, YOLOWorld, YOLOManitou
+from ultralytics.models import NAS, RTDETR, SAM, YOLO, YOLOE, FastSAM, YOLOWorld
+from ultralytics.models import YOLOManitou, YOLOManitou_MultiCam
 from ultralytics.utils import ASSETS, SETTINGS
 from ultralytics.utils.checks import check_yolo as checks
 from ultralytics.utils.downloads import download
@@ -24,6 +25,8 @@ __all__ = (
     "SAM",
     "FastSAM",
     "RTDETR",
+    "YOLOManitou",
+    "YOLOManitou_MultiCam",
     "checks",
     "download",
     "settings",

0924ab8fccbbe6aec4836d94b65f2284f81290ba
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-06-14 15:32:59 +0500

    update

--- ultralytics/solutions/streamlit_inference.py
+++ ultralytics/solutions/streamlit_inference.py
@@ -133,10 +133,11 @@ class Inference:
     def configure(self):
         """Configure the model and load selected classes for inference."""
         # Add dropdown menu for model selection
-        MODEL_ORDER = ["yolo11n", "yolo11s", "yolo11m", "yolo11l", "yolo11x"]
+        M_ORD, T_ORD = ["yolo11n", "yolo11s", "yolo11m", "yolo11l", "yolo11x"], ["", "-seg", "-pose", "-obb", "-cls"]
         available_models = sorted(
-            [x.replace("yolo", "YOLO") for x in GITHUB_ASSETS_STEMS if any(x.startswith(b) for b in MODEL_ORDER)],
-            key=lambda x: MODEL_ORDER.index(x.lower().split("-")[0].replace("YOLO", "yolo")),
+            [x.replace("yolo", "YOLO") for x in GITHUB_ASSETS_STEMS if
+             any(x.startswith(b) for b in M_ORD) and "grayscale" not in x],
+            key=lambda x: (M_ORD.index(x[:7].lower()), T_ORD.index(x[7:].lower() or ""))
         )
         if self.model_path:  # If user provided the custom model, insert model without suffix as *.pt is added later
             available_models.insert(0, self.model_path.split(".pt", 1)[0])

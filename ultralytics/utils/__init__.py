52adde64c8146f1bce93eca01ade79bc381de18d
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-06-17 20:49:53 +0500

    Update __init__.py
    Signed-off-by: Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -180,6 +180,7 @@ class TQDM(rich.tqdm if TQDM_RICH else tqdm.tqdm):
         warnings.filterwarnings("ignore", category=tqdm.TqdmExperimentalWarning)  # suppress tqdm.rich warning
         kwargs["disable"] = not VERBOSE or kwargs.get("disable", False)
         kwargs.setdefault("bar_format", TQDM_BAR_FORMAT)  # override default value if passed
+        kwargs.setdefault("dynamic_ncols", True)  # auto-adjust progress bar width to terminal size
         super().__init__(*args, **kwargs)
 
     def __iter__(self):

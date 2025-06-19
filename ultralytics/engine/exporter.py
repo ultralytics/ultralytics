b43b9f2eeb9595e0de9463aaadc00ab546ea8c2e
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-06-19 17:27:02 +0500

    update

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -85,6 +85,7 @@ from ultralytics.utils import (
     LINUX,
     LOGGER,
     MACOS,
+    MACOS_VERSION,
     RKNN_CHIPS,
     ROOT,
     SETTINGS,
@@ -629,7 +630,8 @@ class Exporter:
     @try_export
     def export_openvino(self, prefix=colorstr("OpenVINO:")):
         """Export YOLO model to OpenVINO format."""
-        check_requirements("openvino>=2024.0.0")
+        # OpenVINO <= 2025.1.0 error on macOS 15.4+: https://github.com/openvinotoolkit/openvino/issues/30023"
+        check_requirements("openvino>=2025.2.0" if MACOS and MACOS_VERSION>="15.4" else "openvino>=2024.0.0")
         import openvino as ov
 
         LOGGER.info(f"\n{prefix} starting export with openvino {ov.__version__}...")

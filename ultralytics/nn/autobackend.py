ea2f0d71f5cedc5a32667b4e073255521366841c
Laughing-q <1185102784@qq.com>
2025-06-19 21:08:06 +0800

    revert pin

--- ultralytics/nn/autobackend.py
+++ ultralytics/nn/autobackend.py
@@ -294,7 +294,7 @@ class AutoBackend(nn.Module):
         # OpenVINO
         elif xml:
             LOGGER.info(f"Loading {w} for OpenVINO inference...")
-            check_requirements("openvino>=2025.2.0")
+            check_requirements("openvino>=2024.0.0")
             import openvino as ov
 
             core = ov.Core()

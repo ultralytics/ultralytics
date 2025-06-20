09ed5d53e86503375d305cbf4c6dfb8022c39eb0
Laughing-q <1185102784@qq.com>
2025-06-20 14:23:03 +0800

    update bias=2 as new baseline

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -155,8 +155,9 @@ class Detect(nn.Module):
         m = self  # self.model[-1]  # Detect() module
         # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1
         # ncf = math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # nominal class frequency
-        box_init = [3.5, 4.5, 5.5]
+        # box_init = [3.5, 4.0, 4.5]
         # box_init = [1.0, 1.0, 1.0]
+        box_init = [2.0, 2.0, 2.0]
         for i, (a, b, s) in enumerate(zip(m.cv2, m.cv3, m.stride)):  # from
             a[-1].bias.data[:] = box_init[i]  # box
             b[-1].bias.data[: m.nc] = math.log(5 / m.nc / (640 / s) ** 2)  # cls (.01 objects, 80 classes, 640 img)

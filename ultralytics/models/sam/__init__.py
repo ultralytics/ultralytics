f25bebb05ac312690557bf356b2ac21e5fa1df22
ShuaiLYU <771084086@qq.com>
2025-06-29 17:08:06 +0800

    add SAM2DynamicInteractivePredictor

--- ultralytics/models/sam/__init__.py
+++ ultralytics/models/sam/__init__.py
@@ -1,6 +1,6 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
 from .model import SAM
-from .predict import Predictor, SAM2Predictor, SAM2VideoPredictor
+from .predict import Predictor, SAM2Predictor, SAM2VideoPredictor,SAM2DynamicInteractivePredictor
 
-__all__ = "SAM", "Predictor", "SAM2Predictor", "SAM2VideoPredictor"  # tuple or list of exportable items
+__all__ = "SAM", "Predictor", "SAM2Predictor", "SAM2VideoPredictor","SAM2DynamicInteractivePredictor"  # tuple or list of exportable items

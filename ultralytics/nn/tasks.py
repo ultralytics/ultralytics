595ceafa5ad2ac5246aae5a19047885f509f9e4d
someuser2026 <forgithub2026@gmail.com>
2025-07-08 21:00:12 +1000

    added imports in __init__.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -68,6 +68,8 @@ from ultralytics.nn.modules import (
     YOLOEDetect,
     YOLOESegment,
     v10Detect,
+    Permute,
+    Reshape,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml

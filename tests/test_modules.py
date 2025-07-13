5124846212ff7faa40c9f061e8811d655e3843fb
someuser2026 <forgithub2026@gmail.com>
2025-07-13 23:36:08 +1000

    Removed print statements from test function

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -12,12 +12,10 @@ from ultralytics.nn.modules import Permute, Reshape
 )
 def test_permute(shape_in, shape_out, permutation):
     """Test for Permute module."""
-    # print(shape_in, shape_out)
     layer = Permute(permutation)
     ip = torch.Tensor(*shape_in)
     out = layer(ip)
     assert out.shape == shape_out, f"Test failed for Permute module. Input shape: {shape_in}, Output shape: {out.shape}, Expected shape: {shape_out}"
-    # assert sorted(out) == sorted(ip), f"Test failed for Permute module. Input values do not mach with the output values."
     torch.testing.assert_close(out.reshape(-1), ip.reshape(-1))
 
 
@@ -26,10 +24,8 @@ def test_permute(shape_in, shape_out, permutation):
 )
 def test_reshape(shape_in, shape_out):
     """Test for Reshape module."""
-    print(shape_in, shape_out)
     layer = Reshape(shape_out)
     ip = torch.Tensor(*shape_in)
     out = layer(ip)
     assert out.shape == shape_out, f"Test failed for Reshape module. Input shape: {shape_in}, Output shape: {out.shape}, Expected shape: {shape_out}"
-    # assert sorted(out) == sorted(ip), f"Test failed for Reshape module. Input values do not mach output values."
     torch.testing.assert_close(out.reshape(-1), ip.reshape(-1))

5d954a1a0ca323f558eb0901633a24ef01ef281f
Stefan Nahstoll <Stefan.Nahstoll@3shape.com>
2025-07-17 15:40:12 +0200

    Enables defining label_path in .yaml file for dataset. directory needs to be in the same folder as images though.

--- ultralytics/data/dataset.py
+++ ultralytics/data/dataset.py
@@ -164,7 +164,7 @@ class YOLODataset(BaseDataset):
         Returns:
             (List[dict]): List of label dictionaries, each containing information about an image and its annotations.
         """
-        self.label_files = img2label_paths(self.im_files)
+        self.label_files = img2label_paths(self.im_files, self.data.get("labels_path", "labels"))
         cache_path = Path(self.label_files[0]).parent.with_suffix(".cache")
         try:
             cache, exists = load_dataset_cache_file(cache_path), True  # attempt to load a *.cache file

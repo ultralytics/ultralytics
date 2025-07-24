8c998555b3e74a1e39cae4a73661f23a59526b6f
Hang <thomas_hang0000@hotmail.com>
2025-07-24 17:27:08 +0800

    Update plotting.py
    During YOLOv11 training, `runs/detect/labels.jpg` rendering is silently skipped with the following warning: 
    WARNING ⚠️ coordinate list must contain exactly 2 coordinates
    
    This is due to ImageDraw.Draw.rectangle() requiring list of integers as first argument. 
    
    Signed-off-by: Hang <thomas_hang0000@hotmail.com>

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -600,6 +600,7 @@ def plot_labels(boxes, cls, names=(), save_dir=Path(""), on_plot=None):
     boxes = np.column_stack([0.5 - boxes[:, 2:4] / 2, 0.5 + boxes[:, 2:4] / 2]) * 1000
     img = Image.fromarray(np.ones((1000, 1000, 3), dtype=np.uint8) * 255)
     for cls, box in zip(cls[:500], boxes[:500]):
+        box = [int(a) for a in box]
         ImageDraw.Draw(img).rectangle(box, width=1, outline=colors(cls))  # plot
     ax[1].imshow(img)
     ax[1].axis("off")

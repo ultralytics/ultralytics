65f69fb518cd2671adb54f2d3644f26aaf3ce2e9
Jing Qiu <61612323+Laughing-q@users.noreply.github.com>
2025-07-25 23:20:39 +0800

    Fix OpenVINO segment inference when `batch>1` (#21508)

--- ultralytics/nn/autobackend.py
+++ ultralytics/nn/autobackend.py
@@ -697,8 +697,8 @@ class AutoBackend(nn.Module):
                     # Start async inference with userdata=i to specify the position in results list
                     async_queue.start_async(inputs={self.input_name: im[i : i + 1]}, userdata=i)  # keep image as BCHW
                 async_queue.wait_all()  # wait for all inference requests to complete
-                y = np.concatenate([list(r.values())[0] for r in results])
-
+                y = [list(r.values()) for r in results]
+                y = [np.concatenate(x) for x in zip(*y)]
             else:  # inference_mode = "LATENCY", optimized for fastest first result at batch-size 1
                 y = list(self.ov_compiled_model(im).values())
 

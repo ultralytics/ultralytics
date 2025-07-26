5ddf42858831a4d7d44a43eebf8c12e9372472e0
Laughing-q <1185102784@qq.com>
2025-07-26 23:03:29 +0800

    fix

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -914,7 +914,7 @@ class BaseTrainer:
                 f"Optimizer '{name}' not found in list of available optimizers {optimizers}. "
                 "Request support for addition optimizers at https://github.com/ultralytics/ultralytics."
             )
-        optimizer_muon = Muon(g[3] * 0.1, lr=lr, weight_decay=decay, momentum=momentum)
+        optimizer_muon = Muon(g[3], lr=lr * 0.1, weight_decay=decay, momentum=momentum)
 
         optimizer.add_param_group({"params": g[0], "weight_decay": decay})  # add g0 with weight_decay
         optimizer.add_param_group({"params": g[1], "weight_decay": 0.0})  # add g1 (BatchNorm2d weights)

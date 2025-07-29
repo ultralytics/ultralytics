05be8cd046485b1bd3b259d1f0a0273da81ad4d9
Laughing-q <1185102784@qq.com>
2025-07-29 18:04:41 +0800

    tal_topk=12

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -792,7 +792,7 @@ class E2EDetectLoss:
     def __init__(self, model):
         """Initialize E2EDetectLoss with one-to-many and one-to-one detection losses using the provided model."""
         self.one2many = v8DetectionLoss(model, tal_topk=10)
-        self.one2one = v8DetectionLoss(model, tal_topk=10, tal_topk2=1)
+        self.one2one = v8DetectionLoss(model, tal_topk=12, tal_topk2=1)
 
     def __call__(self, preds, batch):
         """Calculate the sum of the loss for box, cls and dfl multiplied by batch size."""

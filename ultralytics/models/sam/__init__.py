274f6f40e2c72e2d9cfd973c0c5ff117557ed82d
ShuaiLYU <771084086@qq.com>
2025-08-05 15:36:17 +0800

    move to di_predict.py

--- ultralytics/models/sam/__init__.py
+++ ultralytics/models/sam/__init__.py
@@ -1,6 +1,7 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
 from .model import SAM
-from .predict import Predictor, SAM2Predictor, SAM2VideoPredictor,SAM2DynamicInteractivePredictor
+from .predict import Predictor, SAM2Predictor, SAM2VideoPredictor
+from .di_predict import SAM2DynamicInteractivePredictor
 
 __all__ = "SAM", "Predictor", "SAM2Predictor", "SAM2VideoPredictor","SAM2DynamicInteractivePredictor"  # tuple or list of exportable items

54ad799de632fe4879d358f49384fc6c619d8300
UltralyticsAssistant <web@ultralytics.com>
2025-08-06 15:22:23 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/models/sam/__init__.py
+++ ultralytics/models/sam/__init__.py
@@ -1,8 +1,7 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
 from .model import SAM
-from .predict import Predictor, SAM2Predictor, SAM2VideoPredictor
-from .predict import SAM2DynamicInteractivePredictor
+from .predict import Predictor, SAM2DynamicInteractivePredictor, SAM2Predictor, SAM2VideoPredictor
 
 __all__ = (
     "SAM",

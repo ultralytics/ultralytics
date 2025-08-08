2f2db82d414459edabe859e55580da358655ec9a
UltralyticsAssistant <web@ultralytics.com>
2025-08-08 05:55:45 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -587,7 +587,9 @@ def plot_labels(boxes, cls, names=(), save_dir=Path(""), on_plot=None):
     try:  # Seaborn correlogram
         import seaborn
 
-        seaborn.pairplot(x.to_pandas(), corner=True, diag_kind="auto", kind="hist", diag_kws=dict(bins=50), plot_kws=dict(pmax=0.9))
+        seaborn.pairplot(
+            x.to_pandas(), corner=True, diag_kind="auto", kind="hist", diag_kws=dict(bins=50), plot_kws=dict(pmax=0.9)
+        )
         plt.savefig(save_dir / "labels_correlogram.jpg", dpi=200)
         plt.close()
     except ImportError:

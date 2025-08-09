cf7f790c34b17c757252b326692e2d22c277f847
UltralyticsAssistant <web@ultralytics.com>
2025-08-09 22:20:30 +0000

    Auto-format by https://ultralytics.com/actions

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -149,17 +149,13 @@ def test_export_coreml_matrix(task, dynamic, int8, half, batch):
     "task, dynamic, int8, half, batch, nms",
     [  # generate all combinations except for exclusion cases
         (task, int8, half, batch, nms)
-        for task, int8, half, batch, nms in product(
-            TASKS, [True, False], [True, False], [1], [True, False]
-        )
+        for task, int8, half, batch, nms in product(TASKS, [True, False], [True, False], [1], [True, False])
         if not ((int8 and half) or (task == "classify" and nms) or (ARM64 and nms))
     ],
 )
 def test_export_tflite_matrix(task, dynamic, int8, half, batch, nms):
     """Test YOLO export to TFLite format considering various export configurations."""
-    file = YOLO(TASK2MODEL[task]).export(
-        format="tflite", imgsz=32, int8=int8, half=half, batch=batch, nms=nms
-    )
+    file = YOLO(TASK2MODEL[task]).export(format="tflite", imgsz=32, int8=int8, half=half, batch=batch, nms=nms)
     YOLO(file)([SOURCE] * batch, imgsz=32)  # exported model inference
     Path(file).unlink()  # cleanup
 

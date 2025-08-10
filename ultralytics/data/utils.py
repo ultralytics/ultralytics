fe268bb3ebce2d90198449dc5dc07f63fc40c11e
youngjaesong <thddudwo1313@gmail.com>
2025-08-10 18:21:38 +0900

    feat(utils): add 'label_folder_name' to 'img2label_paths' function

--- ultralytics/data/utils.py
+++ ultralytics/data/utils.py
@@ -41,11 +41,10 @@ PIN_MEMORY = str(os.getenv("PIN_MEMORY", not MACOS)).lower() == "true"  # global
 FORMATS_HELP_MSG = f"Supported formats are:\nimages: {IMG_FORMATS}\nvideos: {VID_FORMATS}"
 
 
-def img2label_paths(img_paths: List[str]) -> List[str]:
-    """Convert image paths to label paths by replacing 'images' with 'labels' and extension with '.txt'."""
-    sa, sb = f"{os.sep}images{os.sep}", f"{os.sep}labels{os.sep}"  # /images/, /labels/ substrings
+def img2label_paths(img_paths, label_folder_name='labels'):
+    """Define label paths as a function of image paths."""
+    sa, sb = f"{os.sep}images{os.sep}", f"{os.sep}{label_folder_name}{os.sep}"  # /images/, /labels/ substrings
     return [sb.join(x.rsplit(sa, 1)).rsplit(".", 1)[0] + ".txt" for x in img_paths]
-
 
 def check_file_speeds(
     files: List[str], threshold_ms: float = 10, threshold_mb: float = 50, max_files: int = 5, prefix: str = ""

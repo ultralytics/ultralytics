a79bff10e3b016d732a74176d8538648158642a4
ShuaiLYU <771084086@qq.com>
2025-08-11 21:47:48 +0800

    Fix the indentation issue in docstrings

--- ultralytics/models/sam/predict.py
+++ ultralytics/models/sam/predict.py
@@ -2285,7 +2285,7 @@ class SAM2DynamicInteractivePredictor(SAM2Predictor):
             obj_id (int): The ID of the object to which the prompts are associated.
             points (torch.Tensor | None): The coordinates of the points of interest.
             labels (torch.Tensor | None): The labels corresponding to the points. where 1 means
-             positive clicks, 0 means negative clicks.
+            positive clicks, 0 means negative clicks.
             mask (torch.Tensor | None): The mask input for the object.
             bbox (torch.Tensor | list | None): The bounding box coordinates for the object.
             normalize_coords (bool): Whether to normalize the coordinates of the points based on the image size.

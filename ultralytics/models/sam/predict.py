91b61eb5360d6ba266fcd8946815a2751e2ef06c
Laughing-q <1185102784@qq.com>
2025-08-15 22:47:11 +0800

    remove _forward_sam_heads

--- ultralytics/models/sam/predict.py
+++ ultralytics/models/sam/predict.py
@@ -1829,6 +1829,7 @@ class SAM2DynamicInteractivePredictor(SAM2Predictor):
                 high_res_features = None
             return high_res_features
 
+        # TODO: optimize this
         self.high_res_features = get_high_res_features(vis_feats, feat_sizes)
         self.vision_feats = vis_feats
         self.vision_pos_embeds = vis_pos_embed
@@ -2050,12 +2051,14 @@ class SAM2DynamicInteractivePredictor(SAM2Predictor):
         else:
             # fused the visual feature with previous memory features in the memory bank
             pix_feat_with_mem = self._prepare_memory_conditioned_features(obj_idx)
-            _, _, _, low_res_masks, high_res_masks, obj_ptr, object_score_logits = self._forward_sam_heads(
+            # calculate the first feature if adding obj_idx exists(means adding prompts)
+            pix_feat_with_mem = pix_feat_with_mem[0:1] if obj_idx is not None else pix_feat_with_mem
+            _, _, _, low_res_masks, high_res_masks, obj_ptr, object_score_logits = self.model._forward_sam_heads(
                 backbone_features=pix_feat_with_mem,
-                obj_idx=obj_idx,
                 point_inputs=points,
                 mask_inputs=mask,
                 multimask_output=False,
+                high_res_features=[feat[: pix_feat_with_mem.size(0)] for feat in self.high_res_features],
             )
         current_out["pred_masks"] = low_res_masks
         current_out["pred_masks_high_res"] = high_res_masks
@@ -2063,149 +2066,6 @@ class SAM2DynamicInteractivePredictor(SAM2Predictor):
         current_out["object_score_logits"] = object_score_logits
 
         return current_out
-
-    # TODO: remove this
-    def _forward_sam_heads(
-        self,
-        backbone_features: torch.Tensor,
-        obj_idx: Optional[int],
-        point_inputs: Optional[Dict[str, torch.Tensor]] = None,
-        mask_inputs: Optional[torch.Tensor] = None,
-        multimask_output: bool = False,
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        """
-        Forward SAM prompt encoders and mask heads.
-
-        Args:
-            backbone_features (torch.Tensor): Image features of [B, C, H, W] shape.
-            obj_idx (Optional[int]): The index of the object for which to predict masks.
-            point_inputs (Optional[Dict[str, torch.Tensor]]): A dictionary with "point_coords" and "point_labels", where
-                1) "point_coords" has [B, P, 2] shape and float32 dtype and contains the
-                   absolute pixel-unit coordinate in (x, y) format of the P input points
-                2) "point_labels" has shape [B, P] and int32 dtype, where 1 means
-                   positive clicks, 0 means negative clicks, and -1 means padding.
-            mask_inputs (Optional[torch.Tensor]): A mask of [B, 1, H*16, W*16] shape, float or bool, with the
-                same spatial size as the image.
-                two feature maps of [B, C, 4*H, 4*W] and [B, C, 2*H, 2*W] shapes respectively,
-                which will be used as high-resolution feature maps for SAM decoder.
-            multimask_output (bool): If True, output 3 candidate masks and their 3
-                corresponding IoU estimates. If False, output only 1 mask and its corresponding IoU estimate.
-            **kwargs (Any): Additional keyword arguments.
-
-        Returns:
-            low_res_multimasks (torch.Tensor): SAM output mask logits with shape [B, M, H*4, W*4].
-            high_res_multimasks (torch.Tensor): Upsampled mask logits with shape [B, M, H*16, W*16].
-            ious (torch.Tensor): Estimated IoU of each output mask with shape [B, M].
-            low_res_masks (torch.Tensor): Best low-resolution mask with shape [B, 1, H*4, W*4].
-            high_res_masks (torch.Tensor): Best high-resolution mask with shape [B, 1, H*16, W*16].
-            obj_ptr (torch.Tensor): Object pointer vector with shape [B, C].
-            object_score_logits (torch.Tensor): Object score logits with shape [B, 1].
-        """
-        B = backbone_features.size(0)
-        if isinstance(obj_idx, int):  # TODO
-            B = 1
-        device = backbone_features.device
-        assert backbone_features.size(1) == self.model.sam_prompt_embed_dim
-        assert backbone_features.size(2) == self.model.sam_image_embedding_size
-        assert backbone_features.size(3) == self.model.sam_image_embedding_size
-        # a) Handle point prompts
-        if point_inputs is not None:
-            sam_point_coords = point_inputs["point_coords"]
-            sam_point_labels = point_inputs["point_labels"]
-            assert sam_point_coords.size(0) == B and sam_point_labels.size(0) == B
-        else:
-            # If no points are provide, pad with an empty point (with label -1)
-            sam_point_coords = torch.zeros(B, 1, 2, device=device)
-            sam_point_labels = -torch.ones(B, 1, dtype=torch.int32, device=device)
-
-        # b) Handle mask prompts
-        if mask_inputs is not None:
-            # If mask_inputs is provided, downsize it into low-res mask input if needed
-            # and feed it as a dense mask prompt into the SAM mask encoder
-
-            assert len(mask_inputs.shape) == 4 and mask_inputs.shape[:2] == (B, 1), (
-                f"mask_inputs shape: {mask_inputs.shape}, expected (B, 1, H, W)"
-            )
-            if mask_inputs.shape[-2:] != self.model.sam_prompt_encoder.mask_input_size:
-                sam_mask_prompt = F.interpolate(
-                    mask_inputs.float(),
-                    size=self.model.sam_prompt_encoder.mask_input_size,
-                    align_corners=False,
-                    mode="bilinear",
-                    antialias=True,  # use antialias for downsampling
-                )
-            else:
-                sam_mask_prompt = mask_inputs
-
-        else:
-            # Otherwise, simply feed None (and SAM's prompt encoder will add
-            # a learned `no_mask_embed` to indicate no mask input in this case).
-            sam_mask_prompt = None
-
-        sparse_embeddings, dense_embeddings = self.model.sam_prompt_encoder(
-            points=(sam_point_coords, sam_point_labels),
-            boxes=None,
-            masks=sam_mask_prompt,
-        )
-
-        low_res_multimasks, ious, sam_output_tokens, object_score_logits = self.model.sam_mask_decoder(
-            image_embeddings=backbone_features[:B],  # TODO: additional ":B"
-            image_pe=self.model.sam_prompt_encoder.get_dense_pe(),
-            sparse_prompt_embeddings=sparse_embeddings,
-            dense_prompt_embeddings=dense_embeddings,
-            multimask_output=multimask_output,
-            repeat_image=False,  # the image is already batched
-            high_res_features=[feat[:B] for feat in self.high_res_features],  # TODO
-        )
-
-        if self.model.pred_obj_scores:
-            is_obj_appearing = object_score_logits > 0
-
-            # Mask used for spatial memories is always a *hard* choice between obj and no obj,
-            # consistent with the actual mask prediction
-            low_res_multimasks = torch.where(is_obj_appearing[:, None, None], low_res_multimasks, self.no_obj_score)
-
-        # convert masks from possibly bfloat16 (or float16) to float32
-        # (older PyTorch versions before 2.1 don't support `interpolate` on bf16)
-        low_res_multimasks = low_res_multimasks.float()
-        high_res_multimasks = F.interpolate(low_res_multimasks, size=self.imgsz, mode="bilinear", align_corners=False)
-
-        sam_output_token = sam_output_tokens[:, 0]
-        if multimask_output:
-            # take the best mask prediction (with the highest IoU estimation)
-            best_iou_inds = torch.argmax(ious, dim=-1)
-            batch_inds = torch.arange(B, device=device)
-            low_res_masks = low_res_multimasks[batch_inds, best_iou_inds].unsqueeze(1)
-            high_res_masks = high_res_multimasks[batch_inds, best_iou_inds].unsqueeze(1)
-            if sam_output_tokens.size(1) > 1:
-                sam_output_token = sam_output_tokens[batch_inds, best_iou_inds]
-        else:
-            low_res_masks, high_res_masks = low_res_multimasks, high_res_multimasks
-
-        # Extract object pointer from the SAM output token (with occlusion handling)
-        obj_ptr = self.model.obj_ptr_proj(sam_output_token)
-        if self.model.pred_obj_scores:
-            # Allow *soft* no obj ptr, unlike for masks
-            if self.model.soft_no_obj_ptr:
-                # Only hard possible with gt
-                assert not self.model.teacher_force_obj_scores_for_mem
-                lambda_is_obj_appearing = object_score_logits.sigmoid()
-            else:
-                lambda_is_obj_appearing = is_obj_appearing.float()
-
-            if self.model.fixed_no_obj_ptr:
-                obj_ptr = lambda_is_obj_appearing * obj_ptr
-            obj_ptr = obj_ptr + (1 - lambda_is_obj_appearing) * self.model.no_obj_ptr
-
-        return (
-            low_res_multimasks,
-            high_res_multimasks,
-            ious,
-            low_res_masks,
-            high_res_masks,
-            obj_ptr,
-            object_score_logits,
-        )
 
     def _get_orig_image_res_output(self, any_res_masks: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """

f2672d8216ff2a379fc6362f0d9021268e1962b2
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-08-18 11:01:03 +0500

    Fix safe device handling for YOLOE predictor

--- ultralytics/models/yolo/model.py
+++ ultralytics/models/yolo/model.py
@@ -414,7 +414,7 @@ class YOLOE(Model):
                         "save": False,
                         "verbose": refer_image is None,
                         "batch": 1,
-                        "device": kwargs["device"],
+                        "device": kwargs.get("device", None),
                     },
                     _callbacks=self.callbacks,
                 )

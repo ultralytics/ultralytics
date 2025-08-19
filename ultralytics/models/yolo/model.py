9ced76dcb19c3ff71b5eb087c86ee9ff80f4e0a7
UltralyticsAssistant <web@ultralytics.com>
2025-08-19 00:34:42 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/models/yolo/model.py
+++ ultralytics/models/yolo/model.py
@@ -406,7 +406,9 @@ class YOLOE(Model):
                 f"Expected equal number of bounding boxes and classes, but got {len(visual_prompts['bboxes'])} and "
                 f"{len(visual_prompts['cls'])} respectively"
             )
-            if type(self.predictor) is not (predictor or type(self.predictor) if self.predictor else yolo.yoloe.YOLOEVPDetectPredictor):
+            if type(self.predictor) is not (
+                predictor or type(self.predictor) if self.predictor else yolo.yoloe.YOLOEVPDetectPredictor
+            ):
                 self.predictor = (predictor or yolo.yoloe.YOLOEVPDetectPredictor)(
                     overrides={
                         "task": self.model.task,

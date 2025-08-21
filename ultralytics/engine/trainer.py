92773471fc14ddddcdbe50ca9cd7778bf42628bf
Laughing-q <1185102784@qq.com>
2025-08-21 11:41:00 +0800

    update to use clip_grad_value_

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -701,8 +701,9 @@ class BaseTrainer:
 
     def optimizer_step(self):
         """Perform a single step of the training optimizer with gradient clipping and EMA update."""
-        # self.scaler.unscale_(self.optimizer)  # unscale gradients
-        # self.scaler.unscale_(self.optimizer2)  # unscale gradients
+        self.scaler.unscale_(self.optimizer)  # unscale gradients
+        self.scaler.unscale_(self.optimizer2)  # unscale gradients
+        torch.nn.utils.clip_grad_value_(self.model.parameters(), clip_value=0.5)  # clip gradients
         # torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=10.0)  # clip gradients
         self.scaler.step(self.optimizer)
         self.scaler.step(self.optimizer2)

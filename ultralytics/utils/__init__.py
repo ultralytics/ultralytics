38ef192d5e985898fcecf31d3f16b05aa01cd685
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-08-24 20:04:43 +0200

    Custom progress bar tests

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -184,7 +184,7 @@ class TQDM:
     """
 
     def __init__(self, iterable=None, desc=None, total=None, leave=True, file=None,
-                 ncols=None, mininterval=0.1, maxinterval=10.0, miniters=None,
+                 ncols=None, mininterval=1.0, maxinterval=10.0, miniters=None,
                  ascii=None, disable=None, unit='it', unit_scale=False,
                  dynamic_ncols=False, smoothing=0.3, bar_format=None, initial=0,
                  position=None, postfix=None, unit_divisor=1000, write_bytes=False,

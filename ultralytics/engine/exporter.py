d5b4caca84357e032afd05c1f9cf385309e8ff04
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-08-28 14:37:48 +0800

    Fix TensorFlow export with `onnx>=1.18.0` (#21758)
    Signed-off-by: Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
    Co-authored-by: UltralyticsAssistant <web@ultralytics.com>
    Co-authored-by: Jing Qiu <61612323+Laughing-q@users.noreply.github.com>
    Co-authored-by: Laughing-q <1185102784@qq.com>

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -582,9 +582,9 @@ class Exporter:
     @try_export
     def export_onnx(self, prefix=colorstr("ONNX:")):
         """Export YOLO model to ONNX format."""
-        requirements = ["onnx>=1.12.0,<1.18.0"]
+        requirements = ["onnx>=1.12.0"]
         if self.args.simplify:
-            requirements += ["onnxslim>=0.1.59", "onnxruntime" + ("-gpu" if torch.cuda.is_available() else "")]
+            requirements += ["onnxslim>=0.1.65", "onnxruntime" + ("-gpu" if torch.cuda.is_available() else "")]
         check_requirements(requirements)
         import onnx  # noqa
 
@@ -960,9 +960,9 @@ class Exporter:
                 "sng4onnx>=1.0.1",  # required by 'onnx2tf' package
                 "onnx_graphsurgeon>=0.3.26",  # required by 'onnx2tf' package
                 "ai-edge-litert>=1.2.0,<1.4.0",  # required by 'onnx2tf' package
-                "onnx>=1.12.0,<1.18.0",
+                "onnx>=1.12.0",
                 "onnx2tf>=1.26.3",
-                "onnxslim>=0.1.59",
+                "onnxslim>=0.1.65",
                 "onnxruntime-gpu" if cuda else "onnxruntime",
                 "protobuf>=5",
             ),

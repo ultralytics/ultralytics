628cea7ffa0da29da5d0c4ff4fd31009236d3585
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-08-30 17:20:58 +0200

    Fix clip_boxes()

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -408,7 +408,7 @@ class Pose(Detect):
         else:
             y = kpts.clone()
             if ndim == 3:
-                y[:, 2::ndim] = y[:, 2::ndim].sigmoid()  # sigmoid (WARNING: inplace .sigmoid_() Apple MPS bug)
+                y[:, 2::ndim].sigmoid_()
             y[:, 0::ndim] = (y[:, 0::ndim] * 2.0 + (self.anchors[0] - 0.5)) * self.strides
             y[:, 1::ndim] = (y[:, 1::ndim] * 2.0 + (self.anchors[1] - 0.5)) * self.strides
             return y

cfc7cdd02f686d973ea1ff11fb1a0b915975473d
UltralyticsAssistant <web@ultralytics.com>
2025-08-30 12:07:37 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/models/yolo/detect/predict.py
+++ ultralytics/models/yolo/detect/predict.py
@@ -1,4 +1,5 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
+
 from ultralytics.engine.predictor import BasePredictor
 from ultralytics.engine.results import Results
 from ultralytics.utils import nms, ops

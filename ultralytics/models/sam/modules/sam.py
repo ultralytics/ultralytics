332dfa1b3df2cbb2806c753492f971e93897bb0b
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-09-01 11:31:42 +0200

    Update sam.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/models/sam/modules/sam.py
+++ ultralytics/models/sam/modules/sam.py
@@ -3,8 +3,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
 
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
 from __future__ import annotations
 
 import torch

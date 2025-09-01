141372bc1e327135b8cfaa6c974b827f15f54d1e
Laughing-q <1185102784@qq.com>
2025-09-01 16:11:23 +0800

    topk from 12 to 10

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -791,8 +791,8 @@ class E2EDetectLoss:
 
     def __init__(self, model):
         """Initialize E2EDetectLoss with one-to-many and one-to-one detection losses using the provided model."""
-        self.one2many = v8DetectionLoss(model, tal_topk=12)
-        self.one2one = v8DetectionLoss(model, tal_topk=12, tal_topk2=1)
+        self.one2many = v8DetectionLoss(model, tal_topk=10)
+        self.one2one = v8DetectionLoss(model, tal_topk=10, tal_topk2=1)
         self.updates = 0
         self.o2o = 0
         self.o2m = 1.0

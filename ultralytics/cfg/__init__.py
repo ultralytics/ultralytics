7b15ae97a63e5918d1107cfa8e5b72f0052919df
Onuralp SEZER <thunderbirdtr@gmail.com>
2025-09-01 12:27:38 +0300

    chore: 🧹  clean up white-space across multiple files (#21893)
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>
    Co-authored-by: UltralyticsAssistant <web@ultralytics.com>

--- ultralytics/cfg/__init__.py
+++ ultralytics/cfg/__init__.py
@@ -78,9 +78,9 @@ SOLUTIONS_HELP_MSG = f"""
         yolo solutions SOLUTION ARGS
 
         Where SOLUTION (optional) is one of {list(SOLUTION_MAP.keys())[:-1]}
-              ARGS (optional) are any number of custom 'arg=value' pairs like 'show_in=True' that override defaults 
+              ARGS (optional) are any number of custom 'arg=value' pairs like 'show_in=True' that override defaults
                   at https://docs.ultralytics.com/usage/cfg
-                
+
     1. Call object counting solution
         yolo solutions count source="path/to/video.mp4" region="[(20, 400), (1080, 400), (1080, 360), (20, 360)]"
 
@@ -95,10 +95,10 @@ SOLUTIONS_HELP_MSG = f"""
 
     5. Generate analytical graphs
         yolo solutions analytics analytics_type="pie"
-    
+
     6. Track objects within specific zones
         yolo solutions trackzone source="path/to/video.mp4" region="[(150, 150), (1130, 150), (1130, 570), (150, 570)]"
-        
+
     7. Streamlit real-time webcam inference GUI
         yolo streamlit-predict
     """

d32d110752449086a2aab1e96ef10a7da28a039a
Laughing-q <1185102784@qq.com>
2025-09-02 18:09:26 +0800

    simplify SPPF

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -213,7 +213,7 @@ class SPPF(nn.Module):
             This module is equivalent to SPP(k=(5, 9, 13)).
         """
         super().__init__()
-        c_ = c1 // 2  # hidden channels
+        c_ = c1 // (n + 1)  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c_ * (n + 1), c2, 1, 1, act=act)
         self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)

3bbadf4237af5f66b5c0cabfeff739d16ec187ef
Laughing-q <1185102784@qq.com>
2025-09-03 13:33:25 +0800

    Update optim/muon.py

--- ultralytics/optim/muon.py
+++ ultralytics/optim/muon.py
@@ -99,8 +99,8 @@ class MuonWithSGD(optim.Optimizer):
                         # continue
                         p.grad = torch.zeros_like(p)  # Force synchronization
                     grad = p.grad
-                    if group["weight_decay"] != 0:
-                        grad = grad.add(p, alpha=group["weight_decay"])
+                    # if group["weight_decay"] != 0:
+                    #     grad = grad.add(p, alpha=group["weight_decay"])
                     state = self.state[p]
                     if len(state) == 0:
                         state["momentum_buffer"] = torch.zeros_like(p)
@@ -116,7 +116,7 @@ class MuonWithSGD(optim.Optimizer):
                         if group["nesterov"]
                         else state["momentum_buffer"]
                     )
-                    sgd_update = update.clone()
+                    # sgd_update = update.clone()
                     if update.ndim == 4:  # for the case of conv filters
                         update = update.view(len(update), -1)
                     update = zeropower_via_newtonschulz5(update)
@@ -124,10 +124,11 @@ class MuonWithSGD(optim.Optimizer):
                     # update = muon_update(
                     #     grad, state["momentum_buffer"], beta=group["momentum"], nesterov=group["nesterov"]
                     # )
-                    lr = self.adjust_lr(group["lr"], p.shape)
-                    # p.mul_(1 - group["lr"] * group["weight_decay"])
-                    p.add_(update.reshape(p.shape), alpha=-lr)
-                    p.add_(sgd_update, alpha=-group["lr"])
+                    # lr = self.adjust_lr(group["lr"], p.shape)
+                    # lr = group["lr"] / 10
+                    p.mul_(1 - group["lr"] * group["weight_decay"])
+                    p.add_(update.reshape(p.shape), alpha=-group["lr"])
+                    # p.add_(sgd_update, alpha=-group["lr"])
             else:  # SGD
                 for p in group["params"]:
                     if p.grad is None:

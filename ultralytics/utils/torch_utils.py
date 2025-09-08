e06b7baae1995a2cb82f5d249decae768f58a2b3
Lakshantha Dissanayake <lakshantha@ultralytics.com>
2025-09-08 12:34:08 -0700

    Disable torch.compile on JetPack 5

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -1064,6 +1064,8 @@ def attempt_compile(
         >>> # Try to compile and warm up a model with a 640x640 input
         >>> model = attempt_compile(model, device=device, imgsz=640, use_autocast=True, warmup=True)
     """
+    if IS_JETSON and is_jetson(jetpack=5):
+        return model
     if not hasattr(torch, "compile"):
         return model
 

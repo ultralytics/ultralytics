b1e8d2059cc16a4474c67114cd2ecedfcc088be1
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-09-08 15:56:36 +0200

    Update nms.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/utils/nms.py
+++ ultralytics/utils/nms.py
@@ -192,7 +192,7 @@ class TorchNMS:
         iou_threshold: float,
         use_triu: bool = True,
         iou_func=box_iou,
-        exit_early=False,
+        exit_early=True,
     ) -> torch.Tensor:
         """
         Fast-NMS implementation from https://arxiv.org/pdf/1904.02689 using upper triangular matrix operations.

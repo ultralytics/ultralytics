64a6fc085a3f2be886a14afb23ece139e6a036ce
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2025-09-09 02:01:41 +0800

    Update hub.py

--- ultralytics/utils/callbacks/hub.py
+++ ultralytics/utils/callbacks/hub.py
@@ -11,13 +11,7 @@ from ultralytics.utils.events import events
 
 def on_pretrain_routine_start(trainer):
     """Create a remote Ultralytics HUB session to log local model training."""
-    if (
-        RANK in {-1, 0}
-        and SETTINGS["hub"] is True
-        and SETTINGS["api_key"]
-        and trainer.hub_session is None
-        and not Model.is_hub_model(trainer.model)
-    ):
+    if RANK in {-1, 0} and SETTINGS["hub"] is True and SETTINGS["api_key"] and trainer.hub_session is None:
         trainer.hub_session = HUBTrainingSession.create_session(trainer.args.model, trainer.args)
 
 

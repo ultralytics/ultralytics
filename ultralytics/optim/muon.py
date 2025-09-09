55e9c282fc9a8b984b9cb8652a7d1bfdb1ec4fbe
Laughing-q <1185102784@qq.com>
2025-09-09 20:29:24 +0800

    no decay

--- ultralytics/optim/muon.py
+++ ultralytics/optim/muon.py
@@ -128,7 +128,7 @@ class MuonWithSGD(optim.Optimizer):
                     # TODO
                     lr = group["lr"] / 10
                     # lr = self.adjust_lr(lr, p.shape)
-                    p.mul_(1 - lr * group["weight_decay"])
+                    # p.mul_(1 - lr * group["weight_decay"])
                     p.add_(update.reshape(p.shape), alpha=-lr)
 
                     # SGD update

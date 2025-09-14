810f0b3572e72c450274c6be074ff7568565742e
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-09-14 23:19:44 +0100

    Update test_cuda.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- tests/test_cuda.py
+++ tests/test_cuda.py
@@ -68,7 +68,7 @@ def test_export_onnx_matrix(task, dynamic, int8, half, batch, simplify, nms):
         half=half,
         batch=batch,
         simplify=simplify,
-        nms=nms and task != "obb",  # disable NMS for OBB task for now on T4 instance
+        nms=nms and task != "obb",  # WARNING: Failing NMS with OBB
         device=DEVICES[0],
     )
     YOLO(file)([SOURCE] * batch, imgsz=64 if dynamic else 32, device=DEVICES[0])  # exported model inference
@@ -76,7 +76,7 @@ def test_export_onnx_matrix(task, dynamic, int8, half, batch, simplify, nms):
 
 
 @pytest.mark.slow
-@pytest.mark.skipif(True, reason="CUDA export tests disabled pending additional Ultralytics GPU server availability")
+@pytest.mark.skipif(True, reason="WARNING: Failing TensorRT export tests")
 @pytest.mark.skipif(not DEVICES, reason="No CUDA devices available")
 @pytest.mark.parametrize(
     "task, dynamic, int8, half, batch",

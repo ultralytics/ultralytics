37047266e54280e8611666ff837416b7fdcb21fe
UltralyticsAssistant <web@ultralytics.com>
2025-09-15 13:57:01 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/utils/export.py
+++ ultralytics/utils/export.py
@@ -47,7 +47,8 @@ def export_onnx(
         input_names=input_names,
         output_names=output_names,
         dynamic_axes=dynamic or None,
-        dynamo=dynamo and (False if (dynamic or check_version(torch.__version__, "<2.7.0")) else True),  # TorchDynamo-based export
+        dynamo=dynamo
+        and (False if (dynamic or check_version(torch.__version__, "<2.7.0")) else True),  # TorchDynamo-based export
         external_data=False,  # do not create .onnx.data file
     )
 

dbc5433bc8058e28ecea422c7c2210a9acae7a32
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-09-17 00:05:53 +0100

    `ultralytics 8.3.201` Expand TensorFlow `ai-edge-litert≥1.2` support (non-macOS) (#22110)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: UltralyticsAssistant <web@ultralytics.com>

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -958,7 +958,7 @@ class Exporter:
                 "tf_keras<=2.19.0",  # required by 'onnx2tf' package
                 "sng4onnx>=1.0.1",  # required by 'onnx2tf' package
                 "onnx_graphsurgeon>=0.3.26",  # required by 'onnx2tf' package
-                "ai-edge-litert>=1.2.0,<1.4.0",  # required by 'onnx2tf' package
+                "ai-edge-litert>=1.2.0" + (",<1.4.0" if MACOS else ""),  # required by 'onnx2tf' package
                 "onnx>=1.12.0",
                 "onnx2tf>=1.26.3",
                 "onnxslim>=0.1.67",

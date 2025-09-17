ca61438fdb9018c1be969bafbb093ffb18d6b0b7
Laughing-q <1185102784@qq.com>
2025-09-17 16:13:47 +0800

    simplify output_names

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -589,7 +589,7 @@ class Exporter:
         opset_version = self.args.opset or get_latest_opset()
         LOGGER.info(f"\n{prefix} starting export with onnx {onnx.__version__} opset {opset_version}...")
         f = str(self.file.with_suffix(".onnx"))
-        output_names = ["output0", "output1"] if isinstance(self.model, SegmentationModel) else ["output0"]
+        output_names = ["output0", "output1"] if self.model.task == "segment" else ["output0"]
         dynamic = self.args.dynamic
         if dynamic:
             dynamic = {"images": {0: "batch", 2: "height", 3: "width"}}  # shape(1,3,640,640)

d56408ab6effde641a5ec09e85dae07b50521e4c
UltralyticsAssistant <web@ultralytics.com>
2025-09-19 05:07:38 +0000

    Auto-update Ultralytics Docs Reference by https://ultralytics.com/actions

--- tests/test_python.py
+++ tests/test_python.py
@@ -74,7 +74,7 @@ def test_model_profile():
     _ = model.predict(im, profile=True)
 
 def test_torchscript_inference():
-    """Test torchscript inference without torchvision import: https://github.com/ultralytics/ultralytics/pull/19747"""
+    """Test torchscript inference without torchvision import: https://github.com/ultralytics/ultralytics/pull/19747."""
     YOLO("yolo11n.pt").export(format="torchscript")
     YOLO("yolo11n.torchscript")(batch=2)
 

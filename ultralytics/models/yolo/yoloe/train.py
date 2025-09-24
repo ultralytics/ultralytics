068782a13782fc493d3a3f8dedbf47360c841fd2
Laughing-q <1185102784@qq.com>
2025-09-24 15:16:06 +0800

    revert changes

--- ultralytics/models/yolo/yoloe/train.py
+++ ultralytics/models/yolo/yoloe/train.py
@@ -195,7 +195,7 @@ class YOLOETrainerFromScratch(YOLOETrainer, WorldTrainerFromScratch):
         batch = DetectionTrainer.preprocess_batch(self, batch)
 
         texts = list(itertools.chain(*batch["texts"]))
-        txt_feats = torch.stack([self.text_embeddings[text] for text in texts]).to(self.device, non_blocking=True)
+        txt_feats = torch.stack([self.text_embeddings[text] for text in texts]).to(self.device, non_blocking=self.device.type == "cuda")
         txt_feats = txt_feats.reshape(len(batch["texts"]), -1, txt_feats.shape[-1])
         batch["txt_feats"] = txt_feats
         return batch

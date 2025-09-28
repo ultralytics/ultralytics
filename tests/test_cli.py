8ba50f01e8f8ee972f682a475cf2a38fdbc881b7
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-09-28 22:53:25 +0800

    Update test_cli.py
    Signed-off-by: Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -54,7 +54,8 @@ def test_qat():
     """Test model training and export with QAT."""
     from ultralytics.utils.checks import check_requirements
 
-    check_requirements(["nvidia-modelopt", "setuptools"])  # installation before torch import
+    check_requirements("nvidia-modelopt")  # install before torch import
+    check_requirements("setuptools", cmds="-U")
     run("yolo train data=coco128.yaml imgsz=32 epochs=1 name=qat int8 exist_ok")
     run(f"yolo export model={TMP}/runs/detect/qat/weights/best.pt format=onnx")
 

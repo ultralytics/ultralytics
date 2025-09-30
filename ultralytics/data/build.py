a9c4bed6621888eb78c1e03c02d0677d9f1099b3
kuazhangxiaoai <yanggang3510@gmail.com>
2025-09-30 23:22:11 +0800

    add semantic segment task

--- ultralytics/data/build.py
+++ ultralytics/data/build.py
@@ -331,7 +331,7 @@ def build_semantic_dataset(
         imgsz=cfg.imgsz,
         batch_size=batch,
         augment=mode == "train",  # augmentation
-        hyp=cfg,  # TODO: probably add a get_hyps_from_cfg function
+        hyp=cfg,
         rect=cfg.rect or rect,  # rectangular batches
         cache=cfg.cache or None,
         single_cls=cfg.single_cls or False,

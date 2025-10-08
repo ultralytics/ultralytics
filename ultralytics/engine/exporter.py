1eab27a04d5b6b22e5c4d7b085221d63ec682920
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-10-08 23:12:56 +0800

    Update exporter.py
    Signed-off-by: Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -328,9 +328,6 @@ class Exporter:
 
         is_tf_format = any((saved_model, pb, tflite, edgetpu, tfjs))
 
-        # Only needed for torchscript/jit and onnx, other formats handle it internally
-        convert_fp16_export = any((jit, onnx))
-
         # Device
         dla = None
         if engine and self.args.device is None:
@@ -363,7 +360,7 @@ class Exporter:
         if self.args.half and self.args.int8:
             LOGGER.warning("half=True and int8=True are mutually exclusive, setting half=False.")
             self.args.half = False
-        if self.args.half and convert_fp16_export and self.device.type == "cpu":
+        if self.args.half and (onnx or jit) and self.device.type == "cpu":
             LOGGER.warning("half=True only compatible with GPU export, i.e. use device=0, setting half=False.")
             self.args.half = False
         self.imgsz = check_imgsz(self.args.imgsz, stride=model.stride, min_dim=2)  # check image size
@@ -466,7 +463,7 @@ class Exporter:
         y = None
         for _ in range(2):  # dry runs
             y = NMSModel(model, self.args)(im) if self.args.nms and not coreml and not imx else model(im)
-        if self.args.half and convert_fp16_export and self.device.type != "cpu":
+        if self.args.half and (onnx or jit) and self.device.type != "cpu":
             im, model = im.half(), model.half()  # to FP16
 
         # Filter warnings

198dd4bc84bef5ecdf5d704360941c7e69dd33d8
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2025-10-08 16:59:19 +0000

    Fix RTDETR TFLite export

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -1025,6 +1025,8 @@ class Exporter:
             attempt_download_asset(f"{onnx2tf_file}.zip", unzip=True, delete=True)
 
         # Export to ONNX
+        if "rtdetr" in self.model.model[-1].name:
+            self.args.opset = 19  # RTDETR export doesn't work with higher opsets
         self.args.simplify = True
         f_onnx = self.export_onnx()
 

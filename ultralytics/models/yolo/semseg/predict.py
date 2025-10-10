e04ea1d83fd736068884d6db8e1310ba631651de
kuazhangxiaoai <yanggang3510@gmail.com>
2025-10-10 16:57:29 +0800

    no strict for build doc

--- ultralytics/models/yolo/semseg/predict.py
+++ ultralytics/models/yolo/semseg/predict.py
@@ -53,8 +53,6 @@ class SemSegPredictor(DetectionPredictor):
         """
         super().__init__(cfg, overrides, _callbacks)
         self.args.task = "semseg"
-        self.data = check_det_dataset(self.args.data)["yaml_file"]
-
 
     def postprocess(self, preds, img, orig_imgs):
         """

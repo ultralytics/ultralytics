aaf449f18c2e5402393a2d6bb1e20456d6f0b64f
kuazhangxiaoai <yanggang3510@gmail.com>
2025-10-10 15:56:57 +0800

    no strict for build doc

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -8,7 +8,7 @@ from ultralytics import YOLO
 from ultralytics.cfg import get_cfg
 from ultralytics.engine.exporter import Exporter
 from ultralytics.models.yolo import classify, detect, segment, semseg
-from ultralytics.utils import ASSETS, DEFAULT_CFG, SEMSEG_CFG, WEIGHTS_DIR
+from ultralytics.utils import ASSETS, DEFAULT_CFG, SEMSEG_CFG, WEIGHTS_DIR, YAML
 
 
 def test_func(*args):

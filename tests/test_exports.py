976020abf7125f054a9db0c051e854a80d03bcbc
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2025-10-11 10:11:56 +0000

    Restrict torch version in test

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -51,7 +51,7 @@ def test_export_openvino():
         for task, dynamic, int8, half, batch, nms in product(
             TASKS, [True, False], [True, False], [True, False], [1, 2], [True, False]
         )
-        if not ((int8 and half) or (task == "classify" and nms))
+        if not ((int8 and half) or (task == "classify" and nms) or (nms and not TORCH_1_13))
     ],
 )
 def test_export_openvino_matrix(task, dynamic, int8, half, batch, nms):

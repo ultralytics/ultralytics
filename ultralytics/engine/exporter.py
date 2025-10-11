3df0645b00209e6aec7f87b0d976b95fa1efecfb
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2025-10-11 11:58:09 +0000

    update

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -832,7 +832,7 @@ class Exporter:
         YAML.save(f / "metadata.yaml", self.metadata)  # add metadata.yaml
 
         # Restore filename with hyphens for guess_model_task
-        if not f.samefile(f_orig):
+        if not f.samefile(Path(f_orig)):
             shutil.rmtree(f_orig)
         shutil.move(f, f_orig)
         return f_orig

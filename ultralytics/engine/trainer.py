2a22c2ebaa434787b5a7f463ab9ca7a7efb6b502
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-10-12 00:00:09 +0200

    Update

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -761,7 +761,7 @@ class BaseTrainer:
         n = len(metrics) + 2  # number of cols
         s = "" if self.csv.exists() else (("%s," * n % tuple(["epoch", "time"] + keys)).rstrip(",") + "\n")  # header
         t = time.time() - self.train_time_start
-        if not self.csv.parent.exists():  # user may accidentally delete save_dir
+        if not self.csv.parent.is_dir():  # user may accidentally delete save_dir
             self.csv.parent.mkdir()
         with open(self.csv, "a", encoding="utf-8") as f:
             f.write(s + ("%.6g," * n % tuple([self.epoch + 1, t] + vals)).rstrip(",") + "\n")

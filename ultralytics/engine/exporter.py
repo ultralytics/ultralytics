ba4d6174e0d18abefb0ae59fcbb5856950300cb2
UltralyticsAssistant <web@ultralytics.com>
2025-10-14 15:32:31 +0000

    Auto-update Ultralytics Docs Reference by https://ultralytics.com/actions

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -77,7 +77,13 @@ from ultralytics.data.dataset import YOLODataset
 from ultralytics.data.utils import check_cls_dataset, check_det_dataset
 from ultralytics.nn.autobackend import check_class_names, default_class_names
 from ultralytics.nn.modules import C2f, Classify, Detect, RTDETRDecoder
-from ultralytics.nn.tasks import ClassificationModel, DetectionModel, RTDETRDetectionModel, SegmentationModel, WorldModel
+from ultralytics.nn.tasks import (
+    ClassificationModel,
+    DetectionModel,
+    RTDETRDetectionModel,
+    SegmentationModel,
+    WorldModel,
+)
 from ultralytics.utils import (
     ARM64,
     DEFAULT_CFG,

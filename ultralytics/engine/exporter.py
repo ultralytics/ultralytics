7d1a849d7e95449644f70e7fc6c836b09bd2b992
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-10-14 23:36:58 +0800

    Update exporter.py
    Signed-off-by: Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -80,9 +80,8 @@ from ultralytics.nn.modules import C2f, Classify, Detect, RTDETRDecoder
 from ultralytics.nn.tasks import (
     ClassificationModel,
     DetectionModel,
-    RTDETRDetectionModel,
     SegmentationModel,
-    WorldModel,
+    WorldModel
 )
 from ultralytics.utils import (
     ARM64,
@@ -391,7 +390,7 @@ class Exporter:
             assert not tflite or not ARM64 or not LINUX, "TFLite export with NMS unsupported on ARM64 Linux"
             assert not is_tf_format or TORCH_1_13, "TensorFlow exports with NMS require torch>=1.13"
             assert not onnx or TORCH_1_13, "ONNX export with NMS requires torch>=1.13"
-            if getattr(model, "end2end", False) or isinstance(model, RTDETRDetectionModel):
+            if getattr(model, "end2end", False) or isinstance(model.model[-1], RTDETRDecoder):
                 LOGGER.warning("'nms=True' is not available for end2end models. Forcing 'nms=False'.")
                 self.args.nms = False
             self.args.conf = self.args.conf or 0.25  # set conf default value for nms export

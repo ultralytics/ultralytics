4331598e6d99700065fa38240bb6b6e1bec182bd
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2025-10-17 16:15:06 +0800

    fix margin

--- ultralytics/utils/ops.py
+++ ultralytics/utils/ops.py
@@ -521,11 +521,16 @@ def crop_mask(masks, boxes, margin=0):
     n, h, w = masks.shape
     pm, nm = 1 + margin, 1 - margin
     if n < 50:  # faster for fewer masks (predict)
-        for i, (x1, y1, x2, y2) in enumerate(boxes.round().int()):
-            masks[i, : y1 * nm] = 0
-            masks[i, y2 * pm :] = 0
-            masks[i, :, : x1 * nm] = 0
-            masks[i, :, x2 * pm :] = 0
+        mboxes = boxes
+        if margin:
+            mboxes = boxes.clone()
+            mboxes[:, :2] *= nm
+            mboxes[:, 2:] *= pm
+        for i, (x1, y1, x2, y2) in enumerate(mboxes.round().int()):
+            masks[i, :y1] = 0
+            masks[i, y2:] = 0
+            masks[i, :, :x1] = 0
+            masks[i, :, x2:] = 0
         return masks
     else:  # faster for more masks (val)
         x1, y1, x2, y2 = torch.chunk(boxes[:, :, None], 4, 1)  # x1 shape(n,1,1)

3497caf9f9a18efd8b5ca446a276f1f68654eb35
UltralyticsAssistant <web@ultralytics.com>
2025-10-17 07:58:24 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/utils/ops.py
+++ ultralytics/utils/ops.py
@@ -522,16 +522,16 @@ def crop_mask(masks, boxes, margin=0):
     pm, nm = 1 + margin, 1 - margin
     if n < 50:  # faster for fewer masks (predict)
         for i, (x1, y1, x2, y2) in enumerate(boxes.round().int()):
-            masks[i, :y1*nm] = 0
-            masks[i, y2*pm:] = 0
-            masks[i, :, :x1*nm] = 0
-            masks[i, :, x2*pm:] = 0
+            masks[i, : y1 * nm] = 0
+            masks[i, y2 * pm :] = 0
+            masks[i, :, : x1 * nm] = 0
+            masks[i, :, x2 * pm :] = 0
         return masks
     else:  # faster for more masks (val)
         x1, y1, x2, y2 = torch.chunk(boxes[:, :, None], 4, 1)  # x1 shape(n,1,1)
         r = torch.arange(w, device=masks.device, dtype=x1.dtype)[None, None, :]  # rows shape(1,1,w)
         c = torch.arange(h, device=masks.device, dtype=x1.dtype)[None, :, None]  # cols shape(1,h,1)
-        return masks * ((r >= x1*nm) * (r < x2*pm) * (c >= y1*nm) * (c < y2*pm))
+        return masks * ((r >= x1 * nm) * (r < x2 * pm) * (c >= y1 * nm) * (c < y2 * pm))
 
 
 def process_mask(protos, masks_in, bboxes, shape, upsample: bool = False):

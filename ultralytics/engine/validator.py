74badabd80380f9cc7e28dffe8aed04d35177032
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2025-10-18 11:37:16 +0000

    Run `on_val_end` only on first GPU

--- ultralytics/engine/validator.py
+++ ultralytics/engine/validator.py
@@ -237,8 +237,8 @@ class BaseValidator:
             self.speed = dict(zip(self.speed.keys(), (x.t / len(self.dataloader.dataset) * 1e3 for x in dt)))
             self.finalize_metrics()
             self.print_results()
+            self.run_callbacks("on_val_end")
 
-        self.run_callbacks("on_val_end")
         if self.training:
             model.float()
             # Reduce loss across all GPUs

59736fff68219639de3304c11d3f9f14674c6557
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-10-28 17:53:07 +0800

    Enable TFLite INT8 export for end2end models
    Removed assertion for TFLite INT8 export on end2end models.
    
    Signed-off-by: Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -396,8 +396,6 @@ class Exporter:
             assert self.args.name in RKNN_CHIPS, (
                 f"Invalid processor name '{self.args.name}' for Rockchip RKNN export. Valid names are {RKNN_CHIPS}."
             )
-        if self.args.int8 and tflite:
-            assert not getattr(model, "end2end", False), "TFLite INT8 export not supported for end2end models."
         if self.args.nms:
             assert not isinstance(model, ClassificationModel), "'nms=True' is not valid for classification models."
             assert not tflite or not ARM64 or not LINUX, "TFLite export with NMS unsupported on ARM64 Linux"

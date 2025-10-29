59657bc6af03d9886eef5f173c869a1d6530b43b
UltralyticsAssistant <web@ultralytics.com>
2025-10-29 08:58:57 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/modules/scconv.py
+++ ultralytics/nn/modules/scconv.py
@@ -1,11 +1,9 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class SCConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, groups=1):
-        super(SCConv, self).__init__()
+        super().__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, groups=groups, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size, stride=1, padding=1, groups=groups, bias=False)

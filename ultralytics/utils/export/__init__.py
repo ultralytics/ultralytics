75f5e80b0b15e21feff95cd60b3516aea294d25c
UltralyticsAssistant <web@ultralytics.com>
2025-10-29 05:38:08 +0000

    Auto-update Ultralytics Docs Reference by https://ultralytics.com/actions

--- ultralytics/utils/export/__init__.py
+++ ultralytics/utils/export/__init__.py
@@ -4,4 +4,4 @@ from .engine import onnx2engine, torch2onnx
 from .imx import torch2imx
 from .tensorflow import keras2pb, onnx2saved_model, pb2tfjs, tflite2edgetpu
 
-__all__ = ["torch2imx", "torch2onnx", "onnx2engine", "onnx2saved_model", "keras2pb", "tflite2edgetpu", "pb2tfjs"]
+__all__ = ["keras2pb", "onnx2engine", "onnx2saved_model", "pb2tfjs", "tflite2edgetpu", "torch2imx", "torch2onnx"]

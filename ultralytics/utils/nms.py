905180b69aba93e552dc7247ad56108733080a4c
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2025-10-30 10:33:29 +0000

    Fix

--- ultralytics/utils/nms.py
+++ ultralytics/utils/nms.py
@@ -230,9 +230,10 @@ class TorchNMS:
             col_idx = torch.arange(n, device=boxes.device).view(1, -1).expand(n, -1)
             upper_mask = row_idx < col_idx
             ious = ious * upper_mask
-            scores.copy_(scores[sorted_idx])  # update original tensor for NMSModel
             # Zeroing these scores ensures the additional indices would not affect the final results
-            scores[~((ious >= iou_threshold).sum(0) <= 0)] = 0
+            scores_ = scores[sorted_idx]
+            scores_[~((ious >= iou_threshold).sum(0) <= 0)] = 0
+            scores[sorted_idx] = scores_  # update original tensor for NMSModel
             # NOTE: return indices with fixed length to avoid TFLite reshape error
             pick = torch.topk(scores, scores.shape[0]).indices
         return sorted_idx[pick]

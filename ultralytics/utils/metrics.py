1ed4da002a8e24cae6e20380fd9f0aa2dca54ab1
Sergey <smska87@bk.ru>
2025-10-30 09:47:25 +0300

    Update metrics.py
    Signed-off-by: Sergey <smska87@bk.ru>

--- ultralytics/utils/metrics.py
+++ ultralytics/utils/metrics.py
@@ -738,8 +738,12 @@ def compute_ap(recall: list[float], precision: list[float]) -> tuple[float, np.n
         mrec (np.ndarray): Modified recall curve with sentinel values added at the beginning and end.
     """
     # Append sentinel values to beginning and end
-    mrec = np.concatenate(([0.0], recall, [recall[-1] if len(recall) > 0 else 1.0], [1.0]))
-    mpre = np.concatenate(([1.0], precision, [0.0], [0.0]))
+    if  if len(recall) > 0:
+        mrec = np.concatenate(([0.0], recall, [recall[-1]], [1.0]))
+        mpre = np.concatenate(([1.0], precision, [0.0], [0.0]))
+    else:
+        mrec = np.concatenate(([0.0], recall, [1.0]))
+        mpre = np.concatenate(([1.0], precision, [0.0]))
 
     # Compute the precision envelope
     mpre = np.flip(np.maximum.accumulate(np.flip(mpre)))

f017b1b0838d3bbe82cc0ebcf9d73db85b551e4d
Laughing-q <1185102784@qq.com>
2025-10-30 15:47:52 +0800

    Update modules/head.py

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -945,7 +945,6 @@ class YOLOEDetect(Detect):
         scores = torch.cat(
             [contrastive_head[i](cls_head[i](x[i]), x[-1]).view(bs, self.nc, -1) for i in range(self.nl)], dim=-1
         )
-        print(boxes.shape, scores.shape)
         self.no = self.nc + self.reg_max * 4  # self.nc could be changed when inference with different texts
         return dict(boxes=boxes, scores=scores, feats=x[:3])
 

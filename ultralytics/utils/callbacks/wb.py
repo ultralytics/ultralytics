eb5c936a62fc9ff0c56c46f2e72425caf54e816b
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-11-03 00:01:42 +0800

    Manually commit WB log

--- ultralytics/utils/callbacks/wb.py
+++ ultralytics/utils/callbacks/wb.py
@@ -145,6 +145,7 @@ def on_fit_epoch_end(trainer):
     _log_plots(trainer.validator.plots, step=trainer.epoch + 1)
     if trainer.epoch == 0:
         wb.run.log(model_info_for_loggers(trainer), step=trainer.epoch + 1)
+    wb.run.log({}, commit=True)  # manual commit
 
 
 def on_train_epoch_end(trainer):

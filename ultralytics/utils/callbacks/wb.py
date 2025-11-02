1af79a66b380aa9776ad4fad8f6d6415e9e81137
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-11-03 00:22:15 +0800

    Commit Weights & Biases logs after each epoch to force dashboard update (#22563)
    Signed-off-by: Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/utils/callbacks/wb.py
+++ ultralytics/utils/callbacks/wb.py
@@ -140,11 +140,11 @@ def on_pretrain_routine_start(trainer):
 
 def on_fit_epoch_end(trainer):
     """Log training metrics and model information at the end of an epoch."""
-    wb.run.log(trainer.metrics, step=trainer.epoch + 1)
     _log_plots(trainer.plots, step=trainer.epoch + 1)
     _log_plots(trainer.validator.plots, step=trainer.epoch + 1)
     if trainer.epoch == 0:
         wb.run.log(model_info_for_loggers(trainer), step=trainer.epoch + 1)
+    wb.run.log(trainer.metrics, step=trainer.epoch + 1, commit=True)  # commit forces sync
 
 
 def on_train_epoch_end(trainer):

c3b0be6f4c999687122c27204113c5e8c0f1ae45
UltralyticsAssistant <web@ultralytics.com>
2025-11-06 01:45:55 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/hub/__init__.py
+++ ultralytics/hub/__init__.py
@@ -3,8 +3,6 @@ from __future__ import annotations
 
 from typing import Optional
 
-from __future__ import annotations
-
 from ultralytics.data.utils import HUBDatasetStats
 from ultralytics.hub.auth import Auth
 from ultralytics.hub.session import HUBTrainingSession

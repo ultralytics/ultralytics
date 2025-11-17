272fa83e66358e48e4fe3461e9c704e64afb75a4
Laughing-q <1185102784@qq.com>
2025-11-17 17:04:23 +0800

    Update modules/head.py

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -144,8 +144,8 @@ class Detect(nn.Module):
         """Concatenates and returns predicted bounding boxes and class probabilities."""
         preds = self.forward_head(x, **self.one2many)
         if self.end2end:
-            # x_detach = [xi.detach() for xi in x]
-            one2one = self.forward_head(x, **self.one2one)
+            x_detach = [xi.detach() for xi in x]
+            one2one = self.forward_head(x_detach, **self.one2one)
             preds = {"one2many": preds, "one2one": one2one}
         if self.training:
             return preds

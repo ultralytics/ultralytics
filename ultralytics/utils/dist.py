2376ce6bf6b9dc18d2eb9c250d7277dda10de4e8
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2025-11-19 09:55:50 +0000

    Add world size

--- ultralytics/utils/dist.py
+++ ultralytics/utils/dist.py
@@ -61,6 +61,7 @@ if __name__ == "__main__":
     dist.init_process_group(
         backend="nccl" if dist.is_nccl_available() else "gloo",
         timeout=timedelta(seconds=10800),  # 3 hours
+        world_size={trainer.world_size},
     )
     torch.cuda.set_device(int(os.getenv("LOCAL_RANK", dist.get_rank())))
     os.environ["TORCH_NCCL_BLOCKING_WAIT"] = "1"  # set to enforce timeout

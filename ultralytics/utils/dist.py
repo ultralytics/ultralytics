fe6036370a4b932ceed62e83744b40bea28b7c47
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2025-11-19 09:47:25 +0000

    Multi-node fix

--- ultralytics/utils/dist.py
+++ ultralytics/utils/dist.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
         backend="nccl" if dist.is_nccl_available() else "gloo",
         timeout=timedelta(seconds=10800),  # 3 hours
     )
-    torch.cuda.set_device(dist.get_rank())
+    torch.cuda.set_device(int(os.getenv("LOCAL_RANK", dist.get_rank())))
     os.environ["TORCH_NCCL_BLOCKING_WAIT"] = "1"  # set to enforce timeout
 
     from {module} import {name}

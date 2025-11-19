dd431ce3e6e95c8753a114416477b9d0e48cf4bf
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2025-11-19 10:07:06 +0000

    rearrange

--- ultralytics/utils/dist.py
+++ ultralytics/utils/dist.py
@@ -59,14 +59,14 @@ if __name__ == "__main__":
     import torch.distributed as dist
 
     RANK = int(os.getenv("LOCAL_RANK", dist.get_rank()))
+    torch.cuda.set_device(RANK)
+    os.environ["TORCH_NCCL_BLOCKING_WAIT"] = "1"  # set to enforce timeout
     dist.init_process_group(
         backend="nccl" if dist.is_nccl_available() else "gloo",
         timeout=timedelta(seconds=10800),  # 3 hours
         world_size={trainer.world_size},
         rank=RANK,
     )
-    torch.cuda.set_device(RANK)
-    os.environ["TORCH_NCCL_BLOCKING_WAIT"] = "1"  # set to enforce timeout
 
     from {module} import {name}
     from ultralytics.utils import DEFAULT_CFG_DICT

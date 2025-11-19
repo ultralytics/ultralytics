04359e498aac4173f9c19ac9768891270936dad8
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2025-11-19 17:46:11 +0800

    Update ultralytics/engine/trainer.py
    Co-authored-by: Ultralytics Assistant <135830346+UltralyticsAssistant@users.noreply.github.com>
    Signed-off-by: Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -123,7 +123,7 @@ class BaseTrainer:
         self.hub_session = overrides.pop("session", None)  # HUB
         self.args = get_cfg(cfg, overrides)
         self.check_resume(overrides)
-        self.device = select_device(self.args.device) if RANK == -1 else torch.device("cuda", RANK)
+        self.device = select_device(self.args.device) if RANK == -1 else torch.device("cuda", LOCAL_RANK)
         # Update "-1" devices so post-training val does not repeat search
         self.args.device = os.getenv("CUDA_VISIBLE_DEVICES") if "cuda" in str(self.device) else str(self.device)
         self.validator = None

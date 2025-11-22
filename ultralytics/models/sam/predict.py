f14d7569ae38a5a8cf0f92019a3a9aabb65afcaf
Laughing-q <1185102784@qq.com>
2025-11-22 15:34:33 +0800

    support multi visual boxes

--- ultralytics/models/sam/predict.py
+++ ultralytics/models/sam/predict.py
@@ -2044,8 +2044,8 @@ class SAM3Predictor(Predictor):
             assert bboxes.shape[-2] == labels.shape[-1], (
                 f"Number of points {bboxes.shape[-2]} should match number of labels {labels.shape[-1]}."
             )
-            bboxes = bboxes.view(1, -1, 4)  # (1, N, 4)
-            labels = labels.view(1, -1)  # (1, N)
+            bboxes = bboxes.view(-1, 1, 4)  # (N, 1, 4)
+            labels = labels.view(-1, 1)  # (N, 1)
         return bboxes, labels
 
     def _inference_features(self, features, bboxes=None, labels=None, text: list[str] = None):
@@ -2054,7 +2054,8 @@ class SAM3Predictor(Predictor):
         nc = 1 if bboxes is not None else len(text) if text is not None else len(self.model.names)
         geometric_prompt = self.model._get_dummy_prompt(nc)
         if bboxes is not None:
-            geometric_prompt.append_boxes(bboxes, labels)
+            for i in range(len(bboxes)):
+                geometric_prompt.append_boxes(bboxes[[i]], labels[[i]])
             if text is None:
                 text = ["visual"]  # bboxes needs this `visual` text prompt if no text passed
         if text is not None and self.model.names != text:

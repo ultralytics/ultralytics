9dbfb909b2fb8ffab08a6cdfa4b5bf459283352d
Lakshantha Dissanayake <lakshantha@ultralytics.com>
2025-11-25 12:51:50 -0800

    Switch back to Ubuntu 22.04

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -1085,7 +1085,7 @@ class Exporter:
 
         check_requirements(
             "axelera-voyager-sdk==1.5.0-rc6",
-            cmds="--extra-index-url https://media.axelera.ai/releases/v1.5.0-rc6/build-packages-ubuntu-24.04/python",
+            cmds="--extra-index-url https://media.axelera.ai/releases/v1.5.0-rc6/build-packages-ubuntu-22.04/python",
         )
 
         from axelera import compiler

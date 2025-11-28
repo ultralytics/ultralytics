19d6a5f74135cd1c23dfd013a87f377409ebef23
lmycross <mengyu@ultralytics.com>
2025-11-28 02:29:18 -0600

    Update semseg branch

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -131,7 +131,7 @@ class Proto26(Proto):
             feat = feat + up_feat
         p = super().forward(self.feat_fuse(feat))
         if self.training:
-            semseg = self.semseg(feat)
+            semseg = self.semseg(x[0])
             return (p, semseg)
         return p
 

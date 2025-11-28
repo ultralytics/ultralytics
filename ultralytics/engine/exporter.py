0c5026c8e9a492e6104bab129a23edc2d8c0f07e
Laughing-q <1185102784@qq.com>
2025-11-28 18:12:12 +0800

    use <22

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -1188,7 +1188,7 @@ class Exporter:
             raise ValueError("IMX export is not supported for end2end models.")
         check_requirements(
             (
-                "packaging==21.3" if ARM64 else "packaging",
+                "packaging<22" if ARM64 else "packaging",
                 "model-compression-toolkit>=2.4.1",
                 "edge-mdt-cl>=1.0.0",
                 "edge-mdt-tpc>=1.2.0",

a549830275d4118d73931160f7998c8bd2f42b89
Francesco Mattioli <Francesco.mttl@gmail.com>
2025-12-09 15:53:31 +0100

    updated exporter env var

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -1096,7 +1096,7 @@ class Exporter:
     @try_export
     def export_axelera(self, prefix=colorstr("Axelera:")):
         """YOLO Axelera export."""
-        # TODO: Make this a function to use also with imx
+        os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
         try:
             from axelera import compiler
         except ImportError:

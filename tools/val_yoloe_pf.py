4dc51b1e04568aed8c2d99fc4404ebaacd695081
ShuaiLYU <shuai.lyu@foxmail.com>
2025-12-11 06:48:04 -0600

    add tools/val_yoloe_pf.py

---
+++ tools/val_yoloe_pf.py
@@ -0,0 +1,80 @@
+import ultralytics,os
+workspace = os.path.dirname(os.path.dirname(os.path.abspath(ultralytics.__file__)))
+os.chdir(workspace)
+print("set workspace:", workspace)
+
+
+
+from ultralytics import YOLOE
+from ultralytics.models.yolo.yoloe.val import YOLOEDetectValidator
+
+
+from pathlib import Path
+import re,yaml
+
+def yaml_load(file="data.yaml", append_filename=False):
+    """
+    Load YAML data from a file.
+
+    Args:
+        file (str, optional): File name. Default is 'data.yaml'.
+        append_filename (bool): Add the YAML filename to the YAML dictionary. Default is False.
+
+    Returns:
+        (dict): YAML data and file name.
+    """
+    assert Path(file).suffix in {".yaml", ".yml"}, f"Attempting to load non-YAML file {file} with yaml_load()"
+    with open(file, errors="ignore", encoding="utf-8") as f:
+        s = f.read()  # string
+
+        # Remove special characters
+        if not s.isprintable():
+            s = re.sub(r"[^\x09\x0A\x0D\x20-\x7E\x85\xA0-\uD7FF\uE000-\uFFFD\U00010000-\U0010ffff]+", "", s)
+
+        # Add YAML filename to dict and return
+        data = yaml.safe_load(s) or {}  # always return a dict (yaml.safe_load() may return None for empty files)
+        if append_filename:
+            data["yaml_file"] = str(file)
+        return data
+
+
+
+def read_pf_det_from_seg_unfused(model_path,yaml_name,unfused_model_weight):
+
+    # load the most model weights
+    det_model = YOLOE(yaml_name).load(model_path)
+
+
+    # set vocab from the unfused model
+    unfused_model=YOLOE(unfused_model_weight)
+    unfused_model.eval()
+    unfused_model.cuda()
+
+    with open('../buffer/ram_tag_list.txt', 'r') as f:
+        names = [x.strip() for x in f.readlines()]
+    # categories = yaml_load("ultralytics/cfg/datasets/lvis.yaml")["names"].values()
+    # names = [c.split("/")[0] for c in categories]     
+    vocab = unfused_model.get_vocab(names)
+    
+
+    det_model.eval()
+    det_model.set_vocab(vocab, names=names)
+    det_model.model.model[-1].is_fused = True
+    det_model.model.model[-1].conf = 0.001
+    det_model.model.model[-1].max_det = 1000
+
+
+
+    return det_model
+
+
+
+version='26s'
+weight_path_tp="./runs/yoloe26s_tp_ultra6/mobileclip2:b_26s_bs128_ptwobject365v1_close2_agdata2_lrf0.5_bn_exp/weights/best.pt"
+model_weight="/home/louis/ultra_louis_work/ultralytics/runs/yoloe26s_pf_ultra6/mobileclip2:b_26s_bs128_ptwobject365v1_close2_agdata2_lrf_bn_o2m0.1_pfA/weights/best.pt"
+# model_weight="runs/yoloe26s_pf_ultra6/mobileclip2:b_26s_bs128_ptwobject365v1_close2_agdata2_lrf0.5_bn_o2m0.1_pf2/weights/best.pt"
+model=read_pf_det_from_seg_unfused(model_weight,f"yoloe-{version}.yaml",weight_path_tp)
+
+
+metrics = model.val(data="lvis.yaml",split="minival", single_cls=False ,max_det=1000) # map 0
+

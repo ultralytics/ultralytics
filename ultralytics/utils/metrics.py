fix: update covariance matrix calculations in metrics.py to include dynamic scaling factors to mitigate isotropic bounding boxes.

ultralytics/utils/metrics.py
@@ -4,10 +4,14 @@
 from __future__ import annotations
 
 import math
+import random
 import warnings
 from collections import defaultdict
 from pathlib import Path
-from typing import Any
+from typing import (
+    Any, 
+    Optional,
+)
 
 import numpy as np
 import torch
@@ -187,11 +191,15 @@ def kpt_iou(
     return ((-e).exp() * kpt_mask[:, None]).sum(-1) / (kpt_mask.sum(-1)[:, None] + eps)
 
 
-def _get_covariance_matrix(boxes: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+def _get_covariance_matrix(
+        boxes: torch.Tensor, pa: Optional[float] = None, pb: Optional[float] = None
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     """Generate covariance matrix from oriented bounding boxes.
 
     Args:
         boxes (torch.Tensor): A tensor of shape (N, 5) representing rotated bounding boxes, with xywhr format.
+        pa (float): Scaling factor for width.
+        pb (float): Scaling factor for height.
 
     Returns:
         (torch.Tensor): Covariance matrices corresponding to original rotated bounding boxes.
@@ -199,6 +207,9 @@ def _get_covariance_matrix(boxes: torch.Tensor) -> tuple[torch.Tensor, torch.Ten
     # Gaussian bounding boxes, ignore the center points (the first two columns) because they are not needed here.
     gbbs = torch.cat((boxes[:, 2:4].pow(2) / 12, boxes[:, 4:]), dim=-1)
     a, b, c = gbbs.split(1, dim=-1)
+    if pa is not None and pb is not None:
+        a = pa * a
+        b = pb * b
     cos = c.cos()
     sin = c.sin()
     cos2 = cos.pow(2)
@@ -226,8 +237,10 @@ def probiou(obb1: torch.Tensor, obb2: torch.Tensor, CIoU: bool = False, eps: flo
     """
     x1, y1 = obb1[..., :2].split(1, dim=-1)
     x2, y2 = obb2[..., :2].split(1, dim=-1)
-    a1, b1, c1 = _get_covariance_matrix(obb1)
-    a2, b2, c2 = _get_covariance_matrix(obb2)
+    pa = random.uniform(1.0, 2.0)
+    pb = 1.0 / pa
+    a1, b1, c1 = _get_covariance_matrix(obb1, pa, pb)
+    a2, b2, c2 = _get_covariance_matrix(obb2, pa, pb)
 
     t1 = (
         ((a1 + a2) * (y1 - y2).pow(2) + (b1 + b2) * (x1 - x2).pow(2)) / ((a1 + a2) * (b1 + b2) - (c1 + c2).pow(2) + eps)
@@ -268,10 +281,13 @@ def batch_probiou(obb1: torch.Tensor | np.ndarray, obb2: torch.Tensor | np.ndarr
     obb1 = torch.from_numpy(obb1) if isinstance(obb1, np.ndarray) else obb1
     obb2 = torch.from_numpy(obb2) if isinstance(obb2, np.ndarray) else obb2
 
+    pa = random.uniform(1.0, 2.0)
+    pb = 1.0 / pa
+
     x1, y1 = obb1[..., :2].split(1, dim=-1)
     x2, y2 = (x.squeeze(-1)[None] for x in obb2[..., :2].split(1, dim=-1))
-    a1, b1, c1 = _get_covariance_matrix(obb1)
-    a2, b2, c2 = (x.squeeze(-1)[None] for x in _get_covariance_matrix(obb2))
+    a1, b1, c1 = _get_covariance_matrix(obb1, pa, pb)
+    a2, b2, c2 = (x.squeeze(-1)[None] for x in _get_covariance_matrix(obb2, pa, pb))
 
     t1 = (
         ((a1 + a2) * (y1 - y2).pow(2) + (b1 + b2) * (x1 - x2).pow(2)) / ((a1 + a2) * (b1 + b2) - (c1 + c2).pow(2) + eps)
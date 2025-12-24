5d03d3663dc4208729ac3e4de0f9d58fe6ccb871
ShuaiLYU <shuai.lyu@foxmail.com>
2025-12-24 09:14:10 -0600

    train_vp.sh with muSGD

--- train_vp.sh
+++ train_vp.sh
@@ -44,6 +44,31 @@ mkdir -p $project_dir
 ###############################################default args #######################################
 
 ############################################################################################. | 
+# project_dir=runs/yoloe26s_vp_ultra6
+# weight_path="./runs/yoloe26s_tp_ultra6/mobileclip2:b_26s_bs128_ptwobject365v1_close2_agdata2_lrf0.5_bn_exp/weights/best.pt"
+# trainer="YOLOEVPTrainer"
+# model=26s
+# epo=10
+# close_mosaic=2
+# batch_size=128
+# ag=True
+# clip_weight_name="mobileclip2:b" # mobileclip2b
+# ptw="object365v1" 
+# optimizer="AdamW"
+# lr0=0.002
+# lrf=0.01
+# momentum=0.9
+# weight_decay=0.025
+# o2m=0.1
+# exp_name=${clip_weight_name}_${model}_bs${batch_size}_ptw${ptw}_close${close_mosaic}_agdata2_lrf${lrf}_bn_o2m${o2m}_newsavpe_vp
+# device=0,1
+
+# using the following command to check the log:
+# tail -f -n 50 ./runs/20251218_012053.log
+# Current screen: 359138.train2
+# exp name: mobileclip2:b_26s_bs128_ptwobject365v1_close2_agdata2_lrf0.01_bn_o2m0.1_newsavpe_vp
+
+############################################################################################. | 
 project_dir=runs/yoloe26s_vp_ultra6
 weight_path="./runs/yoloe26s_tp_ultra6/mobileclip2:b_26s_bs128_ptwobject365v1_close2_agdata2_lrf0.5_bn_exp/weights/best.pt"
 trainer="YOLOEVPTrainer"
@@ -54,19 +79,21 @@ batch_size=128
 ag=True
 clip_weight_name="mobileclip2:b" # mobileclip2b
 ptw="object365v1" 
-optimizer="AdamW"
-lr0=0.002
-lrf=0.01
+
+optimizer="MuSGD"
+lr0=0.00125
+lrf=0.5
 momentum=0.9
-weight_decay=0.025
+weight_decay=0.0007
 o2m=0.1
-exp_name=${clip_weight_name}_${model}_bs${batch_size}_ptw${ptw}_close${close_mosaic}_agdata2_lrf${lrf}_bn_o2m${o2m}_newsavpe_vp
-device=0,1
+
+exp_name=${clip_weight_name}_${model}_bs${batch_size}_ptw${ptw}_close${close_mosaic}_agdata2_optim${optimizer}_bn_o2m${o2m}_vp
+device=2,3
 
 # using the following command to check the log:
-# tail -f -n 50 ./runs/20251218_012053.log
-# Current screen: 359138.train2
-# exp name: mobileclip2:b_26s_bs128_ptwobject365v1_close2_agdata2_lrf0.01_bn_o2m0.1_newsavpe_vp
+#   tail -f -n 50 ./runs/20251224_091127.log
+#   Current screen: 326335.cuda23
+#   exp name: mobileclip2:b_26s_bs128_ptwobject365v1_close2_agdata2_optimMuSGD_bn_o2m0.1_vp
 ###############################################default args #######################################
 
 
@@ -95,10 +122,10 @@ nohup python $pyfile \
 
 ##############################################################################################
 echo "# using the following command to check the log:"
-echo "tail -f -n 50 ./runs/$timestamp.log"
+echo "#   tail -f -n 50 ./runs/$timestamp.log"
 current_screen=$(echo $STY) # get the current screen 
-echo "# Current screen: $current_screen"
-echo "# exp name: $exp_name"
+echo "#   Current screen: $current_screen"
+echo "#   exp name: $exp_name"
 
 
 

dbf0f844f945c910debc2ba6b1e72848a0fca650
lmycross <mengyu@ultralytics.com>
2025-12-24 03:56:22 -0600

    Update format and comments

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -544,61 +544,37 @@ class v8SegmentationLoss(v8DetectionLoss):
 
 
 class RLELoss(nn.Module):
-    """RLE Loss.
+    """Residual Log-Likelihood Estimation Loss.
 
-    `Human Pose Regression With Residual Log-Likelihood Estimation
-    arXiv: <https://arxiv.org/abs/2107.11291>`_.
-
-    Code is modified from `the official implementation
-    <https://github.com/Jeff-sjtu/res-loglikelihood-regression>`_.
+    References:
+        https://arxiv.org/abs/2107.11291
 
     Args:
         use_target_weight (bool): Option to use weighted loss.
-            Different joint types may have different target weights.
         size_average (bool): Option to average the loss by the batch_size.
-        residual (bool): Option to add L1 loss and let the flow
-            learn the residual error distribution.
-        q_dis (string): Option for the identity Q(error) distribution,
-            Options: "laplace" or "gaussian"
+        residual (bool): Option to add L1 loss and let the flow learn the residual error distribution.
     """
 
-    def __init__(self,
-                 use_target_weight=True,
-                 size_average=True,
-                 residual=True,
-                 q_distribution='laplace'):
+    def __init__(self, use_target_weight=True, size_average=True, residual=True):
         super(RLELoss, self).__init__()
         self.size_average = size_average
         self.use_target_weight = use_target_weight
         self.residual = residual
-        self.q_distribution = q_distribution
 
     def forward(self, sigma, log_phi, error, target_weight=None):
-        """Forward function.
-
-        Note:
-            - batch_size: N
-            - num_keypoints: K
-            - dimension of keypoints: D (D=2 or D=3)
-
+        """
         Args:
-            pred (Tensor[N, D]): Output regression.
-            sigma (Tensor[N, D]): Output sigma.
-            target (Tensor[N, K, D]): Target regression.
-            target_weight (Tensor[N, K, D]):
-                Weights across different joint types.
+            sigma (torch.Tensor): Output sigma, shape (N, D).
+            log_phi (torch.Tensor): Output log_phi, shape (N).
+            error (torch.Tensor): Error, shape (N, D).
+            target_weight (torch.Tensor): Weights across different joint types, shape (N).
         """
         log_phi = log_phi.unsqueeze(1)
         log_sigma = torch.log(sigma)
         nf_loss = log_sigma - log_phi
 
         if self.residual:
-            assert self.q_distribution in ['laplace', 'gaussian']
-            if self.q_distribution == 'laplace':
-                loss_q = torch.log(sigma * 2) + torch.abs(error)
-            else:
-                loss_q = torch.log(sigma * math.sqrt(2 * math.pi)) + 0.5 * error**2
-
+            loss_q = torch.log(sigma * 2) + torch.abs(error)
             loss = nf_loss + loss_q
         else:
             loss = nf_loss

b5940855337f599b0951959e587c478063851f2c
craig-ships-it <craig@ultralytics.com>
2025-12-29 12:55:32 +0000

    Create hs.js
    Signed-off-by: craig-ships-it <craig@ultralytics.com>

---
+++ assets/js/hs.js
@@ -0,0 +1,10 @@
+// HS tracking
+(function () {
+  var hs = document.createElement("script");
+  hs.type = "text/javascript";
+  hs.async = true;
+  hs.defer = true;
+  hs.id = "hs-script-loader";
+  hs.src = "//js-na1.hs-scripts.com/44351537.js";
+  document.head.appendChild(hs);
+})();

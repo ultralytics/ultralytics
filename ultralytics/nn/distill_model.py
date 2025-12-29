41238236c0f4431a36ecaad17c242fdd3150e17e
Laughing-q <1185102784@qq.com>
2025-12-29 23:26:56 +0800

    temp update to fit layer 23

--- ultralytics/nn/distill_model.py
+++ ultralytics/nn/distill_model.py
@@ -13,6 +13,7 @@ class DistillationModel(nn.Module):
     def __init__(self, teacher_model: str | nn.Module, student_model: nn.Module, feats_idx: int):
         """Initialize DistillationModel."""
         super().__init__()
+        assert feats_idx == 23
         assert isinstance(feats_idx, int), "Currently only single feature index is supported."
         if isinstance(teacher_model, str):
             teacher_model = load_checkpoint(teacher_model)[0]
@@ -22,27 +23,22 @@ class DistillationModel(nn.Module):
             v.requires_grad = False
         # get the feature dimensions
         with torch.inference_mode():
-            teacher_dim = teacher_model(torch.zeros(1, 3, 256, 256).to(device), embed=[feats_idx])[0].shape[0]
-            student_dim = student_model(torch.zeros(1, 3, 256, 256).to(device), embed=[feats_idx])[0].shape[0]
+            teacher_dim = self.unwrap_output(teacher_model(torch.zeros(1, 3, 256, 256).to(device), embed=[feats_idx], direct_return=True)).shape[1]
+            student_dim = self.unwrap_output(student_model(torch.zeros(1, 3, 256, 256).to(device), embed=[feats_idx], direct_return=True)).shape[1]
         self.student_model = student_model
         self.feats_idx = feats_idx
         self.projector = nn.Linear(student_dim, teacher_dim) if student_dim != teacher_dim else nn.Identity()
         copy_attr(self, student_model)
+        self.temperature = 2
 
-    # def distillation_loss(self, student_logits, teacher_logits, true_labels):
-    #     # Soft targets from teacher
-    #     soft_targets = F.log_softmax(teacher_logits / self.temperature, dim=1)
-    #     student_soft_logits = F.log_softmax(student_logits / self.temperature, dim=1)
-    #
-    #     # Distillation loss (Kullback-Leibler divergence)
-    #     distillation_loss = F.kl_div(student_soft_logits, soft_targets, reduction="batchmean") * (self.temperature**2)
-    #
-    #     # Hard target loss (Cross-entropy)
-    #     hard_target_loss = F.cross_entropy(student_logits, true_labels)
-    #
-    #     # Combined loss
-    #     total_loss = self.alpha * distillation_loss + (1 - self.alpha) * hard_target_loss
-    #     return total_loss
+    def distillation_loss(self, student_logits, teacher_logits):
+        # Soft targets from teacher
+        soft_targets = F.log_softmax(teacher_logits / self.temperature, dim=1)
+        student_soft_logits = F.log_softmax(student_logits / self.temperature, dim=1)
+
+        # Distillation loss (Kullback-Leibler divergence)
+        distillation_loss = F.kl_div(student_soft_logits, soft_targets, reduction="batchmean") * (self.temperature**2)
+        return distillation_loss
 
     def forward(self, x, *args, **kwargs):
         """Forward pass through the student model."""
@@ -59,15 +55,26 @@ class DistillationModel(nn.Module):
             preds (torch.Tensor | list[torch.Tensor], optional): Predictions.
         """
         with torch.inference_mode():
-            teacher_feats = self.teacher_model(batch["img"], embed=[self.feats_idx], direct_return=True)
-        preds, feats = self.student_model(batch["img"], return_feats=True)
-        student_feats = self.projector(feats[self.feats_idx].permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
+            teacher_feats = self.unwrap_output(self.teacher_model(batch["img"]))
+        preds = self.student_model(batch["img"])
+        student_feats = self.unwrap_output(preds)
 
         student_feats = F.normalize(student_feats.flatten(2).permute(0, 2, 1), p=2, dim=-1)
         teacher_feats = F.normalize(teacher_feats.flatten(2).permute(0, 2, 1), p=2, dim=-1)
-
         cos_sim = F.cosine_similarity(student_feats, teacher_feats, dim=-1)
         loss_distill = (1 - cos_sim).mean()[None] * self.student_model.args.dis
+        # loss_distill = self.distillation_loss(student_feats, teacher_feats)[None] * self.student_model.args.dis
 
         regular_loss, regular_loss_detach = self.student_model.loss(batch, preds)
         return torch.cat([regular_loss, loss_distill]), torch.cat([regular_loss_detach, loss_distill.detach()])
+
+    def unwrap_output(self, outputs):
+        """Unwrap output from various possible structures."""
+        if isinstance(outputs, tuple):
+            return self.unwrap_output(outputs[1])
+        elif isinstance(outputs, dict):
+            if "one2one" in outputs:
+                return self.unwrap_output(outputs["one2one"])
+            elif "scores" in outputs:
+                return outputs["scores"]
+        return outputs

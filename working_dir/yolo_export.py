6796635a5464a34234d3edde2413678e20c6d219
esat <esat@ultralytics.com>
2026-01-08 00:12:49 +0300

    example yolo export file

--- working_dir/yolo_export.py
+++ working_dir/yolo_export.py
@@ -2,6 +2,20 @@ from ultralytics.utils.benchmarks import ProfileModels
 from ultralytics import RTDETR
 
 # ProfileModels(paths=["yolo11s.pt"]).run()
-model = RTDETR("ultralytics/cfg/models/11/yolo11-rtdetr_dabpos.yaml")
-model.save("yolo11-rtdetr_dabpos.pt")
-ProfileModels(paths=["yolo11-rtdetr_dabpos.pt"]).run()
+
+# model = RTDETR("ultralytics/cfg/models/11/yolo11-rtdetr_p2_l3.yaml")
+# model.save("yolo11-rtdetr_p2_l3.pt")
+# ProfileModels(paths=["yolo11-rtdetr_p2_l3.pt"]).run()
+
+# model = RTDETR("ultralytics/cfg/models/11/yolo11-rtdetr.yaml")
+# model.save("yolo11-rtdetr.pt")
+# ProfileModels(paths=["yolo11-rtdetr.pt"]).run()
+
+# model = RTDETR("ultralytics/cfg/models/11/yolo11-rtdetr_p2_l3_efms.yaml")
+# model.save("yolo11-rtdetr_p2_l3_efms.pt")
+# ProfileModels(paths=["yolo11-rtdetr_p2_l3_efms.pt"]).run()
+
+
+model = RTDETR("ultralytics/cfg/models/11/yolo11-rtdetr_p2_l3_efms.yaml")
+model.save("yolo11-rtdetr_p2_l3_efms.pt")
+ProfileModels(paths=["yolo11-rtdetr_p2_l3_efms.pt"], imgsz=480).run()

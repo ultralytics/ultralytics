a42e875929afa14496341f9b6c3360fffccb3682
Laughing-q <1185102784@qq.com>
2026-01-08 19:25:43 +0800

    Update overrides.py

--- overrides.py
+++ overrides.py
@@ -37,6 +37,7 @@ obj365_v4_json="Objects365v1/pipeline_outputs/train/v4/merged.json"
 flickr_v5_json="flickr/pipeline_outputs/v5/merged.json"
 mixed_grounding_v5_json="mixed_grounding/pipeline_outputs/v5/merged.json"
 obj365_v5_json="Objects365v1/pipeline_outputs/train/v5/merged.json"
+ye_v5_json="yolo-enterprise/pipeline_outputs/train/v5/merged.json"
 
 
 refer_data_yaml=os.path.abspath(f"../datasets/lvis_train_vps.yaml")

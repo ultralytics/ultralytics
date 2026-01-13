d9b2390e2b4e864a3e89e58e82e6c7f8784fa229
Glenn Jocher <glenn.jocher@ultralytics.com>
2026-01-13 12:06:14 +0000

    Modify ncnn requirement in export_ncnn method
    Updated ncnn requirement to exclude broken version 1.0.20260113.
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -860,7 +860,8 @@ class Exporter:
     @try_export
     def export_ncnn(self, prefix=colorstr("NCNN:")):
         """Export YOLO model to NCNN format using PNNX https://github.com/pnnx/pnnx."""
-        check_requirements("ncnn", cmds="--no-deps")  # no deps to avoid installing opencv-python
+        # excludes broken v1.0.20260113 https://github.com/Tencent/ncnn/issues/6509
+        check_requirements("ncnn!=1.0.20260113", cmds="--no-deps")  # no deps to avoid installing opencv-python
         check_requirements("pnnx")
         import ncnn
         import pnnx

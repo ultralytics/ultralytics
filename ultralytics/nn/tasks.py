965d1e1345f009cdeaa657ce49859181fedcb522
Laughing-q <1185102784@qq.com>
2026-01-13 20:04:57 +0800

    Update nn/tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -76,7 +76,6 @@ from ultralytics.nn.modules import (
 from ultralytics.utils import DEFAULT_CFG_DICT, LOGGER, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
 from ultralytics.utils.loss import (
-    E2EDetectLoss,
     E2ELoss,
     v8ClassificationLoss,
     v8DetectionLoss,

8f852a7edf99f930cb0942178bb161daf6967525
Laughing-q <1185102784@qq.com>
2026-01-13 16:08:46 +0800

    Update model yamls info

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1682,7 +1682,7 @@ def parse_model(d, ch, verbose=True):
             }
         ):
             args.extend([reg_max, end2end, [ch[x] for x in f]])
-            if m is Segment or m is YOLOESegment:
+            if m is Segment or m is YOLOESegment or m is Segment26:  # TODO, check YOLOESegment26 as well
                 args[2] = make_divisible(min(args[2], max_channels) * width, 8)
             if m in {Detect, YOLOEDetect, Segment, Segment26, YOLOESegment, Pose, Pose26, OBB, OBB26}:
                 m.legacy = legacy

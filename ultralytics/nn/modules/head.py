31557458622eaf3e52b7dd0422da0b4a03b584ba
Glenn Jocher <glenn.jocher@ultralytics.com>
2026-01-13 19:30:14 +0000

    Fix TF features dict

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -633,9 +633,15 @@ class Pose(Detect):
         ndim = self.kpt_shape[1]
         bs = kpts.shape[0]
         if self.export:
-            # NCNN fix
             y = kpts.view(bs, *self.kpt_shape, -1)
-            a = (y[:, :, :2] * 2.0 + (self.anchors - 0.5)) * self.strides
+            if self.format in {"tflite", "edgetpu"}:
+                # Precompute normalization factor to increase numerical stability
+                grid_h, grid_w = self.shape[2], self.shape[3]
+                grid_size = torch.tensor([grid_w, grid_h], device=y.device).reshape(1, 2, 1)
+                norm = self.strides / (self.stride[0] * grid_size)
+                a = (y[:, :, :2] * 2.0 + (self.anchors - 0.5)) * norm
+            else:
+                a = (y[:, :, :2] * 2.0 + (self.anchors - 0.5)) * self.strides
             if ndim == 3:
                 a = torch.cat((a, y[:, :, 2:3].sigmoid()), 2)
             return a.view(bs, self.nk, -1)

5ce3c2ace432a0b96171a3a03caa65bb517c304c
Murat Raimbekov <raimbekov_m@auca.kg>
2026-01-15 00:58:17 +0600

    Fix incorrect shape comment in Ensemble.forward (#23207)

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1297,7 +1297,7 @@ class Ensemble(torch.nn.ModuleList):
         y = [module(x, augment, profile, visualize)[0] for module in self]
         # y = torch.stack(y).max(0)[0]  # max ensemble
         # y = torch.stack(y).mean(0)  # mean ensemble
-        y = torch.cat(y, 2)  # nms ensemble, y shape(B, HW, C)
+        y = torch.cat(y, 2)  # nms ensemble, y shape(B, HW, C*num_models)
         return y, None  # inference, train output
 
 

2ce46c13aa566f5a735449c777a2eaece63f4272
UltralyticsAssistant <web@ultralytics.com>
2026-01-14 13:01:16 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/utils/checks.py
+++ ultralytics/utils/checks.py
@@ -854,7 +854,9 @@ def check_amp(model):
         assert amp_allclose(YOLO(f"{model.model_name}"), im)
         LOGGER.info(f"{prefix}checks passed ✅")
     except ConnectionError:
-        LOGGER.warning(f"{prefix}checks skipped. Offline and unable to download {model.model_name} for AMP checks. {warning_msg}")
+        LOGGER.warning(
+            f"{prefix}checks skipped. Offline and unable to download {model.model_name} for AMP checks. {warning_msg}"
+        )
     except (AttributeError, ModuleNotFoundError):
         LOGGER.warning(
             f"{prefix}checks skipped. "

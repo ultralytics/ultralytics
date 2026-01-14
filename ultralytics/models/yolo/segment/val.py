c6b2a4592c4833d9603640bb8af0f5b0f7db1679
UltralyticsAssistant <web@ultralytics.com>
2026-01-14 10:44:38 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/models/yolo/segment/val.py
+++ ultralytics/models/yolo/segment/val.py
@@ -101,7 +101,7 @@ class SegmentationValidator(DetectionValidator):
         """
         proto = preds[1][-1] if len(preds[1]) == 3 else preds[1]  # second output is len 3 if pt, but only 1 if exported
         preds = super().postprocess(preds[0])
-        imgsz = self.dataloader.dataset.batch_shapes[self.batch_i] # get image size from dataloader
+        imgsz = self.dataloader.dataset.batch_shapes[self.batch_i]  # get image size from dataloader
         for i, pred in enumerate(preds):
             coefficient = pred.pop("extra")
             pred["masks"] = (

f0c27d3b0db5a33e43dc290084e6728c8fe01f5f
UltralyticsAssistant <web@ultralytics.com>
2026-01-19 01:05:59 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/data/utils.py
+++ ultralytics/data/utils.py
@@ -503,7 +503,7 @@ def check_semseg_dataset(dataset: str, autodownload: bool = True) -> dict[str, A
     # Read YAML
     data = YAML.load(file, append_filename=True)  # dictionary
 
-    #assert color must be existed
+    # assert color must be existed
     if "color" not in data:
         raise ValueError("Semantic segmentation datasets must define 'colors' in dataset YAML.")
     check_class_colors(data["colors"])

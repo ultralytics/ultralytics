50052e8686cd35715f5b54421820d338fc5a661f
UltralyticsAssistant <web@ultralytics.com>
2026-01-25 15:07:51 +0000

    Auto-update Ultralytics Docs Reference by https://ultralytics.com/actions

--- ultralytics/nn/autobackend.py
+++ ultralytics/nn/autobackend.py
@@ -31,7 +31,6 @@ from ultralytics.utils import (
 from ultralytics.utils.checks import check_requirements, check_suffix, check_version, check_yaml, is_rockchip
 from ultralytics.utils.downloads import attempt_download_asset, is_url
 from ultralytics.utils.nms import non_max_suppression
-from ultralytics.utils.torch_utils import TORCH_2_10
 
 
 def check_class_names(names: list | dict) -> dict[int, str]:

e2775094970448fa8680daee7beee1d2802ed8c8
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2026-01-28 10:35:53 +0000

    Add `onnxscript` autoinstall

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -674,7 +674,7 @@ class Exporter:
     @try_export
     def export_onnx(self, prefix=colorstr("ONNX:"), dynamo=True):
         """Export YOLO model to ONNX format."""
-        requirements = ["onnx>=1.12.0,<2.0.0"]
+        requirements = ["onnx>=1.12.0,<2.0.0", "onnxscript>=0.2.5"]
         if self.args.simplify:
             requirements += ["onnxslim>=0.1.71", "onnxruntime" + ("-gpu" if torch.cuda.is_available() else "")]
         check_requirements(requirements)

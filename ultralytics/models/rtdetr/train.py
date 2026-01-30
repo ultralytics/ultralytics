a76acf3d67fd55f27f5495dfd5ee0984810f30c9
esat <esat@ultralytics.com>
2026-01-30 19:58:18 +0300

    o2m DPP log problem

--- ultralytics/models/rtdetr/train.py
+++ ultralytics/models/rtdetr/train.py
@@ -287,7 +287,9 @@ class RTDETRTrainer(DetectionTrainer):
         if "ddf" in loss_gain:
             loss_names.append("ddf_loss")
         # Add o2m loss names if one_to_many_groups > 0
-        if getattr(self.model.model[-1], "one_to_many_groups", 0) > 0:
+        # Handle DDP wrapper: use .module to get underlying model if wrapped
+        model = getattr(self.model, "module", self.model)
+        if getattr(model.model[-1], "one_to_many_groups", 0) > 0:
             loss_names.extend(["giou_o2m", "cls_o2m", "l1_o2m"])
         self.loss_names = tuple(loss_names)
         return RTDETRValidator(self.test_loader, save_dir=self.save_dir, args=copy(self.args))

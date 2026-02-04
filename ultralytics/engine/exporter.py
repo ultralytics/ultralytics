a8e48a758023b4fcd8200325ec08ceeefebc4f05
UltralyticsAssistant <web@ultralytics.com>
2026-02-04 12:06:21 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -390,7 +390,7 @@ class Exporter:
                 self.args.int8 = True
             if not self.args.data:
                 self.args.data = "coco128.yaml"  # Axelera default to coco128.yaml
-            
+
         if axelera:
             if not IS_PYTHON_3_10:
                 raise SystemError("Axelera export only supported on Python 3.10.")
@@ -1347,14 +1347,16 @@ class Exporter:
             dataset=self.get_int8_calibration_dataloader(prefix),
             prefix=prefix,
         )
-        
+
     def export_deepx(self, prefix=colorstr("Deepx")):
         assert LINUX, "Export only supported on Linux."
         assert not ARM64, "IMX export is not supported on ARM64 architectures."
-        
+
         if not check_requirements("dx_com", install=False):
-            raise Exception("dx_com executable not found. DeepX CLI tools are required but not installed. Please install manually by visiting: https://docs.ultralytics.com/integrations/deepx")
-        
+            raise Exception(
+                "dx_com executable not found. DeepX CLI tools are required but not installed. Please install manually by visiting: https://docs.ultralytics.com/integrations/deepx"
+            )
+
         import dx_com
 
         f = self.export_onnx()
@@ -1362,31 +1364,31 @@ class Exporter:
         export_path.mkdir(exist_ok=True)
         config_path = export_path / "config.json"
         data = self.get_int8_calibration_dataloader()
-        
-        config = { "inputs": {"images": [1, 3, data.dataset.imgsz, data.dataset.imgsz]},
-                    "default_loader": {
-                    "dataset_path": data.dataset.img_path,
-                    "file_extensions": ["jpeg", "jpg", "png", "JPEG"],
-                    "preprocessings": [
-                        {"resize": {"width": self.imgsz[0], "height": self.imgsz[1]}},
-                        {"convertColor": {"form": "BGR2RGB"}},
-                        {"div": {"x": 255.0}},
-                        {"transpose": {"axis": [2, 0, 1]}},
-                        {"expandDim": {"axis": 0}},
-                    ],
-                },
-                "calibration_num": data.dataset.ni,
-                "calibration_method": "minmax",
-            }
-        
+
+        config = {
+            "inputs": {"images": [1, 3, data.dataset.imgsz, data.dataset.imgsz]},
+            "default_loader": {
+                "dataset_path": data.dataset.img_path,
+                "file_extensions": ["jpeg", "jpg", "png", "JPEG"],
+                "preprocessings": [
+                    {"resize": {"width": self.imgsz[0], "height": self.imgsz[1]}},
+                    {"convertColor": {"form": "BGR2RGB"}},
+                    {"div": {"x": 255.0}},
+                    {"transpose": {"axis": [2, 0, 1]}},
+                    {"expandDim": {"axis": 0}},
+                ],
+            },
+            "calibration_num": data.dataset.ni,
+            "calibration_method": "minmax",
+        }
+
         with open(config_path, "w") as file:
             json.dump(config, file)
-        
+
         dx_com.compile(model=f, output_dir=str(export_path), config=str(config_path))
-        
+
         YAML.save(export_path / "metadata.yaml", self.metadata)
         return export_path
-        
 
     def _add_tflite_metadata(self, file):
         """Add metadata to *.tflite models per https://ai.google.dev/edge/litert/models/metadata."""

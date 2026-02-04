ac60de31ea2da311b5119b917db8798856561f94
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2026-02-04 11:17:46 +0800

    Fix disk caching with multispectral images (#23545)
    Signed-off-by: Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
    Co-authored-by: Laughing-q <1185102784@qq.com>

--- ultralytics/data/base.py
+++ ultralytics/data/base.py
@@ -280,7 +280,7 @@ class BaseDataset(Dataset):
         """Save an image as an *.npy file for faster loading."""
         f = self.npy_files[i]
         if not f.exists():
-            np.save(f.as_posix(), imread(self.im_files[i]), allow_pickle=False)
+            np.save(f.as_posix(), imread(self.im_files[i], flags=self.cv2_flag), allow_pickle=False)
 
     def check_cache_disk(self, safety_margin: float = 0.5) -> bool:
         """Check if there's enough disk space for caching images.

f2e2345d4522bc756b01cbd676770310e74ee561
corpse-dog <8603602+corpse-dog@user.noreply.gitee.com>
2026-02-10 08:48:31 +0800

    2

--- ultralytics/utils/ops.py
+++ ultralytics/utils/ops.py
@@ -90,7 +90,7 @@ def segment2box(segment, width: int = 640, height: int = 640):
         x = x.clip(0, width)
         y = y.clip(0, height)
     # inside = (x >= 0) & (y >= 0) & (x <= width) & (y <= height)
-    inside = (x > 0) & (y > 0) & (x < width) & (y < height)
+    inside = (x > 0) & (y > 0) & (x < width) & (y < height) 
     x = x[inside]
     y = y[inside]
     return (

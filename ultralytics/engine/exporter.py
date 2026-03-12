edf61802bada6b4db132da739e31756dbaebf2f7
Lakshantha Dissanayake <lakshantha@ultralytics.com>
2026-03-11 17:16:53 -0700

    Improve warning

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -429,8 +429,7 @@ class Exporter:
                     if check_version(trt.__version__, "<=10.3.0", hard=True):
                         model.end2end = False
                         LOGGER.warning(
-                            f"TensorRT {trt.__version__} detected. TensorRT<=10.3.0 with int8 has known end2end build issues, "
-                            "disabling end2end branch."
+                            f"TensorRT<=10.3.0 with int8 has known end2end build issues, disabling end2end branch."
                         )
                 except ImportError:
                     pass

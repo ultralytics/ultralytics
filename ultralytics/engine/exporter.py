f4618d254df8ea0c24c5e7552f6a414f326368ed
Glenn Jocher <glenn.jocher@ultralytics.com>
2026-03-23 01:33:51 +0100

    Fix tfjs

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -1074,11 +1074,11 @@ class Exporter:
         try:
             import tensorflow as tf
         except ImportError:
-            check_requirements("tensorflow>=2.0.0,<=2.19.0")
+            check_requirements("tensorflow>=2.0.0")
             import tensorflow as tf
         check_requirements(
             (
-                "tf_keras<=2.19.0",  # required by 'onnx2tf' package
+                "tf_keras",  # required by 'onnx2tf' package
                 "sng4onnx>=1.0.1",  # required by 'onnx2tf' package
                 "onnx_graphsurgeon>=0.3.26",  # required by 'onnx2tf' package
                 "ai-edge-litert>=1.2.0" + (",<1.4.0" if MACOS else ""),  # required by 'onnx2tf' package

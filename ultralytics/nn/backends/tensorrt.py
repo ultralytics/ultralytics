5e999deb90ac8401e125d1e232e1308f9b430efc
Lakshantha Dissanayake <lakshantha@ultralytics.com>
2026-04-28 15:21:35 -0700

    Add missing import

--- ultralytics/nn/backends/tensorrt.py
+++ ultralytics/nn/backends/tensorrt.py
@@ -10,7 +10,7 @@ import numpy as np
 import torch
 
 from ultralytics.utils import IS_JETSON, LOGGER, PYTHON_VERSION
-from ultralytics.utils.checks import check_requirements, check_version
+from ultralytics.utils.checks import check_requirements, check_version, check_tensorrt
 
 from .base import BaseBackend
 

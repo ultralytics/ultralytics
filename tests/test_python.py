4cf94402870c9789756e6454eb28636f37a339cf
Laughing-q <1185102784@qq.com>
2026-05-17 23:28:25 +0800

    Clean up tests

--- tests/test_python.py
+++ tests/test_python.py
@@ -322,14 +322,13 @@ def test_results(model: str, tmp_path):
         r = r.cpu().numpy()
         print(r, len(r), r.path)  # print numpy attributes
         r = r.to(device="cpu", dtype=torch.float32)
-        if not is_semseg:  # save_txt / save_crop are no-ops for semseg
-            r.save_txt(txt_file=tmp_path / "runs/tests/label.txt", save_conf=True)
-            r.save_crop(save_dir=tmp_path / "runs/tests/crops/")
+        r.save_txt(txt_file=tmp_path / "runs/tests/label.txt", save_conf=True)
+        r.save_crop(save_dir=tmp_path / "runs/tests/crops/")
         r.to_df(decimals=3)  # Align to_ methods: https://docs.ultralytics.com/modes/predict/#working-with-results
         r.to_csv()
         r.to_json(normalize=True)
         r.plot(pil=True, save=True, filename=tmp_path / "results_plot_save.jpg")
-        r.plot(conf=True, boxes=not is_semseg)
+        r.plot(conf=True, boxes=True)
         print(r, len(r), r.path)  # print after methods
 
 

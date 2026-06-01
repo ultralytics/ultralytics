0dca76257e9a12d0163181dd6ba84499d286270c
Glenn Jocher <glenn.jocher@ultralytics.com>
2026-06-01 17:44:27 +0800

    Allow half arg for RKNN export

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -196,7 +196,7 @@ def export_formats():
         ["MNN", "mnn", ".mnn", True, True, ["batch", "half", "int8"], "mnn"],
         ["NCNN", "ncnn", "_ncnn_model", True, True, ["batch", "half"], "ncnn"],
         ["IMX", "imx", "_imx_model", True, True, ["data", "int8", "fraction", "nms"], "isolated-imx"],
-        ["RKNN", "rknn", "_rknn_model", False, False, ["batch", "name", "int8", "data", "fraction"], "isolated-rknn"],
+        ["RKNN", "rknn", "_rknn_model", False, False, ["batch", "name", "half", "int8", "data", "fraction"], "isolated-rknn"],
         ["ExecuTorch", "executorch", "_executorch_model", True, False, ["batch"], "executorch"],
         [
             "Axelera AI",
@@ -538,6 +538,8 @@ class Exporter:
             assert fmt != "ncnn", "optimize=True not compatible with format='ncnn', i.e. use optimize=False"
             assert self.device.type == "cpu", "optimize=True not compatible with cuda devices, i.e. use device='cpu'"
         if fmt == "rknn":
+            if not self.args.int8:
+                self.args.half = True
             if not self.args.name:
                 LOGGER.warning(
                     "Rockchip RKNN export requires a missing 'name' arg for processor type. "

ccbe2d30e139add64d9341139ec77be9fad2fd8a
Glenn Jocher <glenn.jocher@ultralytics.com>
2026-07-05 18:57:24 +0200

    Apply env_bool() to ULTRALYTICS_SAFE_LOAD for consistency

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -71,7 +71,7 @@ DEFAULT_CFG_PATH = ROOT / "cfg/default.yaml"
 NUM_THREADS = min(8, max(1, os.cpu_count() - 1))  # number of YOLO multiprocessing threads
 AUTOINSTALL = env_bool("YOLO_AUTOINSTALL", True)  # global auto-install mode
 VERBOSE = env_bool("YOLO_VERBOSE", True)  # global verbose mode
-SAFE_LOAD = str(os.getenv("ULTRALYTICS_SAFE_LOAD", False)).lower() == "true"  # opt-in weights_only model loading
+SAFE_LOAD = env_bool("ULTRALYTICS_SAFE_LOAD")  # opt-in weights_only model loading
 LOGGING_NAME = "ultralytics"
 MACOS, LINUX, WINDOWS = (platform.system() == x for x in ["Darwin", "Linux", "Windows"])  # environment booleans
 MACOS_VERSION = platform.mac_ver()[0] if MACOS else None

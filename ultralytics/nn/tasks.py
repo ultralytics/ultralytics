5850ef21888b0e42ae9d8f73a9a2c93fd26478a8
Laughing-q <1185102784@qq.com>
2026-07-07 19:17:01 +0800

    clean up comments

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -77,7 +77,6 @@ from ultralytics.nn.modules import (
     YOLOESegment26,
     v10Detect,
 )
-
 from ultralytics.utils import DEFAULT_CFG_DICT, LOGGER, WINDOWS, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
 from ultralytics.utils.loss import (

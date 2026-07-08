cc6cd2d92537fa4016296076e7e95c112bb1bce6
sjsjsmsmsj <lamgiathinh05@gmail.com>
2026-07-08 13:48:05 +0700

    add hybrid head

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -1875,3 +1875,40 @@ class SemanticSegment(nn.Module):
                 return cls.to(torch.uint8 if self.nc <= 256 else torch.int32)
             return y
         return logits
+
+class AnchorFreeHead(Detect):
+    def __init__(self, ch, nc):
+        super().__init__(nc, ch=(ch,))
+
+        self.nc = nc
+
+        self.bbox = nn.Conv2d(
+            ch,
+            4,
+            kernel_size=1
+        )
+
+        self.cls = nn.Conv2d(
+            ch,
+            nc,
+            kernel_size=1
+        )
+
+        self.center = nn.Conv2d(
+            ch,
+            1,
+            kernel_size=1
+        )
+    def forward(self, x):
+        bbox = self.bbox(x)
+        cls = self.cls(x)
+        center = self.center(x)
+
+        return torch.cat(
+            [
+                bbox,
+                cls,
+                center
+            ],
+            dim=1
+        )

1fbc4a0d56a873c2d341bf3fbcaf3c85b15869f3
Glenn Jocher <glenn.jocher@ultralytics.com>
2026-07-13 10:34:33 +0200

    Reduce fraction validation diff

--- ultralytics/cfg/__init__.py
+++ ultralytics/cfg/__init__.py
@@ -443,9 +443,7 @@ def check_cfg(cfg: dict, hard: bool = True) -> None:
                         )
                     cfg[k] = v = float(v)
                 if not (0.0 <= v <= 1.0) or (k == "fraction" and v == 0.0):
-                    raise ValueError(
-                        f"'{k}={v}' is invalid. Use [0.0, 1.0]; dataset fraction must be greater than 0.0."
-                    )
+                    raise ValueError(f"'{k}={v}' is invalid. Use >0.0 for fraction; [0.0, 1.0] otherwise.")
             elif k in CFG_INT_KEYS:
                 if not isinstance(v, int):
                     if hard:

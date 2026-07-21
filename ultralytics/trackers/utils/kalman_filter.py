056a03e5cc9aea305612f8bafb00e39889bf3cf4
Jesús Royeth <JESUSROYETH@users.noreply.github.com>
2026-07-21 14:22:42 -0400

    Vectorize the per-frame `multi_predict` and `multi_gmc` track loops

--- ultralytics/trackers/utils/kalman_filter.py
+++ ultralytics/trackers/utils/kalman_filter.py
@@ -190,8 +190,7 @@ class KalmanFilterXYAH:
         ]
         sqr = np.square(np.r_[std_pos, std_vel]).T
 
-        motion_cov = [np.diag(sqr[i]) for i in range(len(mean))]
-        motion_cov = np.asarray(motion_cov)
+        motion_cov = sqr[:, :, None] * np.eye(8)
 
         mean = np.dot(mean, self._motion_mat.T)
         left = np.dot(self._motion_mat, covariance).transpose((1, 0, 2))
@@ -458,8 +457,7 @@ class KalmanFilterXYWH(KalmanFilterXYAH):
         ]
         sqr = np.square(np.r_[std_pos, std_vel]).T
 
-        motion_cov = [np.diag(sqr[i]) for i in range(len(mean))]
-        motion_cov = np.asarray(motion_cov)
+        motion_cov = sqr[:, :, None] * np.eye(8)
 
         mean = np.dot(mean, self._motion_mat.T)
         left = np.dot(self._motion_mat, covariance).transpose((1, 0, 2))

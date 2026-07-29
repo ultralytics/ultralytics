92086c21d90cd57bc3e4a9575bc25b3837f52f44
Glenn Jocher <glenn.jocher@ultralytics.com>
2026-07-29 15:26:42 +0200

    Avoid legacy MPS autocast warnings

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -119,7 +119,9 @@ def autocast(enabled: bool, device: str = "cuda"):
         import torch_npu
 
         return torch_npu.npu.amp.autocast(enabled=enabled)
-    elif TORCH_1_13 and (device != "mps" or TORCH_2_5):
+    elif TORCH_1_13 and device == "mps" and not TORCH_2_5:
+        return torch.amp.autocast("cuda", enabled=False)
+    elif TORCH_1_13:
         return torch.amp.autocast(device, enabled=enabled)
     else:
         return torch.cuda.amp.autocast(enabled)

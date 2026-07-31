dee2d07144f6d72f6f0b1fab1664f681e68fcc8c
UltralyticsAssistant <web@ultralytics.com>
2026-07-31 06:00:54 +0000

    Auto-format by https://ultralytics.com/actions

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -64,7 +64,7 @@ def test_predict(task: str, model: str, data: str) -> None:
 @pytest.mark.parametrize("task,model,data", TASK_MODEL_DATA)
 def test_show_fps(task: str, model: str, data: str) -> None:
     """Test YOLO prediction on provided sample assets for specified task and model."""
-    for end2end in {False, True}:
+    for end2end in (False, True):
         run(f"yolo {task} predict model={model} source={ASSETS} imgsz=32 save show_fps end2end={end2end} max_det=100")
 
 

24f9d286f41b38e5ab696e82285d659b92bfc8d1
Glenn Jocher <glenn.jocher@ultralytics.com>
2026-08-22 19:13:46 +0200

    Use correct OpenVINO throughput hint (#25893)

--- ultralytics/nn/backends/openvino.py
+++ ultralytics/nn/backends/openvino.py
@@ -57,7 +57,11 @@ class OpenVINOBackend(BaseBackend):
             self.apply_metadata(YAML.load(metadata_file))
 
         # Set inference mode
-        self.inference_mode = "CUMULATIVE_THROUGHPUT" if self.dynamic and self.batch > 1 else "LATENCY"
+        self.inference_mode = (
+            ("CUMULATIVE_THROUGHPUT" if device_name == "AUTO" else "THROUGHPUT")
+            if self.dynamic and self.batch > 1
+            else "LATENCY"
+        )
         config = {"PERFORMANCE_HINT": self.inference_mode}
         if LINUX and ARM64 and device_name == "CPU":
             config["EXECUTION_MODE_HINT"] = ov.properties.hint.ExecutionMode.ACCURACY

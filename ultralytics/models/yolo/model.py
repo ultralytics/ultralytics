440396fff50fe74cc159e0b8eb81ef81109c861d
Glenn Jocher <glenn.jocher@ultralytics.com>
2026-08-23 21:02:58 +0200

    Inline preprocess_tensor kwarg lookup in YOLOE predict

--- ultralytics/models/yolo/model.py
+++ ultralytics/models/yolo/model.py
@@ -538,8 +538,7 @@ class YOLOE(Model):
             self.predictor.setup_model(model=self.model, verbose=self.predictor.args.verbose)
 
             if refer_image is None and source is not None:
-                preprocess_tensor = kwargs.get("preprocess_tensor", False)
-                dataset = load_inference_source(source, preprocess_tensor=preprocess_tensor)
+                dataset = load_inference_source(source, preprocess_tensor=kwargs.get("preprocess_tensor", False))
                 if dataset.mode in {"video", "stream"}:
                     # NOTE: set the first frame as refer image for videos/streams inference
                     refer_image = next(iter(dataset))[1][0]
